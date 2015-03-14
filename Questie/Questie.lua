e458e2f1223b0c03f938fd93fdea5794948a555c
Schaka <schakaqt@hotmail.com>
2015-03-14 10:51:51 +0100

    refactor structure
    might need more work, like moving all data tables on the Questie frame

--- Questie/Questie.lua
+++ Questie/Questie.lua
@@ -1,11 +1,63 @@
 DEFAULT_CHAT_FRAME:AddMessage("load", 0.95, 0.95, 0.5);
 local function log(msg) DEFAULT_CHAT_FRAME:AddMessage(msg) end -- alias for convenience
 
-selectedNotes = {};
+Questie = CreateFrame("Frame", "QuestieLua", UIParent, "ActionButtonTemplate")
+Questie.TimeSinceLastUpdate = 0
+Questie.lastMinimapUpdate = 0
+Questie.needsUpdate = false;
 currentQuests = {};
+selectedNotes = {};
+
+function Questie:OnEvent() -- functions created in "object:method"-style have an implicit first parameter of "this", which points to object || in 1.12 parsing arguments as ... doesn't work
+	Questie[event](arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8, arg9, arg10) -- route event parameters to Questie:event methods
+end
+Questie:SetScript("OnEvent", Questie.OnEvent)
+
+local _GetQuestLogQuestText;
+local objectives = {};
+local throttle = 0;
+local throttleOverride = false;
+
+function nql()
+	--DEFAULT_CHAT_FRAME:AddMessage("QUESTTEXT", 0.95, 0.95, 0.5);
+	Questie.needsUpdate = true;
+	return _GetQuestLogQuestText();
+end
 
 
-function createQuestNote(name, progress, questName, x, y, icon, selected)
+function Questie:OnUpdate(elapsed)
+	this = Questie
+	if Questie.needsUpdate then
+		Questie.needsUpdate = false;
+		Questie.throttleOverride = true;
+		Questie:QUEST_LOG_UPDATE();
+	end
+	local ttl = GetTime() - Questie.lastMinimapUpdate;
+	if ttl > 3 then -- 3 seconds
+		Questie:pickNearestPOI();
+		Questie.lastMinimapUpdate = GetTime();
+	end	
+end
+
+function Questie:PLAYER_LOGIN()
+	--log(this:GetName())
+	this:RegisterEvent("QUEST_LOG_UPDATE");
+	this:RegisterEvent("UNIT_AURA")
+end
+
+function Questie:UNIT_AURA(unitId)
+	--log("UnitID: "..unitId)
+end
+
+function Questie:PLAYER_ENTERING_WORLD()
+	currentQuests = {};
+	this:fillQuestList();
+	_GetQuestLogQuestText = GetQuestLogQuestText;
+	GetQuestLogQuestText = nql;
+	this:clearAllNotes();
+end
+
+function Questie:createQuestNote(name, progress, questName, x, y, icon, selected)
 	--local id, key = MapNotes_CreateQuestNote(name, lin, olin, x, y, icon, selected)
 	--DEFAULT_CHAT_FRAME:AddMessage(icon)
 	local zone = Cartographer:GetCurrentEnglishZoneName();
@@ -44,19 +96,19 @@ function distance(x, y, px, py)
 	return math.abs(x-px) + math.abs(y-py);
 end
 
-function addMonsterToMap(monsterName, info, quest, icon, mapid, selected)
+function Questie:addMonsterToMap(monsterName, info, quest, icon, mapid, selected)
 	local monsterdata = QuestieMonsters[monsterName];
 	if not (monsterdata == nil) then
 		for b=1,monsterdata['locationCount'] do -- this should be made more efficient (monsterdata[mapid][locations] etc
 			local loc = monsterdata['locations'][b];
 			if loc[1] == mapid then
-				createQuestNote(monsterName, info, quest, loc[2], loc[3], icon, selected);
+				this:createQuestNote(monsterName, info, quest, loc[2], loc[3], icon, selected);
 			end
 		end
 	end
 end
 
-function pickNearestPOI()
+function Questie:pickNearestPOI()
 	local fx, fy = GetPlayerMapPosition("player");
 	local least = 8; -- biggest distance possible is 2.0 but oh well
 	local best;
@@ -74,7 +126,7 @@ function pickNearestPOI()
 	--DEFAULT_CHAT_FRAME:AddMessage("pickNearestPOI", 0.95, 0.95, 0.5);
 end
 
-function clearAllNotes()
+function Questie:clearAllNotes()
 	selectedNotes = {}
 	Cartographer_Notes:ClearMap();
 end
@@ -118,7 +170,7 @@ objectiveProcessors = {
 					for b=1,itemdata['locationCount'] do
 						local loc = itemdata['locations'][b];
 						if loc[1] == mid then
-							createQuestNote(name, quest, "", loc[2], loc[3], "Star", selected);
+							Questie:createQuestNote(name, quest, "", loc[2], loc[3], "Star", selected);
 						end
 					end
 				elseif k == "drop" then
@@ -126,7 +178,7 @@ objectiveProcessors = {
 						--DEFAULT_CHAT_FRAME:AddMessage(e .. " drops " .. name .. " for " .. quest, 0.95, 0.95, 0.5);
 						--local monsterdata = QuestRoot['QuestHelper_StaticData']['enUS']['objective']['monster'][e];
 						--addMonsterToMap(monsterName, info, quest, selected)
-						addMonsterToMap(e, name .. " (" .. amount .. ")", quest, "Skull", mid, selected);
+						Questie:addMonsterToMap(e, name .. " (" .. amount .. ")", quest, "Skull", mid, selected);
 					end
 				end
 			end
@@ -141,14 +193,14 @@ objectiveProcessors = {
 			for b=1,evtdata['locationCount'] do
 				local loc = evtdata['locations'][b];
 				if loc[1] == mid then
-					createQuestNote(name, quest, "", loc[2], loc[3], "Star", selected);
+					Questie:createQuestNote(name, quest, "", loc[2], loc[3], "Star", selected);
 				end
 			end
 		end
 	end,
 	['monster'] = function(quest, name, amount, selected, mid)
 		--DEFAULT_CHAT_FRAME:AddMessage("   MONMON: " .. quest .. ", " .. name .. ", " .. amount, 0.95, 0.2, 0.2);
-		addMonsterToMap(name, amount, quest, "Skull", mid, selected);
+		Questie:addMonsterToMap(name, amount, quest, "Skull", mid, selected);
 	end,
 	['object'] = function(quest, name, amount, selected, mid)
 		local objdata = QuestieObjects[name];
@@ -158,7 +210,7 @@ objectiveProcessors = {
 			for b=1,objdata['locationCount'] do
 				local loc = objdata['locations'][b];
 				if loc[1] == mid then
-					createQuestNote(name, quest, "", loc[2], loc[3], "Star", selected);
+					Questie:createQuestNote(name, quest, "", loc[2], loc[3], "Star", selected);
 				end
 			end
 		end
@@ -166,7 +218,7 @@ objectiveProcessors = {
 
 }
 
-function getQuestFinisherByName(name)
+function Questie:getQuestFinisherByName(name)
 
 end
 
@@ -195,119 +247,81 @@ function processObjective(quest, desc, typ, selected, mid)
 	end
 end
 
-local objectives = {};
-
-local throttle = 0;
-
 function getQuestHashByName(name)
 	return QuestieHashes[name];
-end
-
-local throttleOverride = false;
-
-function questieevt(event)
-	--DEFAULT_CHAT_FRAME:AddMessage("evt " .. event .. " " .. GetQuestLogSelection(), 0.95, 0.95, 0.5);
-	--DEFAULT_CHAT_FRAME:AddMessage(event, 0.95, 0.95, 0.5);-
-	if (event == "QUEST_LOG_UPDATE") or (event == "ZONE_CHANGED") then
-		deleteNoteAfterQuestRemoved();
-		
-		local sind = GetQuestLogSelection();
-		local mid = getCurrentMapID();
-		if not throttleOverride then
-			if throttle == math.floor(GetTime()) then
-				return
-			else
-				throttle = math.floor(GetTime())
-			end
-		else
-			throttleOverride = false;
-		end
-		--DEFAULT_CHAT_FRAME:AddMessage(throttle, 0.95, 0.95, 0.5);
-		clearAllNotes();
-		local numEntries, numQuests = GetNumQuestLogEntries()
-		--DEFAULT_CHAT_FRAME:AddMessage(numEntries .. " entries containing " .. numQuests .. " quests in your quest log.");
-		for v=1,numEntries do
-			local q = GetQuestLogTitle(v);
-			if not (getQuestHashByName(q) == nil) then
-
-				SelectQuestLogEntry(v);
-				local count =  GetNumQuestLeaderBoards();
-				
-				local selected = v == sind;
-				
-				local finisher = QuestieFinishers[q];
-				
-				local questComplete = true; -- there might be something in the api for this
-
-					
-				if not (finisher == nil) and (count == 0) then
-					addMonsterToMap(finisher, "Quest Finisher", q, "Skull", mid, selected);
-					questComplete = false; -- questComplete is used to add the finisher, this avoids adding it twice
-				end
-				--DEFAULT_CHAT_FRAME:AddMessage(q);
-				for r=1,count do
-					local desc, typ, done = GetQuestLogLeaderBoard(r);
-					--DEFAULT_CHAT_FRAME:AddMessage(desc, 0.95, 0.95, 0.5);
-					
-					
-					if not done then
-						questComplete = false;
-						if selected then
-							--DEFAULT_CHAT_FRAME:AddMessage("SELECTED " .. q, 0.95, 0.1, 0.95);
-						else
-							--DEFAULT_CHAT_FRAME:AddMessage("NOTSELECTEd " .. q .. " " .. in, 0.95, 0.1, 0.95);
-						end
-						processObjective(q, desc, typ, selected, mid)
-					end
-					---DEFAULT_CHAT_FRAME:AddMessage(typ, 0.95, 0.95, 0.5);
-					---DEFAULT_CHAT_FRAME:AddMessage(done, 0.95, 0.95, 0.5);
-					
-				end
-				if not (finisher == nil) and questComplete then
-					addMonsterToMap(finisher, "Quest Finisher", q, "Skull", mid, selected);
-				end
-				--DEFAULT_CHAT_FRAME:AddMessage(hash);
-			else
-				--DEFAULT_CHAT_FRAME:AddMessage("ERROR: UNKNOWN QUEST: " .. q, 0.95, 0.55, 0.2);
-			end
-		end
-		SelectQuestLogEntry(sind);
-	end
 end
 
 function fank()
 	DEFAULT_CHAT_FRAME:AddMessage("fank", 0.95, 0.55, 0.2);
 end
 
-local oql;
-
-local needsUpdate = false;
-
-local lastMinimapUpdate = 0;
-
-function questiepoll()
-	if needsUpdate then
-		needsUpdate = false;
-		throttleOverride = true;
-		questieevt("QUEST_LOG_UPDATE");
+function Questie:QUEST_LOG_UPDATE()
+	this:deleteNoteAfterQuestRemoved()
+		
+	local sind = GetQuestLogSelection();
+	local mid = getCurrentMapID();
+	if not throttleOverride then
+		if throttle == math.floor(GetTime()) then
+			return
+		else
+			throttle = math.floor(GetTime())
+		end
+	else
+		throttleOverride = false;
 	end
-	local ttl = GetTime() - lastMinimapUpdate;
-	if ttl > 3 then -- 3 seconds
-		pickNearestPOI();
-		lastMinimapUpdate = GetTime();
+	
+	--DEFAULT_CHAT_FRAME:AddMessage(throttle, 0.95, 0.95, 0.5);
+	this:clearAllNotes();
+	local numEntries, numQuests = GetNumQuestLogEntries()
+	--DEFAULT_CHAT_FRAME:AddMessage(numEntries .. " entries containing " .. numQuests .. " quests in your quest log.");
+	for v=1,numEntries do
+		local q = GetQuestLogTitle(v);
+		if not (getQuestHashByName(q) == nil) then
+				SelectQuestLogEntry(v);
+			local count =  GetNumQuestLeaderBoards();
+			
+			local selected = v == sind;
+			
+			local finisher = QuestieFinishers[q];
+			
+			local questComplete = true; -- there might be something in the api for this
+					
+			if not (finisher == nil) and (count == 0) then
+				addMonsterToMap(finisher, "Quest Finisher", q, "Skull", mid, selected);
+				questComplete = false; -- questComplete is used to add the finisher, this avoids adding it twice
+			end
+			--DEFAULT_CHAT_FRAME:AddMessage(q);
+			for r=1,count do
+				local desc, typ, done = GetQuestLogLeaderBoard(r);
+				--DEFAULT_CHAT_FRAME:AddMessage(desc, 0.95, 0.95, 0.5);
+				
+				
+				if not done then
+					questComplete = false;
+					if selected then
+						--DEFAULT_CHAT_FRAME:AddMessage("SELECTED " .. q, 0.95, 0.1, 0.95);
+					else
+						--DEFAULT_CHAT_FRAME:AddMessage("NOTSELECTEd " .. q .. " " .. in, 0.95, 0.1, 0.95);
+					end
+					processObjective(q, desc, typ, selected, mid)
+				end
+				---DEFAULT_CHAT_FRAME:AddMessage(typ, 0.95, 0.95, 0.5);
+				---DEFAULT_CHAT_FRAME:AddMessage(done, 0.95, 0.95, 0.5);
+				
+			end
+			if not (finisher == nil) and questComplete then
+				addMonsterToMap(finisher, "Quest Finisher", q, "Skull", mid, selected);
+			end
+			--DEFAULT_CHAT_FRAME:AddMessage(hash);
+		else
+			--DEFAULT_CHAT_FRAME:AddMessage("ERROR: UNKNOWN QUEST: " .. q, 0.95, 0.55, 0.2);
+		end
 	end
-	--DEFAULT_CHAT_FRAME:AddMessage("QUESTTEXT", 0.95, 0.95, 0.5);
+	SelectQuestLogEntry(sind);
 end
 
-function nql()
-	--DEFAULT_CHAT_FRAME:AddMessage("QUESTTEXT", 0.95, 0.95, 0.5);
-	--questieevt("QUEST_LOG_UPDATE");
-	needsUpdate = true;
-	return oql();
-end
-
-function deleteNoteAfterQuestRemoved()
-	local finishedQuest = getFinishedQuest();
+function Questie:deleteNoteAfterQuestRemoved()
+	local finishedQuest = this:getFinishedQuest();
 	if (finishedQuest ~= nil) then
 		--log("finished or abandoned quest " .. finishedQuest)
 		for k,v in pairs(currentQuests[finishedQuest]["notes"]) do
@@ -319,8 +333,8 @@ function deleteNoteAfterQuestRemoved()
 	end
 end
 
-function getFinishedQuest()
-	validateQuestList();
+function Questie:getFinishedQuest()
+	this:validateQuestList();
 	for k,v in pairs(currentQuests) do
 		if (v['status'] == false) then
 			return k;
@@ -328,7 +342,7 @@ function getFinishedQuest()
 	end
 end
 
-function fillQuestList()
+function Questie:fillQuestList()
 	for i=1, GetNumQuestLogEntries() do
 		local questLogTitleText, level, questTag, isHeader, isCollapsed, isComplete = GetQuestLogTitle(i);
 		if not (isHeader) then
@@ -341,23 +355,11 @@ function fillQuestList()
 	end
 end
 
-function validateQuestList()
+function Questie:validateQuestList()
 	for k,v in pairs(currentQuests) do
 		v['status'] = false;
 		--log("setting "..k.." to false");
 	end
 	
-	fillQuestList();
+	this:fillQuestList();
 end
-
-function questieinit()
-	--DEFAULT_CHAT_FRAME:AddMessage("init", 0.95, 0.95, 0.5);
-	fillQuestList();
-	this:RegisterEvent("QUEST_LOG_UPDATE");
-	this:RegisterEvent("ZONE_CHANGED");
-	oql = GetQuestLogQuestText;
-	GetQuestLogQuestText = nql;
-	clearAllNotes();
-end
-
-
