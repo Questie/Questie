95b340e9ed8f3a1efb1df3a1f77ee05ed63f5465
AeroScripts <aerob0001@gmail.com>
2015-03-16 07:19:04 -0700

    Added in region scale/offset data for minimap implementation

---
+++ Questie/regionscale.lua
@@ -0,0 +1,21 @@
+QuestieRegionScale = {}
+
+
+-- assemble data from gatherer table (switch to non-locaized zone id as the key)
+for x=0,3 do
+	local sec = GatherRegionData[x];
+	if not (sec == nil) then
+		for y=0,32 do
+			local dat = sec[y];
+			if not (dat == nil) and not (dat['scale'] == nil) then
+				local qzoneid = QuestieZones[dat['name']][1];
+				DEFAULT_CHAT_FRAME:AddMessage(qzoneid);
+				QuestieRegionScale[qzoneid] = {
+					['scale'] = dat['scale'],
+					['xoffset'] = dat['xoffset'],
+					['yoffset'] = dat['yoffset']
+				};
+			end
+		end
+	end
+end
