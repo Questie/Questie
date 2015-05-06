5806ac93bece4cd4fe924d2f91c51f7b3738be32
David Holmstedt <Logon.q@gmail.com>
2015-05-06 18:37:39 +0200

    Addded logic to not show quests that require a quest that is currently in your questlog

--- Questie/QuestieQuest.lua
+++ Questie/QuestieQuest.lua
@@ -748,21 +748,19 @@ function Questie:GetAvailableQuestHashes(mapFileName, levelFrom, levelTo)
 			if content then
 				for k,v in pairs(content) do
 					--Questie:debug_Print("content", tostring(k),tostring(v));
-					if(not QuestieSeenQuests[v]) then
-						local qdata = QuestieHashMap[v];
-						table.insert(hashes, v);
-						if(qdata) then
-							local requiredQuest = qdata['rq'];
-							local requiredRaces = qdata['rr'];
-							local requiredClasses = qdata['rc'];
-							local valid = not QuestieSeenQuests[requiredQuest];-- THIS IS LIKELY INCORRECT NOT SURE HOW QUESTIESEENQUESTS WORKS NOW
-							if(requiredQuest) then valid = QuestieSeenQuests[requiredQuest]; end-- THIS IS LIKELY INCORRECT NOT SURE HOW QUESTIESEENQUESTS WORKS NOW
+					local qdata = QuestieHashMap[v];
+					--table.insert(hashes, v);
+					if(qdata) then
+						local requiredQuest = qdata['rq'];
+						local requiredRaces = qdata['rr'];
+						local requiredClasses = qdata['rc'];
+						local valid = not QuestieSeenQuests[requiredQuest];-- THIS IS LIKELY INCORRECT NOT SURE HOW QUESTIESEENQUESTS WORKS NOW
+						if(requiredQuest) then valid = QuestieSeenQuests[requiredQuest]; end-- THIS IS LIKELY INCORRECT NOT SURE HOW QUESTIESEENQUESTS WORKS NOW
 							
-							valid = valid and checkRequirements(race, class, requiredRaces,requiredClasses);
-							
-							if valid then
-								table.insert(hashes, v);
-							end
+						valid = valid and checkRequirements(race, class, requiredRaces,requiredClasses);
+						
+						if valid and not QuestieHandledQuests[requiredQuest] and not QuestieSeenQuests[v] then
+							table.insert(hashes, v);
 						end
 					end
 				end
