d82fcef5f6776ae4b1ea947f73b1a9ae05686ac9
Muehe <Muehe@users.noreply.github.com>
2017-10-07 06:16:13 +0200

    Add nil check to sort function in Tracker code
    * Credits to @Charrette for reporting and providing the fix.
    * This fixes #431

--- !Questie/Modules/QuestieTracker.lua
+++ !Questie/Modules/QuestieTracker.lua
@@ -480,6 +480,9 @@ function QuestieTracker:SortTrackingFrame()
         end
     end
     sort(distanceNotes, function (a, b)
+        if (not a["dist"]) or (not b["dist"]) then
+            return false;
+        end
         return a["dist"] < b["dist"];
     end)
     for k,v in pairs(distanceNotes) do
