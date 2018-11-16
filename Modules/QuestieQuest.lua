8cb50642fe15020e7c6c86dc15241989ea5d4d4b
logon <logon.q@gmail.com>
2018-11-17 00:21:10 +0100

    Stub for quests

---
+++ Modules/QuestieQuest.lua
@@ -0,0 +1,12 @@
+QuestieQuest = {...}
+local _QuestieQuest = {...}
+
+
+function QuestieQuest:LoadQuest(QuestID)--Should probably be called from some kind of questlog or similar, will have to wait untill classic to find out how tracking really works...
+
+end
+
+
+function QuestieQuest:GetAvailableQuests()--All quests between
+
+end
