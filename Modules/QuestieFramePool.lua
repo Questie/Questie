3df75af8e8bc668bd2c4e3b03b02d24ad8d6a316
logon <logon.q@gmail.com>
2018-11-17 00:21:00 +0100

    Some more frame functions, more may be needed.

--- Modules/QuestieFramePool.lua
+++ Modules/QuestieFramePool.lua
@@ -6,20 +6,41 @@ local allframes = {}
 
 
 -- Global Functions --
-
 function QuestieFrame:GetFrame()
 	for i, frame in ipairs(allframes) do
-
+    if(frame.loaded == nil)then
+      frame.loaded = true;
+      return frame
+    end
 	end
   local Note = _QuestieFrame:QuestieCreateFrame()
+  Note.loaded = true;
 	return Note
 end
 
+
+
+
 -- Local Functions --
+
+function _QuestieFrame:UnloadAll()
+  for i, frame in ipairs(allframes) do
+    QuestieFrame:UnloadFrame(frame);
+  end
+end
+
+--Use FRAME.Unload(FRAME) on frame object to unload!
+function _QuestieFrame:UnloadFrame(frame)
+  HBDPins:RemoveMinimapIcon(Questie, frame);
+  HBDPins:RemoveWorldMapIcon(Questie, frame);
+  frame.data.QuestID = nil; -- Just to be safe
+  frame.loaded = nil;
+end
 
 function _QuestieFrame:QuestieCreateFrame()
 	NumberOfFrames = NumberOfFrames + 1
 	local f = CreateFrame("Button","QuestieFrame"..NumberOfFrames,nil)
+  f:SetFrameStrata("TOOLTIP");
 	f:SetWidth(16) -- Set these to whatever height/width is needed
 	f:SetHeight(16) -- for your Texture
 	local t = f:CreateTexture(nil,"TOOLTIP")
@@ -36,20 +57,21 @@ function _QuestieFrame:QuestieCreateFrame()
   f:SetScript("OnEnter", function(self) _QuestieFrame:Questie_Tooltip(self) end); --Script Toolip
   f:SetScript("OnLeave", function() if(WorldMapTooltip) then WorldMapTooltip:Hide() end if(GameTooltip) then GameTooltip:Hide() end end) --Script Exit Tooltip
   f:SetScript("OnClick", function(self) _QuestieFrame:Questie_Click(self) end);
-
+  f.Unload = function(frame) _QuestieFrame:UnloadFrame(frame) end;
+  f.data = {}
 	table.insert(allframes, f)
+  f:Hide();
 	return f
 end
 
 
 function _QuestieFrame:Questie_Tooltip(self)
   local Tooltip = GameTooltip;
-  Questie:Print(self.data.QuestID)
-  Questie:Print(self)
   Tooltip:SetOwner(self, "ANCHOR_CURSOR"); --"ANCHOR_CURSOR" or (self, self)
 
   --TODO Logic for tooltip!
   Tooltip:AddLine("Tooltip!");
+  Tooltip:AddLine(self.data.QuestID);
   -- Preferably call something outside, keep it "abstract" here
 
 
