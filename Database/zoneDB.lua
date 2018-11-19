3bb4ae00208e8dd1228a2153290c574523e3a239
logon <logon.q@gmail.com>
2018-11-19 16:28:43 +0100

    zoneData, used for convertion

--- Database/zoneDB.lua
+++ Database/zoneDB.lua
@@ -1,4 +1,4 @@
-zoneData = {
+zoneDataClassic = { --AreaTable IDs
 	[1] = 'Dun Morogh',
 	[3] = 'Badlands',
 	[4] = 'Blasted Lands',
@@ -82,6 +82,54 @@ zoneData = {
 	[7307] = 'Upper Blacrock Spire',
 }
 
+zoneLevelList = {{1, 1, 10}, --Gets rebuilt! [ZoneID] = {Min, max}
+                 {3, 35, 45},
+                 {4, 45, 55},
+                 {8, 35, 45},
+                 {10, 18, 30},
+                 {11, 20, 30},
+                 {12, 1, 10},
+                 {14, 1, 10},
+                 {15, 35, 45},
+                 {16, 45, 55},
+                 {17, 10, 25},
+                 {28, 51, 58},
+                 {33, 30, 45},
+                 {36, 30, 40},
+                 {38, 10, 20},
+                 {40, 10, 20},
+                 {41, 55, 60},
+                 {44, 15, 25},
+                 {45, 30, 40},
+                 {46, 50, 58},
+                 {47, 40, 50},
+                 {51, 45, 50},
+                 {85, 1, 10},
+                 {130, 10, 20},
+                 {139, 53, 60},
+                 {141, 1, 10},
+                 {148, 10, 20},
+                 {215, 1, 10},
+                 {267, 20, 30},
+                 {331, 18, 30},
+                 {357, 40, 50},
+                 {361, 48, 55},
+                 {400, 25, 35},
+                 {405, 30, 40},
+                 {406, 15, 27},
+                 {440, 40, 50},
+                 {490, 48, 55},
+                 {493, 55, 60},
+                 {618, 53, 60},
+                 {1377, 55, 60},
+                 {1497, 1, 60},
+                 {1519, 1, 60},
+                 {1537, 1, 60},
+                 {1637, 1, 60},
+                 {1638, 1, 60},
+                 {1657, 1, 60}}
+
+
 instanceData = {
 	[209] = {{130, 45, 68.7}},
 	[491] = {{17, 42.3, 89.9}},
@@ -115,4 +163,166 @@ instanceData = {
 	[3429] = {{1377, 29, 95}},
 	[3456] = {{139, 39.9, 25.8}},
 	[7307] = {{51, 34.8, 84.8}, {46, 31.7, 50.4}},
+}
+
+
+Map = {} --Retail, TODO this needs to be replaced with correct maps for classic, such as the MapID for "The Barrens" for example, currently only use unmodified zones.
+Map[0] = {}
+Map[1] = {}
+--Eastern Kindoms
+	Map[0][14] = "Eastern Kingdoms"
+	Map[0][614] = "Abyssal Depths"
+	Map[0][16] = "Arathi Highlands"
+	Map[0][17] = "Badlands"
+	Map[0][19] = "Blasted Lands"
+	Map[0][29] = "Burning Steppes"
+	Map[0][866] = "Coldridge Valley"
+	Map[0][32] = "Deadwind Pass"
+	Map[0][892] = "Deathknell"
+	Map[0][27] = "Dun Morogh"
+	Map[0][34] = "Duskwood"
+	Map[0][23] = "Eastern Plaguelands"
+	Map[0][30] = "Elwynn Forest"
+	Map[0][462] = "Eversong Woods"
+	Map[0][463] = "Ghostlands"
+	Map[0][545] = "Gilneas"
+	Map[0][611] = "Gilneas City"
+	Map[0][24] = "Hillsbrad Foothills"
+	Map[0][341] = "Ironforge"
+	Map[0][499] = "Isle of Quel'Danas"
+	Map[0][610] = "Kelp'thar Forest"
+	Map[0][35] = "Loch Modan"
+	Map[0][895] = "New Tinkertown"
+	Map[0][37] = "Northern Stranglethorn"
+	Map[0][864] = "Northshire"
+	Map[0][36] = "Redridge Mountains"
+	Map[0][684] = "Ruins of Gilneas"
+	Map[0][685] = "Ruins of Gilneas City"
+	Map[0][28] = "Searing Gorge"
+	Map[0][615] = "Shimmering Expanse"
+	Map[0][480] = "Silvermoon City"
+	Map[0][21] = "Silverpine Forest"
+	Map[0][301] = "Stormwind City"
+	Map[0][689] = "Stranglethorn Vale"
+	Map[0][893] = "Sunstrider Isle"
+	Map[0][38] = "Swamp of Sorrows"
+	Map[0][673] = "The Cape of Stranglethorn"
+	Map[0][26] = "The Hinterlands"
+	Map[0][502] = "The Scarlet Enclave"
+	Map[0][20] = "Tirisfal Glades"
+	Map[0][708] = "Tol Barad"
+	Map[0][709] = "Tol Barad Peninsula"
+	Map[0][700] = "Twilight Highlands"
+	Map[0][382] = "Undercity"
+	Map[0][613] = "Vashj'ir"
+	Map[0][22] = "Western Plaguelands"
+	Map[0][39] = "Westfall"
+	Map[0][40] = "Wetlands"
+
+--Kalimdor
+	Map[1][13] = "Kalimdor"
+	Map[1][772] = "Ahn'Qiraj: The Fallen Kingdom"
+	Map[1][894] = "Ammen Vale"
+	Map[1][43] = "Ashenvale"
+	Map[1][181] = "Azshara"
+	Map[1][464] = "Azuremyst Isle"
+	Map[1][476] = "Bloodmyst Isle"
+	Map[1][890] = "Camp Narache"
+	Map[1][42] = "Darkshore"
+	Map[1][381] = "Darnassus"
+	Map[1][101] = "Desolace"
+	Map[1][4] = "Durotar"
+	Map[1][141] = "Dustwallow Marsh"
+	Map[1][891] = "Echo Isles"
+	Map[1][182] = "Felwood"
+	Map[1][121] = "Feralas"
+	Map[1][795] = "Molten Front"
+	Map[1][241] = "Moonglade"
+	Map[1][606] = "Mount Hyjal"
+	Map[1][9] = "Mulgore"
+	Map[1][11] = "Northern Barrens"
+	Map[1][321] = "Orgrimmar"
+	Map[1][888] = "Shadowglen"
+	Map[1][261] = "Silithus"
+	Map[1][607] = "Southern Barrens"
+	Map[1][81] = "Stonetalon Mountains"
+	Map[1][161] = "Tanaris"
+	Map[1][41] = "Teldrassil"
+	Map[1][471] = "The Exodar"
+	Map[1][61] = "Thousand Needles"
+	Map[1][362] = "Thunder Bluff"
+	Map[1][720] = "Uldum"
+	Map[1][201] = "Un\'Goro Crater"
+	Map[1][889] = "Valley of Trials"
+	Map[1][281] = "Winterspring"
+
+--Old Questie data (Vanilla map ids)
+QuestieZones = {
+    ["WorldMap"] = {1337, 1337, 0, 0}, --
+    ["Azeroth"] = {-1, -1, -1, 2, 0}, --
+    ["Kalimdor"] = {-1, -1, -1, 1, 0}, --
+    ["The Hinterlands"] = {42, 2, 24, 2, 20}, -- -- I found the code questhelper used, didnt do enough searching D:
+    ["Moonglade"] = {20, 1, 12, 1, 10}, --
+    ["Thousand Needles"] = {14, 1, 21, 1, 18}, --
+    ["Winterspring"] = {19, 1, 24, 1, 21}, --
+    ["Bloodmyst Isle"] = {9, 1, 4, -1, -1},--
+    ["Terokkar Forest"] = {55, 3, 7, -1, -1},--
+    ["Arathi"] = {39, 2, 2, 2, 2},--
+    ["Eversong Woods"] = {41, 2, 11, -1, -1}, --
+    ["Dustwallow"] = {10, 1, 9, 1, 7},--
+    ["Badlands"] = {27, 2, 3, 2, 3}, --
+    ["Darkshore"] = {16, 1, 5, 1, 3},--
+    ["Orgrimmar"] = {1, 1, 14, 1, 12},--
+    ["Blades Edge Mountains"] = {54, 3, 1, -1, -1},
+    ["Undercity"] = {45, 2, 26, 2, 22},
+    ["Desolace"] = {4, 1, 7, 1, 5},
+    ["Netherstorm"] = {59, 3, 4, -1, -1},
+    ["Barrens"] = {11, 1, 19, 1, 17},
+    ["Tanaris"] = {8, 1, 17, 1, 15},
+    ["Stormwind"] = {36, 2, 21, 2, 17},--
+    ["Zangarmarsh"] = {57, 3, 8, -1, -1},
+    ["Durotar"] = {7, 1, 8, 1, 6},--
+    ["Hellfire"] = {56, 3, 2, -1, -1},
+    ["Silithus"] = {5, 1, 15, 1, 13},
+    ["Shattrath City"] = {60, 3, 6, -1, -1},
+    ["Shadowmoon Valley"] = {53, 3, 5, -1, -1},
+    ["Swamp of Sorrows"] = {46, 2, 23, 2, 19},
+    ["Silvermoon City"] = {52, 2, 19, -1, -1},
+    ["Darnassus"] = {21, 1, 6, 1, 4},
+    ["Azuremyst Isle"] = {3, 1, 3, -1, -1},
+    ["Elwynn Forest"] = {37, 2, 10, 2, 10},--
+    ["Stranglethorn Vale"] = {38, 2, 22, 2, 18},
+    ["Eastern Plaguelands"] = {34, 2, 9, 2, 9},
+    ["Duskwood"] = {31, 2, 8, 2, 8},
+    ["Western Plaguelands"] = {50, 2, 27, 2, 23},
+    ["Westfall"] = {49, 2, 28, 2, 24},
+    ["Ashenvale"] = {2, 1, 1, 1, 1},
+    ["Teldrassil"] = {24, 1, 18, 1, 16},
+    ["Redridge"] = {30, 2, 17, 2, 14},
+    ["Un\'Goro Crater"] = {18, 1, 23, 1, 20},
+    ["Mulgore"] = {22, 1, 13, 1, 11},
+    ["Ironforge"] = {25, 2, 14, 2, 12},
+    ["Felwood"] = {13, 1, 10, 1, 8},
+    ["Hilsbrad"] = {48, 2, 13, 2, 11},
+    ["Deadwind Pass"] = {47, 2, 6, 2, 6},
+    ["Burning Steppes"] = {40, 2, 5, 2, 5},
+    ["Ghostlands"] = {44, 2, 12, -1, -1},
+    ["Tirisfal Glades"] = {43, 2, 25, 2, 21},
+    ["The Exodar"] = {12, 1, 20, -1, -1},
+    ["Wetlands"] = {51, 2, 29, 2, 25},
+    ["Searing Gorge"] = {32, 2, 18, 2, 15},
+    ["Blasted Lands"] = {33, 2, 4, 2, 4},
+    ["Silverpine Forest"] = {35, 2, 20, 2, 16},
+    ["Loch Modan"] = {29, 2, 16, 2, 13},
+    ["Feralas"] = {17, 1, 11, 1, 9},
+    ["Dun Morogh"] = {28, 2, 7, 2, 7},
+    ["Alterac Mountains"] = {26, 2, 1, 2, 1},
+    ["Thunder Bluff"] = {23, 1, 22, 1, 19},
+    ["Aszhara"] = {15, 1, 2, 1, 2},
+    ["Stonetalon Mountains"] = {6, 1, 16, 1, 14},
+    ["Nagrand"] = {58, 3, 3, -1, -1},
+    ["Kalimdor"] = {61, 1, 0, 1, 0},
+    ["Azeroth"] = {62, 2, 0, 2, 0},
+    ["Expansion01"] = {63, 3, 0, -1, -1},
+    ["Sunwell"] = {64, 2, 15, -1, -1} -- code copied from questhelper (this is actually the only code that was directly copied, the database was put through JavaRefactorProject
 }
