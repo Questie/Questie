8fff6c3163d76f16cfc2b7697c77b29e5d1972a2
logon <logon.q@gmail.com>
2018-11-19 19:36:30 +0100

    Quest functions, easier to use objects.

--- Database/questDB.lua
+++ Database/questDB.lua
@@ -1,19 +1,151 @@
---[[function deleteFaction(str)
-    if (CdbSettings.dbMode) then
-        return;
+QuestieDB = {...}
+local _QuestieDBQuest = {...}
+local cache = {}
+
+-- DB keys
+DB_NAME, DB_NPC, NOTE_TITLE = 1, 1, 1;
+DB_STARTS, DB_OBJ, NOTE_COMMENT, DB_MIN_LEVEL_HEALTH = 2, 2, 2, 2;
+DB_ENDS, DB_ITM, NOTE_ICON, DB_MAX_LEVEL_HEALTH = 3, 3, 3, 3;
+DB_MIN_LEVEL, DB_ZONES, DB_VENDOR, DB_OBJ_SPAWNS, DB_TRIGGER_MARKED = 4, 4, 4, 4, 4;
+DB_LEVEL, DB_ITM_QUEST_REW = 5, 5;
+DB_REQ_RACE, DB_RANK, DB_ITM_NAME = 6, 6, 6;
+DB_REQ_CLASS, DB_NPC_SPAWNS = 7, 7;
+DB_OBJECTIVES, DB_NPC_WAYPOINTS = 8, 8;
+DB_TRIGGER, DB_ZONE = 9, 9;
+DB_REQ_NPC_OR_OBJ_OR_ITM, DB_NPC_STARTS = 10, 10;
+DB_SRC_ITM, DB_NPC_ENDS = 11, 11;
+DB_PRE_QUEST_GROUP = 12;
+DB_PRE_QUEST_SINGLE = 13;
+DB_SUB_QUESTS = 14;
+DB_QUEST_GROUP = 15;
+DB_EXCLUSIVE_QUEST_GROUP = 16;
+
+function QuestieDB:Initialize()
+  _QuestieDBQuest:deleteFaction();
+end
+
+function QuestieDB:GetQuest(QuestID)
+  --[[
+  --key
+  --name = 1
+  --starts = 2
+  --npc = starts1
+  --obj = starts2
+  --itm = starts3
+  --ends = 3
+  --npc = ends1
+  --obj = ends2
+  --minLevel = 4
+  --level = 5
+  --RequiredRaces = 6
+  --RequiredClasses = 7
+  --objectives = 8
+  --trigger = 9
+  --ReqCreatureOrGOOrItm = 10
+  --npc = ReqCreatureOrGOOrItm1
+  --obj = ReqCreatureOrGOOrItm2
+  --itm = ReqCreatureOrGOOrItm3
+  --SrcItemId = 11
+  -- 12 DB_PRE_QUEST_GROUP
+  -- 13 DB_PRE_QUEST_SINGLE
+  -- 14 DB_SUB_QUESTS
+  -- 15 DB_QUEST_GROUP
+  -- 16 DB_EXCLUSIVE_QUEST_GROUP]]--
+  if(cache[QuestID]) then
+    return cache[QuestID]
+  end
+  rawdata = qData[QuestID]
+  if(rawdata)then
+    QO = {}
+    QO.ID = QuestID --Key
+    QO.Name = rawdata[1] --Name - 1
+    QO.Starts = {} --Starts - 2
+    QO.Starts["NPC"] = rawdata[2][1] --2.1
+    QO.Starts["GameObject"] = rawdata[2][2] --2.2
+    QO.Starts["Item"] = rawdata[2][3] --2.3
+    QO.Ends = {} --ends 3
+    QO.Ends["NPC"] = rawdata[3][1]
+    QO.Ends["GameObject"] = rawdata[3][2]
+    QO.MinLevel = rawdata[4]
+    QO.Level = rawdata[5]
+    QO.RequiredRaces = rawdata[6]
+    QO.RequiredClasses = rawdata[7]
+    QO.ObjectiveText = rawdata[8]
+    QO.Triggers = rawdata[9] --List of coordinates
+    QO.Objectives = {}
+    QO.Objectives["NPC"] = rawdata[10][1] --{NPCID, Different name of NPC or object}
+    QO.Objectives["GameObject"] = rawdata[10][2] --{GOID, Different name of NPC or object}
+    QO.Objectives["Item"] = rawdata[10][3]
+    QO.SrcItemId = rawdata[11] --A quest item given by a questgiver of some kind.
+    if(rawdata[12] ~= nil and rawdata[13] ~= nil) then
+      Questie:Debug("ERRRRORRRRRRR not mutually exclusive!")
     end
-    local before = CdbGetTableLength(qData);
+    if(rawdata[12] ~= nil) then
+      QO.RequiredQuest = rawdata[12]
+    else
+      QO.RequiredQuest = rawdata[13]
+    end
+    QO.SubQuests = rawdata[14] --Quests that give questitems that are used in later quests (See STV manual)
+    QO.QuestGroup = rawdata[15] --Quests that are part of the same group, example complete this group of quests to open the next one.
+    QO.ExclusiveQuestGroup = rawdata[16]
+
+    cache[QuestID] = QO
+    return QO
+  else
+    return nil
+  end
+end
+
+-- Local functions --
+
+--Deletes opposite factions quests, saves time!
+function _QuestieDBQuest:deleteFaction()
+    englishFaction, _ = UnitFactionGroup("player")
+    local fac = ""
+    if(englishFaction == "Alliance") then
+      fac = "A"
+    else
+      fac = "H"
+    end
     for key, data in pairs(qData) do
-        if (data[DB_REQ_RACE] == "AH") or (data[DB_REQ_RACE] ~= str) then
-            data[DB_REQ_RACE] = nil;
-        else
+        if (data[DB_REQ_RACE] ~= "AH") and (data[DB_REQ_RACE] ~= fac) then
             qData[key] = nil;
         end
     end
-    local after = CdbGetTableLength(qData);
-    CdbDebugPrint(2, before-after.." opposite faction quests deleted");
+    Questie:Debug("Opposite factions quests deleted");
 end
 
+
+--[[ Remove this? It sucks and is overly complicated...
+qAvailableQuestsS = {
+  --[ZONE] = {ID,ID2}
+}
+questLevel = {}
+function _QuestieDBQuest:CalculateAvailableQuestsOLD()
+  local t = zoneLevelList
+  zoneLevelList = {}
+  for i, v in ipairs(t) do
+    zoneLevelList[v[1] = {v[2], v[3]}
+  end
+
+  for i, v in pairs(qData) do
+    if(questLevel[v[DB_LEVEL] == nil) then
+      questLevel[v[DB_LEVEL] = {}
+    end
+    table.insert(questLevel[v[DB_LEVEL], i)
+  end
+  for i, v in ipairs(zoneLevelList) do
+    for itt = v[1], v[2] do
+      if(qAvailableQuestsS[i] == nil) then
+        qAvailableQuestsS[i] = {}
+      end
+      table.insert(qAvailableQuestsS[i], questLevel[itt])
+    end
+  end
+end]]--
+
+
+--[[
 function deleteClasses()
     if (not CdbSettings.class) or (CdbSettings.dbMode) then
         return;
@@ -65,11 +197,6 @@ function fillQuestLookup()
         end
     end
 end]]--
-
-function GetQuest(QuestID)
-
-
-end
 
 
 qData = {
