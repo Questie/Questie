8be3b41cedd8a939897ae2879d11a475072db25a
logon <logon.q@gmail.com>
2018-11-19 19:37:15 +0100

    Get available quests, looks like the performance of the client is higher, don't really need to play around with the lag

--- Modules/QuestieQuest.lua
+++ Modules/QuestieQuest.lua
@@ -2,11 +2,31 @@ QuestieQuest = {...}
 local _QuestieQuest = {...}
 
 
-function QuestieQuest:LoadQuest(QuestID)--Should probably be called from some kind of questlog or similar, will have to wait untill classic to find out how tracking really works...
+function QuestieQuest:TrackQuest(QuestID)--Should probably be called from some kind of questlog or similar, will have to wait untill classic to find out how tracking really works...
 
 end
 
 
 function QuestieQuest:GetAvailableQuests()--All quests between
 
+end
+
+
+qAvailableQuests = {
+
+}
+
+--TODO Check that this function does what it is supposed to...
+function QuestieQuest:CalculateAvailableQuests()
+  local MinLevel = UnitLevel("player") - Questie.db.global.minLevelFilter
+  local MaxLevel = UnitLevel("player") + Questie.db.global.maxLevelFilter
+  for i, v in pairs(qData) do
+    if(MinLevel >= v[DB_MIN_LEVEL]) then
+      table.insert(qAvailableQuests, i)
+    elseif(MaxLevel >= v[DB_MIN_LEVEL]) then
+      table.insert(qAvailableQuests, i)
+    elseif(MaxLevel >= v[DB_MIN_LEVEL] and Questie.db.char.lowlevel) then
+      table.insert(qAvailableQuests, i)
+    end
+  end
 end
