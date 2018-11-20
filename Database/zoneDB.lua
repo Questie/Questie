28c908cec3b95484c689bdde59f968a9ad97612d
logon <logon.q@gmail.com>
2018-11-20 09:33:00 +0100

    Converter should be pretty much done now.

--- Database/zoneDB.lua
+++ Database/zoneDB.lua
@@ -1,6 +1,11 @@
 
 QuestieDBZone = {...}
 
+--Use these to convert your MapIds!
+zoneDataAreaIDToMapID = { } --Databaseareaids (Vanilla) to MapID(This is not UiMapID!)
+zoneDataAreaIDToUiMapID = { }
+zoneDataUiMapIDToAreaID = { } --You should really never need the MapIDs, but you can convert back using all 3 variables.
+
 --Dumps MAPIDs
 function savefunc()
   Questie.db.global.mapsName = {}
@@ -13,18 +18,31 @@ end
 
 
 function QuestieDBZone:zoneCreateConvertion()
-	for RKey, RValue in pairs(zoneDataRetail) do
+	--[[for RKey, RValue in pairs(zoneDataRetail) do
 		for CKey, CName in pairs(zoneDataClassic) do
 			if(RValue[1] == CName) then
 				zoneDataRetailToClassic[CKey] = RKey
 				zoneDataClassicToRetail[RKey] = CKey
 			end
 		end
+	end]]--
+	for index, Data in ipairs(zoneDataClassicDemo) do
+		--for CKey, CName in pairs(zoneDataClassic) do
+--
+		--end
+		local UiMapID = HBDMigrate:GetUIMapIDFromMapAreaId(Data[4])
+		if(UiMapID == nil) then
+			Questie:Error("Map convertion failed! : ", "DataName("..tostring(Data[1])..")","UiMapID("..tostring(UiMapID)..")", "AreaID("..tostring(Data[3])..")", "MapID("..tostring(Data[4])..")")
+		elseif(UiMapID ~= nil) then
+			zoneDataAreaIDToMapID[Data[3]] = Data[4]
+			zoneDataAreaIDToUiMapID[Data[3]] = UiMapID
+			zoneDataUiMapIDToAreaID[UiMapID] = Data[3]
+			Questie:Debug(Data[1], Data[3], Data[4], UiMapID)
+		end
 	end
 end
 
-
-zoneDataClassic = { --AreaTable IDs
+zoneDataClassic = { --AreaTable IDs --Aka AreaID
 	[1] = 'Dun Morogh',
 	[3] = 'Badlands',
 	[4] = 'Blasted Lands',
@@ -108,6 +126,149 @@ zoneDataClassic = { --AreaTable IDs
 	[7307] = 'Upper Blacrock Spire',
 }
 
+--Exported IDs from Classic DEMO
+zoneDataClassicDemo = {--AreaName, Continent, AreaID, mapID (Yes it is actually misspelled in the datafiles...)
+	{"Durotar", 1,14,4},
+	{"Mulgore", 1,215,9},
+	{"Barrens", 1,17,11},
+	{"Kalimdor", 1,0,13},
+	{"Azeroth", 0,0,14},
+	{"Alterac", 0,36,15},
+	{"Arathi", 0,45,16},
+	{"Badlands", 0,3,17},
+	{"BlastedLands", 0,4,19},
+	{"Tirisfal", 0,85,20},
+	{"Silverpine", 0,130,21},
+	{"WesternPlaguelands", 0,28,22},
+	{"EasternPlaguelands", 0,139,23},
+	{"Hilsbrad", 0,267,24},
+	{"Hinterlands", 0,47,26},
+	{"DunMorogh", 0,1,27},
+	{"SearingGorge", 0,51,28},
+	{"BurningSteppes", 0,46,29},
+	{"Elwynn", 0,12,30},
+	{"DeadwindPass", 0,41,32},
+	{"Duskwood", 0,10,34},
+	{"LochModan", 0,38,35},
+	{"Redridge", 0,44,36},
+	{"Stranglethorn", 0,33,37},
+	{"SwampOfSorrows", 0,8,38},
+	{"Westfall", 0,40,39},
+	{"Wetlands", 0,11,40},
+	{"Teldrassil", 1,141,41},
+	{"Darkshore", 1,148,42},
+	{"Ashenvale", 1,331,43},
+	{"ThousandNeedles", 1,400,61},
+	{"StonetalonMountains", 1,406,81},
+	{"Desolace", 1,405,101},
+	{"Feralas", 1,357,121},
+	{"Dustwallow", 1,15,141},
+	{"Tanaris", 1,440,161},
+	{"Aszhara", 1,16,181},
+	{"Felwood", 1,361,182},
+	{"UngoroCrater", 1,490,201},
+	{"Moonglade", 1,493,241},
+	{"Silithus", 1,1377,261},
+	{"Winterspring", 1,618,281},
+	{"Stormwind", 0,1519,301},
+	{"Ogrimmar", 1,1637,321},
+	{"Ironforge", 0,1537,341},
+	{"ThunderBluff", 1,1638,362},
+	{"Darnassis", 1,1657,381},
+	{"Undercity", 0,1497,382},
+	{"AlteracValley", 30,2597,401},
+	{"WarsongGulch", 489,3277,443},
+	{"ArathiBasin", 529,3358,461}
+}
+
+
+zoneLevelList = {{1, 1, 10},
+                 {3, 35, 45},
+                 {4, 45, 55},
+                 {8, 35, 45},
+                 {10, 18, 30},
+                 {11, 20, 30},
+                 {12, 1, 10},
+                 {14, 1, 10},
+                 {15, 35, 45},
+                 {16, 45, 55},
+                 {17, 10, 25},
+                 {28, 51, 58},
+                 {33, 30, 45},
+                 {36, 30, 40},
+                 {38, 10, 20},
+                 {40, 10, 20},
+                 {41, 55, 60},
+                 {44, 15, 25},
+                 {45, 30, 40},
+                 {46, 50, 58},
+                 {47, 40, 50},
+                 {51, 45, 50},
+                 {85, 1, 10},
+                 {130, 10, 20},
+                 {139, 53, 60},
+                 {141, 1, 10},
+                 {148, 10, 20},
+                 {215, 1, 10},
+                 {267, 20, 30},
+                 {331, 18, 30},
+                 {357, 40, 50},
+                 {361, 48, 55},
+                 {400, 25, 35},
+                 {405, 30, 40},
+                 {406, 15, 27},
+                 {440, 40, 50},
+                 {490, 48, 55},
+                 {493, 55, 60},
+                 {618, 53, 60},
+                 {1377, 55, 60},
+                 {1497, 1, 60},
+                 {1519, 1, 60},
+                 {1537, 1, 60},
+                 {1637, 1, 60},
+                 {1638, 1, 60},
+                 {1657, 1, 60}}
+
+--Locations for instances in the world.
+instanceData = {
+	[209] = {{130, 45, 68.7}},
+	[491] = {{17, 42.3, 89.9}},
+	[717] = {{1519, 40.5, 55.9}},
+	[718] = {{17, 46, 36.5}},
+	[719] = {{331, 14.1, 14.4}},
+	[721] = {{1, 24.4, 39.8}},
+	[722] = {{17, 50.8, 92.8}},
+	[796] = {{85, 83, 34}},
+	[1176] = {{440, 38.7, 20.1}},
+	[1337] = {{3, 44.4, 12.2}, {3, 65.2, 43.5}},
+	[1417] = {{8, 69.4, 56.8}},
+	[1477] = {{8, 69.4, 56.8}},
+	[1581] = {{40, 42.5, 71.1}},
+	[1583] = {{51, 34.8, 84.8}, {46, 31.7, 50.4}},
+	[1584] = {{51, 34.8, 84.8}, {46, 31.7, 50.4}},
+	[1585] = {{51, 34.8, 84.8}, {46, 31.7, 50.4}},
+	[1977] = {{33, 50.6, 17.6}},
+	[2017] = {{139, 30.9, 17}},
+	[2057] = {{28, 69.8, 73.6}},
+	[2100] = {{405, 29.5, 62.5}},
+	[2159] = {{15, 52.4, 76.4}},
+	[2257] = {{1519, 60.3, 12.5}, {1537, 72.8, 50.3}},
+	[2437] = {{1637, 51.7, 49.8}},
+	[2557] = {{357, 59.2, 45.1}},
+	[2677] = {{51, 34.8, 84.8}, {46, 31.7, 50.4}},
+	[2717] = {{51, 34.8, 84.8}, {46, 31.7, 50.4}},
+	[2917] = {{1637, 40.4, 68.3}},
+	[2918] = {{1519, 72.7, 54}},
+	[3428] = {{1377, 29, 95}},
+	[3429] = {{1377, 29, 95}},
+	[3456] = {{139, 39.9, 25.8}},
+	[7307] = {{51, 34.8, 84.8}, {46, 31.7, 50.4}},
+}
+
+--Everything below is probably junk! But keep it for the time being.
+
+
+--[[
 zoneDataRetailToClassic = { } --Get generated [Retail] = Classic
 zoneDataClassicToRetail = { } --Get generated [Classic] = Retail
 
@@ -205,145 +366,9 @@ zoneDataRetail = { --Name, Type (Zone, Dungeon, Orphan, Micro), Parent
 	[1157] = {"The Great Sea", "Orphan", "Azeroth"}
 }
 
---Exported IDs from Classic DEMO
-zoneDataClassicDemo = {--Name, Continent, AreaID, mapID
-	{"Durotar", 1,14,4},
-	{"Mulgore", 1,215,9},
-	{"Barrens", 1,17,11},
-	{"Kalimdor", 1,0,13},
-	{"Azeroth", 0,0,14},
-	{"Alterac", 0,36,15},
-	{"Arathi", 0,45,16},
-	{"Badlands", 0,3,17},
-	{"BlastedLands", 0,4,19},
-	{"Tirisfal", 0,85,20},
-	{"Silverpine", 0,130,21},
-	{"WesternPlaguelands", 0,28,22},
-	{"EasternPlaguelands", 0,139,23},
-	{"Hilsbrad", 0,267,24},
-	{"Hinterlands", 0,47,26},
-	{"DunMorogh", 0,1,27},
-	{"SearingGorge", 0,51,28},
-	{"BurningSteppes", 0,46,29},
-	{"Elwynn", 0,12,30},
-	{"DeadwindPass", 0,41,32},
-	{"Duskwood", 0,10,34},
-	{"LochModan", 0,38,35},
-	{"Redridge", 0,44,36},
-	{"Stranglethorn", 0,33,37},
-	{"SwampOfSorrows", 0,8,38},
-	{"Westfall", 0,40,39},
-	{"Wetlands", 0,11,40},
-	{"Teldrassil", 1,141,41},
-	{"Darkshore", 1,148,42},
-	{"Ashenvale", 1,331,43},
-	{"ThousandNeedles", 1,400,61},
-	{"StonetalonMountains", 1,406,81},
-	{"Desolace", 1,405,101},
-	{"Feralas", 1,357,121},
-	{"Dustwallow", 1,15,141},
-	{"Tanaris", 1,440,161},
-	{"Aszhara", 1,16,181},
-	{"Felwood", 1,361,182},
-	{"UngoroCrater", 1,490,201},
-	{"Moonglade", 1,493,241},
-	{"Silithus", 1,1377,261},
-	{"Winterspring", 1,618,281},
-	{"Stormwind", 0,1519,301},
-	{"Ogrimmar", 1,1637,321},
-	{"Ironforge", 0,1537,341},
-	{"ThunderBluff", 1,1638,362},
-	{"Darnassis", 1,1657,381},
-	{"Undercity", 0,1497,382},
-	{"AlteracValley", 30,2597,401},
-	{"WarsongGulch", 489,3277,443},
-	{"ArathiBasin", 529,3358,461}
-}
 
 
 
-zoneLevelList = {{1, 1, 10},
-                 {3, 35, 45},
-                 {4, 45, 55},
-                 {8, 35, 45},
-                 {10, 18, 30},
-                 {11, 20, 30},
-                 {12, 1, 10},
-                 {14, 1, 10},
-                 {15, 35, 45},
-                 {16, 45, 55},
-                 {17, 10, 25},
-                 {28, 51, 58},
-                 {33, 30, 45},
-                 {36, 30, 40},
-                 {38, 10, 20},
-                 {40, 10, 20},
-                 {41, 55, 60},
-                 {44, 15, 25},
-                 {45, 30, 40},
-                 {46, 50, 58},
-                 {47, 40, 50},
-                 {51, 45, 50},
-                 {85, 1, 10},
-                 {130, 10, 20},
-                 {139, 53, 60},
-                 {141, 1, 10},
-                 {148, 10, 20},
-                 {215, 1, 10},
-                 {267, 20, 30},
-                 {331, 18, 30},
-                 {357, 40, 50},
-                 {361, 48, 55},
-                 {400, 25, 35},
-                 {405, 30, 40},
-                 {406, 15, 27},
-                 {440, 40, 50},
-                 {490, 48, 55},
-                 {493, 55, 60},
-                 {618, 53, 60},
-                 {1377, 55, 60},
-                 {1497, 1, 60},
-                 {1519, 1, 60},
-                 {1537, 1, 60},
-                 {1637, 1, 60},
-                 {1638, 1, 60},
-                 {1657, 1, 60}}
-
-
-instanceData = {
-	[209] = {{130, 45, 68.7}},
-	[491] = {{17, 42.3, 89.9}},
-	[717] = {{1519, 40.5, 55.9}},
-	[718] = {{17, 46, 36.5}},
-	[719] = {{331, 14.1, 14.4}},
-	[721] = {{1, 24.4, 39.8}},
-	[722] = {{17, 50.8, 92.8}},
-	[796] = {{85, 83, 34}},
-	[1176] = {{440, 38.7, 20.1}},
-	[1337] = {{3, 44.4, 12.2}, {3, 65.2, 43.5}},
-	[1417] = {{8, 69.4, 56.8}},
-	[1477] = {{8, 69.4, 56.8}},
-	[1581] = {{40, 42.5, 71.1}},
-	[1583] = {{51, 34.8, 84.8}, {46, 31.7, 50.4}},
-	[1584] = {{51, 34.8, 84.8}, {46, 31.7, 50.4}},
-	[1585] = {{51, 34.8, 84.8}, {46, 31.7, 50.4}},
-	[1977] = {{33, 50.6, 17.6}},
-	[2017] = {{139, 30.9, 17}},
-	[2057] = {{28, 69.8, 73.6}},
-	[2100] = {{405, 29.5, 62.5}},
-	[2159] = {{15, 52.4, 76.4}},
-	[2257] = {{1519, 60.3, 12.5}, {1537, 72.8, 50.3}},
-	[2437] = {{1637, 51.7, 49.8}},
-	[2557] = {{357, 59.2, 45.1}},
-	[2677] = {{51, 34.8, 84.8}, {46, 31.7, 50.4}},
-	[2717] = {{51, 34.8, 84.8}, {46, 31.7, 50.4}},
-	[2917] = {{1637, 40.4, 68.3}},
-	[2918] = {{1519, 72.7, 54}},
-	[3428] = {{1377, 29, 95}},
-	[3429] = {{1377, 29, 95}},
-	[3456] = {{139, 39.9, 25.8}},
-	[7307] = {{51, 34.8, 84.8}, {46, 31.7, 50.4}},
-}
 
 
 Map = {} --Retail, TODO this needs to be replaced with correct maps for classic, such as the MapID for "The Barrens" for example, currently only use unmodified zones.
@@ -506,3 +531,4 @@ QuestieZones = {
     ["Expansion01"] = {63, 3, 0, -1, -1},
     ["Sunwell"] = {64, 2, 15, -1, -1} -- code copied from questhelper (this is actually the only code that was directly copied, the database was put through JavaRefactorProject
 }
+]]--
