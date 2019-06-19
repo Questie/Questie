8d28fba712bea2d736f2543054abd5a523a6514c
Daniel Sink <17501944+sinkda@users.noreply.github.com>
2019-06-19 08:19:47 -0400

    Removing extra Locale
    These folders somehow made it back into the wild.

--- Locale/es/config.lua
+++
@@ -1,9 +0,0 @@
-QuestieLocale = {...};
-QuestieLocale.locale = {...};
-
-QuestieLocale.locale['es'] = {
-    -- Config Windows
-    ['ENABLE_QUESTIE'] = "Permitir Questie",
-    ['ENABLE_ICON'] = "Permitir Minimap Icon",
-
-};
