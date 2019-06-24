8ffafb3d89ccac642e7da6340a72e0ca061843d8
AeroScripts <9335832+AeroScripts@users.noreply.github.com>
2019-06-23 21:29:25 -0400

    blacklist shredder operating manual pages

--- Database/corrections.lua
+++ Database/corrections.lua
@@ -86,7 +86,21 @@ questItemBlacklist = {
     [2748]=true,
     [2749]=true,
     [2750]=true,
-    [2751]=true
+    [2751]=true,
+    
+    -- shredder operating manual
+    [16645]=true,
+    [16654]=true,
+    [16655]=true,
+    [16656]=true,
+    [16646]=true,
+    [16647]=true,
+    [16648]=true,
+    [16649]=true,
+    [16650]=true,
+    [16651]=true,
+    [16652]=true,
+    [16653]=true
     
 }
 
