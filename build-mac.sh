b73ab1e5f954fbcfc8574423fc9311464180ea5a
Benjamin <brannow@users.noreply.github.com>
2019-08-06 17:27:38 +0200

    add mac 10.14 build script
    replace \\ with / and use zip instead of 7z

---
+++ build-mac.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+LOCKFILE="Configuration/package.lock"
+PREFIX="## Version:"
+FOLDERNAME="Questie"
+CLASSICVERSION=11302
+RETAILVERSION=80000
+VERSION=$(grep "## Version:" Configuration/general.toc.conf)
+VERSION=${VERSION#"## Version:"}
+VERSION=${VERSION//[^A-Za-z0-9._-]/_}
+ROOT=$(pwd)
+if [ ! -f $LOCKFILE ] ; then
+    > $LOCKFILE;
+
+    echo "--> Generating develop toc files"
+    echo "----> Classic"
+    echo "#DO NOT MODIFY"> QuestieDev-master.toc
+    echo "#AUTOMATICALLY GENERATED FROM Configuration/general.toc.conf" >> QuestieDev-master.toc
+    echo "## Interface: $CLASSICVERSION" >> QuestieDev-master.toc
+    cat Configuration/general.toc.conf >> QuestieDev-master.toc
+
+    echo "----> Retail"
+    echo "#DO NOT MODIFY"> QuestieDev-master-retail.toc
+    echo "#AUTOMATICALLY GENERATED FROM Configuration/general.toc.conf" >> QuestieDev-master-retail.toc
+    echo "## Interface: $RETAILVERSION" >> QuestieDev-master-retail.toc
+    cat Configuration/general.toc.conf >> QuestieDev-master-retail.toc
+
+    #Had to do this because reading a file with this didn't seem to work because it read linebreak and stuff...
+    echo "Moving code!"
+    mkdir Configuration/$FOLDERNAME
+
+    cp -R ./Database Configuration/$FOLDERNAME
+    cp -R ./Icons Configuration/$FOLDERNAME
+    cp -R ./Libs Configuration/$FOLDERNAME
+    cp -R ./Locale Configuration/$FOLDERNAME
+    cp -R ./Modules Configuration/$FOLDERNAME
+    cp -r embeds.xml Configuration/$FOLDERNAME
+    cp -r Questie.lua Configuration/$FOLDERNAME
+    cp -r README.md Configuration/$FOLDERNAME
+
+    echo "--> Generating develop toc files"
+    echo "----> Classic"
+    echo "#DO NOT MODIFY"> Configuration/$FOLDERNAME/$FOLDERNAME.toc
+    echo "#AUTOMATICALLY GENERATED FROM Configuration/general.toc.conf" >> Configuration/$FOLDERNAME/$FOLDERNAME.toc
+    echo "## Interface: $CLASSICVERSION" >> Configuration/$FOLDERNAME/$FOLDERNAME.toc
+    cat Configuration/general.toc.conf >> Configuration/$FOLDERNAME/$FOLDERNAME.toc
+
+    echo "----> Retail"
+    echo "#DO NOT MODIFY"> Configuration/$FOLDERNAME/$FOLDERNAME-retail.toc
+    echo "#AUTOMATICALLY GENERATED FROM Configuration/general.toc.conf" >> Configuration/$FOLDERNAME/$FOLDERNAME-retail.toc
+    echo "## Interface: $RETAILVERSION" >> Configuration/$FOLDERNAME/$FOLDERNAME-retail.toc
+    cat Configuration/general.toc.conf >> Configuration/$FOLDERNAME/$FOLDERNAME-retail.toc
+
+    echo "Packaging release"
+    rm -rf Configuration/Questie-$VERSION.zip
+    cd Configuration/
+    zip -r Questie_$VERSION.zip $FOLDERNAME
+    cd "$ROOT"
+    rm -rf Configuration/$FOLDERNAME
+
+    echo "Done!"
+    rm $LOCKFILE
+fi
