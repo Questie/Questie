1d7a838e553c20fc60b497bb294abeee85d531cc
Muehe <Muehe@users.noreply.github.com>
2019-08-09 01:10:13 +0200

    Change build process
    * Remove bat file, shell script should be runable through GitBash on
    Windows
    * Remove redundant config
    * Add releases folder to gitignore
    * Do not delete the folder after zip-ing
    * Check for existence of folder and zip before creating a release
    * Create release folder automatically when shell script is first run

--- build.sh
+++ build.sh
@@ -1,63 +1,61 @@
 #!/bin/bash
-LOCKFILE="Configuration/package.lock"
-PREFIX="## Version:"
+
+# Use first argument from command line as type and defaults to "classic"
+RELEASE_TYPE=${0:-"classic"}
+
 FOLDERNAME="Questie"
 CLASSICVERSION=11302
 RETAILVERSION=80000
-VERSION=$(grep "## Version:" Configuration/general.toc.conf)
-VERSION=${VERSION#"## Version:"}
+
+# Find version line in toc
+VERSION_LINE=$(grep "## Version: " QuestieDev-master.toc)
+# Remove prefix
+VERSION=${VERSION_LINE#"## Version: "}
+# Replace whitespace
 VERSION=${VERSION//[^A-Za-z0-9._-]/_}
-ROOT=$(pwd)
-if [ ! -f $LOCKFILE ] ; then
-    > $LOCKFILE;
 
-    echo "--> Generating develop toc files"
-    echo "----> Classic"
-    echo "#DO NOT MODIFY"> QuestieDev-master.toc
-    echo "#AUTOMATICALLY GENERATED FROM Configuration/general.toc.conf" >> QuestieDev-master.toc
-    echo "## Interface: $CLASSICVERSION" >> QuestieDev-master.toc
-    cat Configuration/general.toc.conf >> QuestieDev-master.toc
-    cp QuestieDev-master.toc Questie.toc
-
-    #echo "----> Retail"
-    #echo "#DO NOT MODIFY"> QuestieDev-master-retail.toc
-    #echo "#AUTOMATICALLY GENERATED FROM Configuration/general.toc.conf" >> QuestieDev-master-retail.toc
-    #echo "## Interface: $RETAILVERSION" >> QuestieDev-master-retail.toc
-    #cat Configuration/general.toc.conf >> QuestieDev-master-retail.toc
-
-    #Had to do this because reading a file with this didn't seem to work because it read linebreak and stuff...
-    echo "Moving code!"
-    mkdir Configuration/$FOLDERNAME
-
-    cp -R ./Database Configuration/$FOLDERNAME
-    cp -R ./Icons Configuration/$FOLDERNAME
-    cp -R ./Libs Configuration/$FOLDERNAME
-    cp -R ./Locale Configuration/$FOLDERNAME
-    cp -R ./Modules Configuration/$FOLDERNAME
-    cp -r embeds.xml Configuration/$FOLDERNAME
-    cp -r Questie.lua Configuration/$FOLDERNAME
-    cp -r README.md Configuration/$FOLDERNAME
-
-    echo "--> Generating develop toc files"
-    echo "----> Classic"
-    echo "#DO NOT MODIFY"> Configuration/$FOLDERNAME/$FOLDERNAME.toc
-    echo "#AUTOMATICALLY GENERATED FROM Configuration/general.toc.conf" >> Configuration/$FOLDERNAME/$FOLDERNAME.toc
-    echo "## Interface: $CLASSICVERSION" >> Configuration/$FOLDERNAME/$FOLDERNAME.toc
-    cat Configuration/general.toc.conf >> Configuration/$FOLDERNAME/$FOLDERNAME.toc
-
-    #echo "----> Retail"
-    #echo "#DO NOT MODIFY"> Configuration/$FOLDERNAME/$FOLDERNAME-retail.toc
-    #echo "#AUTOMATICALLY GENERATED FROM Configuration/general.toc.conf" >> Configuration/$FOLDERNAME/$FOLDERNAME-retail.toc
-    #echo "## Interface: $RETAILVERSION" >> Configuration/$FOLDERNAME/$FOLDERNAME-retail.toc
-    #cat Configuration/general.toc.conf >> Configuration/$FOLDERNAME/$FOLDERNAME-retail.toc
-
-    echo "Packaging release"
-    rm -rf Configuration/Questie-v$VERSION.zip
-    cd Configuration/
-    7z a -tzip Questie-v$VERSION.zip $FOLDERNAME
-    cd "$ROOT"
-    rm -rf Configuration/$FOLDERNAME
-
-    echo "Done!"
-    rm $LOCKFILE
+# Create release dir if it doesn't exist
+if [ ! -d "releases" ] ; then
+    mkdir releases/
 fi
+
+# Make sure we do not overwrite anything
+if [ -d "releases/$VERSION" ] || [ -f releases/Questie-v$VERSION.zip ] ; then
+    echo "A release for this version already exists, delete it or bump the version"
+    exit 1
+fi
+
+# If this file exists, the script will abort
+LOCKFILE="releases/package.lock"
+
+if [ -f $LOCKFILE ] ; then
+    echo "$LOCKFILE exists, aborting"
+    exit 2
+fi
+
+> $LOCKFILE;
+
+echo "Copying code!"
+mkdir releases/$VERSION
+mkdir releases/$VERSION/$FOLDERNAME
+
+cp -R ./Database releases/$VERSION/$FOLDERNAME
+cp -R ./Icons releases/$VERSION/$FOLDERNAME
+cp -R ./Libs releases/$VERSION/$FOLDERNAME
+cp -R ./Locale releases/$VERSION/$FOLDERNAME
+cp -R ./Modules releases/$VERSION/$FOLDERNAME
+cp -r embeds.xml releases/$VERSION/$FOLDERNAME
+cp -r Questie.lua releases/$VERSION/$FOLDERNAME
+cp -r README.md releases/$VERSION/$FOLDERNAME
+
+cp QuestieDev-master.toc releases/$VERSION/$FOLDERNAME/$FOLDERNAME.toc
+
+echo "Packaging release"
+ROOT=$(pwd)
+cd releases/$VERSION
+7z a -tzip Questie-v$VERSION.zip $FOLDERNAME
+cd $ROOT
+
+echo "Done!"
+rm $LOCKFILE
+
