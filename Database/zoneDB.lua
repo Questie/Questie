3cd6a257228c9aa83c56d6a8801da4bf5694350a
AeroScripts <9335832+AeroScripts@users.noreply.github.com>
2019-09-01 23:36:40 -0400

    Didn't realize this was used in QuestieQuest
    We really gotta clean up zonedb

--- Database/zoneDB.lua
+++ Database/zoneDB.lua
@@ -312,7 +312,7 @@ local Questie2ZoneTable = {
     ["Sunwell"] = {64, 2, 15, -1, -1} -- code copied from questhelper (this is actually the only code that was directly copied, the database was put through JavaRefactorProject
 }
 
-local Questie2ZoneTableInverse = {};
+Questie2ZoneTableInverse = {};
 
 for k,v in pairs(Questie2ZoneTable) do
 
