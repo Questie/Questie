e303b2f850a8b851a952cce324c2afdee85d4fa7
BreakBB <b.boeing@online.de>
2019-09-22 20:01:57 +0200

    Add file doc to corrections.lua

--- Database/corrections.lua
+++ Database/corrections.lua
@@ -1,3 +1,14 @@
+--[[
+    This file contains the corrections of the database files.
+
+    It is a separate file so we can upstream those changes easier to cmangos and can still
+    update the database files with a script.
+
+    Most of the corrections can be done by accessing a specific key instead of copying the
+    whole object over and change it.
+    You can find the keys at the beginning of each file (e.g. 'questKeys' are at the beginning of 'questDB.lua').
+--]]
+
 QuestieCorrections = {}
 
 QuestieCorrections.questFixes = {
