a2840c6917bcf2cf4c3f24e2e0890e46d663495c
Logonz <logon.q atterino gmail dot com>
2019-09-27 13:18:39 +0200

    Old commented code cleanup, bump to v4.2.0

--- Modules/QuestieEventHandler.lua
+++ Modules/QuestieEventHandler.lua
@@ -101,40 +101,6 @@ function QuestieEventHandler:PLAYER_ENTERING_WORLD()
             QuestieEventHandler:QUEST_LOG_UPDATE()
         end
     end)
-
-
-    --[[C_Timer.After(2, function ()
-        Questie:Debug(DEBUG_ELEVATED, "Player entered world")
-        QuestieQuest:GetAllQuestIds()
-    end)
-
-    C_Timer.After(5, function ()
-        Questie:Debug(DEBUG_ELEVATED, "Player entered world (deferred update)")
-        QuestieQuest:GetAllQuestIds()
-    end)]]--
-
-    -- periodically update the objectives of quests, temporary hold-over until we can properly fix the event based logic
-    --[[Questie:ScheduleRepeatingTimer(function()
-        if (GetNumQuestLogEntries()+1 == __UPDATEFIX_IDX) then
-            __UPDATEFIX_IDX = 1;
-        end
-
-        title, level, _, isHeader, _, isComplete, _, _questId, _, displayQuestId, _, _, _, _, _, _, _ = GetQuestLogTitle(__UPDATEFIX_IDX)
-        if (not isHeader) and _questId ~= nil and _questId > 0 then
-            QuestieQuest:UpdateQuest(_questId);
-        end
-        __UPDATEFIX_IDX = __UPDATEFIX_IDX + 1
-    end, 3);]]--
-
-    --local Note = QuestieFramePool:GetFrame();
-    --THIS WILL BE MOVED!!!
-    --Note.data.QuestID = 1337
-    --Note.data.data..NoteType = NoteType --MiniMapNote or WorldMapNote, Will be moved!
-    --Note.data.IconType = type;
-    --Note.data.questHash = questHash;
-    --Note.data.objectiveid = objectiveid;
-    --HBDPins:AddMinimapIconWorld(Questie, Note, 0, x, y, true)
-    --HBDPins:AddWorldMapIconWorld(Questie, Note, 0, x, y, HBD_PINS_WORLDMAP_SHOW_WORLD)
 end
 
 --Fires when a quest is accepted in anyway.
@@ -155,16 +121,6 @@ function QuestieEventHandler:QUEST_ACCEPTED(questLogIndex, questId)
     data.type = "accept";
     Questie:Print("Register Change", questId, hash);
     updateQuestId[questId] = data;
-
-
-    --[[C_Timer.After(2, function ()
-        QuestieQuest:AcceptQuest(QuestId) -- is it safe to pass params to virtual functions like this?
-    end)
-
-    -- this needs to use a repeating timer maybe? Often times when quest is accepted, it has the same trouble as the other events
-    C_Timer.After(5, function()
-        QuestieQuest:UpdateQuest(QuestId);
-    end)]]--
 
     -- Add quest accept journey note.
     local data = {};
@@ -232,13 +188,6 @@ function QuestieEventHandler:QUEST_REMOVED(QuestId)
         
         table.insert(Questie.db.char.journey, data);
     end
-    
-    -- deferred update (possible desync fix?)
-    --C_Timer.After(3, function()
-    --    QuestieQuest:GetAllQuestIdsNoObjectives();
-    --    QuestieQuest:CalculateAvailableQuests();
-    --    QuestieQuest:DrawAllAvailableQuests();
-    --end)
 end
 
 --Fires when a quest is turned in.
@@ -246,13 +195,6 @@ function QuestieEventHandler:QUEST_TURNED_IN(questID, xpReward, moneyReward)
     _Hack_prime_log()
     Questie:Debug(DEBUG_DEVELOP, "EVENT: QUEST_TURNED_IN", questID, xpReward, moneyReward);
     QuestieQuest:CompleteQuest(questID)
-    
-    -- deferred update (possible desync fix?)
-    --C_Timer.After(3, function()
-    --    QuestieQuest:GetAllQuestIdsNoObjectives()
-    --    QuestieQuest:CalculateAvailableQuests();
-    --    QuestieQuest:DrawAllAvailableQuests();
-    --end)
 
     -- Complete Quest added to Journey
     local data = {};
@@ -330,56 +272,6 @@ function QuestieEventHandler:QUEST_WATCH_UPDATE(QuestLogIndex)
     data.type = "update";
     Questie:Print("Register Change", questId, hash);
     updateQuestId[questId] = data;
-
-
-    --[[_hack_prime_log()
-    title, level, _, isHeader, _, isComplete, _, questId, _, displayQuestId, _, _, _, _, _, _, _ = GetQuestLogTitle(QuestLogIndex)
-    Questie:Debug(DEBUG_DEVELOP, "EVENT: QUEST_WATCH_UPDATE", "QLogIndex: "..QuestLogIndex, "QuestID: "..questId);
-
-    --If a timer exists from previous upda
-    if(QuestWatchTimers.cancelTimer) then
-        Questie:CancelTimer(QuestWatchTimers.cancelTimer)
-        QuestWatchTimers.cancelTimer = nil;
-        Questie:Debug(DEBUG_DEVELOP, "[QuestieEventHandler] Cancel timer exists, canceled!")
-    end
-    if(QuestWatchTimers.repeatTimer) then
-        Questie:CancelTimer(QuestWatchTimers.repeatTimer)
-        QuestWatchTimers.repeatTimer = nil;
-        Questie:Debug(DEBUG_DEVELOP, "[QuestieEventHandler] Repeat timer exists, canceled!")
-    end
-    local _QuestLogIndexFinal = tonumber(QuestLogIndex)
-    local _questIdFinal = tonumber(questId);
-    QuestWatchTimers.cancelTimer = Questie:ScheduleTimer(function()
-            if QuestWatchTimers.repeatTimer ~= nil then
-            Questie:CancelTimer(QuestWatchTimers.repeatTimer)
-            QuestWatchTimers.repeatTimer = nil;
-            QuestWatchTimers.cancelTimer = nil;
-            Questie:Debug(DEBUG_DEVELOP, "[QuestieEventHandler] Repeat timer took to long, cancel it!")
-
-            --if _questIdFinal ~= nil and _questIdFinal > 0 then
-            --  QuestieQuest:UpdateQuest(_questIdFinal);
-            --end
-        end
-        -- always double-update
-        if _questIdFinal ~= nil and _questIdFinal > 0 then
-            QuestieQuest:UpdateQuest(_questIdFinal);
-        end
-    end, 6)
-
-    C_Timer.After(1, function() -- start repeating after 1 sec, first update was incorrectly being detected as a change because this bug is super annoying
-            QuestWatchTimers.repeatTimer = Questie:ScheduleRepeatingTimer(function()
-            local QuestInfo = QuestieQuest:GetRawLeaderBoardDetails(_QuestLogIndexFinal)
-            if(lastState[QuestInfo.Id] == nil or lastState[QuestInfo.Id].compareString ~= QuestInfo.compareString) then
-                Questie:Debug(DEBUG_DEVELOP, "[QuestieEventHandler] QUEST_WATCH_UPDATE found a change!")
-                lastState[QuestInfo.Id] = QuestInfo;
-                Questie:CancelTimer(QuestWatchTimers.repeatTimer)
-                Questie:CancelTimer(QuestWatchTimers.cancelTimer)
-                QuestWatchTimers.cancelTimer = nil;
-                QuestWatchTimers.repeatTimer = nil;
-                QuestieQuest:UpdateQuest(QuestInfo.Id);
-            end
-        end, 1)
-    end)]]--
 end
 
 function QuestieEventHandler:QUEST_LOG_CRITERIA_UPDATE(questID, specificTreeID, description, numFulfilled, numRequired)
@@ -395,8 +287,6 @@ function QuestieEventHandler:PLAYER_LEVEL_UP(level, hitpoints, manapoints, talen
     Questie:Debug(DEBUG_DEVELOP, "EVENT: PLAYER_LEVEL_UP", level);
     
     QuestiePlayer:SetPlayerLevel(level);
-    --QuestieQuest:CalculateAvailableQuests();
-    --QuestieQuest:DrawAllAvailableQuests();
     
     -- deferred update (possible desync fix?)
     C_Timer.After(3, function() 
