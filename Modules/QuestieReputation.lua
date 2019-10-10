a784f2b6e04fd021403e0614435fe7428e4bf06b
BreakBB <b.boeing@online.de>
2019-10-10 20:34:55 +0200

    Change skill and reputation header expanding

--- Modules/QuestieReputation.lua
+++ Modules/QuestieReputation.lua
@@ -5,24 +5,13 @@ UnitLevel = function() return 60; end
 
 function QuestieReputation:Update()
     Questie:Debug(DEBUG_DEVELOP, "QuestieReputation: Update")
-    local expandedHeaders = {}
+    ExpandFactionHeader(0) -- Expand all header
 
     for i=1, GetNumFactions() do
-
         local _, _, _, _, _, barValue, _, _, isHeader, isCollapsed, _, _, _, factionID, _, _ = GetFactionInfo(i)
-        if isHeader == 1 and isCollapsed == true then
-            Questie:Debug(DEBUG_DEVELOP, "QuestieReputation: Expanding header")
-            ExpandFactionHeader(i)
-            table.insert(expandedHeaders, i)
-        end
-
         if isHeader == nil or isHeader == false then
             playerReputations[factionID] = barValue
         end
-    end
-    -- Collapse all expanded headers again
-    for _, v in pairs(expandedHeaders) do
-        CollapseFactionHeader(v)
     end
 end
 
