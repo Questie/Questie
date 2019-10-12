3bf27b909f7f354e58df80994abf99b5c449af4b
BreakBB <b.boeing@online.de>
2019-10-12 23:43:22 +0200

    Bumb versions for 4.2.0

--- build.py
+++ build.py
@@ -127,10 +127,10 @@ def setVersion():
         with open('QuestieDev-master.toc') as toc:
             tocData = toc.read()
             ## Version: 4.1.1 BETA
-            tocData = re.sub(r"## Title:.*", "## Title: |cFFFFFFFF%s|r|cFF00FF00 %s_%s|r|cFFFF0000 %s|r" % (addonDir, versionTag, recentCommit, releaseType), tocData)
+            tocData = re.sub(r"## Title:.*", "## Title: |cFFFFFFFF%s|r|cFF00FF00 %s_%s|r" % (addonDir, versionTag, recentCommit), tocData)
             ## Title: |cFFFFFFFFQuestie|r|cFF00FF00 v4.1.1|r|cFFFF0000 Beta|r
-            tocData = re.sub(r"## Version:.*", "## Version: %s %s %s %s" % (versionTag.lstrip("v"), releaseType, nrOfCommits, recentCommit), tocData)
-            
+            tocData = re.sub(r"## Version:.*", "## Version: %s %s %s" % (versionTag.lstrip("v"), nrOfCommits, recentCommit), tocData)
+
         with open('releases/%s/%s/%s.toc' % (versionDir, addonDir, addonDir), "w") as toc:
             toc.write(tocData)
 
