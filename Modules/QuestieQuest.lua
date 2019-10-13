27a6b35e89ad538eb62811bc3a68e43d07b00e2e
logon <logon.q@gmail.com>
2019-10-13 13:41:19 +0200

    Changed russian string and reenabled "contains" for the time being.

--- Modules/QuestieQuest.lua
+++ Modules/QuestieQuest.lua
@@ -1055,9 +1055,9 @@ function QuestieQuest:GetAllQuestObjectives(Quest)
                         local correctObjective = false;
                         if(oName and oDesc) then
                             -- Does regular check work good? or Regex mayhaps?
-                            if((oName == oDesc)) then -- or strfind(oDesc, oName, 1, true)) then
+                            if((oName == oDesc) or strfind(oDesc, oName, 1, true)) then
                                 correctObjective = true;
-                            elseif(oText == oDesc) then -- or strfind(oDesc, oName, 1, true)) then
+                            elseif(oText == oDesc or strfind(oDesc, oName, 1, true)) then
                                 correctObjective = true;
                             end
                         elseif((oName == nil or oDesc == nil) and objectiveDB.Type ~= "item" and objectiveDB.Type ~= "monster") then
@@ -1228,7 +1228,7 @@ local slainText = {
     frFR = "tué", --Known good
     itIT = "ucciso",
     ptBR = "morto",
-    ruRU = "убито",
+    ruRU = "– убито",
     koKR = "처치",
     zhCN = "已消灭", --Confirmed with chinese, not tested...
     zhTW = "消滅",
