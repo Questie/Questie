f48bb4c8ca441280406ed933ad71150982a2130f
drejjmit <drimit_cs@hotmail.com>
2019-10-20 14:27:53 +0200

    Update config.lua

--- Locale/zhTW/config.lua
+++ Locale/zhTW/config.lua
@@ -133,7 +133,7 @@ QuestieLocale.locale['zhTW'] = {
     ['TOOLTIP_QUEST_COMPLETE'] = "（完成）",
     ['TOOLTIP_QUEST_AVAILABLE'] = "（可接）",
     ['TOOLTIP_QUEST_ACTIVE'] = "（已有）",
-    ['TOOLTIP_QUEST_REPEATABLE'] = "(Repeatable)",
+    ['TOOLTIP_QUEST_REPEATABLE'] = "(可重複)",
 
     -- Slash Commands
     ['SLASH_INVALID'] = "無效指令，要查看指令列表請輸入：",
