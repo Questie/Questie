7686ebc7b4020f001a5da2b9cced96648a3c95be
drejjmit <drimit_cs@hotmail.com>
2019-10-20 14:26:13 +0200

    Update config.lua
    closes #1370
    closes #1371
    closes #1372

--- Locale/zhCN/config.lua
+++ Locale/zhCN/config.lua
@@ -133,7 +133,7 @@ QuestieLocale.locale['zhCN'] = {
     ['TOOLTIP_QUEST_COMPLETE'] = "（完成）",
     ['TOOLTIP_QUEST_AVAILABLE'] = "（可接）",
     ['TOOLTIP_QUEST_ACTIVE'] = "（已有）",
-    ['TOOLTIP_QUEST_REPEATABLE'] = "(Repeatable)",
+    ['TOOLTIP_QUEST_REPEATABLE'] = "(可重复)",
 
     -- Slash Commands
     ['SLASH_INVALID'] = "无效命令，要查看指令列表请输入：",
