4e21ecdf625e2998f622c61bdb5e597334ee5ffe
Martin Nguyen <31273878+NguyenMartin@users.noreply.github.com>
2019-10-28 15:52:44 -0700

    Update Database/Corrections/QuestieQuestFixes.lua
    Co-Authored-By: Björn Böing <b.boeing@online.de>

--- Database/Corrections/QuestieQuestFixes.lua
+++ Database/Corrections/QuestieQuestFixes.lua
@@ -572,7 +572,7 @@ function QuestieQuestFixes:Load()
             [QuestieDB.questKeys.startedBy] = {{5149},nil,nil,},
             [QuestieDB.questKeys.finishedBy] = {{928},nil,},
             [QuestieDB.questKeys.requiredLevel] = 60,
-        [QuestieDB.questKeys.questLevel] = 60,
+            [QuestieDB.questKeys.questLevel] = 60,
         [QuestieDB.questKeys.requiredRaces] = 5,
         [QuestieDB.questKeys.requiredClasses] = 2,
         [QuestieDB.questKeys.objectivesText] = {"Speak with Lord Grayson Shadowbreaker in Stormwind's Cathedral District."},
