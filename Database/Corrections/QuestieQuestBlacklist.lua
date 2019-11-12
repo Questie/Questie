f5a653bf27e4436ac635489473ba53241c31c58f
Björn Böing <b.boeing@online.de>
2019-11-12 09:56:36 +0100

    [quest] Blacklist "The Hunter's Path" started by "Ayanna Everstride"
    Fixes #700

--- Database/Corrections/QuestieQuestBlacklist.lua
+++ Database/Corrections/QuestieQuestBlacklist.lua
@@ -10,6 +10,7 @@ function QuestieQuestBlacklist:Load()
         [2358] = true, -- Horns of Nez'ra is not in the game at this point. See #921
         [787] = true, -- The New Horde is not in the game. See #830
         [6606] = true, -- Quest is not in the game. See #1338
+        [6072] = true, -- Ayanna Everstride doesn't start "Hunter's Path" (this quest is most likely simply not in the game) #700
         --Stray quests
         [3861] = true, --CLUCK!
         --World event quests
