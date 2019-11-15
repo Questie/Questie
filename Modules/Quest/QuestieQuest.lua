f6b66ba69f5d0afacd809344c0f6cf3a738b0a5a
BreakBB <b.boeing@online.de>
2019-11-15 11:17:04 +0100

    Use private field for QuestieQuest

--- Modules/Quest/QuestieQuest.lua
+++ Modules/Quest/QuestieQuest.lua
@@ -23,10 +23,8 @@ local QuestieLib = QuestieLoader:ImportModule("QuestieLib");
 local QuestiePlayer = QuestieLoader:ImportModule("QuestiePlayer");
 ---@type QuestieDB
 local QuestieDB = QuestieLoader:ImportModule("QuestieDB");
----@type QuestieDBZone
-local QuestieDBZone = QuestieLoader:ImportModule("QuestieDBZone")
 
-local _QuestieQuest = {...}
+local _QuestieQuest = QuestieQuest.private
 local libS = LibStub:GetLibrary("AceSerializer-3.0")
 local libC = LibStub:GetLibrary("LibCompress")
 
@@ -231,7 +229,8 @@ end
 
 function QuestieQuest:UpdateHiddenNotes()
     QuestieQuest:GetAllQuestIds() -- add notes that weren't added from previous hidden state
-    if Questie.db.global.enableAvailable then
+    local questieGlobalDB = Questie.db.global
+    if questieGlobalDB.enableAvailable then
         QuestieQuest:DrawAllAvailableQuests();
     end
 
@@ -240,11 +239,11 @@ function QuestieQuest:UpdateHiddenNotes()
         for index, frameName in ipairs(framelist) do -- this may seem a bit expensive, but its actually really fast due to the order things are checked
             local icon = _G[frameName];
             if icon ~= nil and icon.data then
-                if (QuestieQuest.NotesHidden or (((not Questie.db.global.enableObjectives) and (icon.data.Type == "monster" or icon.data.Type == "object" or icon.data.Type == "event" or icon.data.Type == "item"))
-                 or ((not Questie.db.global.enableTurnins) and icon.data.Type == "complete")
-                 or ((not Questie.db.global.enableAvailable) and icon.data.Type == "available"))
-                 or ((not Questie.db.global.enableMapIcons) and (not icon.miniMapIcon))
-                 or ((not Questie.db.global.enableMiniMapIcons) and (icon.miniMapIcon))) or (icon.data.ObjectiveData and icon.data.ObjectiveData.HideIcons) or (icon.data.QuestData and icon.data.QuestData.HideIcons and icon.data.Type ~= "complete") then
+                if (QuestieQuest.NotesHidden or (((not questieGlobalDB.enableObjectives) and (icon.data.Type == "monster" or icon.data.Type == "object" or icon.data.Type == "event" or icon.data.Type == "item"))
+                 or ((not questieGlobalDB.enableTurnins) and icon.data.Type == "complete")
+                 or ((not questieGlobalDB.enableAvailable) and icon.data.Type == "available"))
+                 or ((not questieGlobalDB.enableMapIcons) and (not icon.miniMapIcon))
+                 or ((not questieGlobalDB.enableMiniMapIcons) and (icon.miniMapIcon))) or (icon.data.ObjectiveData and icon.data.ObjectiveData.HideIcons) or (icon.data.QuestData and icon.data.QuestData.HideIcons and icon.data.Type ~= "complete") then
                     icon:FakeHide()
                 else
                     icon:FakeUnhide()
@@ -264,8 +263,8 @@ function QuestieQuest:UpdateHiddenNotes()
             local icon = _G[frameName]
             if icon ~= nil and icon.data then
                 if  QuestieQuest.NotesHidden or
-                    ((not Questie.db.global.enableMapIcons) and (not icon.miniMapIcon)) or
-                    ((not Questie.db.global.enableMiniMapIcons) and (icon.miniMapIcon))
+                    ((not questieGlobalDB.enableMapIcons) and (not icon.miniMapIcon)) or
+                    ((not questieGlobalDB.enableMiniMapIcons) and (icon.miniMapIcon))
                 then
                     icon:FakeHide()
                 else
@@ -543,8 +542,10 @@ end
 
 function QuestieQuest:AddFinisher(quest)
     --We should never ever add the quest if IsQuestFlaggedComplete true.
-    Questie:Debug(DEBUG_INFO, "[QuestieQuest]", "Adding finisher for quest ", quest.Id)
-    if(QuestiePlayer.currentQuestlog[quest.Id] and IsQuestFlaggedCompleted(quest.Id) == false and IsQuestComplete(quest.Id) and not Questie.db.char.complete[quest.Id]) then
+    local questId = quest.Id
+    Questie:Debug(DEBUG_INFO, "[QuestieQuest]", "Adding finisher for quest ", questId)
+
+    if(QuestiePlayer.currentQuestlog[questId] and IsQuestFlaggedCompleted(questId) == false and IsQuestComplete(questId) and not Questie.db.char.complete[questId]) then
         local finisher = nil
         if quest.Finisher ~= nil then
             if quest.Finisher.Type == "monster" then
@@ -552,17 +553,17 @@ function QuestieQuest:AddFinisher(quest)
             elseif quest.Finisher.Type == "object" then
                 finisher = QuestieDB:GetObject(quest.Finisher.Id)
             else
-                Questie:Debug(DEBUG_SPAM, "[QuestieQuest]: ".. QuestieLocale:GetUIString('DEBUG_UNHANDLE_FINISH', quest.Finisher.Type, quest.Id, quest.name))
+                Questie:Debug(DEBUG_SPAM, "[QuestieQuest]: ".. QuestieLocale:GetUIString('DEBUG_UNHANDLE_FINISH', quest.Finisher.Type, questId, quest.name))
             end
         else
-            Questie:Debug(DEBUG_SPAM, "[QuestieQuest]: ".. QuestieLocale:GetUIString('DEBUG_NO_FINISH', quest.Id, quest.name))
+            Questie:Debug(DEBUG_SPAM, "[QuestieQuest]: ".. QuestieLocale:GetUIString('DEBUG_NO_FINISH', questId, quest.name))
         end
         if(finisher ~= nil and finisher.spawns ~= nil) then
             for Zone, Spawns in pairs(finisher.spawns) do
                 if(Zone ~= nil and Spawns ~= nil) then
                     for _, coords in ipairs(Spawns) do
                         local data = {}
-                        data.Id = quest.Id;
+                        data.Id = questId;
                         data.Icon = ICON_TYPE_COMPLETE;
                         data.GetIconScale = function() return Questie.db.global.availableScale or 1.3 end
                         data.IconScale = data:GetIconScale();
@@ -639,133 +640,16 @@ function QuestieQuest:AddFinisher(quest)
     end
 end
 
-ObjectiveSpawnListCallTable = {
-    ["monster"] = function(id, Objective)
-        local npc = QuestieDB:GetNPC(id)
-        if not npc then
-            -- todo: log this
-            return nil
-        end
-        local ret = {}
-        local mon = {};
 
-        mon.Name = npc.name
-        mon.Spawns = npc.spawns
-        mon.Icon = ICON_TYPE_SLAY
-        mon.Id = id
-        mon.GetIconScale = function() return Questie.db.global.monsterScale or 1 end
-        mon.IconScale = mon:GetIconScale();
-        mon.TooltipKey = "m_" .. id -- todo: use ID based keys
-
-        ret[id] = mon;
-        return ret
-    end,
-    ["object"] = function(id, Objective)
-        local object = QuestieDB:GetObject(id)
-        if not object then
-            -- todo: log this
-            return nil
-        end
-        local ret = {}
-        local obj = {}
-
-        obj.Name = object.name
-        obj.Spawns = object.spawns
-        obj.Icon = ICON_TYPE_LOOT
-        obj.GetIconScale = function() return Questie.db.global.objectScale or 1 end
-        obj.IconScale = obj:GetIconScale()
-        obj.TooltipKey = "o_" .. id
-        obj.Id = id
-
-        ret[id] = obj
-        return ret
-    end,
-    ["event"] = function(id, Objective)
-        local ret = {}
-        ret[1] = {};
-        ret[1].Name = Objective.Description or "Event Trigger";
-        ret[1].Icon = ICON_TYPE_EVENT
-        ret[1].GetIconScale = function() return Questie.db.global.eventScale or 1.35 end
-        ret[1].IconScale = ret[1]:GetIconScale();
-        ret[1].Id = id or 0
-        if Objective.Coordinates then
-            ret[1].Spawns = Objective.Coordinates
-        elseif Objective.Description then-- we need to fall back to old questie data, some events are missing in the new DB
-            ret[1].Spawns = {}
-            local questie2data = TEMP_Questie2Events[Objective.Description];
-            if questie2data and questie2data["locations"] then
-                for i, spawn in pairs(questie2data["locations"]) do
-                    local zid = Questie2ZoneTableInverse[spawn[1]];
-                    if zid then
-                        zid = QuestieDBZone:GetAreaIdByUIMapID(zid)
-                        if zid then
-                            if not ret[1].Spawns[zid] then
-                                ret[1].Spawns[zid] = {};
-                            end
-                            local x = spawn[2] * 100;
-                            local y = spawn[3] * 100;
-                            tinsert(ret[1].Spawns[zid], {x, y});
-                        end
-                    end
-                end
-            end
-        end
-        return ret
-    end,
-    ["item"] = function(id, Objective)
-        local ret = {};
-        local item = QuestieDB:GetItem(id);
-        if item ~= nil and item.Sources ~= nil then
-            for _, source in pairs(item.Sources) do
-                if ObjectiveSpawnListCallTable[source.Type] and source.Type ~= "item" then -- anti-recursive-loop check, should never be possible but would be bad if it was
-                    local sourceList = ObjectiveSpawnListCallTable[source.Type](source.Id, Objective);
-                    if sourceList == nil then
-                        -- log this
-                    else
-                        for id, sourceData in pairs(sourceList) do
-                            if not ret[id] then
-                                ret[id] = {};
-                                ret[id].Name = sourceData.Name;
-                                ret[id].Spawns = {};
-                                if source.Type == "object" then
-                                    ret[id].Icon = ICON_TYPE_OBJECT
-                                    ret[id].GetIconScale = function() return Questie.db.global.objectScale or 1 end
-                                    ret[id].IconScale = ret[id]:GetIconScale()
-                                else
-                                    ret[id].Icon = ICON_TYPE_LOOT
-                                    ret[id].GetIconScale = function() return Questie.db.global.lootScale or 1 end
-                                    ret[id].IconScale = ret[id]:GetIconScale()
-                                end
-                                ret[id].TooltipKey = sourceData.TooltipKey
-                                ret[id].Id = id
-                            end
-                            if sourceData.Spawns and not item.Hidden then
-                                for zone, spawns in pairs(sourceData.Spawns) do
-                                    if not ret[id].Spawns[zone] then
-                                        ret[id].Spawns[zone] = {};
-                                    end
-                                    for _, spawn in pairs(spawns) do
-                                        tinsert(ret[id].Spawns[zone], spawn);
-                                    end
-                                end
-                            end
-                        end
-                    end
-                end
-            end
-        end
-        return ret
-    end
-}
 
 -- this is for forcing specific things on to the map (That aren't quest related)
 -- label and customScale can be nil
 function QuestieQuest:ForceToMap(type, id, label, customScale)
-    if ObjectiveSpawnListCallTable[type] and type ~= "event" then
+    if _QuestieQuest.objectiveSpawnListCallTable[type] and type ~= "event" then
         local mapRefs = {}
         local miniRefs = {}
-        --local spawnData = ObjectiveSpawnListCallTable[type](id)[id]
-        for id, spawnData in pairs(ObjectiveSpawnListCallTable[type](id)) do
+        --local spawnData = _QuestieQuest.objectiveSpawnListCallTable[type](id)[id]
+        for id, spawnData in pairs(_QuestieQuest.objectiveSpawnListCallTable[type](id)) do
             spawnData.Type = type
             spawnData.CustomTooltipData = {}
             spawnData.CustomTooltipData.Title = label or "Forced Icon"
@@ -801,8 +685,8 @@ function QuestieQuest:PopulateObjective(Quest, ObjectiveIndex, Objective, BlockI
         Quest.AlreadySpawned = {};
     end
 
-    if ObjectiveSpawnListCallTable[Objective.Type] and (not Objective.spawnList) then
-        Objective.spawnList = ObjectiveSpawnListCallTable[Objective.Type](Objective.Id, Objective);
+    if _QuestieQuest.objectiveSpawnListCallTable[Objective.Type] and (not Objective.spawnList) then
+        Objective.spawnList = _QuestieQuest.objectiveSpawnListCallTable[Objective.Type](Objective.Id, Objective);
     end
 
     local maxPerType = 300
@@ -1786,19 +1670,4 @@ function QuestieQuest:CalculateAvailableQuests()
             end
         end
     end
-end
-
-function _QuestieQuest:LevelRequirementsFulfilled(quest, playerLevel, minLevel, maxLevel)
-    return (quest.level >= minLevel or Questie.db.char.lowlevel) and quest.level <= maxLevel and quest.requiredLevel <= playerLevel
-end
-
--- We always want to show a quest if it is a childQuest and its parent is in the quest log
-function _QuestieQuest:IsParentQuestActive(parentID)
-    if parentID == nil or parentID == 0 then
-        return false
-    end
-    if QuestiePlayer.currentQuestlog[parentID] then
-        return true
-    end
-    return false
 end
