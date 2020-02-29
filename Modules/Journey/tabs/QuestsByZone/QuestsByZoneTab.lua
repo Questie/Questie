9913392c8eb14b4967e03730db87507b200d3ce8
Björn Böing <b.boeing@online.de>
2020-02-29 10:33:05 +0100

    Rename variables

--- Modules/Journey/tabs/QuestsByZone/QuestsByZoneTab.lua
+++ Modules/Journey/tabs/QuestsByZone/QuestsByZoneTab.lua
@@ -21,48 +21,48 @@ function _QuestieJourney.questsByZone:DrawTab(container)
     container:AddChild(header)
     QuestieJourneyUtils:Spacer(container)
 
-    local CDropdown = AceGUI:Create("LQDropdown")
-    local zDropdown = AceGUI:Create("LQDropdown")
+    local contDropdown = AceGUI:Create("LQDropdown")
+    local zoneDropdown = AceGUI:Create("LQDropdown")
     ---@class AceSimpleGroup
     local treegroup = AceGUI:Create("SimpleGroup")
 
     -- Dropdown for Continent
-    CDropdown:SetList(QuestieJourney.continents)
-    CDropdown:SetText(QuestieLocale:GetUIString('JOURNEY_SELECT_CONT'))
+    contDropdown:SetList(QuestieJourney.continents)
+    contDropdown:SetText(QuestieLocale:GetUIString('JOURNEY_SELECT_CONT'))
 
     local currentContinentId = QuestiePlayer:GetCurrentContinentId()
-    CDropdown:SetValue(currentContinentId)
+    contDropdown:SetValue(currentContinentId)
 
-    CDropdown:SetCallback("OnValueChanged", function(key, checked)
+    contDropdown:SetCallback("OnValueChanged", function(key, checked)
         local sortedZones = QuestieJourneyUtils:GetSortedZoneKeys(QuestieJourney.zones[key.value])
-        zDropdown:SetList(QuestieJourney.zones[key.value], sortedZones)
-        zDropdown:SetText(QuestieLocale:GetUIString('JOURNEY_SELECT_ZONE'))
-        zDropdown:SetDisabled(false)
+        zoneDropdown:SetList(QuestieJourney.zones[key.value], sortedZones)
+        zoneDropdown:SetText(QuestieLocale:GetUIString('JOURNEY_SELECT_ZONE'))
+        zoneDropdown:SetDisabled(false)
     end)
-    container:AddChild(CDropdown)
+    container:AddChild(contDropdown)
 
     -- Dropdown for Zone
-    zDropdown:SetText(QuestieLocale:GetUIString('JOURNEY_SELECT_ZONE'))
+    zoneDropdown:SetText(QuestieLocale:GetUIString('JOURNEY_SELECT_ZONE'))
 
     local currentZoneId = QuestiePlayer:GetCurrentZoneId()
     if currentZoneId > 0 then
         local sortedZones = QuestieJourneyUtils:GetSortedZoneKeys(QuestieJourney.zones[currentContinentId])
-        zDropdown:SetList(QuestieJourney.zones[currentContinentId], sortedZones)
-        zDropdown:SetValue(currentZoneId)
+        zoneDropdown:SetList(QuestieJourney.zones[currentContinentId], sortedZones)
+        zoneDropdown:SetValue(currentZoneId)
         local zoneTree = _QuestieJourney.questsByZone:CollectZoneQuests(currentZoneId)
         -- Build Tree
         _QuestieJourney.questsByZone:ManageTree(treegroup, zoneTree)
     else
-        zDropdown:SetDisabled(true)
+        zoneDropdown:SetDisabled(true)
     end
 
-    zDropdown:SetCallback("OnValueChanged", function(key, checked)
+    zoneDropdown:SetCallback("OnValueChanged", function(key, checked)
         -- Create Tree View
         local zoneTree = _QuestieJourney.questsByZone:CollectZoneQuests(key.value)
         -- Build Tree
         _QuestieJourney.questsByZone:ManageTree(treegroup, zoneTree)
     end)
-    container:AddChild(zDropdown)
+    container:AddChild(zoneDropdown)
 
     QuestieJourneyUtils:Spacer(container)
 
