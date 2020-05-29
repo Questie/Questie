57675f624c4a82399d3b77bbe97c07dc51a738fa
Dyaxler <dyaxler@hotmail.com>
2020-05-29 11:04:57 -0700

    Update Modules/Journey/tabs/QuestsByZone/QuestsByZone.lua
    Co-authored-by: Björn Böing <b.boeing@online.de>

--- Modules/Journey/tabs/QuestsByZone/QuestsByZone.lua
+++ Modules/Journey/tabs/QuestsByZone/QuestsByZone.lua
@@ -69,7 +69,7 @@ function _QuestieJourney.questsByZone:ManageTree(container, zoneTree)
         -- Add the quest to the open chat window if it was a shift click
         if (IsModifiedClick("CHATLINK") and ChatEdit_GetActiveWindow()) then
             if Questie.db.global.trackerShowQuestLevel then
-                ChatEdit_InsertLink("[["..quest.level.."] "..quest.name.." ("..quest.Id..")]")
+                ChatEdit_InsertLink("[[" .. quest.level .. "] " .. quest.name .. " (" .. quest.Id .. ")]")
             else
                 ChatEdit_InsertLink("["..quest.name.." ("..quest.Id..")]")
             end
