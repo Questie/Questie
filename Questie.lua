ae71b3802d532727f42d00057d9bd85e3f7ee7fc
Björn Böing <b.boeing@online.de>
2020-05-29 23:02:12 +0200

    Add pop up and coloring to retail error

--- Questie.lua
+++ Questie.lua
@@ -1,11 +1,19 @@
 if WOW_PROJECT_ID == WOW_PROJECT_MAINLINE then
+    StaticPopupDialogs["RETAIL_ERROR"] = {
+        text = "|cffff0000ERROR|r\nYou're trying to use Questie on retail WoW!\nQuestie is supporting WoW Classic only!\n\nYou should come and join the real WoW",
+        button2 = "Okay, maybe I will",
+        hasEditBox = false,
+        whileDead = true
+    }
+
     C_Timer.After(4, function()
         DEFAULT_CHAT_FRAME:AddMessage("---------------------------------")
-        DEFAULT_CHAT_FRAME:AddMessage("ERROR: You're trying to use Questie on retail WoW.!")
-        DEFAULT_CHAT_FRAME:AddMessage("ERROR: Questie is supporting WoW Classic only!")
+        DEFAULT_CHAT_FRAME:AddMessage("|cffff0000ERROR|r: |cff42f5adYou're trying to use Questie on retail WoW!|r")
+        DEFAULT_CHAT_FRAME:AddMessage("|cffff0000ERROR|r: |cff42f5adQuestie is supporting WoW Classic only!")
         DEFAULT_CHAT_FRAME:AddMessage("---------------------------------")
         error("ERROR: You're trying to use Questie on retail WoW. Questie is supporting WoW Classic only!")
     end)
+    StaticPopup_Show("RETAIL_ERROR")
     return nil
 end
 
