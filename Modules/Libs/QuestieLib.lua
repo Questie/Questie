0237584413da527fb7c88d9e66e3fcf7e6e89894
Björn Böing <b.boeing@online.de>
2020-05-31 12:05:05 +0200

    Fix usage of GetQuestTagInfo

--- Modules/Libs/QuestieLib.lua
+++ Modules/Libs/QuestieLib.lua
@@ -190,7 +190,7 @@ end
 ---@param level integer @The quest level
 ---@param blizzLike boolean @True = [40+], false/nil = [40D/R]
 function QuestieLib:GetQuestString(id, name, level, blizzLike)
-    local questType, questTag = QuestieDB:GetQuestTagInfo()
+    local questType, questTag = QuestieDB:GetQuestTagInfo(id)
 
     if questType and questTag then
         local char = "+"
