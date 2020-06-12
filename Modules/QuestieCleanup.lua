503e32186ea67e9181c325e142134a0322dc2a89
Björn Böing <b.boeing@online.de>
2020-06-12 15:14:24 +0200

    Fix typing of QuestieCleanup

--- Modules/QuestieCleanup.lua
+++ Modules/QuestieCleanup.lua
@@ -1,4 +1,6 @@
+---@class Cleanup
 local QuestieCleanup = QuestieLoader:CreateModule("Cleanup")
+---@type QuestieDB
 local QuestieDB = QuestieLoader:ImportModule("QuestieDB")
 
 function QuestieCleanup:Run()
@@ -8,13 +10,13 @@ function QuestieCleanup:Run()
     QuestieDB.questData = nil
     QuestieDB.objectData = nil
     QuestieDB.itemData = nil
-    
+
     -- clean up lang
     LangItemLookup = nil
     LangNameLookup = nil
     LangObjectLookup = nil
     LangQuestLookup = nil
-    
+
     -- we call this here to make sure there isn't a lag spike later on
     collectgarbage()
 end
