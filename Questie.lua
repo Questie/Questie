becac92dba8e5132c15a76538b15f7d205ed9539
Björn Böing <b.boeing@online.de>
2020-06-16 22:51:59 +0200

    [feature] Add check and error message for vanilla WoW servers

--- Questie.lua
+++ Questie.lua
@@ -1,35 +1,3 @@
-if WOW_PROJECT_ID == WOW_PROJECT_MAINLINE then
-    StaticPopupDialogs["RETAIL_ERROR"] = {
-        text = "|cffff0000ERROR|r\nYou're trying to use Questie on retail WoW!\nQuestie is supporting WoW Classic only!\n\nYou should come and join the real WoW",
-        button2 = "Okay, maybe I will",
-        hasEditBox = false,
-        whileDead = true
-    }
-
-    C_Timer.After(4, function()
-        DEFAULT_CHAT_FRAME:AddMessage("---------------------------------")
-        DEFAULT_CHAT_FRAME:AddMessage("|cffff0000ERROR|r: |cff42f5adYou're trying to use Questie on retail WoW!|r")
-        DEFAULT_CHAT_FRAME:AddMessage("|cffff0000ERROR|r: |cff42f5adQuestie is supporting WoW Classic only!")
-        DEFAULT_CHAT_FRAME:AddMessage("---------------------------------")
-        error("ERROR: You're trying to use Questie on retail WoW. Questie is supporting WoW Classic only!")
-    end)
-    StaticPopup_Show("RETAIL_ERROR")
-    return nil
-end
-
-if(Questie) then
-    C_Timer.After(4, function()
-        error("ERROR!! -> Questie already loaded! Please only have one Questie installed!")
-        for i=1, 10 do
-            DEFAULT_CHAT_FRAME:AddMessage("|cFFFF0000ERROR!!|r -> Questie already loaded! Please only have one Questie installed!")
-        end
-    end);
-    error("ERROR!! -> Questie already loaded! Please only have one Questie installed!")
-    DEFAULT_CHAT_FRAME:AddMessage("|cFFFF0000ERROR!!|r -> Questie already loaded! Please only have one Questie installed!")
-    Questie = {}
-    return nil;
-end
-
 if not QuestieConfigCharacter then
     QuestieConfigCharacter = {}
 end
