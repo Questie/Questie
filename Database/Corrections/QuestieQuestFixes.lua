ee3b85a4218ac66d6e6edb25551a65a90ee7576f
drejjmit <drimit_cs@hotmail.com>
2020-06-27 10:38:31 +0200

    [quest] Mark "I Know A Guy..." and "To Gadgetzan You Go!" as exclusive to "Clamlette Surprise"
    Fixes #2070

--- Database/Corrections/QuestieQuestFixes.lua
+++ Database/Corrections/QuestieQuestFixes.lua
@@ -1553,6 +1553,12 @@ function QuestieQuestFixes:Load()
         [6609] = {
             [QuestieDB.questKeys.exclusiveTo] = {6607}, -- #1154
         },
+        [6611] = {
+            [QuestieDB.questKeys.exclusiveTo] = {6610}, -- #2070
+        },
+        [6612] = {
+            [QuestieDB.questKeys.exclusiveTo] = {6610}, -- #2070
+        },
         [6622] = {
             [QuestieDB.questKeys.triggerEnd] = {"15 Patients Saved!", {[15]={{67.79,49.06},},},},
         },
