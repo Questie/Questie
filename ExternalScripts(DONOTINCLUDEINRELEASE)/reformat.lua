727d7a0e03a1143dc88b93357f9e1a637191f223
Muehe <Muehe@users.noreply.github.com>
2020-06-29 01:40:01 +0200

    Fix another missing escape in reformat script

--- ExternalScripts(DONOTINCLUDEINRELEASE)/reformat.lua
+++ ExternalScripts(DONOTINCLUDEINRELEASE)/reformat.lua
@@ -11,7 +11,7 @@ function dump(tableToDump, fd, indent)
     for k, v in pairs(tableToDump) do
         local formatting = string.rep('    ', indent)
         if type(v) == "table" then
-            fd:write(formatting .. "['" .. string.gsub(k, "'", "\\'") .. "'] = {\n")
+            fd:write(formatting .. "['" .. string.gsub(string.gsub(k, "'", "\\'"), '\n', '\\n') .. "'] = {\n")
             dump(v, fd, indent+1)
             fd:write(formatting .. '},\n')
         elseif type(v) == "boolean" then
