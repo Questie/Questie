e580b54102371e1a0917bc8f2a3b861da95bad01
Gogo1951 <55365231+Gogo1951@users.noreply.github.com>
2020-07-01 01:31:50 -0500

    [quest] Fixes for Saving Yenniku
    [[46] Saving Yenniku (592)] -- missing objectives on map, add soul gem / Filled Soul Gem
    
        Saving Yenniku - Quest - World of Warcraft || https://classic.wowhead.com/quest=592/saving-yenniku
    
        Filled Soul Gem - Item - World of Warcraft || https://classic.wowhead.com/item=3913/filled-soul-gem
    
        Yenniku - NPC - World of Warcraft || https://classic.wowhead.com/npc=2530/yenniku
    
        [592] = {"Saving Yenniku",{{2519,},nil,nil,},{{2497,},nil,},30,46,178,nil,{"Bring the Filled Soul Gem to Nimboya.",},nil,{nil,nil,{{3913,nil},},nil,},3912,nil,{591,},{593,},nil,nil,33,nil,nil,nil,nil,nil,nil,nil,nil,},
    
        [2530] = {'Yenniku',1902,1902,41,41,0,{[33]={{39.02,58.35},},},nil,33,{593,},{593,},28,nil,"Darkspear Hostage",2
    
    
    https://github.com/AeroScripts/QuestieDev/pull/2107

--- Database/Corrections/QuestieItemFixes.lua
+++ Database/Corrections/QuestieItemFixes.lua
@@ -197,6 +197,9 @@ function QuestieItemFixes:Load()
         [3864] = {
             [itemKeys.npcDrops] = {},
         },
+        [3913] = {
+            [QuestieDB.itemKeys.npcDrops] = {2530},
+        },
         [4016] = {
             [itemKeys.npcDrops] = {1488,1489,1490,1491,2530,2534,2535,2536,2537},
         },
