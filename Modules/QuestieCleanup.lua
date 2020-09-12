68ad13a94fb0290fc4091c89b9e3169437594baf
AeroScripts <9335832+AeroScripts@users.noreply.github.com>
2020-09-12 15:36:59 -0400

    Validate DB data when debug is enabled

--- Modules/QuestieCleanup.lua
+++ Modules/QuestieCleanup.lua
@@ -2,8 +2,19 @@
 local QuestieCleanup = QuestieLoader:CreateModule("Cleanup")
 ---@type QuestieDB
 local QuestieDB = QuestieLoader:ImportModule("QuestieDB")
+---@tyle QuestieDBCompiler
+local QuestieDBCompiler = QuestieLoader:ImportModule("DBCompiler")
 
 function QuestieCleanup:Run()
+
+    if Questie.db.global.debugEnabled then
+        print("Validating objects...")
+        QuestieDBCompiler:Validate()
+        print("Validating items...")
+        QuestieDBCompiler:ValidateItems()
+        print("Validating quests...")
+        QuestieDBCompiler:ValidateQuests()
+    end
 
     -- clean up raw db
     QuestieDB.npcData = nil
