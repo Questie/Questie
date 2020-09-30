6fad4b18f66d4bbe9c89377b5f7103b371a0054d
anon1231823 <67269448+anon1231823@users.noreply.github.com>
2020-09-30 18:13:14 +0000

    [locale][ptBR] Update UI translations (Thanks @anon1231823)

--- Locale/ptBR/config.lua
+++ Locale/ptBR/config.lua
@@ -194,8 +194,8 @@ QuestieLocale.locale['ptBR'] = {
     ['RESET_QUESTIE_BTN_DESC'] = "Redefina a Questie para valores para todas as configurações.",
     ['RECOMPILE_DATABASE_BTN'] = "Recompilar banco de dados",
     ['RECOMPILE_DATABASE_BTN_DESC'] = "Força uma recompilação do banco de dados do Questie. Isso também recarregará a interface do usuário.",
-    ['SHOW_PROFILER_BTN'] = "Open Profiler",
-    ['SHOW_PROFILER_BTN_DESC'] = "Open the Questie profiler, this is useful for tracking down the source of lag / frame spikes.",
+    ['SHOW_PROFILER_BTN'] = "Abrir perfilador",
+    ['SHOW_PROFILER_BTN_DESC'] = "Abre o perfilador Questie, isso é útil para rastrear a fonte de picos da latência.",
     ['QUESTIE_DEV_MESSAGE'] = "Questie está em desenvolvimento para World of Warcraft: Classic. Verifique o GitHub para obter as versões mais recentes ou relatar problemas. Ou junte-se a nós em nossa discórdia!(( https://github.com/AeroScripts/QuestieDev/ )) ",
 
     --Elementos da interface do utilisador
@@ -464,7 +464,7 @@ QuestieLocale.locale['ptBR'] = {
     ['No quests to list.'] = "Nenhuma missão para listar.",
     ['No spawn data available.'] = "Não há dados de desova disponíveis.",
     ['The database needs to be updated to change language. Press reload to apply the new language'] = "The database needs to be updated to change language. Press reload to apply the new language",
-    ['Reload UI'] = "Reload UI",
+    ['Reload UI'] = "Recarregar IU",
     ["\124cFFAAEEFFQuestie DB has updated!\124r\124cFFFF6F22 Data is being processed, this may take a few moments and cause some lag..."] = "\124cFFAAEEFFO Questie DB foi atualizado!\124r\124cFFFF6F22 Os dados estão sendo processados, isso pode levar alguns instantes e causar alguma latência...",
     ["\124cFF4DDBFF [1/4] Updating NPCs..."] = "\124cFF4DDBFF [1/4] Atualizando PNJs...",
     ["\124cFF4DDBFF [2/4] Updating objects..."] = "\124cFF4DDBFF [2/4] Atualizando objetos...",  
