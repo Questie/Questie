02832f175e62c02008d6ac5f1005137921627718
drejjmit <drimit_cs@hotmail.com>
2020-10-08 13:39:17 +0200

    [quest] Add "Stranglethorn Fever" to disallowedQuests for auto accept

--- Modules/Auto/DisallowedIDs.lua
+++ Modules/Auto/DisallowedIDs.lua
@@ -47,6 +47,7 @@ _QuestieAuto.disallowedQuests = {
     [155] = true, -- The Defias Traitor (The Defias Brotherhood)
     [219] = true, -- Corporal Keeshan (Missing In Action)
     [309] = true, -- Miran (Protecting the Shipment)
+    [349] = true, -- Stranglethorn Fever (Consumes items)
     [434] = true, -- Tyrion (The Attack!)
     [435] = true, -- Deathstalker Erland (Escorting Erland)
     [648] = true, -- Homing Robot OOX-17/TN (Rescue OOX-17/TN!)
