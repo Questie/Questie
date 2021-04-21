2c7e02318b80ea6a1b6b02493433a66d36fe2694
AeroScripts <9335832+AeroScripts@users.noreply.github.com>
2021-04-20 21:58:39 -0400

    Add keys to top of DB files for easier referencing

--- Database/TBC/tbcItemDB.lua
+++ Database/TBC/tbcItemDB.lua
@@ -9,6 +9,24 @@ if (not isTBCClient) then
     return
 end
 
+QuestieDB.itemKeys = {
+    ['name'] = 1, -- string
+    ['npcDrops'] = 2, -- table or nil, NPC IDs
+    ['objectDrops'] = 3, -- table or nil, object IDs
+    ['itemDrops'] = 4, -- table or nil, item IDs
+    ['startQuest'] = 5, -- int or nil, ID of the quest started by this item
+    ['questRewards'] = 6, -- table or nil, quest IDs
+    ['flags'] = 7, -- int or nil, see: https://github.com/cmangos/issues/wiki/Item_template#flags
+    ['foodType'] = 8, -- int or nil, see https://github.com/cmangos/issues/wiki/Item_template#foodtype
+    ['itemLevel'] = 9, -- int, the level of this item
+    ['requiredLevel'] = 10, -- int, the level required to equip/use this item
+    ['ammoType'] = 11, -- int,
+    ['class'] = 12, -- int,
+    ['subClass'] = 13, -- int,
+    ['vendors'] = 14, -- table or nil, NPC IDs
+    ['relatedQuests'] = 15, -- table or nil, IDs of quests that are related to this item
+}
+
 QuestieDB.itemData = {
 [25] = {'Worn Shortsword',{19994,21382,23324,},nil,nil,nil,nil,nil,nil,2,1,0,2,7,nil,},
 [35] = {'Bent Staff',nil,nil,nil,nil,nil,nil,nil,2,1,0,2,10,nil,},
