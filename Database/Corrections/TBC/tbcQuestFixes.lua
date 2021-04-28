520381cca4473ff74cdda85f05f99388913ef0f7
BreakBB <b.boeing@online.de>
2021-04-28 21:06:21 +0200

    [quest] Add objective location for "Challenge to the Black Flight"

--- Database/Corrections/TBC/tbcQuestFixes.lua
+++ Database/Corrections/TBC/tbcQuestFixes.lua
@@ -1006,6 +1006,9 @@ function QuestieTBCQuestFixes:Load()
         [11142] = {
             [questKeys.triggerEnd] = {"Survey Alcaz Island", {[zoneIDs.DUSTWALLOW_MARSH]={{69.96,19.55},{67.36,50.87},},}},
         },
+        [11162] = {
+            [questKeys.extraObjectives] = {{nil, ICON_TYPE_EVENT, "Plant the Stonemaul Banner", 0, {{"object", 186336}}}},
+        },
         [11169] = {
             [questKeys.objectives] = {nil,nil,nil,nil,{{4344,4345,},4344,"Totem Tests Performed"}},
         },
