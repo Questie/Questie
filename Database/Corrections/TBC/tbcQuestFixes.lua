4d7d3032b6233d0b5cd9897b23c47677f60ecbbf
BreakBB <b.boeing@online.de>
2021-05-02 20:24:25 +0200

    [quest] Mark "The Moss-twined Heart" as Alliance quest

--- Database/Corrections/TBC/tbcQuestFixes.lua
+++ Database/Corrections/TBC/tbcQuestFixes.lua
@@ -72,6 +72,9 @@ function QuestieTBCQuestFixes:Load()
         [870] = {
             [questKeys.triggerEnd] = {"Explore the waters of the Forgotten Pools", {[zoneIDs.THE_BARRENS]={{45.06,22.56},},}},
         },
+        [927] = {
+            [questKeys.requiredRaces] = raceIDs.ALL_ALLIANCE,
+        },
         [1448] = {
             [questKeys.triggerEnd] = {"Search for the Temple of Atal'Hakkar", {[zoneIDs.SWAMP_OF_SORROWS]={{64.67,48.82},{64.36,56.12},{64.09,51.95},{69.6,44.18},{73.97,46.36},},}},
         },
