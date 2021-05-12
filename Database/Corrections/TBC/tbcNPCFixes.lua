c34c9af5048500cf693040951d7d14385d2bb804
Gill <Gill@gillsitworld.com>
2021-05-12 19:56:40 -0400

    Correct NPC Location for Quest 11505

--- Database/Corrections/TBC/tbcNPCFixes.lua
+++ Database/Corrections/TBC/tbcNPCFixes.lua
@@ -213,6 +213,9 @@ function QuestieTBCNpcFixes:Load()
         [21638] = {
             [npcKeys.spawns] = {[zoneIDs.TEROKKAR_FOREST] = {{50.88,54.76},},},
         },
+        [24885] = {
+            [npcKeys.spawns] = {[zoneIDs.TEROKKAR_FOREST] = {{55.8,53.8},},},
+        },
 
         -- Below are fake IDs to show specific quest starts/ends only at one specific location even though the
         -- corresponding real NPC has multiple spawns (e.g. "The Kessel Run" requires you to run to Azure Watch even
