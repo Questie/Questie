36d640a29c8592110727f3fe02ac95bbc88c1b21
BreakBB <b.boeing@online.de>
2021-05-20 14:27:20 +0200

    [fix] Fix "Quests by Zone" determining the wrong "current zone"

--- Modules/QuestiePlayer.lua
+++ Modules/QuestiePlayer.lua
@@ -99,10 +99,6 @@ function QuestiePlayer:GetCurrentContinentId()
         end
     end
 
-    if currentContinentId == 0 then
-        return 1 -- Default to Eastern Kingdom
-    end
-
     return currentContinentId
 end
 
