73116097562165c15f7ff69506480d42ecf98889
Drejjmit <drimit_cs@hotmail.com>
2021-06-06 15:45:52 +0200

    Blacklist unavailable quest "FLAG Shadow Council/Warmaul Questline"

--- Database/Corrections/QuestieQuestBlacklist.lua
+++ Database/Corrections/QuestieQuestBlacklist.lua
@@ -781,6 +781,7 @@ function QuestieQuestBlacklist:Load()
         [9735] = true, -- Replaced with [True Masters of the Light (64143)]
         [9736] = true, -- Replaced with [True Masters of the Light (64144)]
         [9737] = true, -- Replaced with [True Masters of the Light  (64145)]
+        [9926] = true, -- FLAG Shadow Council/Warmaul Questline
         [10169] = true, -- Losing Gracefully (removed with 2.4.0)
         [10364] = true, -- Caedmos (Unavailable Priest quest)
         [10379] = true, -- Touch of Weakness (Followup of NOT A QUEST)
