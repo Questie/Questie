74c786f193a4b69970f287220360e88ded712121
AeroScripts <9335832+AeroScripts@users.noreply.github.com>
2021-06-08 13:41:58 -0400

    More announce bandaids
    gotta redo the entire thing

--- Modules/QuestieAnnounce.lua
+++ Modules/QuestieAnnounce.lua
@@ -9,6 +9,8 @@ local l10n = QuestieLoader:ImportModule("l10n")
 
 QuestieAnnounce._itemCache = {} -- cache data since this happens on item looted it could happen a lot with auto loot
 
+QuestieAnnounce._AlreadySentBandaid = {} -- TODO: rewrite the entire thing its a lost cause
+
 function QuestieAnnounce:Announce(questId, progressType, itemId, objectiveText, objectiveProgress)
     if "disabled" ~= Questie.db.char.questAnnounce and UnitInParty("player") then
         local message
@@ -27,6 +29,12 @@ function QuestieAnnounce:Announce(questId, progressType, itemId, objectiveText, 
         elseif progressType == "item" then
             message = "{rt1} Questie : " .. l10n("Picked up %s which starts %s!", (select(2,GetItemInfo(itemId))), "[["..tostring(questLevel).."] "..questName.." ("..tostring(questId)..")]")
         end
+
+        if (not message) or QuestieAnnounce._AlreadySentBandaid[message] then
+            return
+        end
+
+        QuestieAnnounce._AlreadySentBandaid[message] = true
 
         SendChatMessage(message, "PARTY")
     end
