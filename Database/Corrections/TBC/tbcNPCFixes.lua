256136d845c5ef969679438a9768b72edd662a1a
BreakBB <b.boeing@online.de>
2021-07-11 00:39:04 +0200

    [quest] Add objective location for "Capturing the Keystone"

--- Database/Corrections/TBC/tbcNPCFixes.lua
+++ Database/Corrections/TBC/tbcNPCFixes.lua
@@ -198,6 +198,10 @@ function QuestieTBCNpcFixes:Load()
         [17923] = {
             [npcKeys.waypoints] = {[zoneIDs.ZANGARMARSH] = {{{19,62.4},{19,63.5},{19.2,64.6},},},},
         },
+        [17976] = {
+            [npcKeys.spawns] = {[zoneIDs.THE_BOTANICA] = {{-1,-1},},},
+            [npcKeys.zoneID] = zoneIDs.THE_BOTANICA,
+        },
         [18099] = {
             [npcKeys.waypoints] = {[zoneIDs.NAGRAND] = {{{60.68,25.15},{60.07,24.67},{59.31,24.05},{60.03,24.71},{61.12,24.83},},},},
         },
