62e7887f67077251f43153344a38ff16377151d4
BreakBB <b.boeing@online.de>
2021-07-10 10:01:53 +0200

    Minor cleanup

--- Modules/QuestieAnnounce.lua
+++ Modules/QuestieAnnounce.lua
@@ -10,9 +10,9 @@ local QuestieLink = QuestieLoader:ImportModule("QuestieLink")
 ---@type l10n
 local l10n = QuestieLoader:ImportModule("l10n")
 
-QuestieAnnounce._itemCache = {} -- cache data since this happens on item looted it could happen a lot with auto loot
+local itemCache = {} -- cache data since this happens on item looted it could happen a lot with auto loot
 
-QuestieAnnounce._AlreadySentBandaid = {} -- TODO: rewrite the entire thing its a lost cause
+local alreadySentBandaid = {} -- TODO: rewrite the entire thing its a lost cause
 
 function QuestieAnnounce:AnnounceParty(questId, progressType, itemId, objectiveText, objectiveProgress)
     if Questie.db.char.questAnnounce and UnitInParty("player") then
@@ -36,11 +36,11 @@ function QuestieAnnounce:AnnounceParty(questId, progressType, itemId, objectiveT
             message = "{rt1} Questie : " .. l10n("Picked up %s which starts %s!", itemLink, questLink)
         end
 
-        if (not message) or QuestieAnnounce._AlreadySentBandaid[message] then
+        if (not message) or alreadySentBandaid[message] then
             return
         end
 
-        QuestieAnnounce._AlreadySentBandaid[message] = true
+        alreadySentBandaid[message] = true
 
         SendChatMessage(message, "PARTY")
     end
@@ -53,20 +53,22 @@ function _QuestieAnnounce:AnnounceSelf(questId, itemId)
     Questie:Print(l10n("You picked up %s which starts %s!", itemLink, questLink));
 end
 
-local _playerName
+local playerNameCache
 ---@return string
 local function _GetPlayerName()
-    _playerName = UnitName("player")
-    return _playerName
+    playerNameCache = UnitName("player")
+    return playerNameCache
 end
 
 function QuestieAnnounce:ItemLooted(text, notPlayerName, _, _, playerName)
-    if (_playerName or _GetPlayerName()) == playerName or (string.len(playerName) == 0 and _playerName == notPlayerName) then
+    if (playerNameCache or _GetPlayerName()) == playerName or (string.len(playerName) == 0 and playerNameCache == notPlayerName) then
         local itemId = tonumber(string.match(text, "item:(%d+)"))
-        if not QuestieAnnounce._itemCache[itemId] and QuestieDB.QueryItemSingle then -- check QueryItemSingle because this event can fire before db init is complete
-            QuestieAnnounce._itemCache[itemId] = QuestieDB.QueryItemSingle(itemId, "startQuest") or false -- we do "or false" here because nil cant be inserted into _itemCache
+        
+        if (not itemCache[itemId]) and QuestieDB.QueryItemSingle then -- check QueryItemSingle because this event can fire before db init is complete
+            itemCache[itemId] = QuestieDB.QueryItemSingle(itemId, "startQuest") or false -- we do "or false" here because nil cant be inserted into _itemCache
         end
-        local startQuestId = QuestieAnnounce._itemCache[itemId]
+        
+        local startQuestId = itemCache[itemId]
         if startQuestId and startQuestId > 0 then
             if (not UnitInParty("player")) or (not Questie.db.char.questAnnounce) then
                 _QuestieAnnounce:AnnounceSelf(startQuestId, itemId)
