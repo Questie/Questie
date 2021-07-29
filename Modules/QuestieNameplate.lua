84a6bcadd66e1888700309fd831f6e1127432f98
BreakBB <b.boeing@online.de>
2021-07-29 07:30:01 +0200

    Restructure the code and add more cleanups

--- Modules/QuestieNameplate.lua
+++ Modules/QuestieNameplate.lua
@@ -19,73 +19,7 @@ function QuestieNameplate:Initialize()
     -- Nothing to initialize
 end
 
-function _QuestieNameplate:GetFrame(guid)
-    if npFrames[guid] and npFrames[guid] ~= nil then
-        return npFrames[guid]
-    end
-
-    local parent = C_NamePlate.GetNamePlateForUnit(activeGUIDs[guid])
-
-    local frame = tremove(npUnusedFrames)
-
-    if(not frame) then
-        frame = CreateFrame("Frame")
-        npFramesCount = npFramesCount + 1
-    end
-
-    local iconScale = Questie.db.global.nameplateScale
-
-    frame:SetFrameStrata("LOW")
-    frame:SetFrameLevel(10)
-    frame:SetWidth(16 * iconScale)
-    frame:SetHeight(16 * iconScale)
-    frame:EnableMouse(false)
-    frame:SetParent(parent)
-    frame:SetPoint("LEFT", Questie.db.global.nameplateX, Questie.db.global.nameplateY)
-
-    frame.Icon = frame:CreateTexture(nil, "ARTWORK")
-    frame.Icon:ClearAllPoints()
-    frame.Icon:SetAllPoints(frame)
-
-
-    npFrames[guid] = frame
-
-    return frame
-end
-
-function QuestieNameplate:RedrawIcons()
-    for _, frame in pairs(npFrames) do
-        local iconScale = Questie.db.global.nameplateScale
-
-        frame:SetPoint("LEFT", Questie.db.global.nameplateX, Questie.db.global.nameplateY)
-        frame:SetWidth(16 * iconScale)
-        frame:SetHeight(16 * iconScale)
-    end
-end
-
-function _QuestieNameplate:RemoveFrame(guid)
-    if npFrames[guid] then
-        table.insert(npUnusedFrames, npFrames[guid])
-        npFrames[guid].Icon:SetTexture(nil) -- fix for overlapping icons
-        npFrames[guid]:Hide()
-        npFrames[guid] = nil
-    end
-end
-
-function _QuestieNameplate:GetValidIcon(tooltips) -- helper function to get the first valid (incomplete) icon from the specified tooltip, or nil if there is none
-    if tooltips then
-        for _, tooltip in pairs(tooltips) do
-            if tooltip.objective and tooltip.objective.Update then
-                tooltip.objective:Update() -- get latest qlog data if its outdated
-                if (not tooltip.objective.Completed) and tooltip.objective.Icon then
-                    return tooltip.objective.Icon
-                end
-            end
-        end
-    end
-    return nil
-end
-
+---@param token string
 function QuestieNameplate:NameplateCreated(token)
     Questie:Debug(DEBUG_SPAM, "[QuestieNameplate:NameplateCreated]")
     -- if nameplates are disbaled, don't create new nameplates.
@@ -105,24 +39,25 @@ function QuestieNameplate:NameplateCreated(token)
         return
     end
 
-    -- we only need to use put this over creatures.
-    -- to avoid running this code over Pet, Player, etc.
     local unitType, _, _, _, _, npcId, _ = strsplit("-", unitGUID)
+    if unitType ~= "Creature" then
+        -- We only draw name plates on NPCs/creatures and skip players, pets, etc
+        return
+    end
 
-    if unitType == "Creature" then
-        local icon = _QuestieNameplate:GetValidIcon(QuestieTooltips.lookupByKey["m_" .. npcId])
+    local icon = _QuestieNameplate:GetValidIcon(QuestieTooltips.lookupByKey["m_" .. npcId])
 
-        if icon then
-            activeGUIDs[unitGUID] = token
+    if icon then
+        activeGUIDs[unitGUID] = token
 
-            local f = QuestieNameplate:GetFrame(unitGUID)
-            f.Icon:SetTexture(icon)
-            f.lastIcon = icon -- this is used to prevent updating the texture when it's already what it needs to be
-            f:Show()
-        end
+        local f = _QuestieNameplate:GetFrame(unitGUID)
+        f.Icon:SetTexture(icon)
+        f.lastIcon = icon -- this is used to prevent updating the texture when it's already what it needs to be
+        f:Show()
     end
 end
 
+---@param token string
 function QuestieNameplate:NameplateDestroyed(token)
     Questie:Debug(DEBUG_SPAM, "[QuestieNameplate:NameplateDestroyed]")
 
@@ -134,7 +69,7 @@ function QuestieNameplate:NameplateDestroyed(token)
 
     if unitGUID and activeGUIDs[unitGUID] then
         activeGUIDs[unitGUID] = nil
-        QuestieNameplate:RemoveFrame(unitGUID)
+        _QuestieNameplate:RemoveFrame(unitGUID)
     end
 end
 
@@ -163,15 +98,25 @@ function QuestieNameplate:UpdateNameplate()
         else
             -- tooltip removed but we still have the frame active, remove it
             activeGUIDs[guid] = nil
-            QuestieNameplate:RemoveFrame(guid)
+            _QuestieNameplate:RemoveFrame(guid)
         end
+    end
+end
+
+function QuestieNameplate:RedrawIcons()
+    for _, frame in pairs(npFrames) do
+        local iconScale = Questie.db.global.nameplateScale
+
+        frame:SetPoint("LEFT", Questie.db.global.nameplateX, Questie.db.global.nameplateY)
+        frame:SetWidth(16 * iconScale)
+        frame:SetHeight(16 * iconScale)
     end
 end
 
 function QuestieNameplate:HideCurrentFrames()
     for guid, _ in pairs(activeGUIDs) do
         activeGUIDs[guid] = nil
-        QuestieNameplate:RemoveFrame(guid)
+        _QuestieNameplate:RemoveFrame(guid)
     end
 end
 
@@ -197,44 +142,22 @@ function QuestieNameplate:DrawTargetFrame()
     end
 
     local unitType, _, _, _, _, npcId, _ = strsplit("-", unitGUID)
-
-    if unitType == "Creature" then
-        local icon = _QuestieNameplate:GetValidIcon(QuestieTooltips.lookupByKey["m_" .. npcId])
-
-        if icon then
-            if activeTargetFrame == nil then
-                activeTargetFrame = CreateFrame("Frame")
-
-                local iconScale = Questie.db.global.nameplateTargetFrameScale
-
-                activeTargetFrame:SetFrameStrata("LOW")
-                activeTargetFrame:SetFrameLevel(10)
-                activeTargetFrame:SetWidth(16 * iconScale)
-                activeTargetFrame:SetHeight(16 * iconScale)
-                activeTargetFrame:EnableMouse(false)
-
-                local targetFrame = TargetFrame -- Default Blizzard target frame
-                if ElvUF_Target then
-                    targetFrame = ElvUF_Target
-                elseif PitBull4_Frames_Target then
-                    targetFrame = PitBull4_Frames_Target
-                elseif SUFUnittarget then
-                    targetFrame = SUFUnittarget
-                    activeTargetFrame:SetFrameLevel(SUFUnittarget:GetFrameLevel() + 1)
-                end
-
-                activeTargetFrame:SetParent(targetFrame)
-                activeTargetFrame:SetPoint("RIGHT", Questie.db.global.nameplateTargetFrameX, Questie.db.global.nameplateTargetFrameY)
-
-                activeTargetFrame.Icon = activeTargetFrame:CreateTexture(nil, "ARTWORK")
-                activeTargetFrame.Icon:ClearAllPoints()
-                activeTargetFrame.Icon:SetAllPoints(activeTargetFrame)
-            end
-
-            activeTargetFrame.Icon:SetTexture(icon)
-            activeTargetFrame:Show()
-        end
+    if unitType ~= "Creature" then
+        -- We only draw name plates on NPCs/creatures and skip players, pets, etc
+        return
     end
+
+    local icon = _QuestieNameplate:GetValidIcon(QuestieTooltips.lookupByKey["m_" .. npcId])
+    if (not icon) then
+        return
+    end
+
+    if activeTargetFrame == nil then
+        activeTargetFrame = _QuestieNameplate:GetTargetFrameIconFrame()
+    end
+
+    activeTargetFrame.Icon:SetTexture(icon)
+    activeTargetFrame:Show()
 end
 
 function QuestieNameplate:HideCurrentTargetFrame()
@@ -247,7 +170,6 @@ function QuestieNameplate:HideCurrentTargetFrame()
     activeTargetFrame = nil
 end
 
-
 function QuestieNameplate:RedrawFrameIcon()
     if (not Questie.db.global.nameplateTargetFrameEnabled) or (not activeTargetFrame) then
         return
@@ -257,4 +179,98 @@ function QuestieNameplate:RedrawFrameIcon()
     activeTargetFrame:SetWidth(16 * iconScale)
     activeTargetFrame:SetHeight(16 * iconScale)
     activeTargetFrame:SetPoint("RIGHT", Questie.db.global.nameplateTargetFrameX, Questie.db.global.nameplateTargetFrameY)
+end
+
+
+---@param guid string
+function _QuestieNameplate:GetFrame(guid)
+    if npFrames[guid] then
+        return npFrames[guid]
+    end
+
+    local parent = C_NamePlate.GetNamePlateForUnit(activeGUIDs[guid])
+
+    local frame = tremove(npUnusedFrames)
+
+    if (not frame) then
+        frame = CreateFrame("Frame")
+        npFramesCount = npFramesCount + 1
+    end
+
+    local iconScale = Questie.db.global.nameplateScale
+
+    frame:SetFrameStrata("LOW")
+    frame:SetFrameLevel(10)
+    frame:SetWidth(16 * iconScale)
+    frame:SetHeight(16 * iconScale)
+    frame:EnableMouse(false)
+    frame:SetParent(parent)
+    frame:SetPoint("LEFT", Questie.db.global.nameplateX, Questie.db.global.nameplateY)
+
+    frame.Icon = frame:CreateTexture(nil, "ARTWORK")
+    frame.Icon:ClearAllPoints()
+    frame.Icon:SetAllPoints(frame)
+
+    npFrames[guid] = frame
+
+    return frame
+end
+
+function _QuestieNameplate:GetTargetFrameIconFrame()
+    local frame = CreateFrame("Frame")
+
+    local iconScale = Questie.db.global.nameplateTargetFrameScale
+
+    frame:SetFrameStrata("LOW")
+    frame:SetFrameLevel(10)
+    frame:SetWidth(16 * iconScale)
+    frame:SetHeight(16 * iconScale)
+    frame:EnableMouse(false)
+
+    local targetFrame = TargetFrame -- Default Blizzard target frame
+    if ElvUF_Target then
+        targetFrame = ElvUF_Target
+    elseif PitBull4_Frames_Target then
+        targetFrame = PitBull4_Frames_Target
+    elseif SUFUnittarget then
+        targetFrame = SUFUnittarget
+        frame:SetFrameLevel(SUFUnittarget:GetFrameLevel() + 1)
+    end
+
+    frame:SetParent(targetFrame)
+    frame:SetPoint("RIGHT", Questie.db.global.nameplateTargetFrameX, Questie.db.global.nameplateTargetFrameY)
+
+    frame.Icon = frame:CreateTexture(nil, "ARTWORK")
+    frame.Icon:ClearAllPoints()
+    frame.Icon:SetAllPoints(frame)
+    
+    return frame
+end
+
+---@param guid string
+function _QuestieNameplate:RemoveFrame(guid)
+    if (not npFrames[guid]) then
+        return
+    end
+
+    table.insert(npUnusedFrames, npFrames[guid])
+    npFrames[guid].Icon:SetTexture(nil) -- fix for overlapping icons
+    npFrames[guid]:Hide()
+    npFrames[guid] = nil
+end
+
+---@param tooltips table<string, table>
+function _QuestieNameplate:GetValidIcon(tooltips) -- helper function to get the first valid (incomplete) icon from the specified tooltip, or nil if there is none
+    if (not tooltips) then
+        return
+    end
+
+    for _, tooltip in pairs(tooltips) do
+        if tooltip.objective and tooltip.objective.Update then
+            tooltip.objective:Update() -- get latest qlog data if its outdated
+            if (not tooltip.objective.Completed) and tooltip.objective.Icon then
+                return tooltip.objective.Icon
+            end
+        end
+    end
 end
