e55ffd51bb2dd5d5e09da38ea5f3191e3f935ca4
BreakBB <b.boeing@online.de>
2021-08-05 22:26:47 +0200

    Use a timer so we can interrupt abandon logic

--- Modules/Quest/QuestEventHandler.lua
+++ Modules/Quest/QuestEventHandler.lua
@@ -28,7 +28,9 @@ function _QuestEventHandler:PlayerLogin()
     for i=1, numEntries do
         local _, _, _, isHeader, _, _, _, questId, _ = GetQuestLogTitle(i)
         if (not isHeader) then
-            questLog[questId] = QUEST_LOG_STATES.QUEST_ACCEPTED
+            questLog[questId] = {
+                state = QUEST_LOG_STATES.QUEST_ACCEPTED
+            }
 
             if (not questLogEventTrace[questId]) then
                 questLogEventTrace[questId] = {}
@@ -45,11 +47,21 @@ end
 function _QuestEventHandler:QuestAccepted(questLogIndex, questId)
     print("[Quest Event] QUEST_ACCEPTED", questId)
 
+    if questLog[questId] and questLog[questId].timer then
+        -- We had a QUEST_REMOVED event which started this timer and now it was accepted again.
+        -- So the quest was abandoned before, because QUEST_TURNED_IN would have run before QUEST_ACCEPTED.
+        questLog[questId].timer:Cancel()
+        questLog[questId].timer = nil
+        _QuestEventHandler:MarkQuestAsAbandoned(questId)
+    end
+
     if (not questLogEventTrace[questId]) then
         questLogEventTrace[questId] = {}
     end
 
-    questLog[questId] = QUEST_LOG_STATES.QUEST_ACCEPTED
+    questLog[questId] = {
+        state = QUEST_LOG_STATES.QUEST_ACCEPTED
+    }
     table.insert(questLogEventTrace[questId], QUEST_LOG_STATES.QUEST_ACCEPTED)
 
     --TODO: Call quest accepted logic
@@ -64,16 +76,20 @@ function _QuestEventHandler:QuestTurnedIn(questId, xpReward, moneyReward)
     table.insert(questLogEventTrace[questId], QUEST_LOG_STATES.QUEST_TURNED_IN)
 
     -- QUEST_REMOVED fired before QUEST_TURNED_IN --> quest was still turned in
-    if questLog[questId] == QUEST_LOG_STATES.QUEST_REMOVED then
+    if questLog[questId].state == QUEST_LOG_STATES.QUEST_REMOVED then
+        -- Cancel the timer so the quest is not marked as abandoned
+        questLog[questId].timer:Cancel()
+        questLog[questId].timer = nil
+
         print("Quest", questId, "was turned in. Marking it as completed")
-        questLog[questId] = QUEST_LOG_STATES.QUEST_COMPLETED
+        questLog[questId].state = QUEST_LOG_STATES.QUEST_COMPLETED
         table.insert(questLogEventTrace[questId], QUEST_LOG_STATES.QUEST_COMPLETED)
 
         -- TODO: Call quest completed logic
         return
     end
 
-    questLog[questId] = QUEST_LOG_STATES.QUEST_TURNED_IN
+    questLog[questId].state = QUEST_LOG_STATES.QUEST_TURNED_IN
 end
 
 --- Fires when a quest is removed from the quest log. This includes turning it in and abandoning it.
@@ -83,9 +99,9 @@ function _QuestEventHandler:QuestRemoved(questId)
     table.insert(questLogEventTrace[questId], QUEST_LOG_STATES.QUEST_REMOVED)
 
     -- QUEST_TURNED_IN was called before QUEST_REMOVED --> quest was turned in
-    if questLog[questId] == QUEST_LOG_STATES.QUEST_TURNED_IN then
+    if questLog[questId].state == QUEST_LOG_STATES.QUEST_TURNED_IN then
         print("Quest", questId, "was turned in. Marking it as completed")
-        questLog[questId] = QUEST_LOG_STATES.QUEST_COMPLETED
+        questLog[questId].state = QUEST_LOG_STATES.QUEST_COMPLETED
         table.insert(questLogEventTrace[questId], QUEST_LOG_STATES.QUEST_COMPLETED)
 
         -- TODO: Call quest completed logic
@@ -94,17 +110,22 @@ function _QuestEventHandler:QuestRemoved(questId)
 
     -- QUEST_REMOVED can fire before QUEST_TURNED_IN. If QUEST_TURNED_IN is not called after X seconds the quest
     -- was abandoned
-    C_Timer.After(1, function()
-        if questLog[questId] == QUEST_LOG_STATES.QUEST_REMOVED then
-            print("Quest", questId, "was removed before. Marking it as abandoned")
-            questLog[questId] = QUEST_LOG_STATES.QUEST_ABANDONED
-            table.insert(questLogEventTrace[questId], QUEST_LOG_STATES.QUEST_ABANDONED)
+    questLog[questId] = {
+        state = QUEST_LOG_STATES.QUEST_REMOVED,
+        timer = C_Timer.NewTicker(1, function()
+            _QuestEventHandler:MarkQuestAsAbandoned(questId)
+        end, 1)
+    }
+end
 
-            -- TODO: Call quest abandoned logic
-        end
-    end)
+function _QuestEventHandler:MarkQuestAsAbandoned(questId)
+    if questLog[questId].state == QUEST_LOG_STATES.QUEST_REMOVED then
+        print("Quest", questId, "was removed before. Marking it as abandoned")
+        questLog[questId].state = QUEST_LOG_STATES.QUEST_ABANDONED
+        table.insert(questLogEventTrace[questId], QUEST_LOG_STATES.QUEST_ABANDONED)
 
-    questLog[questId] = QUEST_LOG_STATES.QUEST_REMOVED
+        -- TODO: Call quest abandoned logic
+    end
 end
 
 --- Is executed whenever an event is fired and triggers relevant event handling.
