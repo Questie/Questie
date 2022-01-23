a45e78d44f5ce068a4b2d1a910aa9b9b512e1500
Laume <75958501+Laumesis@users.noreply.github.com>
2022-01-23 14:49:00 +0200

    Update new message texts

--- Modules/GameVersionError.lua
+++ Modules/GameVersionError.lua
@@ -1,12 +1,18 @@
 -- No timeres or other fancy stuff as 1.12 client is very limited.
 
+-- StaticPopup has very limited width, so text is split to many lines.
 local msg = {
-    "You're trying to use Questie Addon on unsupported WoW Game client!",
-    "WoW \"retail\" and private servers are not supported.",
-    "Questie Addon is supporting WoW Classic (TBC/Era/SoM/etc) only!",
+    "You're trying to use Questie addon",
+    "on an unsupported WoW game client!",
+
+    "WoW \"retail\" and private servers",
+    "are not supported.",
+
+    "Questie only supports",
+    "WoW Classic (TBC/Era/SoM)!",
 }
 StaticPopupDialogs["QUESTIE_VERSION_ERROR"] = {
-    text = "|cffff0000ERROR|r\n"..msg[1].."\n"..msg[2].."\n"..msg[3],
+    text = "|cffff0000ERROR|r\n"..msg[1].."\n"..msg[2].."\n\n"..msg[3].."\n"..msg[4].."\n\n"..msg[5].."\n"..msg[6],
     button2 = "OK",
     hasEditBox = false,
     whileDead = true
@@ -14,7 +20,7 @@ StaticPopupDialogs["QUESTIE_VERSION_ERROR"] = {
 StaticPopup_Show("QUESTIE_VERSION_ERROR")
 
 DEFAULT_CHAT_FRAME:AddMessage("---------------------------------")
-DEFAULT_CHAT_FRAME:AddMessage("|cffff0000ERROR|r: |cff42f5ad"..msg[1].."|r")
-DEFAULT_CHAT_FRAME:AddMessage("|cffff0000ERROR|r: |cff42f5ad"..msg[2].."|r")
-DEFAULT_CHAT_FRAME:AddMessage("|cffff0000ERROR|r: |cff42f5ad"..msg[3].."|r")
+DEFAULT_CHAT_FRAME:AddMessage("|cffff0000ERROR|r: |cff42f5ad"..msg[1].." "..msg[2].."|r")
+DEFAULT_CHAT_FRAME:AddMessage("|cffff0000ERROR|r: |cff42f5ad"..msg[3].." "..msg[4].."|r")
+DEFAULT_CHAT_FRAME:AddMessage("|cffff0000ERROR|r: |cff42f5ad"..msg[5].." "..msg[6].."|r")
 DEFAULT_CHAT_FRAME:AddMessage("---------------------------------")
