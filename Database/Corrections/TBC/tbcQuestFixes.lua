39f64d6ed2a661249f724d04c07a5525ba32c095
BreakBB <b.boeing@online.de>
2022-05-14 17:03:26 +0200

    [quest] Remove pre quest from "Gaining the Advantage"

--- Database/Corrections/TBC/tbcQuestFixes.lua
+++ Database/Corrections/TBC/tbcQuestFixes.lua
@@ -2975,6 +2975,9 @@ function QuestieTBCQuestFixes:Load()
         [11863] = {
             [questKeys.startedBy] = {{25947},nil,nil},
         },
+        [11875] = {
+            [questKeys.preQuestSingle] = {},
+        },
         [11877] = {
             [questKeys.preQuestSingle] = {},
         },
