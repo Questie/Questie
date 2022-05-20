a4712ce727ddf7cc678c3fbeb01ee002d997f3fe
Muehe <Muehe@users.noreply.github.com>
2022-05-20 21:03:05 +0200

    Remove raidmarker from ShutUp search string, so we don't need to localise it

--- Modules/QuestieShutUp.lua
+++ Modules/QuestieShutUp.lua
@@ -4,15 +4,13 @@ QuestieShutUp:RegisterEvent("PLAYER_ENTERING_WORLD");
 QuestieShutUp:SetScript("OnEvent", function(self, event, arg1, arg2, arg3, arg4, arg5) QuestieShutUp_OnEvent(event, arg1, arg2, arg3, arg4, arg5) end);
 
 local Questie = _G.Questie;
-local l10n = QuestieLoader:ImportModule("l10n");
-local raidMarker = l10n:GetUILocale() == "ruRU" and "{звезда}" or "{rt1}";
 
 function FilterFunc(self, event, msg, author, ...)
     if not Questie.db.global.questieShutUp then
         return false;
     end
 
-    if msg:find(raidMarker .. " Questie : ") then
+    if msg:find(" Questie : ") then
         return true;
     end
 end
