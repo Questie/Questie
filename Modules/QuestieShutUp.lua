783ad8af7e04d49fed2fcb4c71267ac1365c04d4
Laume <75958501+Laumesis@users.noreply.github.com>
2022-05-21 22:00:46 +0300

    Add debug messages for on/off

--- Modules/QuestieShutUp.lua
+++ Modules/QuestieShutUp.lua
@@ -15,11 +15,13 @@ end
 function QuestieShutUp:ToggleFilters(value)
     if value then
         pattern = "^"..(l10n:GetUILocale() == "ruRU" and "{звезда}" or "{rt1}").." Questie : "
+        Questie:Debug(Questie.DEBUG_DEVELOP, "QuestieShutUp toggle on. Pattern:", pattern)
         ChatFrame_AddMessageEventFilter("CHAT_MSG_PARTY", QuestieShutUp.FilterFunc)
         ChatFrame_AddMessageEventFilter("CHAT_MSG_PARTY_LEADER", QuestieShutUp.FilterFunc)
         ChatFrame_AddMessageEventFilter("CHAT_MSG_RAID", QuestieShutUp.FilterFunc)
         ChatFrame_AddMessageEventFilter("CHAT_MSG_RAID_LEADER", QuestieShutUp.FilterFunc)
     else
+        Questie:Debug(Questie.DEBUG_DEVELOP, "QuestieShutUp toggle off.")
         ChatFrame_RemoveMessageEventFilter("CHAT_MSG_PARTY", QuestieShutUp.FilterFunc)
         ChatFrame_RemoveMessageEventFilter("CHAT_MSG_PARTY_LEADER", QuestieShutUp.FilterFunc)
         ChatFrame_RemoveMessageEventFilter("CHAT_MSG_RAID", QuestieShutUp.FilterFunc)
