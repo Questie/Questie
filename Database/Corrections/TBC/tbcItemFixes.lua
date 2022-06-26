5b9516f847e5f8f91de28e546f7142a4fffb07d7
BreakBB <b.boeing@online.de>
2022-06-26 11:50:56 +0200

    [db] Update drop locations for "Encoded Fragment"
    Fixes #3558

--- Database/Corrections/TBC/tbcItemFixes.lua
+++ Database/Corrections/TBC/tbcItemFixes.lua
@@ -22,6 +22,9 @@ function QuestieTBCItemFixes:Load()
         [12366] = {
             [itemKeys.npcDrops] = {7457,7458,7459,7460},
         },
+        [20023] = {
+            [itemKeys.npcDrops] = {6375,6377,6378,6379,6380,8759,8761,8762,8763,8764,8766,},
+        },
         [21771] = {
             [itemKeys.npcDrops] = {15668,15669},
         },
