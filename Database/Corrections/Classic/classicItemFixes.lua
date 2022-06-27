e6093aeb43ee350232f9c5281ee98da4c501e1c3
Robert Goulet <gouletr@users.noreply.github.com>
2022-06-27 05:21:07 -0400

    [db] Improve sources for item "Frost Oil" (Thanks @gouletr)

--- Database/Corrections/Classic/classicItemFixes.lua
+++ Database/Corrections/Classic/classicItemFixes.lua
@@ -187,6 +187,7 @@ function QuestieItemFixes:Load()
         [3829] = {
             [itemKeys.relatedQuests] = {713,1193},
             [itemKeys.npcDrops] = {},
+            [itemKeys.itemDrops] = {11887},
         },
         [3823] = {
             [itemKeys.relatedQuests] = {715}
