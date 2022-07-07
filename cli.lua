6317982a0b29d5b9382c7f886ac27fd29876f422
BreakBB <b.boeing@online.de>
2022-07-07 15:34:03 +0200

    Define missing IsAddOnLoaded API function in cli.lua

--- cli.lua
+++ cli.lua
@@ -23,6 +23,7 @@ end
 GetTime = function()
     return os.time(os.date("!*t")) - 1616930000 -- convert unix time to wow time (actually accurate)
 end
+IsAddOnLoaded = function() return false, true end
 UnitFactionGroup = function()
     return arg[1] or "Horde"
 end
