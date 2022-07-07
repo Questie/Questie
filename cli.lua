8b02535f8c258e08a270cc7829b3b111323dc735
BreakBB <b.boeing@online.de>
2022-07-07 15:41:03 +0200

    Add more missing API functions to cli.lua

--- cli.lua
+++ cli.lua
@@ -10,6 +10,7 @@ mod = function(a, b)
     return a % b
 end
 bit = require("bit32")
+gsub = function(ret) return ret end
 
 GetBuildInfo = function()
     return "2.5.1", "38644", "May 11 2021", "20501"
@@ -32,6 +33,12 @@ UnitClass = function()
 end
 GetLocale = function()
     return "enUS"
+end
+GetQuestGreenRange = function()
+    return 10
+end
+UnitName = function()
+    return "QuestieNPC"
 end
 LibStub = function()
     return {["NewAddon"] = function() return {} end}
