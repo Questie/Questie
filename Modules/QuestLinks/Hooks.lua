8f059c23a20566814e243edcefdf770ab0421bb7
BreakBB <b.boeing@online.de>
2022-07-15 13:27:24 +0200

    Disable Questie Tracker link for now

--- Modules/QuestLinks/Hooks.lua
+++ Modules/QuestLinks/Hooks.lua
@@ -26,14 +26,15 @@ function Hooks:HookQuestLogTitle()
             ChatEdit_InsertLink("["..string.gsub(self:GetText(), " *(.*)", "%1").." ("..questId..")]")
         else
             -- only call if we actually want to fix this quest (normal quests already call AQW_insert)
-            if GetNumQuestLeaderBoards(questLogLineIndex) == 0 and (not IsQuestWatched(questLogLineIndex)) then
-                QuestieTracker:AQW_Insert(questLogLineIndex, QUEST_WATCH_NO_EXPIRE)
-                QuestWatch_Update()
-                QuestLog_SetSelection(questLogLineIndex)
-                QuestLog_Update()
-            else
+            -- FIXME: QuestWatch_Update is nil
+            --if GetNumQuestLeaderBoards(questLogLineIndex) == 0 and (not IsQuestWatched(questLogLineIndex)) then
+            --    QuestieTracker:AQW_Insert(questLogLineIndex, QUEST_WATCH_NO_EXPIRE)
+            --    QuestWatch_Update()
+            --    QuestLog_SetSelection(questLogLineIndex)
+            --    QuestLog_Update()
+            --else
                 baseQLTB_OnClick(self, button)
-            end
+            --end
         end
     end
 end
