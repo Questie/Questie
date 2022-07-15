d7ea8008714491263befab3f41531e6ee1f28642
BreakBB <b.boeing@online.de>
2022-07-15 20:44:02 +0200

    [quest] Add missing translation entry for "Banish the Demons"

--- Localization/Translations/ExtraObjectives/TbcObjectives.lua
+++ Localization/Translations/ExtraObjectives/TbcObjectives.lua
@@ -1334,6 +1334,18 @@ local tbcObjectiveLocales = {
         ["zhTW"] = false,
         ["zhCN"] = false,
     },
+    ["Use the Banishing Crystal and slay demons near the summoned portal"] = { -- 11026
+        ["ptBR"] = false,
+        ["ruRU"] = false,
+        ["deDE"] = false,
+        ["koKR"] = false,
+        ["esMX"] = false,
+        ["enUS"] = true,
+        ["frFR"] = false,
+        ["esES"] = false,
+        ["zhTW"] = false,
+        ["zhCN"] = false,
+    },
 }
 
 for k, v in pairs(tbcObjectiveLocales) do
