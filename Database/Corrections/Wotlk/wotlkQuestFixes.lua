c59e6084c12c505f158d165d9b4834226a6c1e16
BreakBB <b.boeing@online.de>
2022-07-15 19:31:51 +0200

    Fix objective location for "Re-Cursive"
    Fixes #3597

--- Database/Corrections/Wotlk/wotlkQuestFixes.lua
+++ Database/Corrections/Wotlk/wotlkQuestFixes.lua
@@ -17,5 +17,8 @@ function QuestieWotlkQuestFixes:Load()
         [11590] = {
             [questKeys.objectives] = {{{25316,"Captured Beryl Sorcerer"},},nil,nil,nil,},
         },
+        [11712] = {
+            [questKeys.objectives] = {nil,nil,nil,nil,{{25765,25767,25783,25814,26601,26619},25814,"Fizzcrank Gnome cursed & ported"}},
+        },
     }
 end
