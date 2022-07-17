0d060e3efd0f207406360242702a8e423e5581be
BreakBB <b.boeing@online.de>
2022-07-17 17:06:27 +0200

    Fix objective location for "Steel Gate Patrol"
    Fixes #3665

--- Database/Corrections/Wotlk/wotlkNPCFixes.lua
+++ Database/Corrections/Wotlk/wotlkNPCFixes.lua
@@ -11,5 +11,12 @@ function QuestieWotlkNpcFixes:Load()
     local zoneIDs = ZoneDB.zoneIDs
     local npcFlags = QuestieDB.npcFlags
 
-    return {}
+    return {
+        [24440] = {
+            [npcKeys.spawns] = {
+                [zoneIDs.HOWLING_FJORD] = {{30.2,26.4},{30.4,27},{30.6,24},{30.8,23.4},{30.8,26.6},{30.8,28.2},{30.8,28.6},{31,24.6},{31,26.4},{31.2,31},{31.6,27.2},{31.6,27.6},{31.8,26},},
+            },
+            [npcKeys.zoneID] = zoneIDs.HOWLING_FJORD,
+        },
+    }
 end
