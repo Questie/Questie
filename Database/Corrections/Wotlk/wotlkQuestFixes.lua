f25ed0ddfa9f2ab9c98c1e6d1e55f6a47d88ca30
BreakBB <b.boeing@online.de>
2022-07-20 18:32:32 +0200

    Fix objective location for "Tails Up"
    Fixes #3682

--- Database/Corrections/Wotlk/wotlkQuestFixes.lua
+++ Database/Corrections/Wotlk/wotlkQuestFixes.lua
@@ -43,5 +43,8 @@ function QuestieWotlkQuestFixes:Load()
         [11712] = {
             [questKeys.objectives] = {nil,nil,nil,nil,{{25765,25767,25783,25814,26601,26619},25814,"Fizzcrank Gnome cursed & ported"}},
         },
+        [13549] = {
+            [questKeys.objectives] = {{{29327,"Female Frost Leopards recovered"},{29319,"Female Icepaw Bears recovered"},},nil,nil,nil,},
+        },
     }
 end
