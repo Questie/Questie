caac0b0f600859779a2aa9bdc8b81fdd0ed8517c
Drejjmit <drimit_cs@hotmail.com>
2022-07-22 14:00:30 +0200

    Fix objective location for "The Clutches of Evil"
    Fixes #3634

--- Database/Corrections/Wotlk/wotlkQuestFixes.lua
+++ Database/Corrections/Wotlk/wotlkQuestFixes.lua
@@ -16,6 +16,9 @@ function QuestieWotlkQuestFixes:Load()
     local sortKeys = QuestieDB.sortKeys
 
     return {
+        [11157] = {
+            [questKeys.objectives] = {{{23777,"Proto-Drake Egg destroyed"},},nil,nil,nil,{{23688,23750,},23688},},
+        },
         [11246] = {
             [questKeys.objectives] = {nil,nil,nil,nil,{{23666,23662,23661,223664,23663,23665,23667,23670,23668,23669,},23666,"Winterskorn Vrykul Dismembered"},},
         },
