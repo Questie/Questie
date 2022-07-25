Questie/Questie

commit f095d3ac06c3dbf8c591e0dbbb30b7336b80695f

Make QuestieSearch nil safe

Modules/Journey/QuestieSearch.lua
@@ -100,7 +100,8 @@ function QuestieSearch:Search(query, searchType, queryType)
         local queryToFind = string.lower(query)
 
         for id, _ in pairs(databaseKeys) do
-            if string.find(string.lower(databaseQueryHandle(id, "name")), queryToFind) then
+            local name = databaseQueryHandle(id, "name") -- Some entries don't have a 'name' because of the way we load corrections
+            if name and string.find(string.lower(), queryToFind) then
                 -- We have a search result or a favourite to display
                 searchCount = searchCount + 1;
                 QuestieSearch.LastResult[searchType][id] = true;

commit d3ecb1fa199289e5cfdd26aac5a58c86cf8d1c0d

Fix search broken by f095d3ac06c3dbf8c591e0dbbb30b7336b80695f

Modules/Journey/QuestieSearch.lua
@@ -101,7 +101,7 @@ function QuestieSearch:Search(query, searchType, queryType)
 
         for id, _ in pairs(databaseKeys) do
             local name = databaseQueryHandle(id, "name") -- Some entries don't have a 'name' because of the way we load corrections
-            if name and string.find(string.lower(), queryToFind) then
+            if name and string.find(string.lower(name), queryToFind) then
                 -- We have a search result or a favourite to display
                 searchCount = searchCount + 1;
                 QuestieSearch.LastResult[searchType][id] = true;