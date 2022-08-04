35f71b39b80fef5b4702e337dc245f777cf60e1c
BreakBB <b.boeing@online.de>
2022-08-04 19:08:10 +0200

    Extract reusable _TrackAchievement function

--- Modules/Tracker/AchievementTracker.lua
+++ Modules/Tracker/AchievementTracker.lua
@@ -14,6 +14,8 @@ local trackedAchievements = {}
 
 local baseFrame
 
+local _TrackAchievement
+
 ---@param trackerBaseFrame Frame
 function AchievementTracker.Initialize(trackerBaseFrame)
     baseFrame = CreateFrame("Frame", "Questie_TrackedAchievements", trackerBaseFrame)
@@ -23,6 +25,40 @@ function AchievementTracker.Initialize(trackerBaseFrame)
     local header = baseFrame:CreateFontString(nil, "ARTWORK", "GameFontNormal")
     header:SetText("Tracked Achievements")
     baseFrame.header = header
+end
+
+---Creates the required frames to display an Achievement name and its criteria
+---@param achievementId integer
+_TrackAchievement = function(achievementId)
+    local achievementName = select(2, GetAchievementInfo(achievementId))
+
+    local line = CreateFrame("Button", nil, baseFrame)
+    line:SetPoint("CENTER")
+    line:SetSize(1, 1)
+
+    local label = line:CreateFontString(nil, "ARTWORK", "GameFontNormal")
+    label:SetJustifyH("LEFT")
+    label:SetPoint("TOPLEFT", line)
+    label:SetText(achievementName)
+    line.label = label
+
+    local numCriteria = GetAchievementNumCriteria(achievementId)
+
+    for i=1, numCriteria do
+        local quantityString = select(9, GetAchievementCriteriaInfo(achievementId, i))
+
+        local criteriaLine = line:CreateFontString(nil, "ARTWORK", "GameTooltipText")
+        criteriaLine:SetJustifyH("LEFT")
+        criteriaLine:SetText("- " .. quantityString)
+        if i > 1 then
+            criteriaLine:SetPoint("TOP", line, "BOTTOM", 0, 0)
+        else
+            -- First criteria
+            criteriaLine:SetPoint("TOP", 40, -13)
+        end
+    end
+
+    trackedAchievements[achievementId] = line
 end
 
 ---The corresponding TRACKED_ACHIEVEMENT_LIST_CHANGED event is fired whenever a user tracks or untracks an achievemnt
@@ -36,37 +72,7 @@ function AchievementTracker:TrackedArchievementListChanged(achievementId, should
 
     if shouldTrack then
         -- Create Tracker line and add Achievement information
-
-        local achievementName = select(2, GetAchievementInfo(achievementId))
-        print("achievementName", achievementName)
-
-        local line = CreateFrame("Button", nil, baseFrame)
-        line:SetPoint("CENTER")
-        line:SetSize(1, 1)
-
-        local label = line:CreateFontString(nil, "ARTWORK", "GameFontNormal")
-        label:SetJustifyH("LEFT")
-        label:SetPoint("TOPLEFT", line)
-        label:SetText(achievementName)
-        line.label = label
-
-        local numCriteria = GetAchievementNumCriteria(achievementId)
-
-        for i=1, numCriteria do
-            local quantityString = select(9, GetAchievementCriteriaInfo(achievementId, i))
-
-            local criteriaLine = line:CreateFontString(nil, "ARTWORK", "GameTooltipText")
-            criteriaLine:SetJustifyH("LEFT")
-            criteriaLine:SetText("- " .. quantityString)
-            if i > 1 then
-                criteriaLine:SetPoint("TOP", line, "BOTTOM", 0, 0)
-            else
-                -- First criteria
-                criteriaLine:SetPoint("TOP", 40, -13)
-            end
-        end
-
-        trackedAchievements[achievementId] = line
+        _TrackAchievement(achievementId)
     else
         -- remove from tracker
         if trackedAchievements[achievementId] then
