6e127a6de142977ce15b24e63ac5598880c99f2a
BreakBB <b.boeing@online.de>
2022-09-04 12:10:28 +0200

    Activate daily quest filtering

--- Modules/QuestieInit.lua
+++ Modules/QuestieInit.lua
@@ -271,7 +271,7 @@ QuestieInit.Stages[3] = function() -- run as a coroutine
 
     Questie.started = true
 
-    if Questie.IsTBC and QuestiePlayer.GetPlayerLevel() == 70 then
+    if Questie.IsWotlk and QuestiePlayer.GetPlayerLevel() == 70 then
         local lastRequestWasYesterday = Questie.db.char.lastDailyRequestDate ~= date("%d-%m-%y"); -- Yesterday or some day before
         local isPastDailyReset = Questie.db.char.lastDailyRequestResetTime < GetQuestResetTime();
 
