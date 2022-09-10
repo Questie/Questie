20d4587b6971d6c73548a6c15643273abca315f7
BreakBB <b.boeing@online.de>
2022-09-10 14:11:02 +0200

    [db] Fix wotlk location of "Keeper of the Rolls"

--- Database/Corrections/Wotlk/wotlkNPCFixes.lua
+++ Database/Corrections/Wotlk/wotlkNPCFixes.lua
@@ -21,6 +21,9 @@ function QuestieWotlkNpcFixes:Load()
                 [zoneIDs.STORMWIND_CITY]={{83.47,33.66}},
             },
         },
+        [16281] = {
+            [npcKeys.spawns] = {[zoneIDs.EASTERN_PLAGUELANDS] = {{75.16,54.39}}},
+        },
         [16361] = {
             [npcKeys.spawns] = {[zoneIDs.EASTERN_PLAGUELANDS] = {{75.16,54.45}}},
         },
