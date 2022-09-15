f5110a0d67315bb544354e312caddc89df63562d
BreakBB <b.boeing@online.de>
2022-09-15 22:14:57 +0200

    [quest] Show "The Fall of Magtheridon"
    Fixes #3975

--- Database/Corrections/TBC/tbcQuestFixes.lua
+++ Database/Corrections/TBC/tbcQuestFixes.lua
@@ -2089,6 +2089,12 @@ function QuestieTBCQuestFixes:Load()
         [11000] = {
             [questKeys.extraObjectives] = {{nil, ICON_TYPE_EVENT, l10n("Use Vim'gol's Grimoire at Soulgrinder's Altar"), 0, {{"object", 185880}}}},
         },
+        [11002] = {
+            [questKeys.startedBy] = {{17257},nil,{33102,},},
+        },
+        [11003] = {
+            [questKeys.startedBy] = {{17257},nil,{33102,},},
+        },
         [11010] = {
             [questKeys.requiredClasses] = classIDs.WARLOCK, -- Everything except Druid (since Warlock has the highest number except for Druid this works)
             [questKeys.extraObjectives] = {{nil, ICON_TYPE_SLAY, l10n("Use the Skyguard Bombs to destroy 15 Fel Cannonball Stacks"), 0, {{"object", 185861}}}},
