eb4cf083deb00b4129bd5e972e51ff9b72bd4d6d
BreakBB <b.boeing@online.de>
2022-09-20 23:26:41 +0200

    [db] Add spawn for "Bizzle Quicklift"

--- Database/Corrections/Wotlk/wotlkNPCFixes.lua
+++ Database/Corrections/Wotlk/wotlkNPCFixes.lua
@@ -74,6 +74,10 @@ function QuestieWotlkNpcFixes:Load()
             [npcKeys.spawns] = {[zoneIDs.THE_NEXUS] = {{-1,-1}}},
             [npcKeys.zoneID] = zoneIDs.THE_NEXUS,
         },
+        [27216] = {
+            [npcKeys.spawns] = {[zoneIDs.DUROTAR]={{45.25,17.33}}},
+            [npcKeys.zoneID] = zoneIDs.DUROTAR,
+        },
         [27315] = {
             [npcKeys.spawns] = {
                 [zoneIDs.DRAGONBLIGHT] = {{77.2,49.8},{78.2,50.6},{78.8,50.8},{79.8,49.6},{80,49.4},{80,51},{81.8,50.6},{82.2,50.4},{83,49.2},{83,50.2},{83.4,51},{84.2,50.4},{84.6,51.6},{84.8,50.4},}
