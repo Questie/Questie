6dcfdaf0d91c0cc6e0fd9dd493e041e4bd42b8d6
BreakBB <b.boeing@online.de>
2022-09-25 23:21:09 +0200

    [quest] Fix objective locations for "A Lesson in Fear"
    Fixes #3643

--- Database/Corrections/Wotlk/wotlkQuestFixes.lua
+++ Database/Corrections/Wotlk/wotlkQuestFixes.lua
@@ -90,6 +90,10 @@ function QuestieWotlkQuestFixes:Load()
         [11252] = {
             [questKeys.preQuestSingle] = {},
         },
+        [11282] = {
+            [questKeys.objectives] = {{{24161,"Oric the Baleful's Corpse Impaled"},{24016,"Ulf the Bloodletter's Corpse Impaled"},{24162,"Gunnar Thorvardsson's Corpse Impaled"}}},
+            [questKeys.extraObjectives] = {{nil, ICON_TYPE_SLAY, l10n("Slay Vrykul across the Forsaken blockade until they appear"), 0, {{"monster", 24015}}}},
+        },
         [11302] = {
             [questKeys.preQuestSingle] = {11269,11329},
         },
