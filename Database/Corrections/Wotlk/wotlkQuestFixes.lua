155d3bb70cfc01d9668bd77e576dcf8cbe3800cb
Björn Böing <b.boeing@online.de>
2022-10-01 11:27:45 +0200

    Update Database/Corrections/Wotlk/wotlkQuestFixes.lua

--- Database/Corrections/Wotlk/wotlkQuestFixes.lua
+++ Database/Corrections/Wotlk/wotlkQuestFixes.lua
@@ -543,7 +543,7 @@ function QuestieWotlkQuestFixes:Load()
         },
         [12249] = {
             [questKeys.objectives] = {{{26633,}}},
-            [questKeys.extraObjectives] = {{nil, ICON_TYPE_EVENT, l10n("Talk to Tur Ragepaw to summon Ursoc"),0,{{"monster", 27328}}}},
+            [questKeys.extraObjectives] = {{nil, ICON_TYPE_TALK, l10n("Talk to Tur Ragepaw to summon Ursoc"),0,{{"monster", 27328}}}},
         },
         [12258] = {
             [questKeys.preQuestSingle] = {12251},
