6b095669c89805a04b6906d0b035f2a293cf71b1
BreakBB <b.boeing@online.de>
2022-10-03 23:05:47 +0200

    Add translations for 4ac632fce5cbed9d408db73cf797ed6b3b35f841

--- Localization/Translations/ExtraObjectives/WotlkObjectives.lua
+++ Localization/Translations/ExtraObjectives/WotlkObjectives.lua
@@ -974,6 +974,18 @@ local wotlkObjectiveLocales = {
         ["zhTW"] = false,
         ["zhCN"] = false,
     },
+    ["Use Eye of Dominion"] = { -- 13168
+        ["ptBR"] = false,
+        ["ruRU"] = false,
+        ["deDE"] = false,
+        ["koKR"] = false,
+        ["esMX"] = false,
+        ["enUS"] = true,
+        ["frFR"] = false,
+        ["esES"] = false,
+        ["zhTW"] = false,
+        ["zhCN"] = false,
+    },
 }
 
 for k, v in pairs(wotlkObjectiveLocales) do
