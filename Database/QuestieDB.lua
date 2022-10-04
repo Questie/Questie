2ba2c0b4fb1d86e8b8f341001ad9dcd6af66374c
BreakBB <b.boeing@online.de>
2022-10-04 21:16:25 +0200

    Add comment to explain reversedKillCreditQuestIDs recent changes

--- Database/QuestieDB.lua
+++ Database/QuestieDB.lua
@@ -848,6 +848,8 @@ function QuestieDB:GetQuest(questId) -- /dump QuestieDB:GetQuest(867)
 
         -- There are quest(s) which have the killCredit at first so we need to switch them
         if QuestieCorrections.reversedKillCreditQuestIDs[questId] then
+            -- Quests like 13373 have three objectives, two regular slay objectives and one killCreditObjective.
+            -- This swappes the first and last objective.
             if QO.ObjectiveData[3] then
                 local tmp = QO.ObjectiveData[1]
                 QO.ObjectiveData[1] = QO.ObjectiveData[3]
