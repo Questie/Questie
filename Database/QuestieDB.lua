6283863b544f9dddff6aac5e4ca438f0b811a458
Logon <Logon.q@gmail.com>
2022-10-05 12:55:25 +0200

    Remove dead code

--- Database/QuestieDB.lua
+++ Database/QuestieDB.lua
@@ -1073,26 +1073,3 @@ function _QuestieDB:DeleteGatheringNodes()
         QuestieDB.objectData[v][objectSpawnsKey] = nil
     end
 end
-
----------------------------------------------------------------------------------------------------
--- Modifications to questDB
-
-function _QuestieDB:HideClassAndRaceQuests()
-    local questKeys = QuestieDB.questKeys
-    for _, entry in pairs(QuestieDB.questData) do
-        -- check requirements, set hidden flag if not met
-        local requiredClasses = entry[questKeys.requiredClasses]
-        if (requiredClasses) and (requiredClasses ~= 0) then
-            if (not QuestiePlayer:HasRequiredClass(requiredClasses)) then
-                entry.hidden = true
-            end
-        end
-        local requiredRaces = entry[questKeys.requiredRaces]
-        if (requiredRaces) and (requiredRaces ~= 0) and (requiredRaces ~= 255) then
-            if (not QuestiePlayer:HasRequiredRace(requiredRaces)) then
-                entry.hidden = true
-            end
-        end
-    end
-    Questie:Debug(Questie.DEBUG_DEVELOP, "Other class and race quests hidden");
-end
