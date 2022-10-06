23e026f6c96a486a641facb7c42d7081d030847e
BreakBB <b.boeing@online.de>
2022-10-06 20:47:25 +0200

    [quest] Remove pre quests from "The Forsaken Blight and You: How Not to Die"
    Related to #4307

--- Database/Corrections/Wotlk/wotlkQuestFixes.lua
+++ Database/Corrections/Wotlk/wotlkQuestFixes.lua
@@ -637,6 +637,9 @@ function QuestieWotlkQuestFixes:Load()
                 {nil, ICON_TYPE_EVENT, l10n("Intercept the message from Loken"),1,{{"object", 188596}}},
             },
         },
+        [12188] = {
+            [questKeys.preQuestSingle] = {},
+        },
         [12198] = {
             [questKeys.objectives] = {nil,nil,nil,nil,{{{26407,27017},26407,}}},
             [questKeys.extraObjectives] = {
