a351f79b0d3794080b252cb31647eacaf0377a54
Drejjmit <drimit_cs@hotmail.com>
2022-10-07 16:02:10 +0200

    [quest] Mark "The Kor'kron Vanguard!" to require all pre quests

--- Database/Corrections/Wotlk/wotlkQuestFixes.lua
+++ Database/Corrections/Wotlk/wotlkQuestFixes.lua
@@ -681,7 +681,7 @@ function QuestieWotlkQuestFixes:Load()
             },
         },
         [12224] = {
-            [questKeys.preQuestSingle] = {12221,12140,12072},
+            [questKeys.preQuestGroup] = {12221,12140,12072},
         },
         [12229] = {
             [questKeys.preQuestGroup] = {12207,12213},
