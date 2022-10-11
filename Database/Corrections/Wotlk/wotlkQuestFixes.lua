fe58d5607c9dcbb009a7dffe8b8d92b47cc6c236
Drejjmit <drimit_cs@hotmail.com>
2022-10-11 20:22:33 +0200

    [quest] Fix pre quest for "Loyal Companions"

--- Database/Corrections/Wotlk/wotlkQuestFixes.lua
+++ Database/Corrections/Wotlk/wotlkQuestFixes.lua
@@ -1325,6 +1325,9 @@ function QuestieWotlkQuestFixes:Load()
         [12864] = {
             [questKeys.triggerEnd] = {"Locate Missing Scout",{[zoneIDs.STORM_PEAKS]={{37.68,66.75},{38.49,77.19},{31.65,64.53},{34.56,64.64},{36.43,77.3},},},},
         },
+        [12865] = {
+            [questKeys.preQuestSingle] = {12863},
+        },
         [12887] = {
             [questKeys.objectives] = {{{29747,"The Ocular has been destroyed"}}},
         },
