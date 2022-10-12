ccbba2a37a083904b774463d9bd79f1d115cad71
BreakBB <b.boeing@online.de>
2022-10-12 20:14:54 +0200

    [quest] Mark "Stiff Negotiations" as breadcrumb quest for "Slim Pickings"

--- Database/Corrections/Wotlk/wotlkQuestFixes.lua
+++ Database/Corrections/Wotlk/wotlkQuestFixes.lua
@@ -536,6 +536,9 @@ function QuestieWotlkQuestFixes:Load()
         [12069] = {
             [questKeys.extraObjectives] = {{nil, ICON_TYPE_EVENT, l10n("Free Roanauk Icemist"),0,{{"object", 188463}}}},
         },
+        [12075] = {
+            [questKeys.preQuestSingle] = {},
+        },
         [12076] = {
             [questKeys.extraObjectives] = {{nil, ICON_TYPE_EVENT, l10n("Use Zort's Scraper when covered in Corrosive Spit"),0,{{"monster", 26358},{"monster", 26359}}}},
         },
