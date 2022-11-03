56303c17c637b2e4696a75030ffe4ef7a01f24f0
Logon <Logon.q@gmail.com>
2022-11-03 22:13:46 +0100

    Moved around some code

--- ModulesNew/Quest/Quest.lua
+++ ModulesNew/Quest/Quest.lua
@@ -111,61 +111,60 @@ QuestieQuest.Show = {
     }
 }
 
-
-local function AddQuestGivers(questId)
-    -- print("Add questgives", questId)
-    local show = QuestieQuest.Show
-    local starts = QuestieDB.QueryQuestSingle(questId, "startedBy") or {}
-    if(starts[1] ~= nil)then
-        local npcs = starts[1]
-        for i=1, #npcs do
-            local npcId = npcs[i]
-            -- print("Adding quest giver NPC :", npcId, "for quest", questId)
-            show.NPC[npcId] = show.NPC[npcId] or {}
-            if show.NPC[npcId] == nil then
-                show.NPC[npcId] = {}
-            end
-            if show.NPC[npcId].available == nil then
-                show.NPC[npcId].available = {}
-            end
-            show.NPC[npcId].available[questId] = relationTypes.availablePickup
-        end
-    end
-    if(starts[2] ~= nil)then
-        local gameobjects = starts[2]
-        for i=1, #gameobjects do
-            local gameObjectId = gameobjects[i]
-            if show.GameObject[gameObjectId] == nil then
-                show.GameObject[gameObjectId] = {}
-            end
-            if show.GameObject[gameObjectId].available == nil then
-                show.GameObject[gameObjectId].available = {}
-            end
-            show.GameObject[gameObjectId].available[questId] = relationTypes.availablePickup
-        end
-    end
-    if(starts[3] ~= nil)then
-        local items = starts[3]
-        for i=1, #items do
-            local itemId = items[i]
-            -- print("Adding quest giver ITEM:", itemId, "for quest", questId)
-            if show.Item[itemId] == nil then
-                show.Item[itemId] = {}
-            end
-            if show.Item[itemId].available == nil then
-                show.Item[itemId].available = {}
-            end
-            show.Item[itemId].available[questId] = relationTypes.availableDrop
-        end
-    end
-end
-
 --? Creates a localized space where the local variables and functions are stored
 do
     --- Used to keep track of the active timer for CalculateAvailableQuests
     --- Is used by the QuestieQuest.CalculateAndDrawAvailableQuestsIterative func
     ---@type Ticker|nil
     local timer
+
+    local function AddQuestGivers(questId)
+        -- print("Add questgives", questId)
+        local show = QuestieQuest.Show
+        local starts = QuestieDB.QueryQuestSingle(questId, "startedBy") or {}
+        if(starts[1] ~= nil)then
+            local npcs = starts[1]
+            for i=1, #npcs do
+                local npcId = npcs[i]
+                -- print("Adding quest giver NPC :", npcId, "for quest", questId)
+                show.NPC[npcId] = show.NPC[npcId] or {}
+                if show.NPC[npcId] == nil then
+                    show.NPC[npcId] = {}
+                end
+                if show.NPC[npcId].available == nil then
+                    show.NPC[npcId].available = {}
+                end
+                show.NPC[npcId].available[questId] = relationTypes.availablePickup
+            end
+        end
+        if(starts[2] ~= nil)then
+            local gameobjects = starts[2]
+            for i=1, #gameobjects do
+                local gameObjectId = gameobjects[i]
+                if show.GameObject[gameObjectId] == nil then
+                    show.GameObject[gameObjectId] = {}
+                end
+                if show.GameObject[gameObjectId].available == nil then
+                    show.GameObject[gameObjectId].available = {}
+                end
+                show.GameObject[gameObjectId].available[questId] = relationTypes.availablePickup
+            end
+        end
+        if(starts[3] ~= nil)then
+            local items = starts[3]
+            for i=1, #items do
+                local itemId = items[i]
+                -- print("Adding quest giver ITEM:", itemId, "for quest", questId)
+                if show.Item[itemId] == nil then
+                    show.Item[itemId] = {}
+                end
+                if show.Item[itemId].available == nil then
+                    show.Item[itemId].available = {}
+                end
+                show.Item[itemId].available[questId] = relationTypes.availableDrop
+            end
+        end
+    end
 
     local function CalculateAvailableQuests()
 
@@ -232,40 +231,6 @@ do
 
                     if isLevelRequirementsFulfilled(questId, minLevel, maxLevel, playerLevel) and isDoable(questId, debugEnabled) then
                         AddQuestGivers(questId)
-                        -- QuestieQuest.availableQuests[questId] = true
-                        -- --If the quest is not drawn draw the quest, otherwise skip.
-                        -- if (not QuestieMap.questIdFrames[questId]) then
-                        --     --? This looks expensive, and it kind of is but it offloads the work to a thread, which happens "next frame"
-                        --     -- NewThread(function()
-                        --     --     ---@type Quest
-                        --     --     local quest = QuestieDB:GetQuest(questId)
-                        --     --     if (not quest.tagInfoWasCached) then
-                        --     --         Questie:Debug(Questie.DEBUG_SPAM, "Caching tag info for quest", questId)
-                        --     --         QuestieDB.GetQuestTagInfo(questId) -- cache to load in the tooltip
-                        --     --         quest.tagInfoWasCached = true
-                        --     --     end
-                        --     --     --Draw a specific quest through the function
-                        --     --     _QuestieQuest:DrawAvailableQuest(quest)
-                        --     -- end, 0)
-                        -- else
-                        --     --* TODO: How the frames are handled needs to be reworked, why are we getting them from _G
-                        --     --We might have to update the icon in this situation (config changed/level up)
-                        --     -- for _, frame in ipairs(QuestieMap:GetFramesForQuest(questId)) do
-                        --     --     if frame and frame.data and frame.data.QuestData then
-                        --     --         local newIcon = _QuestieQuest:GetQuestIcon(frame.data.QuestData)
-                        --     --         if newIcon ~= frame.data.Icon then
-                        --     --             frame:UpdateTexture(newIcon)
-                        --     --         end
-                        --     --     end
-                        --     -- end
-                        -- end
-                    else
-                        --If the quests are not within level range we want to unload them
-                        --(This is for when people level up or change settings etc)
-                        -- QuestieMap:UnloadQuestFrames(questId)
-                        -- if QuestieQuest.availableQuests[questId] then
-                        --     QuestieTooltips:RemoveQuest(questId)
-                        -- end
                     end
                 end
             end
@@ -292,47 +257,7 @@ do
         --? Run this first because there are parts that depend on the Show data still being there.
         MapEventBus:Fire(MapEventBus.events.MAP.REMOVE_ALL_AVAILABLE)
 
-        --TODO: This should not wipe everything because we will have multiple things here.
-        -- wipe(QuestieQuest.Show.NPC)
-        -- wipe(QuestieQuest.Show.GameObject)
-        -- wipe(QuestieQuest.Show.Item)
-
-
         timer = ThreadLib.Thread(CalculateAvailableQuests, 0, "Error in CalculateAvailableQuests", function() print("test") end)
-    end
-end
-
-local function AddQuestFinishers(questId)
-    -- print("Add questgives", questId)
-    local show = QuestieQuest.Show
-    local finishes = QuestieDB.QueryQuestSingle(questId, "finishedBy") or {}
-    if(finishes[1] ~= nil)then
-        local npcs = finishes[1]
-        for i=1, #npcs do
-            local npcId = npcs[i]
-            print("Adding quest giver NPC :", npcId, "for quest", questId)
-            if show.NPC[npcId] == nil then
-                show.NPC[npcId] = {}
-            end
-            if show.NPC[npcId].finisher == nil then
-                show.NPC[npcId].finisher = {}
-            end
-            show.NPC[npcId].finisher[questId] = relationTypes.finisherComplete
-        end
-    end
-    if(finishes[2] ~= nil)then
-        local gameobjects = finishes[2]
-        for i=1, #gameobjects do
-            local gameObjectId = gameobjects[i]
-            print("Adding quest giver GO  :", gameObjectId, "for quest", questId)
-            if show.GameObject[gameObjectId] == nil then
-                show.GameObject[gameObjectId] = {}
-            end
-            if show.GameObject[gameObjectId].finisher == nil then
-                show.GameObject[gameObjectId].finisher = {}
-            end
-            show.GameObject[gameObjectId].finisher[questId] = relationTypes.finisherComplete
-        end
     end
 end
 
@@ -341,6 +266,40 @@ do
     --- Is used by the QuestieQuest.CalculateAndDrawAvailableQuestsIterative func
     ---@type Ticker|nil
     local timer
+
+    local function AddQuestFinishers(questId)
+        -- print("Add questgives", questId)
+        local show = QuestieQuest.Show
+        local finishes = QuestieDB.QueryQuestSingle(questId, "finishedBy") or {}
+        if(finishes[1] ~= nil)then
+            local npcs = finishes[1]
+            for i=1, #npcs do
+                local npcId = npcs[i]
+                print("Adding quest giver NPC :", npcId, "for quest", questId)
+                if show.NPC[npcId] == nil then
+                    show.NPC[npcId] = {}
+                end
+                if show.NPC[npcId].finisher == nil then
+                    show.NPC[npcId].finisher = {}
+                end
+                show.NPC[npcId].finisher[questId] = relationTypes.finisherComplete
+            end
+        end
+        if(finishes[2] ~= nil)then
+            local gameobjects = finishes[2]
+            for i=1, #gameobjects do
+                local gameObjectId = gameobjects[i]
+                print("Adding quest giver GO  :", gameObjectId, "for quest", questId)
+                if show.GameObject[gameObjectId] == nil then
+                    show.GameObject[gameObjectId] = {}
+                end
+                if show.GameObject[gameObjectId].finisher == nil then
+                    show.GameObject[gameObjectId].finisher = {}
+                end
+                show.GameObject[gameObjectId].finisher[questId] = relationTypes.finisherComplete
+            end
+        end
+    end
 
     local function CalculateCompleteQuests()
 
@@ -382,12 +341,6 @@ do
 
         --? Run this first because there are parts that depend on the Show data still being there.
         MapEventBus:Fire(MapEventBus.events.MAP.REMOVE_ALL_COMPLETED)
-
-        --TODO: This should not wipe everything because we will have multiple things here.
-        -- wipe(QuestieQuest.Show.NPC)
-        -- wipe(QuestieQuest.Show.GameObject)
-        -- wipe(QuestieQuest.Show.Item)
-
 
         timer = ThreadLib.Thread(CalculateCompleteQuests, 0, "Error in CalculateCompleteQuests", function() print("test") end)
     end
