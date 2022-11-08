88ff416ff4f6000bfc977e37da160887c20608c3
Logon <Logon.q@gmail.com>
2022-11-08 13:25:40 +0100

    Changed pin template reference, removed OnAcquired and added mouseover logic plus scaling logic

--- ModulesNew/Map/Pins/WaypointPin.lua
+++ ModulesNew/Map/Pins/WaypointPin.lua
@@ -1,4 +1,5 @@
----@class WaypointPinMixin : BasePinMixin
+---@class WaypointPinMixin : BasePinMixin, Button
+---@field lineCornerPoints {[1]: XYPoint, [2]: XYPoint, [3]: XYPoint, [4]: XYPoint} @The points of the line corners, values between 0-1 is expected
 local WaypointPinMixin = QuestieLoader:CreateModule("WaypointPinMixin")
 
 ----- System Imports -----
@@ -12,6 +13,9 @@ local MapCoordinates = QuestieLoader:ImportModule("MapCoordinates")
 ---@type FramePoolWaypoint
 local FramePoolWaypoint = QuestieLoader:ImportModule("FramePoolWaypoint")
 
+---@type PinTemplates
+local PinTemplates = QuestieLoader:ImportModule("PinTemplates")
+
 ----- Imports -----
 local QuestieQuest = QuestieLoader:ImportModule("QQuest")
 local MapTooltip = QuestieLoader:ImportModule("MapTooltip")
@@ -24,17 +28,11 @@ local QuestieEVent = QuestieLoader:ImportModule("QuestieEvent")
 
 --Up value
 local questieTooltip = QuestieTooltip --Localize the tooltip
+local wipe = wipe
+local ScrollContainer = WorldMapFrame.ScrollContainer;
 
 function WaypointPinMixin:GetType()
     return "WaypointPin"
-end
-
-function WaypointPinMixin:OnAcquired() -- the arguments here are anything that are passed into AcquirePin after the pinTemplate
-    -- local uiMapId = self.data and self.data.uiMapId and self.data.uiMapId or nil
-    -- local normalScale = (self:GetIconScale(uiMapId) or 1)
-    -- local zoomedScale = (normalScale * 1.2)
-
-    -- self:SetScalingLimits(1, normalScale, zoomedScale)
 end
 
 ---Get the IconScale for the pin, use UiMapId to get the scale for a specific map
@@ -48,28 +46,30 @@ end
 function WaypointPinMixin:DrawLine(UiMapID, sX, sY, eX, eY, Linedata, maskTextures)
     -- Override in your mixin, called when this pin is being acquired by a data provider but before its added to the map
     local lineData = Linedata or {}
-    local dx,dy = eX - sX, eY - sY;
+    local dx, dy = eX - sX, eY - sY;
     -- Normalize direction if necessary
     if (dx < 0) then
-        dx,dy = -dx,-dy;
+        dx, dy = -dx, -dy;
     end
-    local cx,cy = (sX + eX) / 2, (sY + eY) / 2;
+    local cx, cy = (sX + eX) / 2, (sY + eY) / 2;
     local width, height = WorldMapFrame:GetCanvas():GetSize();
     width = width
     height = height
     local minX = math.min(sX, eX);
     local minY = math.min(sY, eY);
-    local framePosX, framePosY = minX*width, minY*height
-    self:ClearAllPoints()
+    local framePosX, framePosY = minX * width, minY * height
+    -- self:ClearAllPoints()
     --self:SetParent(WorldMapFrame:GetCanvas())
     --self:SetPosition(minX+(windowX/2), minY+(windowY/2))
     --self:SetPosition(cx, cy)
-    if(not self.minimapPin) then
+    if (not self.minimapPin) then
         --pcall(self.SetPosition, self, cx, cy);
         self:SetPosition(cx, cy)
     end
-
-    self:SetSize(dx*width, dy*height)
+    local fWidth, fHeight = dx * width, dy * height;
+    self:SetSize(fWidth, fHeight)
+    self.framePosX = framePosX
+    self.framePosY = framePosY
 
     -- self.lineTexture = self.lineTexture or FramePoolWaypoint.LinePool:Acquire();
     local lineTexture = FramePoolWaypoint.LinePool:Acquire();
@@ -82,18 +82,19 @@ function WaypointPinMixin:DrawLine(UiMapID, sX, sY, eX, eY, Linedata, maskTextur
 
     --Due to thickness being used in linemixin we instead just use defaultThickness
     lineTexture.defaultThickness = lineData.thickness or 5;
-    lineTexture.color = {r=lineData.r or 1, g=lineData.g or 1, b=lineData.b or 1, a=lineData.a or 0.6}
-    if(maskTextures) then
+    lineTexture.color = { r = lineData.r or 1, g = lineData.g or 1, b = lineData.b or 1, a = lineData.a or 0.6 }
+    if (maskTextures) then
         for _, maskTexture in pairs(maskTextures) do
             lineTexture:AddMaskTexture(maskTexture)
         end
         lineTexture.maskTextures = maskTextures
     end
     lineTexture:SetVertexColor(lineTexture.color.r, lineTexture.color.g, lineTexture.color.b, lineTexture.color.a)
-    lineTexture:SetStartPoint(startX-framePosX, -(startY-framePosY))
-    lineTexture:SetEndPoint(endX-framePosX, -(endY-framePosY))
+    lineTexture:SetStartPoint(startX - framePosX, -(startY - framePosY))
+    lineTexture:SetEndPoint(endX - framePosX, -(endY - framePosY))
     lineTexture:SetThickness(lineTexture.defaultThickness);
-    lineTexture:SetPoint("CENTER", endX-framePosX, -(endY-framePosY))
+    lineTexture:SetPoint("CENTER", endX - framePosX, -(endY - framePosY))
+
 
     --? Minimap Stuff
     -- local fWidth, fHeight = self:GetSize();
@@ -118,8 +119,9 @@ function WaypointPinMixin:DrawLine(UiMapID, sX, sY, eX, eY, Linedata, maskTextur
 
     --self.self.lineTexture = self.lineTexture
 
+
     lineTexture:Show()
-    lineTexture.redraw(lineTexture)
+    -- lineTexture.redraw(lineTexture)
     self.lineTexture = lineTexture
 end
 
@@ -138,12 +140,119 @@ end
 --! End fucky wucky, read above
 
 function WaypointPinMixin:OnTooltip()
+
 end
 
+function WaypointPinMixin:OnCanvasScaleChanged()
+    -- Called when the canvas scale changes
+    --? We redraw the line here with the new scale, we fetch the scale from the scrollcontainer
+    --? because we need the performance. But Map:GetCanvasScale() also works.
+    --* ScrollContainer.currentScale or ScrollContainer.targetScale or 1 == Map:GetCanvasScale
 
-function WaypointPinMixin:OnMouseEnter()
+    --? Shrinks the width of the line when zooming in
+    self.lineTexture:redraw(ScrollContainer.currentScale or ScrollContainer.targetScale or 1)
 end
 
-function WaypointPinMixin:OnMouseLeave()
-    -- Override in your mixin, called when the mouse leaves this pin
+function WaypointPinMixin:OnMouseEnterLine()
+    -- Override in your mixin, called when the mouse enters this pin
+    Questie:Debug(Questie.DEBUG_DEVELOP, "WaypointPinMixin:OnMouseEnterLine")
+    print(self:GetName(), "OnMouseEnterLine")
+end
+
+function WaypointPinMixin:OnMouseLeaveLine()
+    -- Override in your mixin, called when the mouse enters this pin
+    Questie:Debug(Questie.DEBUG_DEVELOP, "WaypointPinMixin:OnMouseLeaveLine")
+    print(self:GetName(), "OnMouseLeaveLine")
+end
+
+do
+    --- This function checks if a point is inside a polygon (In this case a rectangle)
+    ---@param line table<number, XYPoint> @A table with the outside points
+    ---@param point XYPoint
+    ---@return boolean @Inside or not, true or false
+    local function insideLine(line, point)
+        local oddNodes = false
+        local j = #line
+        for i = 1, #line do
+            if (line[i].y < point.y and line[j].y >= point.y or line[j].y < point.y and line[i].y >= point.y) then
+                if (line[i].x + (point.y - line[i].y) / (line[j].y - line[i].y) * (line[j].x - line[i].x) < point.x) then
+                    oddNodes = not oddNodes;
+                end
+            end
+            j = i;
+        end
+        return oddNodes
+    end
+
+    --Mouse specific function
+    local lastX, lastY = 0, 0
+    local hoveredLines = {}
+    local mouseMovementTimer
+
+    -- A value between 0-1 is expected for x and y
+    ---@type XYPoint
+    ---@diagnostic disable-next-line: assign-type-mismatch
+    local mousePoint = { x = 0, y = 0 }
+
+    function WaypointPinMixin:OnMouseEnter()
+        --? If you want to do OnEnter stuff you are probably looking for the OnMouseEnterLine function
+
+        --* Reset the mouse check timer
+        if mouseMovementTimer ~= nil then
+            mouseMovementTimer:Cancel()
+            mouseMovementTimer = nil
+            wipe(hoveredLines)
+        end
+        if self.data.lineCornerPoints ~= nil and #self.data.lineCornerPoints > 0 then
+            ---@type MapX, MapY
+            local x, y
+            mouseMovementTimer = C_Timer.NewTicker(0.1, function()
+                x, y = ScrollContainer:GetNormalizedCursorPosition()
+                if (x ~= lastX or y ~= lastY) then
+                    mousePoint.x = x
+                    mousePoint.y = y
+
+                    --? We have to enumerate all frame because sometimes two overlap and stop the mouse events
+                    --? Such as OnMouseEnter and OnMouseLeave
+                    ---@param line WaypointPinMixin
+                    for line in self:GetMap():EnumeratePinsByTemplate(PinTemplates.WaypointPinTemplate) do
+                        if insideLine(line.data.lineCornerPoints, mousePoint) then
+                            --? If the line is not in the hoveredLines table, then we add it and call the OnMouseEnterLine
+                            if not hoveredLines[line] then
+                                line:OnMouseEnterLine()
+                                hoveredLines[line] = true
+                            end
+                        else
+                            --? If inside is false and the line is in the hoveredLines table, then it means that the mouse is no longer hovering over the line
+                            if hoveredLines[line] then
+                                line:OnMouseLeaveLine()
+                                hoveredLines[line] = nil
+                            end
+                        end
+                    end
+
+                    -- Set the last mouse position
+                    lastX = x
+                    lastY = y
+                end
+            end)
+        end
+    end
+
+    function WaypointPinMixin:OnMouseLeave()
+        print("OnMouseLeave")
+        -- Override in your mixin, called when the mouse leaves this pin
+        if mouseMovementTimer ~= nil then
+            mouseMovementTimer:Cancel()
+            mouseMovementTimer = nil
+
+            --? We left the frame, push OnMouseLeaveLine to all lines
+            for line, entered in pairs(hoveredLines or {}) do
+                if entered then
+                    line:OnMouseLeaveLine()
+                end
+            end
+            wipe(hoveredLines)
+        end
+    end
 end
