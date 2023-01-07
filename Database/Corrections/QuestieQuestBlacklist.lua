91f6fc20fc8b44d21cc0c9e1d12e96ca54bb3026
BreakBB <b.boeing@online.de>
2023-01-07 23:44:15 +0100

    [quest] Blacklist "PvP Test" quest
    Fixes #4565

--- Database/Corrections/QuestieQuestBlacklist.lua
+++ Database/Corrections/QuestieQuestBlacklist.lua
@@ -1109,6 +1109,7 @@ function QuestieQuestBlacklist:Load()
         [12233] = true, -- Not in the game
         [12426] = true, -- Not in the game
         [12490] = true, -- Not in the game
+        [12493] = true, -- Not in the game
         [25229] = true, -- Not in the game
         [12780] = true, -- Not in the game
         [12590] = true, -- Not in the game
