9924e699bdd0f651d9d571354d3e0a45fc0c3887
BreakBB <b.boeing@online.de>
2023-01-19 19:16:28 +0100

    [quest] Mark "The Shadow Guard" as repeatable quest
    Fixes #4580

--- Database/Corrections/Wotlk/wotlkQuestFixes.lua
+++ Database/Corrections/Wotlk/wotlkQuestFixes.lua
@@ -101,6 +101,9 @@ function QuestieWotlkQuestFixes:Load()
             [questKeys.finishedBy] = {{16281}},
             [questKeys.questLevel] = -1,
         },
+        [9213] = {
+            [questKeys.specialFlags] = 1,
+        },
         [9247] = {
             [questKeys.finishedBy] = {{16281}},
         },
