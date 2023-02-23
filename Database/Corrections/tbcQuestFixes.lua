4b4800d893aaee5aba0a011393f8881caf23dea4
Yttrium tYcLief <tyclief@gmail.com>
2023-02-23 09:39:57 -0500

    [quest] Fix race and level regressions for "Return to Onu"
    These regressions actually occurred in our TBC DB. Our wrath DB has the correct level, but both TBC+wrath have incorrect race.
    
    Fixes #4521

--- Database/Corrections/tbcQuestFixes.lua
+++ Database/Corrections/tbcQuestFixes.lua
@@ -109,6 +109,10 @@ function QuestieTBCQuestFixes:Load()
         [927] = {
             [questKeys.requiredRaces] = raceIDs.ALL_ALLIANCE,
         },
+        [950] = {
+            [questKeys.questLevel] = 12,
+            [questKeys.requiredRaces] = raceIDs.ALL_ALLIANCE,
+        },
         [968] = {
             [questKeys.requiredRaces] = raceIDs.ALL_ALLIANCE,
         },
