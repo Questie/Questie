1e3ba5cdd0dcf79c43f445fb5e6610359fed0933
BreakBB <b.boeing@online.de>
2023-05-19 10:16:43 +0200

    Add CurseForge upload

--- upload.sh
+++ upload.sh
@@ -3,7 +3,41 @@
 LATEST_GIT_TAG=$(git describe --tags --abbrev=0)
 CHANGELOG=$(jq --slurp --raw-input '.' < "CHANGELOG.md")
 
-#WAGO_API_TOKEN=$(cat wago-token.txt)
+#### CurseForge Upload
+
+CF_METADATA=$(cat <<-EOF
+{
+    "displayName": "$LATEST_GIT_TAG",
+    "releaseType": "alpha",
+    "changelog": $CHANGELOG,
+    "changelogType": "markdown",
+    "gameVersions": [9894, 9895],
+    "relations": {
+        "projects": [
+            {slug: "Ace3", type: "embeddedLibrary"},
+            {slug: "CallbackHandler", type: "embeddedLibrary"},
+            {slug: "HereBeDragons", type: "embeddedLibrary"},
+            {slug: "LibCompress", type: "embeddedLibrary"},
+            {slug: "LibDataBroker-1-1", type: "embeddedLibrary"},
+            {slug: "LibDBIcon-1-0", type: "embeddedLibrary"},
+            {slug: "LibSharedMedia-3-0", type: "embeddedLibrary"},
+            {slug: "LibStub", type: "embeddedLibrary"},
+            {slug: "LibUIDropDownMenu", type: "embeddedLibrary"}
+        ]
+    }
+}
+EOF
+)
+
+curl -sS \
+    -H "X-API-TOKEN: $CF_API_TOKEN" \
+    -F "metadata=$CF_METADATA" \
+    -F "file=@releases/$LATEST_GIT_TAG/Questie-$LATEST_GIT_TAG.zip" \
+    "http://localhost:3000"
+#    "https://wow.curseforge.com/api/projects/334372/upload-file"
+
+
+### WAGO Upload
 
 WAGO_METADATA=$(cat <<-EOF
 {
