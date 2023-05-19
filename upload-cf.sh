3305a64b01bc57fcbc05f8d6084a12ec42046d85
BreakBB <b.boeing@online.de>
2023-05-19 10:28:14 +0200

    Split upload scripts

--- upload-cf.sh
+++ upload-cf.sh
@@ -35,25 +35,3 @@ curl -sS \
     -F "file=@releases/$LATEST_GIT_TAG/Questie-$LATEST_GIT_TAG.zip" \
     "http://localhost:3000"
 #    "https://wow.curseforge.com/api/projects/334372/upload-file"
-
-
-### WAGO Upload
-
-WAGO_METADATA=$(cat <<-EOF
-{
-   "label": "$LATEST_GIT_TAG",
-   "stability": "alpha",
-   "changelog": $CHANGELOG,
-   "supported_wotlk_patch": "3.4.2",
-   "supported_classic_patch": "1.14.4"
-}
-EOF
-)
-
-curl -sS \
-    -H "authorization: Bearer $WAGO_API_TOKEN" \
-    -H "accept: application/json" \
-    -F "metadata=$WAGO_METADATA" \
-    -F "file=@releases/$LATEST_GIT_TAG/Questie-$LATEST_GIT_TAG.zip" \
-    "http://localhost:3000"
-#    "https://addons.wago.io/api/projects/qv634BKb/version"
---
+++ upload-wago.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+LATEST_GIT_TAG=$(git describe --tags --abbrev=0)
+CHANGELOG=$(jq --slurp --raw-input '.' < "CHANGELOG.md")
+
+### WAGO Upload
+
+WAGO_METADATA=$(cat <<-EOF
+{
+   "label": "$LATEST_GIT_TAG",
+   "stability": "alpha",
+   "changelog": $CHANGELOG,
+   "supported_wotlk_patch": "3.4.2",
+   "supported_classic_patch": "1.14.4"
+}
+EOF
+)
+
+curl -sS \
+    -H "authorization: Bearer $WAGO_API_TOKEN" \
+    -H "accept: application/json" \
+    -F "metadata=$WAGO_METADATA" \
+    -F "file=@releases/$LATEST_GIT_TAG/Questie-$LATEST_GIT_TAG.zip" \
+    "http://localhost:3000"
+#    "https://addons.wago.io/api/projects/qv634BKb/version"
