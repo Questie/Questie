77d8b6c07af2b5e61df39f82f3a457acfdce556e
Yttrium tYcLief <tyclief@gmail.com>
2023-06-30 01:17:50 -0400

    tweaked dirty fix to be more in line with ace's actual fix

--- Libs/AceDB-3.0/AceDB-3.0.lua
+++ Libs/AceDB-3.0/AceDB-3.0.lua
@@ -260,8 +260,7 @@ local factionrealmKey = factionKey .. " - " .. realmKey
 local localeKey = GetLocale():lower()
 
 local regionTable = { "US", "KR", "EU", "TW", "CN" }
-local regionKey = regionTable[GetCurrentRegion()]
-if not regionKey then regionKey = "US" end
+local regionKey = regionTable[GetCurrentRegion()] or GetCurrentRegionName() or "TR"
 local factionrealmregionKey = factionrealmKey .. " - " .. regionKey
 
 -- Actual database initialization function
