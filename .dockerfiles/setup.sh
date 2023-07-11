9f9111e6fec266055199d7c4ae84d03b2adc7ceb
Logon <Logon.q@gmail.com>
2023-07-12 01:27:23 +0200

    Running the dumper through docker

--- .dockerfiles/setup.sh
+++ .dockerfiles/setup.sh
@@ -5,5 +5,31 @@
 #luarocks install serpent
 #luarocks install penlight
 #luarocks install lua-nucleo
+luarocks install argparse
 cd code
-lua ./cli.lua
+# Dump all expansions
+# lua ./.dockerfiles/cli-dump.lua &
+
+# Each expansion has its own dump
+# lua ./.dockerfiles/cli-dump.lua -e classic &
+# lua ./.dockerfiles/cli-dump.lua -e tbc &
+# lua ./.dockerfiles/cli-dump.lua -e wotlk &
+
+# Each expansion has its own dump for each type
+lua ./.dockerfiles/cli-dump.lua -e era -d item &
+lua ./.dockerfiles/cli-dump.lua -e era -d npc &
+lua ./.dockerfiles/cli-dump.lua -e era -d object &
+lua ./.dockerfiles/cli-dump.lua -e era -d quest &
+
+lua ./.dockerfiles/cli-dump.lua -e tbc -d item &
+lua ./.dockerfiles/cli-dump.lua -e tbc -d npc &
+lua ./.dockerfiles/cli-dump.lua -e tbc -d object &
+lua ./.dockerfiles/cli-dump.lua -e tbc -d quest &
+
+lua ./.dockerfiles/cli-dump.lua -e wotlk -d item &
+lua ./.dockerfiles/cli-dump.lua -e wotlk -d npc &
+lua ./.dockerfiles/cli-dump.lua -e wotlk -d object &
+lua ./.dockerfiles/cli-dump.lua -e wotlk -d quest &
+
+wait
+echo "Done"
