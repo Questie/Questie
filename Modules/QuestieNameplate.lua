7662471c62f3a675a56e08b53e07d30fbea01a45
Cabro <1155017+Cabro@users.noreply.github.com>
2023-09-05 10:25:32 +0300

    [fix] Quest icons should now also be drawn on vehicles nameplates

--- Modules/QuestieNameplate.lua
+++ Modules/QuestieNameplate.lua
@@ -41,8 +41,8 @@ function QuestieNameplate:NameplateCreated(token)
     end
 
     local unitType, _, _, _, _, npcId, _ = strsplit("-", unitGUID)
-    if unitType ~= "Creature" then
-        -- We only draw name plates on NPCs/creatures and skip players, pets, etc
+    if unitType ~= "Creature" and unitType ~= "Vehicle" then
+        -- We only draw name plates on NPCs/creatures and Vehicles (oddness with Chillmaw being a Vehicle?!?!) and skip players, pets, etc
         return
     end
 
@@ -143,8 +143,8 @@ function QuestieNameplate:DrawTargetFrame()
     end
 
     local unitType, _, _, _, _, npcId, _ = strsplit("-", unitGUID)
-    if unitType ~= "Creature" then
-        -- We only draw name plates on NPCs/creatures and skip players, pets, etc
+    if unitType ~= "Creature" and unitType ~= "Vehicle" then
+        -- We only draw name plates on NPCs/creatures and Vehicles (oddness with Chillmaw being a Vehicle?!?!) and skip players, pets, etc
         return
     end
 
