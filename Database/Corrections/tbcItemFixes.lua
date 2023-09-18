557b08b13ddb9675a7e1d6a3bf2bd5b90a96e3a2
Cabro <1155017+Cabro@users.noreply.github.com>
2023-09-18 12:31:38 +0300

    [quest] Seaforium PU-36 Explosive Nether Modulator should show in QIBs

--- Database/Corrections/tbcItemFixes.lua
+++ Database/Corrections/tbcItemFixes.lua
@@ -182,6 +182,9 @@ function QuestieTBCItemFixes:Load()
         [25658] = {
             [itemKeys.class] = 12,
         },
+        [28038] = {
+            [itemKeys.class] = 12,
+        },
         [28106] = {
             [itemKeys.class] = 12,
         },
