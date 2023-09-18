200e5472d786368d72925c830c0abbfa86a90acc
BreakBB <b.boeing@online.de>
2023-09-18 22:24:21 +0200

    Add missing types for requiredSpell and requiredSpecialization

--- .types/data/Quest.t.lua
+++ .types/data/Quest.t.lua
@@ -117,6 +117,8 @@
 ---@field public parentQuest QuestId
 ---@field public reputationReward ReputationPair[]
 ---@field public extraObjectives ExtraObjective[]
+---@field public requiredSpell number
+---@field public requiredSpecialization number
 ---@field public requiredMaxLevel Level
 
 -- table: {{spawnlist, iconFile, text, objectiveIndex (optional), {{dbReferenceType, id}, ...} (optional)},...}, a list of hidden special objectives for a quest. Similar to requiredSourceItems
