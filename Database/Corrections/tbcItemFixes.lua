834829a3491c8b620bbe1e2e8b3c79e4e2011611
Cabro <1155017+Cabro@users.noreply.github.com>
2023-09-27 16:52:39 +0300

    [quest] Warp-Attuned Orb should show in QIB

--- Database/Corrections/tbcItemFixes.lua
+++ Database/Corrections/tbcItemFixes.lua
@@ -203,6 +203,9 @@ function QuestieTBCItemFixes:Load()
         [29162] = {
             [itemKeys.objectDrops] = {184162},
         },
+        [29324] = {
+            [itemKeys.class] = 12,
+        },
         [30426] = {
             [itemKeys.npcDrops] = {19762,19768,19789},
         },
