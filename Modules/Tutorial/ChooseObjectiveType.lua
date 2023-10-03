89e2ba339b598c816d73f1fe9733e56431b89bca
BreakBB <b.boeing@online.de>
2023-10-03 13:17:17 +0200

    Positioning improvements

--- Modules/Tutorial/ChooseObjectiveType.lua
+++ Modules/Tutorial/ChooseObjectiveType.lua
@@ -1,7 +1,7 @@
 -- Create a custom frame to serve as the dialog
 local baseFrame = CreateFrame("Frame", "QuestieTutorialChooseObjectiveType", UIParent, BackdropTemplateMixin and "BackdropTemplate")
 baseFrame:SetSize(600, 400)
-baseFrame:SetPoint("CENTER", UIParent, 0, 0)
+baseFrame:SetPoint("CENTER", 0, 50)
 baseFrame:EnableMouse(true)
 baseFrame:SetMovable(true)
 baseFrame:RegisterForDrag("LeftButton")
@@ -15,7 +15,7 @@ baseFrame:SetBackdrop({
     insets = { left = 4, right = 4, top = 4, bottom = 4 },
 })
 baseFrame:SetBackdropColor(0, 0, 0, 1)
-baseFrame:SetBackdropBorderColor(1, 1, 1, 0)
+baseFrame:SetBackdropBorderColor(1, 1, 1, 1)
 --objectiveChooser:Hide()
 
 local titleText = baseFrame:CreateFontString(nil, "OVERLAY", "GameFontHighlight")
@@ -24,7 +24,7 @@ titleText:SetFont("Fonts\\FRIZQT__.TTF", 16)
 titleText:SetPoint("TOP", 0, -10)
 
 local customText = baseFrame:CreateFontString(nil, "OVERLAY", "GameFontNormal")
-customText:SetText("With Wotlk Phase 4 Blizzard introduced their own quest objective system.\n\nPlease choose the objective types you want to use:")
+customText:SetText("With Wotlk Phase 4 Blizzard introduced their own quest objective system.\n\nPlease choose the objective type you want to use:")
 customText:SetPoint("TOP", 0, -35)
 
 
@@ -41,7 +41,7 @@ onlyBlizzardImage:SetPoint("TOPRIGHT", -30, -90)
 local acceptOnlyQuestieButton = CreateFrame("Button", nil, baseFrame, "UIPanelButtonTemplate")
 acceptOnlyQuestieButton:SetText("Questie Objectives")
 acceptOnlyQuestieButton:SetSize(140, 24)
-acceptOnlyQuestieButton:SetPoint("BOTTOMLEFT", 85, 10)
+acceptOnlyQuestieButton:SetPoint("BOTTOMLEFT", 85, 20)
 acceptOnlyQuestieButton:SetScript("OnClick", function()
     -- TODO: Hide Blizzard objective markers
 end)
@@ -49,7 +49,7 @@ end)
 local acceptOnlyBlizzardButton = CreateFrame("Button", nil, baseFrame, "UIPanelButtonTemplate")
 acceptOnlyBlizzardButton:SetText("Blizzard Objectives")
 acceptOnlyBlizzardButton:SetSize(140, 24)
-acceptOnlyBlizzardButton:SetPoint("BOTTOMRIGHT", -85, 10)
+acceptOnlyBlizzardButton:SetPoint("BOTTOMRIGHT", -85, 20)
 acceptOnlyBlizzardButton:SetScript("OnClick", function()
     -- TODO: Disable Questies objectives marker
 end)
