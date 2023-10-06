480cfe1fe03e4645e024152763bc099c40b64748
BreakBB <b.boeing@online.de>
2023-10-06 14:38:32 +0200

    Minor cleanup

--- Modules/Compatibility.lua
+++ Modules/Compatibility.lua
@@ -1,13 +1,13 @@
 -- Add missing Seasons object, if not available (e.g. 1.14.0 and below is missing it)
 if not C_Seasons then
-    C_Seasons = {}
-    
-    C_Seasons.HasActiveSeason = function()
+    C_Seasons = {
+        HasActiveSeason = function()
             return false
-    end
-    C_Seasons.GetActiveSeason = function()
-        return 0
-    end
+        end,
+        GetActiveSeason = function()
+            return 0
+        end
+    }
 end
 
 -- Specific subclass of this mixin was added in a minor version and is missing in earlier patches, functionality this makes next to no visual difference
