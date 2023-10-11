bd3214414178c3e1641f2282cca44026f0b20540
BreakBB <b.boeing@online.de>
2023-10-11 14:19:01 +0200

    WIP Use Krowi_WorldMapButtons for world map button creation

--- Modules/WorldMapButton/WorldMapButton.lua
+++ Modules/WorldMapButton/WorldMapButton.lua
@@ -7,33 +7,24 @@ local l10n = QuestieLoader:ImportModule("l10n")
 ---@type QuestieQuest
 local QuestieQuest = QuestieLoader:ImportModule("QuestieQuest")
 
+local KButtons = LibStub("Krowi_WorldMapButtons-1.4")
+
 local mapButton
 
 function WorldMapButton.Initialize()
-    mapButton = CreateFrame("Button", "Questie_Toggle", WorldMapFrame, "UIPanelButtonTemplate")
+    mapButton = KButtons:Add("QuestieWorldMapButtonTemplate", "BUTTON")
 
-    mapButton:SetSize(120, 20)
-    mapButton:SetPoint("RIGHT", WorldMapFrame.MaximizeMinimizeFrame or WorldMapFrameCloseButton, "LEFT", 4, 0)
-    mapButton:SetFrameLevel(10)
-    mapButton:SetScript("OnClick", function()
-        Questie.db.char.enabled = (not Questie.db.char.enabled)
-        QuestieQuest:ToggleNotes(Questie.db.char.enabled)
-    end)
-
-    WorldMapButton.UpdateText()
-    WorldMapButton.Toggle(Questie.db.global.mapShowHideEnabled)
-
-    C_Timer.After(1, function()
-        WorldMapButton.UpdatePosition()
-    end)
+    Questie.WorldMap = {
+        Button = mapButton
+    }
 end
 
 function WorldMapButton.UpdateText()
-    if Questie.db.char.enabled then
-        mapButton:SetText(l10n("Hide Questie"))
-    else
-        mapButton:SetText(l10n("Show Questie"))
-    end
+    --if Questie.db.char.enabled then
+    --    mapButton:SetText(l10n("Hide Questie"))
+    --else
+    --    mapButton:SetText(l10n("Show Questie"))
+    --end
 end
 
 ---@param shouldShow boolean
@@ -46,19 +37,32 @@ function WorldMapButton.Toggle(shouldShow)
 end
 
 function WorldMapButton.UpdatePosition()
-    if (not WorldMapContinentDropDown:IsShown()) then
-        mapButton:ClearAllPoints()
-
-        if IsAddOnLoaded("Leatrix_Maps") then
-            if AtlasToggleFromWorldMap and AtlasToggleFromWorldMap:IsShown() then -- Atlas addon in Classic Era (see #1498)
-                mapButton:SetPoint('RIGHT', AtlasToggleFromWorldMap, 'LEFT', -16, 0)
-            elseif Krowi_WorldMapButtons1 and Krowi_WorldMapButtons1:IsShown() then -- Atlas addon in Wotlk
-                mapButton:SetPoint('RIGHT', Krowi_WorldMapButtons1, 'LEFT', -16, 0)
-            else
-                mapButton:SetPoint('RIGHT', WorldMapFrame.MaximizeMinimizeFrame or WorldMapFrameCloseButton, 'LEFT', 4, 0)
-            end
-        else
-            mapButton:SetPoint('RIGHT', WorldMapFrame.MaximizeMinimizeFrame or WorldMapFrameCloseButton, 'LEFT', 4, 0)
-        end
-    end
+    --if (not WorldMapContinentDropDown:IsShown()) then
+    --    mapButton:ClearAllPoints()
+    --
+    --    if IsAddOnLoaded("Leatrix_Maps") then
+    --        if AtlasToggleFromWorldMap and AtlasToggleFromWorldMap:IsShown() then -- Atlas addon in Classic Era (see #1498)
+    --            mapButton:SetPoint('RIGHT', AtlasToggleFromWorldMap, 'LEFT', -16, 0)
+    --        elseif Krowi_WorldMapButtons1 and Krowi_WorldMapButtons1:IsShown() then -- Atlas addon in Wotlk
+    --            mapButton:SetPoint('RIGHT', Krowi_WorldMapButtons1, 'LEFT', -16, 0)
+    --        else
+    --            mapButton:SetPoint('RIGHT', WorldMapFrame.MaximizeMinimizeFrame or WorldMapFrameCloseButton, 'LEFT', 4, 0)
+    --        end
+    --    else
+    --        mapButton:SetPoint('RIGHT', WorldMapFrame.MaximizeMinimizeFrame or WorldMapFrameCloseButton, 'LEFT', 4, 0)
+    --    end
+    --end
 end
+
+QuestieWorldMapButtonMixin = {
+    OnLoad = function() end,
+    OnMouseDown = function(button) print("button", button) end,
+    OnEnter = function() end,
+    OnLeave = function() end,
+    OnClick = function(clicked)
+        print("clicked", clicked)
+        Questie.db.char.enabled = (not Questie.db.char.enabled)
+        QuestieQuest:ToggleNotes(Questie.db.char.enabled)
+    end,
+    Refresh = function() end,
+}
