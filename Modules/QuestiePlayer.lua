70c943a016c6d18c92a67497f911904ae45b4d10
BreakBB <b.boeing@online.de>
2023-10-17 06:44:18 +0200

    [fix] Actually fix "You're not in a party" spam
    Related to 8925eee426ae127639db26650084224b2f1949ab

--- Modules/QuestiePlayer.lua
+++ Modules/QuestiePlayer.lua
@@ -72,7 +72,7 @@ end
 function QuestiePlayer:GetGroupType()
     if(UnitInRaid("player")) then
         return "raid";
-    elseif(IsInGroup(Enum.ChatChannelType.PublicParty)) then
+    elseif(IsInGroup(LE_PARTY_CATEGORY_INSTANCE)) then
         return "instance";
     elseif(UnitInParty("player")) then
         return "party";
