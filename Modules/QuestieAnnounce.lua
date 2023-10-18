2612435217973a7947e3296f92ac85f9025bb61f
BreakBB <b.boeing@online.de>
2023-10-19 00:54:07 +0200

    Improve previous commit to respect Era

--- Modules/QuestieAnnounce.lua
+++ Modules/QuestieAnnounce.lua
@@ -92,6 +92,16 @@ function _QuestieAnnounce:AnnounceEnabledAndPlayerInChannel()
     end
 end
 
+function _QuestieAnnounce.GetChatMessageChannel()
+    if IsInRaid() then
+        return "RAID"
+    elseif IsInGroup() then
+        return Questie.IsWotlk and "INSTANCE_CHAT" or "PARTY"
+    else
+        return "PARTY"
+    end
+end
+
 function _QuestieAnnounce:AnnounceToChannel(message)
     Questie:Debug(Questie.DEBUG_DEVELOP, "[QuestieAnnounce] raw msg: ", message)
     if (not message) or alreadySentBandaid[message] or Questie.db.global.questieShutUp then
@@ -101,7 +111,7 @@ function _QuestieAnnounce:AnnounceToChannel(message)
     alreadySentBandaid[message] = true
 
     if IsInRaid() or IsInGroup() then
-        SendChatMessage(message, (IsInRaid() and "RAID") or (IsInGroup(LE_PARTY_CATEGORY_INSTANCE) and "INSTANCE_CHAT") or (IsInGroup() and "PARTY"))
+        SendChatMessage(message, _QuestieAnnounce.GetChatMessageChannel())
     elseif Questie.db.char.questAnnounceLocally == true then
         Questie:Print(message)
     end
