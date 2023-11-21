5ae925254f5a7c826245b834d9de9e7eddbfee13
Ryan Squires <63078967+rsquires1988@users.noreply.github.com>
2023-11-21 14:24:58 -0800

    rearrange for readability, comments

--- Modules/Tracker/TrackerExpansionStateToggle.lua
+++ Modules/Tracker/TrackerExpansionStateToggle.lua
@@ -10,20 +10,25 @@ local TrackerBaseFrame = QuestieLoader:ImportModule("TrackerBaseFrame")
 
 local TrackerCollapseBtn
 
-function TrackerExpansionStateToggle.Initialize()
+function TrackerExpansionStateToggle.Initialize() -- Called by QuestieTracker.lua
     -- Create a secure button to execute TrackerExpansionStateToggle with ESC
     TrackerCollapseBtn = CreateFrame("Button", "TESTButton", UIParent, "SecureActionButtonTemplate")
     TrackerCollapseBtn:SetAttribute("type", "macro")
     TrackerCollapseBtn:SetAttribute("macrotext", "/run G_TrackerExpansionStateToggle()")
 end
 
+-- Create button for Escape functionality
 function TrackerExpansionStateToggle.CreateButton()
     if QuestieTracker and Questie.db.char.isTrackerExpanded and Questie.db.global.useEscapeKeyForTracker then
         SetOverrideBinding(TrackerCollapseBtn, false, "ESCAPE", "CLICK TESTButton:LeftButton")
     end
 end
 
--- Toggles the Questie tracker expansion state (called by the keybind)
+function G_TrackerCreateButton() -- Global called by QuestieOptionsTracker.lua
+    TrackerExpansionStateToggle:CreateButton()
+end
+
+-- Toggles the Questie tracker expansion state
 function TrackerExpansionStateToggle.Toggle()
     if UnitAffectingCombat("player") then
         return
@@ -37,29 +42,21 @@ function TrackerExpansionStateToggle.Toggle()
     end
 end
 
-function G_TrackerCreateButton()
-    TrackerExpansionStateToggle:CreateButton()
-end
-
--- Global function to toggle the tracker
-function G_TrackerExpansionStateToggle()
+function G_TrackerExpansionStateToggle() -- Global called by Bindings.xml
     TrackerExpansionStateToggle:Toggle()
 end
 
--- Event frame for detecting combat state changes
+-- Detect combat state changes
 local eventFrame = CreateFrame("Frame")
-eventFrame:RegisterEvent("PLAYER_REGEN_DISABLED") -- Entering combat
-eventFrame:RegisterEvent("PLAYER_REGEN_ENABLED")  -- Leaving combat
+eventFrame:RegisterEvent("PLAYER_REGEN_DISABLED")
+eventFrame:RegisterEvent("PLAYER_REGEN_ENABLED")
 
--- Event handler
 eventFrame:SetScript("OnEvent", function(self, event)
-    if event == "PLAYER_REGEN_DISABLED" then
-        -- Player entered combat
+    if event == "PLAYER_REGEN_DISABLED" then -- Player entered combat
         if QuestieTracker and Questie.db.char.isTrackerExpanded then
             ClearOverrideBindings(TrackerCollapseBtn)
         end
-    elseif event == "PLAYER_REGEN_ENABLED" then
-        -- Player left combat
+    elseif event == "PLAYER_REGEN_ENABLED" then -- Player left combat
         TrackerExpansionStateToggle.CreateButton()
     end
 end)
