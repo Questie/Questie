a10c93ec55c44f7d0b2961ffd4745ee96abc6b55
BreakBB <b.boeing@online.de>
2023-11-23 08:36:17 +0100

    Add tooltip

--- Modules/WorldMapButton/WorldMapButton.lua
+++ Modules/WorldMapButton/WorldMapButton.lua
@@ -4,6 +4,8 @@ local WorldMapButton = QuestieLoader:CreateModule("WorldMapButton")
 
 ---@type l10n
 local l10n = QuestieLoader:ImportModule("l10n")
+---@type QuestieLib
+local QuestieLib = QuestieLoader:ImportModule("QuestieLib")
 ---@type QuestieQuest
 local QuestieQuest = QuestieLoader:ImportModule("QuestieQuest")
 ---@type QuestieMenu
@@ -58,7 +60,6 @@ end
 
 QuestieWorldMapButtonMixin = {
     OnLoad = function() end,
-    OnEnter = function() end,
     OnHide = function() end,
     OnMouseDown = function(_, button)
         if button == "LeftButton" then
@@ -69,6 +70,13 @@ QuestieWorldMapButtonMixin = {
         end
     end,
     OnMouseUp = function() end,
+    OnEnter = function(self)
+        GameTooltip:SetOwner(self, "ANCHOR_CURSOR");
+        GameTooltip:AddLine("Questie ".. QuestieLib:GetAddonVersionString(), 1, 1, 1);
+        GameTooltip:AddLine(Questie:Colorize(l10n('Left Click') , 'gray') .. ": ".. l10n('Toggle Questie'));
+        GameTooltip:AddLine(Questie:Colorize(l10n('Right Click') , 'gray') .. ": ".. l10n('Toggle Menu'));
+        GameTooltip:Show()
+    end,
     OnLeave = function() end,
     OnClick = function() end, -- Only fires on left click
     Refresh = function() end,
