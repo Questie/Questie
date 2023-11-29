7c4abbae578b69eb86967094fdd49839782c65e7
Cabro <1155017+Cabro@users.noreply.github.com>
2023-11-29 15:37:32 +0200

    [locale] Missing a string for "A Suitable Disguise"

--- Localization/Translations/ExtraObjectives/WotlkObjectives.lua
+++ Localization/Translations/ExtraObjectives/WotlkObjectives.lua
@@ -2870,6 +2870,18 @@ local wotlkObjectiveLocales = {
         ["zhTW"] = false,
         ["zhCN"] = false,
     },
+    ["Ask Shandy Glossgleam to lend you a tabard"] = { -- 20438 24556
+        ["ptBR"] = false,
+        ["ruRU"] = false,
+        ["deDE"] = false,
+        ["koKR"] = false,
+        ["esMX"] = false,
+        ["enUS"] = true,
+        ["frFR"] = false,
+        ["esES"] = false,
+        ["zhTW"] = false,
+        ["zhCN"] = false,
+    },
 }
 
 for k, v in pairs(wotlkObjectiveLocales) do
