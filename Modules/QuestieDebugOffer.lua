c9e5da073012dac1d371c582e4c02941488242c1
Yttrium tYcLief <tyclief@gmail.com>
2023-11-30 18:45:37 -0500

    fix previous silly illegal code

--- Modules/QuestieDebugOffer.lua
+++ Modules/QuestieDebugOffer.lua
@@ -102,7 +102,7 @@ end
 -- Missing questID when conversing
 function QuestieDebugOffer.QuestDialog()
     local questID = GetQuestID() -- obtain quest ID from dialog
-    if not questID > 0 then
+    if not (questID > 0) then
         return -- invalid data from API, abandon offer attempt
     end
     if QuestieDB.QueryQuestSingle(questID, "name") == nil then -- if ID not in our DB
