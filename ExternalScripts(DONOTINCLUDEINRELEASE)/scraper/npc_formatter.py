bb3dcbf9d1924d194045d9bce39680cb27e32885
BreakBB <b.boeing@online.de>
2023-12-02 17:48:18 +0100

    First step for NPCs

---
+++ ExternalScripts(DONOTINCLUDEINRELEASE)/scraper/npc_formatter.py
@@ -0,0 +1,45 @@
+import json
+from pathlib import Path
+
+
+class NPCFormatter:
+
+    def __call__(self, **kwargs):
+        self.__format()
+
+    def __format(self) -> None:
+        npc_input = self.__load_json_file("npc_data.json")
+        with Path("npc_data.lua").open("w", encoding="utf-8") as g:
+            g.write("return {\n")
+            for item in npc_input:
+                g.write("\t[{id}] = {{\n".format(id=item["npcId"]))
+                g.write("\t\t[npcKeys.name] = \"{name}\",\n".format(name=item["name"]))
+                g.write("\t\t[npcKeys.minLevel] = {min_level},\n".format(min_level=item["minLevel"]))
+                g.write("\t\t[npcKeys.maxLevel] = {max_level},\n".format(max_level=item["maxLevel"]))
+                g.write("\t\t[npcKeys.zoneID] = {zone_id},\n".format(zone_id=item["zoneId"]))
+                g.write("\t\t[npcKeys.friendlyToFaction] = \"{friendly_to}\",\n".format(friendly_to=self.__get_race_string(item["reactAlliance"], item["reactHorde"])))
+                g.write("\t},\n")
+            g.write("}\n")
+
+    def __load_json_file(self, file_name: str):
+        print("Loading '{}'...".format(file_name))
+        with Path(file_name).open("r", encoding="utf-8") as f:
+            data = json.load(f)
+        print("Data contains {} entries".format(len(data)))
+        return data
+
+    def __get_race_string(self, react_alliance: str, react_horde) -> str:
+        friendly_to = ""
+        if react_alliance == "1":
+            friendly_to += "A"
+        if react_horde == "1":
+            friendly_to += "H"
+        if not friendly_to:
+            friendly_to = "nil"
+
+        return friendly_to
+
+
+if __name__ == '__main__':
+    formatter = NPCFormatter()
+    formatter()
--- ExternalScripts(DONOTINCLUDEINRELEASE)/scraper/runner.py
+++ ExternalScripts(DONOTINCLUDEINRELEASE)/scraper/runner.py
@@ -3,6 +3,7 @@ from pathlib import Path
 
 from scrapy.crawler import CrawlerProcess
 
+from spiders.npc_spider import NPCSpider
 from spiders.quest_spider import QuestSpider
 
 
@@ -11,6 +12,7 @@ class Runner:
     def __init__(self) -> None:
         self.logger = getLogger(__name__)
         Path("quest_data.json").unlink(missing_ok=True)
+        Path("npc_data.json").unlink(missing_ok=True)
 
     def run(self) -> None:
         process = CrawlerProcess(settings={
@@ -18,11 +20,16 @@ class Runner:
             "FEED_EXPORT_ENCODING": "utf-8",
             "FEED_FORMAT": "json",
             "CONCURRENT_REQUESTS": 32,
-            "FEED_URI": "quest_data.json",
+            "FEED_URI": "npc_data.json",
             "COOKIES_ENABLED": False
         })
 
-        process.crawl(QuestSpider)
+        # process.settings.attributes["FEED_URI"].value = "json"
+        # Path("quest_data.json").unlink(missing_ok=True)
+        # process.crawl(QuestSpider)
+        # process.settings.attributes["FEEDS"].value = "npc_data.json"
+        # Path("npc_data.json").unlink(missing_ok=True)
+        process.crawl(NPCSpider)
 
         process.start()
 
