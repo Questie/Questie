772a75deebe8adc44722e48e43bfeb6367ebce40
BreakBB <b.boeing@online.de>
2023-12-06 11:35:34 +0100

    Extract more functions

--- ExternalScripts(DONOTINCLUDEINRELEASE)/scraper/quest/quest_spider.py
+++ ExternalScripts(DONOTINCLUDEINRELEASE)/scraper/quest/quest_spider.py
@@ -20,22 +20,25 @@ class QuestSpider(scrapy.Spider):
         result = {}
         for script in response.xpath('//script/text()').extract():
             if script.startswith('//<![CDATA[\nWH.Gatherer.addData') and script.endswith('//]]>'):
-                result["questId"] = re.search(r'g_quests\[(\d+)\]', script).group(1)
+                result["questId"] = re.search(r'g_quests\[(\d+)]', script).group(1)
                 result["name"] = re.search(r'"name":"([^"]+)"', script).group(1)
-                level_match = re.search(r'"level":(\d+)', script)
-                result["level"] = level_match.group(1) if level_match else 0
-                req_level_match = re.search(r'"reqlevel":(\d+)', script)
-                result["reqLevel"] = req_level_match.group(1) if req_level_match else 0
+                result["level"] = self.__match_level(re.search(r'"level":(\d+)', script))
+                result["reqLevel"] = self.__match_level(re.search(r'"reqlevel":(\d+)', script))
                 result["reqClass"] = re.search(r'"reqclass":(\d+)', script).group(1)
                 result["reqRace"] = re.search(r'"reqrace":(\d+)', script).group(1)
             if script.lstrip().startswith('WH.markup'):
-                start_match = re.search(r'Start:.*?npc=(\d+)', script)
-                result["start"] = start_match.group(1) if start_match else "nil"
-                result["end"] = self.__find_end(script)
+                result["start"] = self.__match_start(re.search(r'Start:.*?npc=(\d+)', script))
+                result["end"] = self.__match_end(script)
         if result:
             yield result
 
-    def __find_end(self, script):
+    def __match_level(self, level_match):
+        return level_match.group(1) if level_match else 0
+
+    def __match_start(self, start_match):
+        return start_match.group(1) if start_match else "nil"
+
+    def __match_end(self, script):
         end_match = re.findall(r'End:.*?npc=(\d+)', script)
 
         if end_match:
