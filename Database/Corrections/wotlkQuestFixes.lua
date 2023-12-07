8cecbe4f7368dfeedabb142ac9e3f957573e8af3
Cabro <1155017+Cabro@users.noreply.github.com>
2023-12-07 15:24:11 +0200

    [quest] Portable Seismograph should show up in QIB

--- Database/Corrections/wotlkQuestFixes.lua
+++ Database/Corrections/wotlkQuestFixes.lua
@@ -1148,6 +1148,9 @@ function QuestieWotlkQuestFixes:Load()
         [12007] = {
             [questKeys.extraObjectives] = {{nil, Questie.ICON_TYPE_EVENT, l10n("Drink Drakuru's Elixir after gathering Zim'bo's Mojo"),0,{{"object", 400047}}}},
         },
+        [12014] = {
+            [questKeys.sourceItemId] = 35837,
+        },
         [12017] = {
             [questKeys.extraObjectives] = {{nil, Questie.ICON_TYPE_EVENT, l10n("Use Tu'u'gwar's Bait"),0,{{"object", 188370}}}},
         },
