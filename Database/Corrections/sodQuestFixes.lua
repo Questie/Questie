9da67bb87fea35da9023a5f81501141cca60f127
Björn Böing <b.boeing@online.de>
2023-12-12 09:48:29 +0100

    [quest] Add SoD Waylaid quests
    * Add extra layer for generated SoD data
    
    * Load SoD base entries
    
    * Add "Waylaid Shipment" quests
    
    * Fix waylaid NPC locations
    
    * Fix previous commit

--- Database/Corrections/sodQuestFixes.lua
+++ Database/Corrections/sodQuestFixes.lua
@@ -13,6 +13,7 @@ function SeasonOfDiscovery:LoadQuests()
     local raceIDs = QuestieDB.raceKeys
     local classIDs = QuestieDB.classKeys
     local sortKeys = QuestieDB.sortKeys
+    local specialFlags = QuestieDB.specialFlags
     local profKeys = QuestieProfessions.professionKeys
     local specKeys = QuestieProfessions.specializationKeys
 
@@ -26,6 +27,35 @@ function SeasonOfDiscovery:LoadQuests()
             [questKeys.objectives] = {nil, nil, nil, nil, nil, {{403919, nil, 205230}}},
             [questKeys.zoneOrSort] = sortKeys.WARLOCK,
             [questKeys.requiredSpell] = -403919,
+        },
+        [78611] = {
+            [questKeys.startedBy] = {{213077,214070,214096,214098,214099,214101,}},
+            [questKeys.specialFlags] = specialFlags.REPEATABLE,
+        },
+        [78612] = {
+            [questKeys.startedBy] = {{213077,214070,214096,214098,214099,214101,}},
+            [questKeys.specialFlags] = specialFlags.REPEATABLE,
+        },
+        [78872] = {
+            [questKeys.startedBy] = {{213077,214070,214096,214098,214099,214101,}},
+            [questKeys.specialFlags] = specialFlags.REPEATABLE,
+        },
+        [79100] = {
+            [questKeys.startedBy] = {{213077,214070,214096,214098,214099,214101,}},
+            [questKeys.specialFlags] = specialFlags.REPEATABLE,
+        },
+        [79101] = {
+            [questKeys.startedBy] = {{213077,214070,214096,214098,214099,214101,}},
+            [questKeys.specialFlags] = specialFlags.REPEATABLE,
+        },
+        [79102] = {
+            [questKeys.startedBy] = {{213077,214070,214096,214098,214099,214101,}},
+            [questKeys.specialFlags] = specialFlags.REPEATABLE,
+        },
+        [79103] = {
+            [questKeys.startedBy] = {{213077,214070,214096,214098,214099,214101,}},
+            [questKeys.finishedBy] = {{213077,214070,214096,214098,214099,214101,}},
+            [questKeys.specialFlags] = specialFlags.REPEATABLE,
         },
     }
 end
