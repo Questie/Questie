1ad12dd4cd9c0ecd864669a2d6aeca5a16e0aea3
BreakBB <b.boeing@online.de>
2023-12-12 09:45:12 +0100

    Fix waylaid NPC locations

--- Database/Corrections/sodNPCFixes.lua
+++ Database/Corrections/sodNPCFixes.lua
@@ -11,5 +11,30 @@ function SeasonOfDiscovery:LoadNPCs()
     local npcFlags = QuestieDB.npcFlags
     local waypointPresets = QuestieDB.waypointPresets
 
-    return {}
+    return {
+        [213077] = {
+            [npcKeys.spawns] = {[zoneIDs.STORMWIND_CITY] = {{54.4,61.2}}},
+            [npcKeys.friendlyToFaction] = "A",
+        },
+        [214070] = {
+            [npcKeys.spawns] = {[zoneIDs.ORGRIMMAR] = {{51.4,63.8}}},
+            [npcKeys.friendlyToFaction] = "H",
+        },
+        [214096] = {
+            [npcKeys.spawns] = {[zoneIDs.THUNDER_BLUFF] = {{39.2,53.4}}},
+            [npcKeys.friendlyToFaction] = "H",
+        },
+        [214098] = {
+            [npcKeys.spawns] = {[zoneIDs.UNDERCITY] = {{64,39.2}}},
+            [npcKeys.friendlyToFaction] = "H",
+        },
+        [214098] = {
+            [npcKeys.spawns] = {[zoneIDs.IRONFORGE] = {{24.4,67.6}}},
+            [npcKeys.friendlyToFaction] = "A",
+        },
+        [214101] = {
+            [npcKeys.spawns] = {[zoneIDs.DARNASSUS] = {{59.8,56.4}}},
+            [npcKeys.friendlyToFaction] = "A",
+        },
+    }
 end
