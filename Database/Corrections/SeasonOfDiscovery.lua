93acb4b28ed9d63523fb3226b5fadf4cae62e699
Emil Lindegaard <emillindegaardjensen@hotmail.com>
2023-12-17 21:33:40 +0100

    [quest] Add warlock "Demonic Tactics" rune

--- Database/Corrections/SeasonOfDiscovery.lua
+++ Database/Corrections/SeasonOfDiscovery.lua
@@ -102,6 +102,8 @@ local runeQuestsInSoD = {-- List quests here to have them flagged as Rune quests
     [90062] = true,  -- Shadow Bolt Volley The Barrens end
     [90063] = true,  -- Shadow Bolt Volley Silverpine Forest
     [90064] = true,  -- Incinerate
+    [90065] = true,  -- Demonic Pact Alliance
+    [90066] = true,  -- Demonic Pact Horde
     --- Warlock runes end
     [90040] = true, -- Paladin Beacon of Light Alliance
     [90041] = true, -- Shaman Healing Rain Horde
