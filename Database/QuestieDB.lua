7d21e57b47ac52bd402924e69e592fef090346cf
BreakBB <b.boeing@online.de>
2023-12-26 23:25:13 +0100

    [quest] Mark "Living Seed" rune as Elite quest

--- Database/QuestieDB.lua
+++ Database/QuestieDB.lua
@@ -94,6 +94,7 @@ local questTagCorrections = {
     [12437] = {41, "PvP"},
     [12443] = {41, "PvP"},
     [12446] = {41, "PvP"},
+    [90054] = {1, "Elite"},
 }
 
 -- race bitmask data, for easy access
