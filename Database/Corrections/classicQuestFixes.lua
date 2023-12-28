68d8c8bc4f716ee2b79b52464ed0c0a2e2cce1f4
BreakBB <b.boeing@online.de>
2023-12-28 14:23:51 +0100

    Fix previous commit

--- Database/Corrections/classicQuestFixes.lua
+++ Database/Corrections/classicQuestFixes.lua
@@ -862,7 +862,7 @@ function QuestieQuestFixes:Load()
             [questKeys.triggerEnd] = {"Go to the bank in Darnassus, otherwise known as the Bough of the Eternals.", {[zoneIDs.DARNASSUS]={{41.31,43.54}}}},
         },
         [1480] = {
-            [questKeys.startedBy] = {{4663,4664,4665,4666,4667,4668,4705,13019,300033},nil,{20310}},
+            [questKeys.startedBy] = {{4663,4664,4665,4666,4667,4668,4705,13019,300034},nil,{20310}},
         },
         [1483] = {
             [questKeys.exclusiveTo] = {1093},
