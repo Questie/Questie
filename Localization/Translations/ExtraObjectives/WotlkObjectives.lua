0149a44776cef663b4c6f9856fc171afe6ace048
Cabro <1155017+Cabro@users.noreply.github.com>
2023-12-29 20:16:59 +0200

    [quest] Fixed various quests in the ICC chains

--- Localization/Translations/ExtraObjectives/WotlkObjectives.lua
+++ Localization/Translations/ExtraObjectives/WotlkObjectives.lua
@@ -3362,6 +3362,18 @@ local wotlkObjectiveLocales = {
         ["zhTW"] = false,
         ["zhCN"] = false,
     },
+    ["Free the slave"] = { -- 24498 24507
+        ["ptBR"] = false,
+        ["ruRU"] = false,
+        ["deDE"] = false,
+        ["koKR"] = false,
+        ["esMX"] = false,
+        ["enUS"] = true,
+        ["frFR"] = false,
+        ["esES"] = false,
+        ["zhTW"] = false,
+        ["zhCN"] = false,
+    },
 }
 
 for k, v in pairs(wotlkObjectiveLocales) do
