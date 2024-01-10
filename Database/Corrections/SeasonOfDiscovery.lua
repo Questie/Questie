0dda720d22ec3c0cbfec4128f157591eec250e67
Sommos <Sommosgaming@gmail.com>
2024-01-10 21:28:07 +0000

    REMOVED: QuestID From Blacklist As Spawn Points Have Been Manually Set

--- Database/Corrections/SeasonOfDiscovery.lua
+++ Database/Corrections/SeasonOfDiscovery.lua
@@ -285,7 +285,6 @@ local questsToBlacklistBySoDPhase = {
         [90145] = true, -- Hiding Druid Lacerate Darkshore for now as there are too many icons
         [90146] = true, -- Hiding Druid Mangle Mulgore for now as there are too many icons
         [90147] = true, -- Hiding Paladin Hand of Reckoning Westfall for now as there are too many icons
-        [90150] = true, -- Hiding Hunter Sniper Training Darkshore for now as there are too many icons
     },
     [2] = { -- SoD Phase 2 - level cap 40
         [1152] = true, -- Test of Lore; minLevel raised to 26 in P1 for some reason, might be retooled as part of P2?
