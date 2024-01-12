726d609043348181ee137f19c1b36f83869d67c7
Sommos <Sommosgaming@gmail.com>
2024-01-12 07:19:59 +0000

    ADDED: 'Friend of the Library' Mage Rune Quest To QuestDB

--- Database/Corrections/Automatic/sodBaseQuests.lua
+++ Database/Corrections/Automatic/sodBaseQuests.lua
@@ -481,7 +481,7 @@ function SeasonOfDiscovery:LoadBaseQuests()
         [78124] = {
             [questKeys.name] = "Nar'thalas Almanac",
             [questKeys.startedBy] = {{211033}},
-            [questKeys.finishedBy] = {{211022,211033,}},
+            [questKeys.finishedBy] = nil,
             [questKeys.requiredLevel] = 1,
             [questKeys.questLevel] = 20,
             [questKeys.requiredRaces] = raceIDs.NONE,
@@ -490,7 +490,7 @@ function SeasonOfDiscovery:LoadBaseQuests()
         [78127] = {
             [questKeys.name] = "The Dalaran Digest",
             [questKeys.startedBy] = {{211033}},
-            [questKeys.finishedBy] = {{211022,211033,}},
+            [questKeys.finishedBy] = nil,
             [questKeys.requiredLevel] = 1,
             [questKeys.questLevel] = 20,
             [questKeys.requiredRaces] = raceIDs.NONE,
@@ -529,7 +529,7 @@ function SeasonOfDiscovery:LoadBaseQuests()
         [78142] = {
             [questKeys.name] = "Bewitchments and Glamours",
             [questKeys.startedBy] = {{211033}},
-            [questKeys.finishedBy] = {{211022,211033,}},
+            [questKeys.finishedBy] = nil,
             [questKeys.requiredLevel] = 1,
             [questKeys.questLevel] = 20,
             [questKeys.requiredRaces] = raceIDs.NONE,
@@ -538,7 +538,7 @@ function SeasonOfDiscovery:LoadBaseQuests()
         [78143] = {
             [questKeys.name] = "Secrets of the Dreamers",
             [questKeys.startedBy] = {{211033}},
-            [questKeys.finishedBy] = {{211022,211033,}},
+            [questKeys.finishedBy] = nil,
             [questKeys.requiredLevel] = 1,
             [questKeys.questLevel] = 20,
             [questKeys.requiredRaces] = raceIDs.NONE,
@@ -558,7 +558,7 @@ function SeasonOfDiscovery:LoadBaseQuests()
         [78145] = {
             [questKeys.name] = "Arcanic Systems Manual",
             [questKeys.startedBy] = {{211033}},
-            [questKeys.finishedBy] = {{211022,211033,}},
+            [questKeys.finishedBy] = nil,
             [questKeys.requiredLevel] = 1,
             [questKeys.questLevel] = 20,
             [questKeys.requiredRaces] = raceIDs.NONE,
@@ -567,7 +567,7 @@ function SeasonOfDiscovery:LoadBaseQuests()
         [78146] = {
             [questKeys.name] = "Goaz Scrolls",
             [questKeys.startedBy] = {{211033}},
-            [questKeys.finishedBy] = {{211022,211033,}},
+            [questKeys.finishedBy] = nil,
             [questKeys.requiredLevel] = 1,
             [questKeys.questLevel] = 20,
             [questKeys.requiredRaces] = raceIDs.NONE,
@@ -576,7 +576,7 @@ function SeasonOfDiscovery:LoadBaseQuests()
         [78147] = {
             [questKeys.name] = "Crimes Against Anatomy",
             [questKeys.startedBy] = {{211033}},
-            [questKeys.finishedBy] = {{211022,211033,}},
+            [questKeys.finishedBy] = nil,
             [questKeys.requiredLevel] = 1,
             [questKeys.questLevel] = 20,
             [questKeys.requiredRaces] = raceIDs.NONE,
@@ -585,7 +585,7 @@ function SeasonOfDiscovery:LoadBaseQuests()
         [78148] = {
             [questKeys.name] = "Runes of the Sorceror-Kings",
             [questKeys.startedBy] = {{211033}},
-            [questKeys.finishedBy] = {{211022,211033,}},
+            [questKeys.finishedBy] = nil,
             [questKeys.requiredLevel] = 1,
             [questKeys.questLevel] = 20,
             [questKeys.requiredRaces] = raceIDs.NONE,
@@ -594,6 +594,15 @@ function SeasonOfDiscovery:LoadBaseQuests()
         [78149] = {
             [questKeys.name] = "Fury of the Land",
             [questKeys.startedBy] = {{211033}},
+            [questKeys.finishedBy] = nil,
+            [questKeys.requiredLevel] = 1,
+            [questKeys.questLevel] = 20,
+            [questKeys.requiredRaces] = raceIDs.NONE,
+            [questKeys.requiredClasses] = classIDs.MAGE,
+        },
+        [78150] = {
+            [questKeys.name] = "Friend of the Library",
+            [questKeys.startedBy] = {{211033,211022,}},
             [questKeys.finishedBy] = {{211022,211033,}},
             [questKeys.requiredLevel] = 1,
             [questKeys.questLevel] = 20,
@@ -1167,7 +1176,7 @@ function SeasonOfDiscovery:LoadBaseQuests()
         [79091] = {
             [questKeys.name] = "Archmage Antonidas: The Unabridged Autobiography",
             [questKeys.startedBy] = {{211033}},
-            [questKeys.finishedBy] = {{211033,}},
+            [questKeys.finishedBy] = nil,
             [questKeys.requiredLevel] = 1,
             [questKeys.questLevel] = 20,
             [questKeys.requiredRaces] = raceIDs.NONE,
@@ -1176,7 +1185,7 @@ function SeasonOfDiscovery:LoadBaseQuests()
         [79092] = {
             [questKeys.name] = "Archmage Theocritus's Research Journal",
             [questKeys.startedBy] = {{211033}},
-            [questKeys.finishedBy] = {{211033,}},
+            [questKeys.finishedBy] = nil,
             [questKeys.requiredLevel] = 1,
             [questKeys.questLevel] = 20,
             [questKeys.requiredRaces] = raceIDs.NONE,
@@ -1185,7 +1194,7 @@ function SeasonOfDiscovery:LoadBaseQuests()
         [79093] = {
             [questKeys.name] = "Rumi of Gnomeregan: The Collected Works",
             [questKeys.startedBy] = {{211033}},
-            [questKeys.finishedBy] = {{211033,}},
+            [questKeys.finishedBy] = nil,
             [questKeys.requiredLevel] = 1,
             [questKeys.questLevel] = 20,
             [questKeys.requiredRaces] = raceIDs.NONE,
@@ -1194,7 +1203,7 @@ function SeasonOfDiscovery:LoadBaseQuests()
         [79094] = {
             [questKeys.name] = "The Lessons of Ta'zo",
             [questKeys.startedBy] = {{211022}},
-            [questKeys.finishedBy] = {{211022,}},
+            [questKeys.finishedBy] = nill,
             [questKeys.requiredLevel] = 1,
             [questKeys.questLevel] = 20,
             [questKeys.requiredRaces] = raceIDs.NONE,
@@ -1203,7 +1212,7 @@ function SeasonOfDiscovery:LoadBaseQuests()
         [79095] = {
             [questKeys.name] = "The Apothecary's Metaphysical Primer",
             [questKeys.startedBy] = {{211022}},
-            [questKeys.finishedBy] = {{211022,}},
+            [questKeys.finishedBy] = nill,
             [questKeys.requiredLevel] = 1,
             [questKeys.questLevel] = 20,
             [questKeys.requiredRaces] = raceIDs.NONE,
@@ -1212,7 +1221,7 @@ function SeasonOfDiscovery:LoadBaseQuests()
         [79096] = {
             [questKeys.name] = "Ataeric: On Arcane Curiosities",
             [questKeys.startedBy] = {{211022}},
-            [questKeys.finishedBy] = {{211022,}},
+            [questKeys.finishedBy] = nill,
             [questKeys.requiredLevel] = 1,
             [questKeys.questLevel] = 20,
             [questKeys.requiredRaces] = raceIDs.NONE,
@@ -1221,7 +1230,7 @@ function SeasonOfDiscovery:LoadBaseQuests()
         [79097] = {
             [questKeys.name] = "Baxtan: On Destructive Magics",
             [questKeys.startedBy] = {{211033}},
-            [questKeys.finishedBy] = {{211022,211033,}},
+            [questKeys.finishedBy] = nil,
             [questKeys.requiredLevel] = 1,
             [questKeys.questLevel] = 20,
             [questKeys.requiredRaces] = raceIDs.NONE,
