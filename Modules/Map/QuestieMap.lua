2fef762508efe6a0242045d008202eb45855e376
Sommos <Sommosgaming@gmail.com>
2024-01-22 07:48:23 +0000

    [npc] ADDED: Weapon Skills To Weapon Trainer NPCs

--- Modules/Map/QuestieMap.lua
+++ Modules/Map/QuestieMap.lua
@@ -359,6 +359,27 @@ function QuestieMap:ShowNPC(npcID, icon, scale, title, body, disableShiftToRemov
     data.IsObjectiveNote = false
     data.ManualTooltipData = {}
     data.ManualTooltipData.Title = title or (npc.name .. " (" .. l10n("NPC") .. ")")
+    local weaponMasterSkills = {
+        -- Alliance Weapon Trainers
+        [11865] = {"Fist Weapons", "Guns", "One-Handed Axes", "One-Handed Maces", "Two-Handed Axes", "Two-Handed Maces"},
+        [11866] = {"Bows", "Daggers", "Fist Weapons", "Staves", "Thrown"},
+        [11867] = {"Crossbows", "Daggers", "One-Handed Swords", "Polearms", "Staves", "Two-Handed Swords"},
+        [13084] = {"Crossbows", "Daggers", "Thrown"},
+        -- Horde Weapon Trainers
+        [2704] = {"Bows", "One-Handed Axes", "Staves", "Thrown", "Two-Handed Axes"},
+        [11868] = {"Bows", "Daggers", "Fist Weapons", "One-Handed Axes", "Thrown", "Two-Handed Axes"},
+        [11869] = {"Guns", "One-Handed Maces", "Staves", "Two-Handed Maces"},
+        [11870] = {"Crossbows", "Daggers", "One-Handed Swords", "Polearms", "Two-Handed Swords"},
+    }
+    if npc.subName == "Weapon Master" then
+        local skills = weaponMasterSkills[data.id]
+        if skills then
+            data.ManualToolTipData.Title = data.ManualToolTipData.Title .. "\n Trains The Following Skills:"
+            for _, skill in ipairs(skills) do
+                data.ManualTooltipData.Title = data.ManualTooltipData.Title .. "\n - " .. skill
+            end
+        end
+    end
     local level = tostring(npc.minLevel)
     local health = tostring(npc.minLevelHealth)
     if npc.minLevel ~= npc.maxLevel then
