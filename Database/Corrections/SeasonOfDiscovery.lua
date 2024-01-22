ffade6e89c89378a6c9765f626b7a05e8267374e
Sommos <Sommosgaming@gmail.com>
2024-01-22 18:17:04 +0000

    [quest] ADDED: Direct Spawn Points For Leprithus

--- Database/Corrections/SeasonOfDiscovery.lua
+++ Database/Corrections/SeasonOfDiscovery.lua
@@ -336,7 +336,6 @@ local questsToBlacklistBySoDPhase = {
         [90182] = true, -- Hiding Priest Homunculi Dun Morogh for now as there are too many icons
         [90183] = true, -- Hiding Priest Homunculi Elwynn Forest for now as there are too many icons
         [90184] = true, -- Hiding Priest Homunculi Teldrassil for now as there are too many icons
-        [90187] = true, -- Hiding Priest Shadow Word: Death Westfall for now as there are too many icons
     },
     [2] = { -- SoD Phase 2 - level cap 40
         [1152] = true, -- Test of Lore; minLevel raised to 26 in P1 for some reason, might be retooled as part of P2?
