6fc9de4772926316021d3e678147e5bc95efd8c2
Sommos <Sommosgaming@gmail.com>
2024-01-22 08:25:09 +0000

    [quest] ADDED: 'Shadow Word: Death' Darkshore Priest Fake Rune Quest To QuestDB

--- Database/Corrections/sodQuestFixes.lua
+++ Database/Corrections/sodQuestFixes.lua
@@ -2928,6 +2928,18 @@ function SeasonOfDiscovery:LoadQuests()
             [questKeys.requiredSpell] = -402852,
             [questKeys.zoneOrSort] = sortKeys.PRIEST,
         },
+        [90191] = {
+            [questKeys.name] = "Shadow Word: Death",
+            [questKeys.startedBy] = {nil,{414646}},
+            [questKeys.finishedBy] = nil,
+            [questKeys.requiredLevel] = 1,
+            [questKeys.questLevel] = 12,
+            [questKeys.requiredRaces] = raceIDs.NONE,
+            [questKeys.requiredClasses] = classIDs.PRIEST,
+            [questKeys.objectivesText] = {"Loot the Remnant on the island and receive the rune."},
+            [questKeys.requiredSpell] = -402849,
+            [questKeys.zoneOrSort] = sortKeys.PRIEST,
+        },
     }
 end
 
