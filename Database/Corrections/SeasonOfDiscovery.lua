dfc680893f36c4b38a26c601b71d1b820bac4dc1
Samuel <Sommosgaming@gmail.com>
2024-01-25 06:40:24 +0000

    [quest] Add Shaman's "Lava Burst" rune (Thanks @Sommos)

--- Database/Corrections/SeasonOfDiscovery.lua
+++ Database/Corrections/SeasonOfDiscovery.lua
@@ -310,6 +310,7 @@ local runeQuestsInSoD = {-- List quests here to have them flagged as Rune quests
     [90202] = true, -- Shaman Shamanistic Rage Stonetalon Mountains
     [90203] = true, -- Shaman Way of Earth The Barrens
     [90204] = true, -- Shaman Way of Earth Silverpine Forest
+    [90205] = true, -- Shaman Lava Burst Hillsbrad Foothills
 }
 
 ---@param questId number
@@ -361,6 +362,7 @@ local questsToBlacklistBySoDPhase = {
         [90200] = true, -- Hiding Rogue Mutilate Dun Morogh for now as there are too many icons
         [90201] = true, -- Hiding Rogue Shiv Duskwood for now as there are too many icons
         [90204] = true, -- Hiding Shaman Way of Earth Silverpine Forest for now as there are too many icons
+        [90205] = true, -- Hiding Shaman Lava Burst Hillsbrad Foothills for now as there are too many icons
     },
     [2] = { -- SoD Phase 2 - level cap 40
         [1152] = true, -- Test of Lore; minLevel raised to 26 in P1 for some reason, might be retooled as part of P2?
