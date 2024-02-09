183f2fccabce2b7a0b46137dec79d091417dc914
Cabro <1155017+Cabro@users.noreply.github.com>
2024-02-09 15:49:01 +0200

    [quest] Removed redundant tooltip for Crushing the Crown quests

--- Localization/Translations/ExtraObjectives/WotlkObjectives.lua
+++ Localization/Translations/ExtraObjectives/WotlkObjectives.lua
@@ -2162,18 +2162,6 @@ local wotlkObjectiveLocales = {
         ["zhTW"] = false,
         ["zhCN"] = false,
     },
-    ["Destroy Chemical Wagon using Snagglebolt's Khorium Bomb"] = { -- 24638, 24645, 24647, 24648, 24649, 24650, 24651, 24652, 24658, 24659, 24660, 24662, 24663, 24664, 24665, 24666
-        ["ptBR"] = false,
-        ["ruRU"] = false,
-        ["deDE"] = false,
-        ["koKR"] = false,
-        ["esMX"] = false,
-        ["enUS"] = true,
-        ["frFR"] = false,
-        ["esES"] = false,
-        ["zhTW"] = false,
-        ["zhCN"] = false,
-    },
     ["Use Snagglebolt's Air Analyzer on perfumed guards"] = { -- 24536, 24655
         ["ptBR"] = false,
         ["ruRU"] = false,
