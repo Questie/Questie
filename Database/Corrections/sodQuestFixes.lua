e4bd564ba511d93d48fabd91babc4243802b1154
Sommos <Sommosgaming@gmail.com>
2024-02-25 14:55:49 +0000

    FIXED: Missing Brackets

--- Database/Corrections/sodQuestFixes.lua
+++ Database/Corrections/sodQuestFixes.lua
@@ -763,7 +763,7 @@ function SeasonOfDiscovery:LoadQuests()
             [questKeys.requiredClasses] = classIDs.DRUID,
             [questKeys.preQuestSingle] = {79348},
             [questKeys.zoneOrSort] = zoneIDs.MOONGLADE,
-            [questKeys.objectives] = {{216437,216451,216448,216445},nil,{{213036}}},
+            [questKeys.objectives] = {{{216437,216451,216448,216445}},nil,{{213036}}},
         },
         [79442] = { -- Catching Up
             [questKeys.requiredClasses] = classIDs.SHAMAN,
