900fac65db4d60b61815d8d16e9265374b3369e4
BreakBB <b.boeing@online.de>
2024-03-14 08:07:23 +0100

    Add correction for "Beating Them Back!"

--- Database/Corrections/cataNPCFixes.lua
+++ Database/Corrections/cataNPCFixes.lua
@@ -10,5 +10,19 @@ function CataNpcFixes.Load()
     local npcKeys = QuestieDB.npcKeys
     local zoneIDs = ZoneDB.zoneIDs
 
-    return {}
+    return {
+        [49871] = {
+            [npcKeys.name] = "Blackrock Worg",
+            [npcKeys.minLevel] = 1,
+            [npcKeys.maxLevel] = 59,
+            [npcKeys.zoneID] = 12,
+            [npcKeys.spawns] = {
+                [zoneIDs.ELWYNN_FOREST] = {{46, 42.6},{46.2, 41},{46.2, 42},{46.4, 38},{46.4, 38.6},{46.6, 40.6},{47, 38},{47.4, 38.8},{47.4, 40},{47.6, 40},{48.6, 37.6},},
+                --[6170] = {{24, 58},{24.8, 61.6},{25.2, 37.6},{25.6, 36.8},{27.2, 37.4},{27.6, 59.4},{28.4, 49.8},{28.8, 36.6},{28.8, 45.2},{28.8, 49.2},{28.8, 53.6},{29.2, 46},{29.8, 45},{30.8, 37.6},{30.8, 44.2},{31.2, 47.4},{31.4, 47.8},{31.6, 46.6},{31.6, 48.6},{31.8, 47.8},{33, 40.2},{33.8, 39},{33.8, 40.2},{34.6, 37.2},{45.6, 57.8},},
+            },
+            [npcKeys.friendlyToFaction] = nil,
+            [npcKeys.questStarts] = nil,
+            [npcKeys.questEnds] = nil,
+        },
+    }
 end
--- Database/Corrections/cataQuestFixes.lua
+++ Database/Corrections/cataQuestFixes.lua
@@ -12,5 +12,18 @@ function CataQuestFixes.Load()
     local classIDs = QuestieDB.classKeys
     local zoneIDs = ZoneDB.zoneIDs
 
-    return {}
+    return {
+        [28762] = {
+            [questKeys.name] = "Beating Them Back!",
+            [questKeys.startedBy] = {{197}},
+            [questKeys.finishedBy] = {{197,}},
+            [questKeys.requiredLevel] = 1,
+            [questKeys.questLevel] = 1,
+            [questKeys.requiredRaces] = raceIDs.ALL_ALLIANCE,
+            [questKeys.requiredClasses] = classIDs.PALADIN,
+            [questKeys.objectivesText] = {"Kill 6 Blackrock Worgs."},
+            [questKeys.objectives] = {{{49871}}},
+            [questKeys.zoneOrSort] = zoneIDs.ELWYNN_FOREST, -- TODO: This is actually "Northshire"
+        },
+    }
 end
