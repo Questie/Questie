89d0b2d2ebcacbd280868249a2e0f3ecab587d45
BreakBB <b.boeing@online.de>
2024-03-19 16:56:41 +0100

    Remove placeholder correction

--- Database/Corrections/cataQuestFixes.lua
+++ Database/Corrections/cataQuestFixes.lua
@@ -12,18 +12,5 @@ function CataQuestFixes.Load()
     local classIDs = QuestieDB.classKeys
     local zoneIDs = ZoneDB.zoneIDs
 
-    return {
-        [28762] = {
-            [questKeys.name] = "Beating Them Back!",
-            [questKeys.startedBy] = {{197}},
-            [questKeys.finishedBy] = {{197,}},
-            [questKeys.requiredLevel] = 1,
-            [questKeys.questLevel] = 1,
-            [questKeys.requiredRaces] = raceIDs.ALL_ALLIANCE,
-            [questKeys.requiredClasses] = classIDs.PALADIN,
-            [questKeys.objectivesText] = {"Kill 6 Blackrock Worgs."},
-            [questKeys.objectives] = {{{49871}}},
-            [questKeys.zoneOrSort] = zoneIDs.ELWYNN_FOREST, -- TODO: This is actually "Northshire"
-        },
-    }
+    return {}
 end
