69b54933f10e7c788c21beb65fd7da79520a1d38
BreakBB <b.boeing@online.de>
2024-03-21 10:29:17 +0100

    Add Kezan PhaseIds

--- Modules/Phasing.lua
+++ Modules/Phasing.lua
@@ -3,8 +3,13 @@ local Phasing = QuestieLoader:CreateModule("Phasing")
 
 -- https://old.wow.tools/dbc/?dbc=phase&build=4.3.4.15595
 local phases = {
-    KEZAN_CHAPTER_1 = 1, --378,
-    KEZAN_CHAPTER_2 = 2, --379,
+    KEZAN_CHAPTER_1 = 378,
+    KEZAN_CHAPTER_2 = 379,
+    KEZAN_CHAPTER_3 = 380,
+    KEZAN_CHAPTER_4 = 381,
+    KEZAN_CHAPTER_5 = 382,
+    KEZAN_CHAPTER_6 = 383,
+    KEZAN_CHAPTER_7 = 384,
 }
 
 function Phasing.IsSpawnVisible(phase)
