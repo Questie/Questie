c450e8ade32a505393abe3c36bba5869b7d235c3
BreakBB <b.boeing@online.de>
2024-04-07 11:26:41 +0200

    Fix import of QuestieDB

--- Database/Corrections/cataItemFixes.lua
+++ Database/Corrections/cataItemFixes.lua
@@ -1,6 +1,8 @@
 ---@class CataItemFixes
 local CataItemFixes = QuestieLoader:CreateModule("CataItemFixes")
 
+---@type QuestieDB
+local QuestieDB = QuestieLoader:ImportModule("QuestieDB")
 
 function CataItemFixes.Load()
     local itemKeys = QuestieDB.itemKeys
