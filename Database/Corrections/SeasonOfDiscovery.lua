bf332e3399baaca30aca589148f8025a51af0a49
Sommos <Sommosgaming@gmail.com>
2024-04-10 08:52:36 +0100

    Add 'T.N.T' Hunter Fake Rune Quest

--- Database/Corrections/SeasonOfDiscovery.lua
+++ Database/Corrections/SeasonOfDiscovery.lua
@@ -431,6 +431,7 @@ local runeQuestsInSoD = {-- List quests here to have them flagged as Rune quests
 
     -- P3 SoD Runes
     [90269] = 3, -- Druid Gale Winds
+    [90272] = 3, -- Hunter T.N.T
 }
 
 --- "automatic" phase detection for the first few phases;
