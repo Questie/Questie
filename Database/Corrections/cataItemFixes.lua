97eaf4f1d54c547a7f2516259642ce4bb4a92a5a
BreakBB <b.boeing@online.de>
2024-04-14 11:08:50 +0200

    Mark quest items as such

--- Database/Corrections/cataItemFixes.lua
+++ Database/Corrections/cataItemFixes.lua
@@ -12,6 +12,12 @@ function CataItemFixes.Load()
         [46858] = { -- Personal Riches
             [itemKeys.objectDrops] = {195525},
         },
+        [48707] = { -- Gilnean Mastiff Collar
+            [itemKeys.class] = itemClasses.QUEST,
+        },
+        [49743] = { -- Sten's First Aid Kit
+            [itemKeys.class] = itemClasses.QUEST,
+        },
         [48766] = { -- Kaja\'mite Chunk
             [itemKeys.npcDrops] = {},
         },
@@ -27,10 +33,112 @@ function CataItemFixes.Load()
         [47047] = { -- Cool Shades
             [itemKeys.vendors] = {35130},
         },
-        [55122] = { -- Tholo\'s Horn
+        [49944] = { -- Belysra's Talisman
             [itemKeys.class] = itemClasses.QUEST,
         },
-        [60382] = { -- Mylra\'s Knife
+        [50134] = { -- Horn of Tal'doren
+            [itemKeys.class] = itemClasses.QUEST,
+        },
+        [50218] = { -- Krennan's Potion of Stealth
+            [itemKeys.class] = itemClasses.QUEST,
+        },
+        [50220] = { -- Half-Burnt Torch
+            [itemKeys.class] = itemClasses.QUEST,
+        },
+        [50334] = { -- Rapier of the Gilnean Patriots
+            [itemKeys.class] = itemClasses.QUEST,
+        },
+        [50405] = { -- Fossil-Finder 3000
+            [itemKeys.class] = itemClasses.QUEST,
+        },
+        [50430] = { -- Scraps of Rotting Meat
+            [itemKeys.class] = itemClasses.QUEST,
+        },
+        [50441] = { -- Garl's Net
+            [itemKeys.class] = itemClasses.QUEST,
+        },
+        [50742] = { -- Tara's Tar Scraper
+            [itemKeys.class] = itemClasses.QUEST,
+        },
+        [50746] = { -- Tara's Tar Scraper
+            [itemKeys.class] = itemClasses.QUEST,
+        },
+        [51956] = { -- Blessed Offerings
+            [itemKeys.class] = itemClasses.QUEST,
+        },
+        [52505] = { -- Poison Extraction Totem
+            [itemKeys.class] = itemClasses.QUEST,
+        },
+        [52514] = { -- Thonk's Spyglass
+            [itemKeys.class] = itemClasses.QUEST,
+        },
+        [52717] = { -- Fiery Leash
+            [itemKeys.class] = itemClasses.QUEST,
+        },
+        [52819] = { -- Frostgale Crystal
+            [itemKeys.class] = itemClasses.QUEST,
+        },
+        [52828] = { -- Orb of Ascension
+            [itemKeys.class] = itemClasses.QUEST,
+        },
+        [53107] = { -- Flameseer's Staff
+            [itemKeys.class] = itemClasses.QUEST,
+        },
+        [54463] = { -- Flameseer's Staff
+            [itemKeys.class] = itemClasses.QUEST,
+        },
+        [54788] = { -- Twilight Pick
+            [itemKeys.class] = itemClasses.QUEST,
+        },
+        [54814] = { -- Talisman of Flame Ascendancy
+            [itemKeys.class] = itemClasses.QUEST,
+        },
+        [55122] = { -- Tholo's Horn
+            [itemKeys.class] = itemClasses.QUEST,
+        },
+        [55137] = { -- Ogre Disguise
+            [itemKeys.class] = itemClasses.QUEST,
+        },
+        [55153] = { -- Horn of Cenarius
+            [itemKeys.class] = itemClasses.QUEST,
+        },
+        [55173] = { -- Young Twilight Drake Skull
+            [itemKeys.class] = itemClasses.QUEST,
+        },
+        [55179] = { -- Drums of the Turtle God
+            [itemKeys.class] = itemClasses.QUEST,
+        },
+        [55238] = { -- Concentrated Solvent
+            [itemKeys.class] = itemClasses.QUEST,
+        },
+        [56012] = { -- Stone Knife of Sealing
+            [itemKeys.class] = itemClasses.QUEST,
+        },
+        [56178] = { -- Duarn's Rope
+            [itemKeys.class] = itemClasses.QUEST,
+        },
+        [58167] = { -- Spirit Totem
+            [itemKeys.class] = itemClasses.QUEST,
+        },
+        [58365] = { -- Horn of the Ancients
+            [itemKeys.class] = itemClasses.QUEST,
+        },
+        [60382] = { -- Mylra's Knife
+            [itemKeys.class] = itemClasses.QUEST,
+        },
+        [60680] = { -- S.A.F.E. "Parachute"
+            [itemKeys.class] = itemClasses.QUEST,
+        },
+        [60835] = { -- Depleted Totem
+            [itemKeys.class] = itemClasses.QUEST,
+        },
+        [62534] = { -- Horn of Ramkahen
+            [itemKeys.class] = itemClasses.QUEST,
+        },
+        [62542] = { -- Mech Control Scrambler
+            [itemKeys.class] = itemClasses.QUEST,
+        },
+        [63351] = { -- Tahret Dynasty Mallet
             [itemKeys.class] = itemClasses.QUEST,
         },
     }
