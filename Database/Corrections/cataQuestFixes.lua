6a9671db47840bce0f65ad19007015fc07a65888
BreakBB <b.boeing@online.de>
2024-04-14 12:46:30 +0200

    Fix exclusiveness of "Preserving the Barrens"

--- Database/Corrections/cataQuestFixes.lua
+++ Database/Corrections/cataQuestFixes.lua
@@ -190,6 +190,9 @@ function CataQuestFixes.Load()
         [26512] = { -- Tuning the Gnomecorder
             [questKeys.triggerEnd] = {"Test the Gnomecorder at the Lakeshire Graveyard", {[zoneIDs.REDRIDGE_MOUNTAINS]={{32.3,39.5}}}},
         },
+        [26642] = { -- Preserving the Barrens
+            [questKeys.exclusiveTo] = {28494},
+        },
         [26656] = { -- Don't. Stop. Moving.
             [questKeys.triggerEnd] = {"Opalescent Guardians Escorted to safety", {[zoneIDs.DEEPHOLM]={{51,14.8}}}},
             [questKeys.objectives] = {{{42466},{43597}},nil,nil,nil,},
