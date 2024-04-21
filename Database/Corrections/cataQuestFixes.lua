f5abd7c038ef9304c3c60393146c9b1b28819296
BreakBB <b.boeing@online.de>
2024-04-21 20:10:18 +0200

    Fix pre quests for "Release the Valves"

--- Database/Corrections/cataQuestFixes.lua
+++ Database/Corrections/cataQuestFixes.lua
@@ -273,6 +273,10 @@ function CataQuestFixes.Load()
         [25203] = { -- What Kind of Name is Chip, Anyway?
             [questKeys.exclusiveTo] = {25244},
         },
+        [25204] = { -- Release the Valves
+            [questKeys.preQuestSingle] = {},
+            [questKeys.preQuestGroup] = {25200,25201},
+        },
         [25217] = { -- Totem Modification
             [questKeys.requiredSourceItems] = {53052,54214,54216,54217},
             [questKeys.extraObjectives] = {{{[zoneIDs.SHIMMERING_EXPANSE]={{40.4,34.2}}}, Questie.ICON_TYPE_EVENT, l10n("Place a totem on the ground and defend it")}},
