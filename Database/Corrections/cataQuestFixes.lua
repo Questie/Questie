3682b79a29e7b23ebed3b4e4e85dfdb2f1aa3c8a
BreakBB <b.boeing@online.de>
2024-04-21 11:48:58 +0200

    Fix objective locations for "Bad Datas"

--- Database/Corrections/cataQuestFixes.lua
+++ Database/Corrections/cataQuestFixes.lua
@@ -461,6 +461,10 @@ function CataQuestFixes.Load()
         [28376] = { -- Myzerian's Head
             [questKeys.startedBy] = {{48428},nil,{63700}},
         },
+        [28403] = { -- Bad Datas
+            [questKeys.triggerEnd] = {"Titan Data Uploaded",{[zoneIDs.ULDUM]={{36.18,23.25}}}},
+            [questKeys.objectives] = {},
+        },
         [28486] = { -- Salhet's Gambit
             [questKeys.triggerEnd] = {"Higher ground secured", {[zoneIDs.ULDUM]={{54.,71.1}}}},
             [questKeys.extraObjectives] = {{nil, Questie.ICON_TYPE_TALK, l10n("Talk to Ranmkahen Ranger Captain"), 0, {{"monster", 49244}}}},
