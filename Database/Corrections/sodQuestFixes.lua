c33db987444c5fc970475f13102f78247c9acfb9
Samuel <Sommosgaming@gmail.com>
2024-04-28 10:06:43 +0100

    [quest] Add Shaman's 'Burn' Rune (Thanks @Sommos)

--- Database/Corrections/sodQuestFixes.lua
+++ Database/Corrections/sodQuestFixes.lua
@@ -5185,6 +5185,18 @@ function SeasonOfDiscovery:LoadQuests()
             [questKeys.requiredSpell] = -432297,
             [questKeys.zoneOrSort] = sortKeys.ROGUE,
         },
+        [90278] = {
+            [questKeys.name] = "Burn",
+            [questKeys.startedBy] = {{222685,222687,222684,222686}},
+            [questKeys.finishedBy] = nil,
+            [questKeys.requiredLevel] = 1,
+            [questKeys.questLevel] = 23,
+            [questKeys.requiredRaces] = raceIDs.NONE,
+            [questKeys.requiredClasses] = classIDs.SHAMAN,
+            [questKeys.objectivesText] = {"Reach Friendly with the Emerald Wardens, then buy the Burn rune from the Quartermaster."},
+            [questKeys.requiredSpell] = -416066,
+            [questKeys.zoneOrSort] = sortKeys.SHAMAN,
+        },
     }
 end
 
