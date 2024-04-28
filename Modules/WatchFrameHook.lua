0c5c7635b9e7e9983de66d7b3a67ee585c354578
BreakBB <b.boeing@online.de>
2024-04-28 10:54:37 +0200

    Fix WatchFrameHook in Era

--- Modules/WatchFrameHook.lua
+++ Modules/WatchFrameHook.lua
@@ -1,7 +1,7 @@
 ---@class WatchFrameHook
 local WatchFrameHook = QuestieLoader:CreateModule("WatchFrameHook")
 
-local WatchFrame, QuestTimerFrame = WatchFrame, QuestTimerFrame
+local WatchFrame, QuestTimerFrame = WatchFrame or QuestWatchFrame, QuestTimerFrame
 
 function WatchFrameHook.Hide()
     if (Questie.IsWotlk or Questie.IsCata) and QuestTimerFrame then
