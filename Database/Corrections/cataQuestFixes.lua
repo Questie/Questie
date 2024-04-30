3432237565028bcaf55187639fb9decbade5f24e
BreakBB <b.boeing@online.de>
2024-04-30 23:20:31 +0200

    [quest] Fix pre quest for "Come Prepared"

--- Database/Corrections/cataQuestFixes.lua
+++ Database/Corrections/cataQuestFixes.lua
@@ -769,6 +769,9 @@ function CataQuestFixes.Load()
         [25948] = { -- Bring It On!
             [questKeys.preQuestGroup] = {25944,25947},
         },
+        [25970] = { -- Come Prepared
+            [questKeys.preQuestSingle] = {25968},
+        },
         [25971] = { -- Unfurling Plan
             [questKeys.preQuestSingle] = {25968},
         },
