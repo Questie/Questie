5001b4e62123b9b0196afbdcdd0ce4b361851b28
BreakBB <b.boeing@online.de>
2024-04-30 23:11:44 +0200

    [quest] Fix pre quest for "The Great Sambino"

--- Database/Corrections/cataQuestFixes.lua
+++ Database/Corrections/cataQuestFixes.lua
@@ -636,6 +636,9 @@ function CataQuestFixes.Load()
         [25215] = { -- A Distracting Scent
             [questKeys.preQuestSingle] = {25222},
         },
+        [25216] = { -- The Great Sambino
+            [questKeys.preQuestSingle] = {25222},
+        },
         [25217] = { -- Totem Modification
             [questKeys.requiredSourceItems] = {53052,54214,54216,54217},
             [questKeys.extraObjectives] = {{{[zoneIDs.SHIMMERING_EXPANSE]={{40.4,34.2}}}, Questie.ICON_TYPE_EVENT, l10n("Place a totem on the ground and defend it")}},
