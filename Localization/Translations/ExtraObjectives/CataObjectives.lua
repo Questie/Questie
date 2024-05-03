ba2b83fc1e35e6ef66c7e4668e8e0147e3ebc356
Cabro <1155017+Cabro@users.noreply.github.com>
2024-05-03 15:48:35 +0300

    [locale] Forgot a translation

--- Localization/Translations/ExtraObjectives/CataObjectives.lua
+++ Localization/Translations/ExtraObjectives/CataObjectives.lua
@@ -266,6 +266,18 @@ local cataObjectiveLocales = {
         ["zhTW"] = false,
         ["zhCN"] = false,
     },
+    ["Use Grimnur's Bait on yourself"] = {
+        ["ptBR"] = false,
+        ["ruRU"] = false,
+        ["deDE"] = false,
+        ["koKR"] = false,
+        ["esMX"] = false,
+        ["enUS"] = true,
+        ["frFR"] = false,
+        ["esES"] = false,
+        ["zhTW"] = false,
+        ["zhCN"] = false,
+    },
 }
 
 for k, v in pairs(cataObjectiveLocales) do
