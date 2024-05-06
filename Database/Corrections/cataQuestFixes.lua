9357462f27837972888abd138a5e30a2d48c3d0f
BreakBB <b.boeing@online.de>
2024-05-06 07:58:11 +0200

    [quest] Add pre quest to "End of the Supply Line"

--- Database/Corrections/cataQuestFixes.lua
+++ Database/Corrections/cataQuestFixes.lua
@@ -837,6 +837,9 @@ function CataQuestFixes.Load()
         [25220] = { -- Slippery Threat
             [questKeys.preQuestSingle] = {25222},
         },
+        [25233] = { -- End of the Supply Line
+            [questKeys.preQuestSingle] = {25584},
+        },
         [25234] = { -- In the Rear With the Gear
             [questKeys.preQuestSingle] = {25584},
         },
