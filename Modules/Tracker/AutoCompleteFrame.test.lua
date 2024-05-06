4ad64ea8f2cefe62dfb4f1acabf8eb41a30b9f26
BreakBB <b.boeing@online.de>
2024-05-07 00:18:29 +0200

    Add test to set quest info on AutoCompleteFrame

--- Modules/Tracker/AutoCompleteFrame.test.lua
+++ Modules/Tracker/AutoCompleteFrame.test.lua
@@ -12,6 +12,23 @@ describe("AutoCompleteFrame", function()
     end)
 
     describe("ShowAutoComplete", function()
+        it("should set quest data and show frame", function()
+            AutoCompleteFrame.Initialize({
+                GetPoint = function()
+                    return "BOTTOMLEFT", nil, nil, screenWidth
+                end
+            })
+            local frame = CreateFrame.mockedFrames[1]
+            frame.Show = spy.new(frame.Show)
+            frame.questTitle.SetText = spy.new(frame.questTitle.SetText)
+
+            AutoCompleteFrame.ShowAutoComplete(1)
+
+            assert.spy(frame.questTitle.SetText).was_called_with(frame.questTitle, "Test Quest")
+            assert.equals(1, frame.questId)
+            assert.spy(frame.Show).was_called()
+        end)
+
         it("should show pop up on the left if Tracker is on the right side of the screen and anchored BOTTOMLEFT", function()
             local baseFrame = {
                 GetPoint = function()
