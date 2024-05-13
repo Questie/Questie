20694cd1f3c8b1e39dafe48de9d61603d32d8085
BreakBB <b.boeing@online.de>
2024-05-13 23:18:27 +0200

    [quest] Fix pre quest for "Check in on the Edunes"

--- Database/Corrections/cataQuestFixes.lua
+++ Database/Corrections/cataQuestFixes.lua
@@ -526,6 +526,9 @@ function CataQuestFixes.Load()
         [13961] = { -- Drag it Out of Them
             [questKeys.triggerEnd] = {"Razormane Prisoner Delivered", {[zoneIDs.THE_BARRENS]={{56.4,40.3}}}},
         },
+        [13965] = { -- Check in on the Edunes
+            [questKeys.preQuestSingle] = {26478},
+        },
         [13975] = { -- Crossroads Caravan Delivery
             [questKeys.objectives] = {},
             [questKeys.triggerEnd] = {"Crossroads Caravan Escorted", {[zoneIDs.THE_BARRENS]={{50.3,58.5}}}},
