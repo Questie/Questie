e290937d77c03fd3d1fd3151fa77c4d30696d6b5
BreakBB <b.boeing@online.de>
2024-05-23 11:02:47 +0200

    [quest] Hide old version of "Nogg's Ring Redo" in SoD
    Fixes #5897

--- Database/Corrections/QuestieQuestBlacklist.lua
+++ Database/Corrections/QuestieQuestBlacklist.lua
@@ -1870,6 +1870,7 @@ function QuestieQuestBlacklist:Load()
         [29742] = true, -- Menkhaf the Elder
 
         ----- SoD -------------- SoD quests --------------- SoD -----
+        [2950] = QuestieCorrections.SOD_HIDE,
         [3446] = QuestieCorrections.SOD_HIDE,
         [78611] = true, -- A Waylaid Shipment (no longer available in P2)
         [79100] = true, -- A Waylaid Shipment (no longer available in P2)
