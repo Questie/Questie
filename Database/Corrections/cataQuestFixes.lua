8bc886a77e2c76cebf6c1fe72a0784634c391cac
BreakBB <b.boeing@online.de>
2024-05-24 21:58:00 +0200

    [quest] Fix pre quest for "Firebeard Bellows"

--- Database/Corrections/cataQuestFixes.lua
+++ Database/Corrections/cataQuestFixes.lua
@@ -2894,6 +2894,9 @@ function CataQuestFixes.Load()
             [questKeys.preQuestSingle] = {27745},
             [questKeys.exclusiveTo] = {27786},
         },
+        [27809] = { -- Firebeard Bellows
+            [questKeys.preQuestSingle] = {27806},
+        },
         [27811] = { -- The Scent of Battle
             [questKeys.preQuestSingle] = {27807},
         },
