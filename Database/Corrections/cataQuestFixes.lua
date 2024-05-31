455f068d2c1405de8163738da1d3455e07d967c9
Cabro <1155017+Cabro@users.noreply.github.com>
2024-05-31 23:38:45 +0300

    [quest] Fixed prequest for "In Search of Bravo Company"

--- Database/Corrections/cataQuestFixes.lua
+++ Database/Corrections/cataQuestFixes.lua
@@ -2668,6 +2668,9 @@ function CataQuestFixes.Load()
         [26585] = { -- Corruption Destruction
             [questKeys.preQuestGroup] = {26578,26579,26582},
         },
+        [26586] = { -- In Search of Bravo Company
+            [questKeys.preQuestSingle] = {26568},
+        },
         [26588] = { -- A Furious Catch
             [questKeys.exclusiveTo] = {26572,26557,26543,26556,29349,29345,29354,29346,29348,29317,29320,29361,29319,29322},
             [questKeys.extraObjectives] = {{{[zoneIDs.THE_BARRENS]={{71.1,7.9}}}, Questie.ICON_TYPE_NODE_FISH, l10n("Fish for Giant Furious Pike")}},
