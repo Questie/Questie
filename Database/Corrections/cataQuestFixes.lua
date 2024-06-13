ebd04223281206f4f5980cf22aaed4cdccb4865c
BreakBB <b.boeing@online.de>
2024-06-13 07:50:18 +0200

    [quest] Mark both versions of "The Holy Water of Clarity" as exclusive to each other

--- Database/Corrections/cataQuestFixes.lua
+++ Database/Corrections/cataQuestFixes.lua
@@ -3246,6 +3246,9 @@ function CataQuestFixes.Load()
         [26425] = { -- Meet the High Tinker
             [questKeys.zoneOrSort] = zoneIDs.CHILL_BREEZE_VALLEY,
         },
+        [26433] = { -- The Holy Water of Clarity
+            [questKeys.exclusiveTo] = {26590},
+        },
         [26440] = { -- Clingy
             [questKeys.triggerEnd] = {"Pebble brought to crystal formation",{[zoneIDs.DEEPHOLM]={{29,45}}}},
             [questKeys.objectives] = {},
@@ -3382,6 +3385,9 @@ function CataQuestFixes.Load()
         [26588] = { -- A Furious Catch
             [questKeys.exclusiveTo] = {26572,26557,26543,26556,29349,29345,29354,29346,29348,29317,29320,29361,29319,29322},
             [questKeys.extraObjectives] = {{{[zoneIDs.THE_BARRENS]={{71.1,7.9}}}, Questie.ICON_TYPE_NODE_FISH, l10n("Fish for Giant Furious Pike")}},
+        },
+        [26590] = { -- The Holy Water of Clarity
+            [questKeys.exclusiveTo] = {26433},
         },
         [26591] = { -- Battlefront Triage
             [questKeys.preQuestSingle] = {26501},
