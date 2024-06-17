cad93b3e3d8ecb7f8995732f360e50218761b7a8
BreakBB <b.boeing@online.de>
2024-06-17 08:11:13 +0200

    Add tests for QuestieDB.IsTrivial

--- Database/QuestieDB.test.lua
+++ Database/QuestieDB.test.lua
@@ -1,8 +1,15 @@
 dofile("setupTests.lua")
 
 describe("QuestieDB", function()
+    ---@type QuestiePlayer
+    local QuestiePlayer
     ---@type QuestieDB
-    local QuestieDB = require("Database.QuestieDB")
+    local QuestieDB
+
+    before_each(function()
+        QuestiePlayer = require("Modules.QuestiePlayer")
+        QuestieDB = require("Database.QuestieDB")
+    end)
 
     describe("GetQuestTagInfo", function()
         it("should return the API value", function()
@@ -36,6 +43,45 @@ describe("QuestieDB", function()
             assert.are.same(81, questType2)
             assert.are.same("Dungeon", questTag2)
             assert.spy(_G.GetQuestTagInfo).was_called(1)
+        end)
+    end)
+
+    describe("IsTrivial", function()
+        it("should return false for scaling quests", function()
+            QuestiePlayer.GetPlayerLevel = spy.new(function() return 60 end)
+
+            assert.is_false(QuestieDB.IsTrivial(-1))
+            assert.spy(QuestiePlayer.GetPlayerLevel).was_not_called()
+        end)
+
+        it("should return false for red quests", function()
+            QuestiePlayer.GetPlayerLevel = spy.new(function() return 60 end)
+
+            assert.is_false(QuestieDB.IsTrivial(66))
+            assert.is_false(QuestieDB.IsTrivial(65))
+        end)
+
+        it("should return false for orange quests", function()
+            QuestiePlayer.GetPlayerLevel = spy.new(function() return 60 end)
+
+            assert.is_false(QuestieDB.IsTrivial(64))
+            assert.is_false(QuestieDB.IsTrivial(63))
+        end)
+
+        it("should return false for green quests", function()
+            QuestiePlayer.GetPlayerLevel = spy.new(function() return 60 end)
+            _G.GetQuestGreenRange = spy.new(function() return 12 end)
+
+            assert.is_false(QuestieDB.IsTrivial(48))
+            assert.is_false(QuestieDB.IsTrivial(49))
+        end)
+
+        it("should return true for grey quests", function()
+            QuestiePlayer.GetPlayerLevel = spy.new(function() return 60 end)
+            _G.GetQuestGreenRange = spy.new(function() return 12 end)
+
+            assert.is_true(QuestieDB.IsTrivial(47))
+            assert.is_true(QuestieDB.IsTrivial(46))
         end)
     end)
 end)
