b161fa93b61ef73fa5973de5367b3902fc368cf6
Cabro <1155017+Cabro@users.noreply.github.com>
2024-07-06 20:11:04 +0300

    [locale] Added the rest of the missing translation strings

---
+++ Localization/Translations/Zones/TheMaelstrom.lua
@@ -0,0 +1,46 @@
+---@type l10n
+local l10n = QuestieLoader:ImportModule("l10n")
+
+
+local maelstromLocales = {
+    ["Deepholm"] = {
+        ["ptBR"] = false,
+        ["ruRU"] = false,
+        ["deDE"] = false,
+        ["koKR"] = false,
+        ["esMX"] = false,
+        ["enUS"] = true,
+        ["frFR"] = false,
+        ["esES"] = false,
+        ["zhTW"] = false,
+        ["zhCN"] = false,
+    },
+    ["The Lost Isles"] = {
+        ["ptBR"] = false,
+        ["ruRU"] = false,
+        ["deDE"] = false,
+        ["koKR"] = false,
+        ["esMX"] = false,
+        ["enUS"] = true,
+        ["frFR"] = false,
+        ["esES"] = false,
+        ["zhTW"] = false,
+        ["zhCN"] = false,
+    },
+    ["Kezan"] = {
+        ["ptBR"] = false,
+        ["ruRU"] = false,
+        ["deDE"] = false,
+        ["koKR"] = false,
+        ["esMX"] = false,
+        ["enUS"] = true,
+        ["frFR"] = false,
+        ["esES"] = false,
+        ["zhTW"] = false,
+        ["zhCN"] = false,
+    },
+}
+
+for k, v in pairs(maelstromLocales) do
+    l10n.translations[k] = v
+end
