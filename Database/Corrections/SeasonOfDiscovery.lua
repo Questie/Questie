a49df4872ecdedf54fe67ebb5c3c5a17143cb3f0
Samuel <Sommosgaming@gmail.com>
2024-07-13 14:56:51 +0100

    [quest] Add Comments

--- Database/Corrections/SeasonOfDiscovery.lua
+++ Database/Corrections/SeasonOfDiscovery.lua
@@ -476,6 +476,8 @@ local runeQuestsInSoD = {-- List quests here to have them flagged as Rune quests
     [90296] = 3, -- Rogue Focused Attacks
     [90297] = 3, -- Shaman Tidal Waves
     [90298] = 3, -- Shaman Rolling Thunder
+
+    -- P4 SoD Runes
     [90300] = 4, -- Druid Starfall
 }
 
