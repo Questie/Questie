747fecb26f626c401785f5cce2f8f26ca4f3238d
Sommos <Sommosgaming@gmail.com>
2024-07-17 20:18:04 +0100

    [quest] Removed Icon Type

--- Database/Corrections/cataQuestFixes.lua
+++ Database/Corrections/cataQuestFixes.lua
@@ -3248,7 +3248,7 @@ function CataQuestFixes.Load()
             [questKeys.extraObjectives] = {{nil,Questie.ICON_TYPE_EVENT,l10n("Disable the Shadow Cloak Generator"),0,{{"object",203208}}}},
         },
         [25766] = { -- Arcane Legacy
-            [questKeys.objectives] = {{{34931,34932,34938,nil,Questie.ICON_TYPE_INTERACT}}},
+            [questKeys.objectives] = {{{34931,34932,34938}}},
         },
         [25776] = { -- Sethria's Demise
             [questKeys.preQuestSingle] = {},
