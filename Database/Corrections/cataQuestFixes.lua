9ad2794bcf6685b3de7e20b39f955dae44588a62
Sommos <Sommosgaming@gmail.com>
2024-07-20 11:40:30 +0100

    [quest] Fix 'Hunting Brendol' Prerequisite Quest

--- Database/Corrections/cataQuestFixes.lua
+++ Database/Corrections/cataQuestFixes.lua
@@ -1727,6 +1727,9 @@ function CataQuestFixes.Load()
             [questKeys.preQuestGroup] = {14370,14371},
             [questKeys.objectives] = {{{36297,nil,Questie.ICON_TYPE_TALK}}},
         },
+        [14378] = { -- Hunting Brendol
+            [questKeys.preQuestGroup] = {14373,14374},
+        },
         [14382] = { -- Two By Sea
             [questKeys.extraObjectives] = {
                 {nil,Questie.ICON_TYPE_INTERACT,l10n("Use the catapult to board the ship"),0,{{"monster",36283}}},
