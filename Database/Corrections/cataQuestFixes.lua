b88da8461ed97a575ab7cea3e96a3624f76cf41a
Sommos <Sommosgaming@gmail.com>
2024-07-21 09:06:49 +0100

    [quest] Add 'Sasquatch Sighting' Prerequisite Quest

--- Database/Corrections/cataQuestFixes.lua
+++ Database/Corrections/cataQuestFixes.lua
@@ -2930,6 +2930,9 @@ function CataQuestFixes.Load()
             [questKeys.preQuestSingle] = {25459},
             [questKeys.finishedBy] = {{100009}},
         },
+        [25433] = { -- Sasquatch Sighting
+            [questKeys.preQuestSingle] = {25432},
+        },
         [25439] = { -- Vengeful Heart
             [questKeys.preQuestSingle] = {25222},
         },
