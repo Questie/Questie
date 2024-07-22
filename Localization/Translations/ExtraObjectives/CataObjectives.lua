28b6d2741f66c25cc2d4a83800c89fe404d98a47
Sommos <Sommosgaming@gmail.com>
2024-07-22 22:25:23 +0100

    [quest] Add translations

--- Localization/Translations/ExtraObjectives/CataObjectives.lua
+++ Localization/Translations/ExtraObjectives/CataObjectives.lua
@@ -2534,6 +2534,18 @@ local cataObjectiveLocales = {
         ["zhTW"] = false,
         ["zhCN"] = false,
     },
+    ["Collect 10 Blood-filled Leeches"] = {
+        ["ptBR"] = false,
+        ["ruRU"] = false,
+        ["deDE"] = false,
+        ["koKR"] = false,
+        ["esMX"] = false,
+        ["enUS"] = true,
+        ["frFR"] = false,
+        ["esES"] = false,
+        ["zhTW"] = false,
+        ["zhCN"] = false,
+    },
 }
 
 for k, v in pairs(cataObjectiveLocales) do
