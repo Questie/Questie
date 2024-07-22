f044d77c802cd64d6b3f5d8a66e4f718c44341be
Sommos <Sommosgaming@gmail.com>
2024-07-22 22:20:41 +0100

    [quest] Fix pre quest for 'Mixmaster Jasper'

--- Database/Corrections/cataQuestFixes.lua
+++ Database/Corrections/cataQuestFixes.lua
@@ -4310,6 +4310,9 @@ function CataQuestFixes.Load()
             [questKeys.extraObjectives] = {{{[zoneIDs.STORMWIND_CITY]={{69.12,88.51},{73.31,83.29}}}, Questie.ICON_TYPE_NODE_FISH, l10n("Fish for Royal Monkfish")}},
             [questKeys.requiredSourceItems] = {58856},
         },
+        [26494] = { -- Mixmaster Jasper
+            [questKeys.preQuestSingle] = {26493},
+        },
         [26495] = { -- Chabal
             [questKeys.preQuestSingle] = {26493},
         },
