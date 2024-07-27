2280322e7f376784be16a906df71538de6634b11
BreakBB <b.boeing@online.de>
2024-07-27 12:05:55 +0200

    Wrap test for checkRequiredSourceItems

--- cli/validators.test.lua
+++ cli/validators.test.lua
@@ -16,50 +16,52 @@ describe("Validators", function()
         _G.print = function() end -- disable print
     end)
 
-    it("should find and reports requiredSourceItems which are also a sourceItemId or itemObjective", function()
-        local quests = {
-            [1] = {
-                sourceItemId = 1,
-                requiredSourceItems = {1, 2, 3},
-            },
-            [2] = {
-                sourceItemId = 2,
-                requiredSourceItems = {4, 5, 6},
-            },
-            [3] = {
-                objectives = {nil,nil,{{3}}},
-                requiredSourceItems = {7, 8, 3},
+    describe("checkRequiredSourceItems", function()
+        it("should find and report requiredSourceItems which are also a sourceItemId or itemObjective", function()
+            local quests = {
+                [1] = {
+                    sourceItemId = 1,
+                    requiredSourceItems = {1, 2, 3},
+                },
+                [2] = {
+                    sourceItemId = 2,
+                    requiredSourceItems = {4, 5, 6},
+                },
+                [3] = {
+                    objectives = {nil,nil,{{3}}},
+                    requiredSourceItems = {7, 8, 3},
+                }
             }
-        }
 
-        local matchingQuests = Validators.checkRequiredSourceItems(quests, questKeys)
+            local matchingQuests = Validators.checkRequiredSourceItems(quests, questKeys)
 
-        assert.are.same(matchingQuests, {
-            [1] = "sourceItemId in requiredSourceItems: 1",
-            [3] = "itemObjectiveId in requiredSourceItems: 3"
-        })
-        assert.spy(exitMock).was_called_with(1)
-    end)
+            assert.are.same(matchingQuests, {
+                [1] = "sourceItemId in requiredSourceItems: 1",
+                [3] = "itemObjectiveId in requiredSourceItems: 3"
+            })
+            assert.spy(exitMock).was_called_with(1)
+        end)
 
-    it("should not report anything when requiredSourceItems are fine", function()
-        local quests = {
-            [1] = {
-                sourceItemId = 1,
-                requiredSourceItems = {2, 3, 4},
-            },
-            [2] = {
-                sourceItemId = 2,
-                requiredSourceItems = {5, 6, 7},
-            },
-            [3] = {
-                requiredSourceItems = {8, 9, 10},
-                objectives = {nil,nil,{{3}}},
+        it("should not report anything when requiredSourceItems are fine", function()
+            local quests = {
+                [1] = {
+                    sourceItemId = 1,
+                    requiredSourceItems = {2, 3, 4},
+                },
+                [2] = {
+                    sourceItemId = 2,
+                    requiredSourceItems = {5, 6, 7},
+                },
+                [3] = {
+                    requiredSourceItems = {8, 9, 10},
+                    objectives = {nil,nil,{{3}}},
+                }
             }
-        }
 
-        local matchingQuests = Validators.checkRequiredSourceItems(quests, questKeys)
+            local matchingQuests = Validators.checkRequiredSourceItems(quests, questKeys)
 
-        assert.are.same(matchingQuests, nil)
-        assert.spy(exitMock).was_not_called()
+            assert.are.same(matchingQuests, nil)
+            assert.spy(exitMock).was_not_called()
+        end)
     end)
 end)
