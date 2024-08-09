60a92cf16add91fb02dcf7c2b02857918f5c8288
coldpasta123 <pastacooldown@gmail.com>
2024-08-09 10:03:02 +0100

    [quest] Ashenvale Quest Fixes (Cata)
    Add text for Crisis at Splintertree

--- Localization/Translations/ExtraObjectives/CataObjectives.lua
+++ Localization/Translations/ExtraObjectives/CataObjectives.lua
@@ -2642,6 +2642,19 @@ local cataObjectiveLocales = {
         ["zhTW"] = false,
         ["zhCN"] = false,
     },
+    ["Speak to Gorka"] = {
+        ["ptBR"] = false,
+        ["ruRU"] = false,
+        ["deDE"] = false,
+        ["koKR"] = false,
+        ["esMX"] = false,
+        ["enUS"] = true,
+        ["frFR"] = false,
+        ["esES"] = false,
+        ["zhTW"] = false,
+        ["zhCN"] = false,
+    },
+    
 }
 
 for k, v in pairs(cataObjectiveLocales) do
