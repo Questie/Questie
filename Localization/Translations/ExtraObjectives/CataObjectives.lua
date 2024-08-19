809ec44453f406154b563c7e768314028818a457
Cabro <1155017+Cabro@users.noreply.github.com>
2024-08-19 21:43:01 +0300

    [locale] Add strings for "Speak to Durek"

--- Localization/Translations/ExtraObjectives/CataObjectives.lua
+++ Localization/Translations/ExtraObjectives/CataObjectives.lua
@@ -2666,6 +2666,18 @@ local cataObjectiveLocales = {
         ["zhTW"] = false,
         ["zhCN"] = false,
     },
+    ["Speak to Durek"] = {
+        ["ptBR"] = false,
+        ["ruRU"] = false,
+        ["deDE"] = false,
+        ["koKR"] = false,
+        ["esMX"] = false,
+        ["enUS"] = true,
+        ["frFR"] = false,
+        ["esES"] = false,
+        ["zhTW"] = false,
+        ["zhCN"] = false,
+    },
 }
 
 for k, v in pairs(cataObjectiveLocales) do
