42d93281ac0211e569626b13446dbd3f1845bfbe
Cabro <1155017+Cabro@users.noreply.github.com>
2024-08-27 19:34:20 +0300

    [db] Adjusted max rep level for "Seek and Destroy"

--- Database/Corrections/cataQuestFixes.lua
+++ Database/Corrections/cataQuestFixes.lua
@@ -4085,7 +4085,7 @@ function CataQuestFixes.Load()
             [questKeys.objectives] = {{{41955,nil,Questie.ICON_TYPE_INTERACT}}},
         },
         [26009] = { -- Seek and Destroy
-            [questKeys.requiredMaxRep] = {76,20999},
+            [questKeys.requiredMaxRep] = {76,21000},
         },
         [26010] = { -- Ashes to Ashes
             [questKeys.preQuestSingle] = {26004},
