6df5ed453f62e65fc011d4074c9f0cc1433ee5b4
coldpasta123 <170130800+coldpasta123@users.noreply.github.com>
2024-10-02 19:05:34 +0100

    [quest] Add prequest for Get The Centipaarty Started

--- Database/Corrections/cataQuestFixes.lua
+++ Database/Corrections/cataQuestFixes.lua
@@ -2973,11 +2973,14 @@ function CataQuestFixes.Load()
             [questKeys.preQuestGroup] = {24942,24945,24946},
             [questKeys.objectives] = {{{38842,nil,Questie.ICON_TYPE_EVENT}}},
         },
-        [24952] = { -- Just Trying to Kill Some Bugs
+        [24953] = { -- Just Trying to Kill Some Bugs
             [questKeys.extraObjectives] = {{nil,Questie.ICON_TYPE_TALK,l10n("Talk to Narain to mount your bug"),0,{{"monster",11811}}}},
         },
         [24955] = { -- Un-Chartered
             [questKeys.preQuestSingle] = {},
+        },
+        [24957] = { -- Get The Centipaarty Started
+            [questKeys.preQuestSingle] = {24955},
         },
         [24958] = { -- Volcanoth!
             [questKeys.objectives] = {{{38855}}},
