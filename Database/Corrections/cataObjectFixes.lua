18362143d0827cd03d82317604382201c7bc6534
coldpasta123 <170130800+coldpasta123@users.noreply.github.com>
2024-10-06 19:24:21 +0100

    Fix OH barrel coords
    Co-authored-by: Björn Böing <b.boeing@online.de>

--- Database/Corrections/cataObjectFixes.lua
+++ Database/Corrections/cataObjectFixes.lua
@@ -330,7 +330,7 @@ function CataObjectFixes.Load()
         },
         [460015] = {
             [objectKeys.name] = "Durnholde Keep Barrel",
-            [objectKeys.spawns] = {[zoneIDs.OLD_HILLSBRAD_FOOTHILLS]={{76.60,68.80},{77.14,66.30},{74.68,68.79},{69.09,62.61},{68.03,59.82}}},
+            [objectKeys.spawns] = {[zoneIDs.OLD_HILLSBRAD_FOOTHILLS]={{76.60,68.80},{77.14,66.30},{74.68,68.79},{69.09,62.61},{68.03,59.82},{-1,-1}}},
             [objectKeys.zoneID] = zoneIDs.OLD_HILLSBRAD_FOOTHILLS,
         },
     }
