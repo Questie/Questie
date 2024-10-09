24a7abf48e39962e8e0197ea916ca33635ede2c2
TorpedoCXC <github.20.torpedo@a-bc.net>
2024-10-09 13:30:33 +0200

    [quest] Add 25533 Pirate Accuracy Increasing
    Add 3 prerequisite quests that all need to be completed before.

--- Database/Corrections/cataQuestFixes.lua
+++ Database/Corrections/cataQuestFixes.lua
@@ -3824,6 +3824,9 @@ function CataQuestFixes.Load()
             [questKeys.preQuestSingle] = {25505},
             [questKeys.requiredSourceItems] = {54821},
         },
+        [25533] = { -- Pirate Accuracy Increasing
+            [questKeys.preQuestGroup] = {25516,25518,25526},
+        },
         [25534] = { -- Going Off-Task [Horde]
             [questKeys.exclusiveTo] = {};
         },
