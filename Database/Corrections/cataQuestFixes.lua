7698fc98c51e947093d8767b7609059646111b13
TorpedoCXC <github.20.torpedo@a-bc.net>
2024-10-18 19:51:41 +0200

    [quest] Into the Mountain - add prerequisite quests

--- Database/Corrections/cataQuestFixes.lua
+++ Database/Corrections/cataQuestFixes.lua
@@ -4157,6 +4157,9 @@ function CataQuestFixes.Load()
         [25678] = { -- Pick Your Fate
             [questKeys.objectives] = {{{5996},{5997},{5998}}},
         },
+        [25679] = { -- Into the Mountain
+            [questKeys.preQuestGroup] = {25677,25678},
+        },
         [25696] = { -- The Sunveil Excursion
             [questKeys.preQuestSingle] = {25695},
         },
