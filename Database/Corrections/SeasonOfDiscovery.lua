c94ffc977cf51fac6fc48b26f0b096bf7f7bbb38
BreakBB <b.boeing@online.de>
2024-11-13 08:34:08 +0100

    [quest] Show "Report to General Kirika" in SoD

--- Database/Corrections/SeasonOfDiscovery.lua
+++ Database/Corrections/SeasonOfDiscovery.lua
@@ -774,6 +774,7 @@ local questsToBlacklistBySoDPhase = {
         [8287] = true,
         [8314] = true,
         [9415] = true,
+        [9416] = true,
     },
     [6] = { -- SoD Phase 6 == Era Phase 5: AQ
         [8276] = true, -- Taking Back Silithus
@@ -909,7 +910,6 @@ local questsToBlacklistBySoDPhase = {
         [9248] = true,
         [9422] = true,
         [9419] = true,
-        [9416] = true,
     },
     [8] = { -- Never appearing in Season of Discovery
         [1203] = true, -- Jarl Needs a Blade - Replaced by 81570
