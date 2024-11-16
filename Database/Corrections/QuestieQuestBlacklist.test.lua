6f27233dfd6dad8debbeb72e6520423c4c0164ac
BreakBB <b.boeing@online.de>
2024-11-16 17:01:54 +0100

    Add tests for anniversary and QuestieQuestBlacklist.Load

--- Database/Corrections/QuestieQuestBlacklist.test.lua
+++ Database/Corrections/QuestieQuestBlacklist.test.lua
@@ -1,13 +1,16 @@
 dofile("setupTests.lua")
 
 dofile("Database/Corrections/QuestieCorrections.lua")
+dofile("Database/Corrections/ContentPhases/Classic.lua")
 dofile("Database/Corrections/ContentPhases/SeasonOfDiscovery.lua")
 
 local QuestieQuestBlacklist = require("Database.Corrections.QuestieQuestBlacklist")
+local ContentPhases = require("Database.Corrections.ContentPhases.ContentPhases")
 
 describe("QuestieQuestBlacklist", function()
     it("should blacklist SoD quests which are never available", function()
         Questie.IsSoD = true
+
         local questToBlacklist = QuestieQuestBlacklist:Load()
 
         assert.is_true(questToBlacklist[971]) -- Blackfathom Deeps
@@ -15,5 +18,18 @@ describe("QuestieQuestBlacklist", function()
         assert.is_true(questToBlacklist[1446]) -- Sunken Temple
         assert.is_true(questToBlacklist[1203]) -- Jarl Needs a Blade - Replaced by 81570
         assert.is_true(questToBlacklist[1878]) -- Water Pouch Bounty - Replaced by 82209
+    end)
+
+    it("should blacklist Classic Anniversary quests", function()
+        Questie.IsSoD = false
+        Questie.IsAnniversary = true
+        ContentPhases.activePhases.Anniversary = 3
+
+        local questToBlacklist = QuestieQuestBlacklist:Load()
+
+        assert.is_nil(questToBlacklist[7761]) -- Phase 3
+        assert.is_true(questToBlacklist[8411]) -- Phase 4
+        assert.is_true(questToBlacklist[8277]) -- Phase 5
+        assert.is_true(questToBlacklist[9085]) -- Phase 6
     end)
 end)
