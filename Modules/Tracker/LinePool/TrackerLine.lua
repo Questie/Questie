da1b54cd3a610ea62e6a6ad3d0bbb9b9eaefe4c1
BreakBB <b.boeing@online.de>
2024-11-23 22:10:03 +0100

    Extract ExpandZoneButton.lua

--- Modules/Tracker/LinePool/TrackerLine.lua
+++ Modules/Tracker/LinePool/TrackerLine.lua
@@ -17,6 +17,8 @@ local TrackerMenu = QuestieLoader:ImportModule("TrackerMenu")
 local VoiceOverPlayButton = QuestieLoader:ImportModule("VoiceOverPlayButton")
 ---@type ExpandQuestButton
 local ExpandQuestButton = QuestieLoader:ImportModule("ExpandQuestButton")
+---@type ExpandZoneButton
+local ExpandZoneButton = QuestieLoader:ImportModule("ExpandZoneButton")
 ---@type QuestieCombatQueue
 local QuestieCombatQueue = QuestieLoader:ImportModule("QuestieCombatQueue")
 ---@type QuestieLink
@@ -203,110 +205,7 @@ function TrackerLine.New(index, parent, previousLine, OnEnter, OnLeave, OnQuestA
 
     line.criteriaMark = criteriaMark
 
-    -- create expanding zone headers for quests sorted by zones
-    local expandZone = CreateFrame("Button", "linePool.expandZone" .. index, line)
-    expandZone:SetWidth(1)
-    expandZone:SetHeight(1)
-    expandZone:SetPoint("TOPLEFT", line, "TOPLEFT", 0, 0)
-
-    function expandZone:SetMode(mode)
-        if mode ~= self.mode then
-            self.mode = mode
-        end
-    end
-
-    expandZone:SetMode(1) -- maximized
-    expandZone:EnableMouse(true)
-    expandZone:RegisterForDrag("LeftButton")
-    expandZone:RegisterForClicks("LeftButtonUp", "LeftButtonDown", "RightButtonUp", "RightButtonDown")
-
-    expandZone:SetScript("OnMouseDown", function(self, button)
-        if button == "LeftButton" then
-            if IsShiftKeyDown() then
-                -- This sets up the minAllQuestsInZone table upon first click
-                if not Questie.db.char.collapsedZones[self.zoneId] then
-                    if not Questie.db.char.minAllQuestsInZone[self.zoneId] then
-                        Questie.db.char.minAllQuestsInZone[self.zoneId] = {}
-                        -- This flag prevents repopulating QuestID's where we don't want them.
-                        Questie.db.char.minAllQuestsInZone[self.zoneId].isTrue = true
-
-                        QuestieCombatQueue:Queue(function()
-                            QuestieTracker:Update()
-                        end)
-                    end
-                end
-            end
-        end
-    end)
-
-    expandZone:SetScript("OnMouseUp", function(self, button)
-        if button == "LeftButton" then
-            if IsShiftKeyDown() then
-                if not Questie.db.char.collapsedZones[self.zoneId] then
-                    C_Timer.After(0.1, function()
-                        if Questie.db.char.minAllQuestsInZone[self.zoneId].isTrue then
-                            -- Places all QuestID's into the collapsedQuests table and keeps the Min/Max buttons in sync.
-                            Questie:Debug(Questie.DEBUG_DEVELOP, "[TrackerLine:minAllQuestsInZone] - Minimize")
-                            for questId, _ in pairs(Questie.db.char.minAllQuestsInZone[self.zoneId]) do
-                                if type(questId) == "number" then
-                                    Questie.db.char.collapsedQuests[questId] = true
-                                end
-                            end
-
-                            Questie.db.char.minAllQuestsInZone[self.zoneId].isTrue = nil
-                        else
-                            -- Removes all QuestID's from the collapsedQuests table.
-                            Questie:Debug(Questie.DEBUG_DEVELOP, "[TrackerLine:minAllQuestsInZone] - Maximize")
-                            for questId, _ in pairs(Questie.db.char.minAllQuestsInZone[self.zoneId]) do
-                                if type(questId) == "number" then
-                                    Questie.db.char.collapsedQuests[questId] = nil
-                                end
-                            end
-
-                            Questie.db.char.minAllQuestsInZone[self.zoneId] = nil
-                        end
-
-                        QuestieCombatQueue:Queue(function()
-                            QuestieTracker:Update()
-                        end)
-                    end)
-                end
-            else
-                if self.mode == 1 then
-                    self:SetMode(0)
-                    Questie:Debug(Questie.DEBUG_DEVELOP, "[TrackerLine:expandZone] - Minimize")
-                else
-                    self:SetMode(1)
-                    Questie:Debug(Questie.DEBUG_DEVELOP, "[TrackerLine:expandZone] - Maximize")
-                end
-
-                if Questie.db.char.collapsedZones[self.zoneId] == true then
-                    Questie.db.char.collapsedZones[self.zoneId] = nil
-                else
-                    Questie.db.char.collapsedZones[self.zoneId] = true
-                end
-
-                QuestieCombatQueue:Queue(function()
-                    QuestieTracker:Update()
-                end)
-            end
-        end
-    end)
-
-    expandZone:SetScript("OnEnter", function(self)
-        OnEnter(self)
-        TrackerFadeTicker.Unfade()
-    end)
-
-    expandZone:SetScript("OnLeave", function(self)
-        OnLeave(self)
-        TrackerFadeTicker.Fade()
-    end)
-
-    expandZone:Hide()
-
-    line.expandZone = expandZone
-
+    line.expandZone = ExpandZoneButton.New(index, line, OnEnter, OnLeave)
     line.playButton = VoiceOverPlayButton.New(index, line)
     line.expandQuest = ExpandQuestButton.New(index, line)
 
