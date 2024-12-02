b22828313addb106db50cb4f6eeed12df5d467cb
Sommos <Sommosgaming@gmail.com>
2024-12-02 17:07:29 +0000

    Fix typo

--- Database/Corrections/cataQuestFixes.lua
+++ Database/Corrections/cataQuestFixes.lua
@@ -4357,7 +4357,7 @@ function CataQuestFixes.Load()
             [questKeys.preQuestSingle] = {25505},
             [questKeys.requiredSourceItems] = {54821},
         },
-        [25533] = { -- Pirate Accuracy Increasing (Alliance)
+        [25532] = { -- Pirate Accuracy Increasing (Alliance)
             [questKeys.preQuestSingle] = {},
             [questKeys.preQuestGroup] = {25515,25517,25524},
         },
