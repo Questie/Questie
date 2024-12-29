35a959173d6fdaf250f47ea3fa9907d0e5cf1225
BreakBB <b.boeing@online.de>
2024-12-29 16:23:12 +0100

    Extract _SetupEnglishData and change quest to check

--- Localization/l10n.test.lua
+++ Localization/l10n.test.lua
@@ -12,13 +12,21 @@ describe("l10n", function()
     ---@type l10n
     local l10n
 
-    before_each(function()
-        l10n = require("Localization.l10n")
-        QuestieDB = require("Database.QuestieDB")
+    local function _SetupEnglishData()
         QuestieDB.itemData = {[750] = {[QuestieDB.itemKeys.name] = "Tough Wolf Meat"}}
         QuestieDB.npcData = {[99] = {[QuestieDB.npcKeys.name] = "Morgaine the Sly"}}
         QuestieDB.objectData = {[60] = {[QuestieDB.objectKeys.name] = "WANTED: Gath’Ilzogg"}}
-        QuestieDB.questData = {[1499] = {[QuestieDB.questKeys.name] = "Vile Familiars"}}
+        QuestieDB.questData = {[1359] = {[QuestieDB.questKeys.name] = "Zinge's Delivery"}}
+        l10n.itemLookup = {}
+        l10n.questLookup = {}
+        l10n.npcNameLookup = {}
+        l10n.objectLookup = {}
+    end
+
+    before_each(function()
+        l10n = require("Localization.l10n")
+        QuestieDB = require("Database.QuestieDB")
+        _SetupEnglishData()
     end)
 
     it("should keep enUS names without lookup", function()
@@ -30,7 +38,7 @@ describe("l10n", function()
         assert.are_same("Tough Wolf Meat", QuestieDB.itemData[750][QuestieDB.itemKeys.name])
         assert.are_same("Morgaine the Sly", QuestieDB.npcData[99][QuestieDB.npcKeys.name])
         assert.are_same("WANTED: Gath’Ilzogg", QuestieDB.objectData[60][QuestieDB.objectKeys.name])
-        assert.are_same("Vile Familiars", QuestieDB.questData[1499][QuestieDB.questKeys.name])
+        assert.are_same("Zinge's Delivery", QuestieDB.questData[1359][QuestieDB.questKeys.name])
     end)
 
     it("should load deDE lookups", function()
@@ -46,7 +54,7 @@ describe("l10n", function()
         assert.are_same("Zähes Wolfsfleisch", QuestieDB.itemData[750][QuestieDB.itemKeys.name])
         assert.are_same("Morgaine die Verschlagene", QuestieDB.npcData[99][QuestieDB.npcKeys.name])
         assert.are_same("GESUCHT: Gath’Ilzogg", QuestieDB.objectData[60][QuestieDB.objectKeys.name])
-        assert.are_same("Üble Familiare", QuestieDB.questData[1499][QuestieDB.questKeys.name])
+        assert.are_same("Zinges Lieferung", QuestieDB.questData[1359][QuestieDB.questKeys.name])
     end)
 
     it("should load esES lookups", function()
@@ -62,7 +70,7 @@ describe("l10n", function()
         assert.are_same("Carne de lobo dura", QuestieDB.itemData[750][QuestieDB.itemKeys.name])
         assert.are_same("Morgaine el Astuto", QuestieDB.npcData[99][QuestieDB.npcKeys.name])
         assert.are_same("Se busca: Gath'Ilzogg", QuestieDB.objectData[60][QuestieDB.objectKeys.name])
-        assert.are_same("Familiares viles", QuestieDB.questData[1499][QuestieDB.questKeys.name])
+        assert.are_same("Una entrega de Zinge", QuestieDB.questData[1359][QuestieDB.questKeys.name])
     end)
 
     it("should load esMX lookups", function()
@@ -78,7 +86,7 @@ describe("l10n", function()
         assert.are_same("Carne de lobo dura", QuestieDB.itemData[750][QuestieDB.itemKeys.name])
         assert.are_same("Morgaine el Astuto", QuestieDB.npcData[99][QuestieDB.npcKeys.name])
         assert.are_same("Se busca: Gath'Ilzogg", QuestieDB.objectData[60][QuestieDB.objectKeys.name])
-        assert.are_same("Familiares viles", QuestieDB.questData[1499][QuestieDB.questKeys.name])
+        assert.are_same("Una entrega de Zinge", QuestieDB.questData[1359][QuestieDB.questKeys.name])
     end)
 
     it("should load frFR lookups", function()
@@ -94,7 +102,7 @@ describe("l10n", function()
         assert.are_same("Viande de loup coriace", QuestieDB.itemData[750][QuestieDB.itemKeys.name])
         assert.are_same("Morgaine la rusée", QuestieDB.npcData[99][QuestieDB.npcKeys.name])
         assert.are_same("Recherché : Gath'Ilzogg", QuestieDB.objectData[60][QuestieDB.objectKeys.name])
-        assert.are_same("Les vils quasits", QuestieDB.questData[1499][QuestieDB.questKeys.name])
+        assert.are_same("Une livraison pour Zinge", QuestieDB.questData[1359][QuestieDB.questKeys.name])
     end)
 
     it("should load koKR lookups", function()
@@ -110,7 +118,7 @@ describe("l10n", function()
         assert.are_same("질긴 늑대 고기", QuestieDB.itemData[750][QuestieDB.itemKeys.name])
         assert.are_same("교활한 도적 몰게니", QuestieDB.npcData[99][QuestieDB.npcKeys.name])
         assert.are_same("현상수배: 가스일조그", QuestieDB.objectData[60][QuestieDB.objectKeys.name])
-        assert.are_same("악마의 하수인", QuestieDB.questData[1499][QuestieDB.questKeys.name])
+        assert.are_same("진게의 소포", QuestieDB.questData[1359][QuestieDB.questKeys.name])
     end)
 
     it("should load ptBR lookups", function()
@@ -126,7 +134,7 @@ describe("l10n", function()
         assert.are_same("Carne Dura de Lobo", QuestieDB.itemData[750][QuestieDB.itemKeys.name])
         assert.are_same("Morgana, a Dissimulada", QuestieDB.npcData[99][QuestieDB.npcKeys.name])
         assert.are_same("Procura-se: Gath'Ilzogg", QuestieDB.objectData[60][QuestieDB.objectKeys.name])
-        assert.are_same("Familiares torpes", QuestieDB.questData[1499][QuestieDB.questKeys.name])
+        assert.are_same("Entrega para Zilda", QuestieDB.questData[1359][QuestieDB.questKeys.name])
     end)
 
     it("should load ruRU lookups", function()
@@ -142,7 +150,7 @@ describe("l10n", function()
         assert.are_same("Жесткое волчье мясо", QuestieDB.itemData[750][QuestieDB.itemKeys.name])
         assert.are_same("Моргана Лукавая", QuestieDB.npcData[99][QuestieDB.npcKeys.name])
         assert.are_same("Разыскивается: Гат'Илзогг", QuestieDB.objectData[60][QuestieDB.objectKeys.name])
-        assert.are_same("Злобные фамильяры", QuestieDB.questData[1499][QuestieDB.questKeys.name])
+        assert.are_same("Посылка для Зинг", QuestieDB.questData[1359][QuestieDB.questKeys.name])
     end)
 
     it("should load zhCN lookups", function()
@@ -158,7 +166,7 @@ describe("l10n", function()
         assert.are_same("硬狼肉", QuestieDB.itemData[750][QuestieDB.itemKeys.name])
         assert.are_same("狡猾的莫加尼", QuestieDB.npcData[99][QuestieDB.npcKeys.name])
         assert.are_same("通缉：加塞尔佐格", QuestieDB.objectData[60][QuestieDB.objectKeys.name])
-        assert.are_same("邪灵劣魔", QuestieDB.questData[1499][QuestieDB.questKeys.name])
+        assert.are_same("给金格的货物", QuestieDB.questData[1359][QuestieDB.questKeys.name])
     end)
 
     it("should load zhTW lookups", function()
@@ -174,6 +182,6 @@ describe("l10n", function()
         assert.are_same("硬狼肉", QuestieDB.itemData[750][QuestieDB.itemKeys.name])
         assert.are_same("狡猾的莫加尼", QuestieDB.npcData[99][QuestieDB.npcKeys.name])
         assert.are_same("懸賞:加塞爾佐格", QuestieDB.objectData[60][QuestieDB.objectKeys.name])
-        assert.are_same("邪靈劣魔", QuestieDB.questData[1499][QuestieDB.questKeys.name])
+        assert.are_same("給金格的貨物", QuestieDB.questData[1359][QuestieDB.questKeys.name])
     end)
 end)
