a95e247f0838ed3317f761c34ff3aee8e6dc604a
BreakBB <b.boeing@online.de>
2025-01-19 12:02:56 +0100

    Refactor match_dungeon_spawns.py

--- ExternalScripts(DONOTINCLUDEINRELEASE)/scraper/match_dungeon_spawns.py
+++ ExternalScripts(DONOTINCLUDEINRELEASE)/scraper/match_dungeon_spawns.py
@@ -1,46 +1,31 @@
 import re
 
+dungeons = [
+    ["Blackfathom Deeps", "719"],
+    ["Shadowfang Keep", "209"],
+    ["Scarlet Monastery", "796"],
+    ["Gnomeregan", "721"],
+    ["The Temple of Atal'Hakkar", "1477"],
+    ["Molten Core", "2717"],
+    ["Demon Fall Canyon", "15475"],
+    ["The Burning of Andorhal", "15828"]
+]
 
 def match_dungeon_spawns(text):
     spawns = []
     zone_id = None
     zone_id_match = re.search(r"zone=(\d+)", text)
-    zone_name_match = re.search(r"Shadowfang Keep|Blackfathom Deeps|Scarlet Monastery|Gnomeregan|The Temple of Atal'Hakkar|Molten Core|Demon Fall Canyon|The Burning of Andorhal", text)
+    dungeon_names_regex = "|".join([dungeon[0] for dungeon in dungeons])
+    zone_name_match = re.search(dungeon_names_regex, text)
     if zone_id_match:
         zone_id = zone_id_match.group(1)
-        if (zone_id == "719" or  # Blackfathom Deeps
-                zone_id == "209" or  # Shadowfang Keep
-                zone_id == "796" or  # Scarlet Monastery
-                zone_id == "1477" or  # The Temple of Atal'Hakkar
-                zone_id == "2717" or  # Molten Core
-                zone_id == "721" or  # Gnomeregan
-                zone_id == "15475" or # Demon Fall Canyon
-                zone_id == "15828"): # The Burning of Andorhal
+        dungeon_zone_ids = [dungeon[1] for dungeon in dungeons]
+        if zone_id in dungeon_zone_ids:
             spawns = [[zone_id, "[-1,-1]"]]
     elif zone_name_match:
         zone_name = zone_name_match.group(0)
-        if zone_name == "Blackfathom Deeps":
-            zone_id = "719"
-            spawns = [["719", "[-1,-1]"]]
-        elif zone_name == "Shadowfang Keep":
-            zone_id = "209"
-            spawns = [["209", "[-1,-1]"]]
-        elif zone_name == "Scarlet Monastery":
-            zone_id = "796"
-            spawns = [["796", "[-1,-1]"]]
-        elif zone_name == "Gnomeregan":
-            zone_id = "721"
-            spawns = [["721", "[-1,-1]"]]
-        elif zone_name == "The Temple of Atal'Hakkar":
-            zone_id = "1477"
-            spawns = [["1477", "[-1,-1]"]]
-        elif zone_name == "Molten Core":
-            zone_id = "2717"
-            spawns = [["2717", "[-1,-1]"]]
-        elif zone_name == "Demon Fall Canyon":
-            zone_id = "15475"
-            spawns = [["15475", "[-1,-1]"]]
-        elif zone_name == "The Burning of Andorhal":
-            zone_id = "15828"
-            spawns = [["15828", "[-1,-1]"]]
+
+        dungeon = [dungeon for dungeon in dungeons if dungeon[0] == zone_name][0]
+        zone_id = dungeon[1]
+        spawns = [[zone_id, "[-1,-1]"]]
     return spawns, zone_id
