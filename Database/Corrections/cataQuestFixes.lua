7de11281a5e87c167d8d1a874235b3c4a7d55d5b
coldpasta123 <170130800+coldpasta123@users.noreply.github.com>
2025-03-16 16:25:16 +0000

    [quest] Fix prequests for Lindsay Ravensun, Revealed

--- Database/Corrections/cataQuestFixes.lua
+++ Database/Corrections/cataQuestFixes.lua
@@ -8269,6 +8269,9 @@ function CataQuestFixes.Load()
         [27086] = { -- Ashes to Ashes (Horde)
             [questKeys.preQuestSingle] = {27083},
         },
+        [27087] = { -- Lindsay Ravensun, Revealed
+            [questKeys.preQuestGroup] = {27084,27085,27086},
+        },
         [27091] = { -- Charge!
             [questKeys.objectives] = {{{44937}},nil,nil,nil,nil,{{100}}},
             [questKeys.extraObjectives] = {{nil, Questie.ICON_TYPE_TALK, l10n("Learn Spell: Charge"), 2, {{"monster", 43010}}}},
