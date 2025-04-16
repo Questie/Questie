95856d5449739810dc7396ec4a6fc113a60536df
BreakBB <b.boeing@online.de>
2025-04-16 07:37:56 +0200

    [quest] Add finisher for "Familiar Foulness"

--- Database/Corrections/sodQuestFixes.lua
+++ Database/Corrections/sodQuestFixes.lua
@@ -4898,7 +4898,7 @@ function SeasonOfDiscovery:LoadQuests()
         },
         [90560] = { -- Familiar Foulness
             [questKeys.startedBy] = {nil,{531291},{241655}},
-            [questKeys.finishedBy] = nil,
+            [questKeys.finishedBy] = {{2055}},
             [questKeys.objectives] = {nil,nil,{{241652}}},
             [questKeys.sourceItemId] = 241655,
             [questKeys.zoneOrSort] = zoneIDs.NEW_AVALON,
