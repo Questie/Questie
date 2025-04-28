31e5a6a9bb51c27caf9355b06689928923c2ec87
coldpasta123 <170130800+coldpasta123@users.noreply.github.com>
2025-04-28 22:03:49 +0100

    Fix phasing
    Co-authored-by: Björn Heiß <b.boeing@online.de>

--- Modules/Phasing.lua
+++ Modules/Phasing.lua
@@ -666,7 +666,7 @@ function Phasing.IsSpawnVisible(phase)
     end
 
     if phase == phases.RHEA_NEW_KARGATH then
-        return complete[27769] and (questLog[27889] and questLog[27889].isComplete == 1) and ((not complete[27897]) and ((not questLog[27897]) or (questLog[27897] and questLog[27897].isComplete == 0))) or false
+        return complete[27769] and questLog[27889] and (not complete[27897]) and (not questLog[27897]) or false
     end
 
     if phase == phases.RHEA_DRAGONS_MOUTH then
