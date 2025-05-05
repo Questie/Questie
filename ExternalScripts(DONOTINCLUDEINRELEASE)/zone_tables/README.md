1b56df0912ba98b0177a24a964f7a48c14b6070d
BreakBB <b.boeing@online.de>
2025-05-05 14:37:02 +0200

    Add a first script version to generate MoP zone tables

---
+++ ExternalScripts(DONOTINCLUDEINRELEASE)/zone_tables/README.md
@@ -0,0 +1,14 @@
+# Generate zoneTables
+
+This directory contains files to generate the zoneTables Questie uses to map areaIds to the actual ingame UiMapIDs. 
+
+## Where does the data come from?
+
+The CSV files used in the script were taken from https://wago.tools/. The patch and build number is included in the file name.
+
+## Running the script
+
+1. Make sure you have Python 3.x installed on your system.
+2. Run the `generate_zone_tables.py` script.
+3. Take the content of the generated `areaIdToUiMapId.lua` file and replace the previously used data.
+
---
+++ ExternalScripts(DONOTINCLUDEINRELEASE)/zone_tables/generate_zone_tables.py
@@ -0,0 +1,68 @@
+import csv
+
+print('Reading cata files...')
+
+cata_area_table = {}
+with open('../DBC - WoW.tools/areatable_cata.csv', 'r') as f:
+    reader = csv.DictReader(f)
+    for row in reader:
+        cata_area_table[row['ID']] = row
+
+cata_uimap = {}
+with open('../DBC - WoW.tools/uimap_cata.csv', 'r') as f:
+    reader = csv.DictReader(f)
+    for row in reader:
+        cata_uimap[row['ID']] = row
+
+cata_uimap_assignment = {}
+with open('../DBC - WoW.tools/uimapassignment_cata.csv', 'r') as f:
+    reader = csv.DictReader(f)
+    for row in reader:
+        cata_uimap_assignment[row['ID']] = row
+
+print('Reading mop files...')
+
+mop_area_table = {}
+with open('../DBC - WoW.tools/AreaTable.5.5.0.60548.csv', 'r') as f:
+    reader = csv.DictReader(f)
+    for row in reader:
+        mop_area_table[row['ID']] = row
+
+mop_uimap = {}
+with open('../DBC - WoW.tools/Uimap.5.5.0.60548.csv', 'r') as f:
+    reader = csv.DictReader(f)
+    for row in reader:
+        mop_uimap[row['ID']] = row
+
+mop_uimap_assignment = {}
+with open('../DBC - WoW.tools/Uimapassignment.5.5.0.60548.csv', 'r') as f:
+    reader = csv.DictReader(f)
+    for row in reader:
+        mop_uimap_assignment[row['ID']] = row
+
+print("Successfully read files")
+print("Generating mappings...")
+
+area_id_to_ui_map_id = {}
+ui_map_id_to_area_id = {}
+for row_id in mop_uimap_assignment:
+    row = mop_uimap_assignment[row_id]
+    if row['OrderIndex'] == '0' and row['AreaID'] != '0':
+        if row['AreaID'] not in area_id_to_ui_map_id:
+            area_id_to_ui_map_id[row['AreaID']] = row['UiMapID']
+        else:
+            print('double for AreaID:', row['AreaID'])
+        if row['UiMapID'] not in ui_map_id_to_area_id:
+            ui_map_id_to_area_id[row['UiMapID']] = row['AreaID']
+        else:
+            print('double for UiMapID:', row['UiMapID'])
+
+print("Successfully generated mappings")
+
+print("Printing results into areaIdToUiMapId.lua...")
+with open('areaIdToUiMapId.lua', 'w') as f:
+    f.write('ZoneDB.private.areaIdToUiMapId = {\n')
+    for area_id in sorted(area_id_to_ui_map_id.keys(), key=lambda x: int(x)):
+        map_id = area_id_to_ui_map_id[area_id]
+        f.write(f'    [{area_id}] = {map_id}, -- {mop_uimap[map_id]["Name_lang"]}\n')
+    f.write('}\n')
