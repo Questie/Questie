3d93a6491d4ce87dc3798ac0c49d2648dd02d8c6
BreakBB <b.boeing@online.de>
2025-05-09 16:21:24 +0200

    Also generate cata files

--- ExternalScripts(DONOTINCLUDEINRELEASE)/zone_tables/generate_zone_tables.py
+++ ExternalScripts(DONOTINCLUDEINRELEASE)/zone_tables/generate_zone_tables.py
@@ -42,7 +42,7 @@ with open('../DBC - WoW.tools/Uimapassignment.5.5.0.60548.csv', 'r') as f:
         mop_uimap_assignment[row['ID']] = row
 
 print("Successfully read files")
-print("Generating mappings...")
+print("Generating MoP mappings...")
 
 area_id_to_ui_map_id = {}
 ui_map_id_to_area_id = {}
@@ -76,7 +76,7 @@ for row_id in mop_uimap_assignment:
         else:
             print('double for UiMapID:', map_id)
 
-print("Successfully generated mappings")
+print("Successfully generated MoP mappings")
 
 if not os.path.exists('mop'):
     os.makedirs('mop')
@@ -99,4 +99,63 @@ with open('mop/uiMapIdToAreaId.lua', 'w') as f:
         f.write(f'    [{map_id}] = {area_id}, -- {mop_uimap[map_id]["Name_lang"]}{" - (Dungeon/Raid)" if is_dungeon_or_raid else ""}\n')
     f.write('}]]')
 
-print("Done!")
+print("Done with MoP!")
+
+print("Generating Cata mappings...")
+
+area_id_to_ui_map_id = {}
+ui_map_id_to_area_id = {}
+for row_id in cata_uimap_assignment:
+    row = cata_uimap_assignment[row_id]
+    if row['OrderIndex'] == '0':
+        area_id = row['AreaID']
+        map_id = row['UiMapID']
+
+        if cata_uimap[map_id]['Type'] == '5': # 5 = "Micro-Dungeon"
+            map_id = cata_uimap[map_id]['ParentUiMapID']
+
+        if area_id == '0':
+            # Some entries in cata_uimap_assignment have AreaID 0, even though there is an areaId in cata_area_table.
+            # So we search for it by the zone name.
+            name_lang = cata_uimap[map_id]['Name_lang']
+            for cata_area_id, area in cata_area_table.items():
+                if area['AreaName_lang'] == name_lang:
+                    area_id = cata_area_id
+                    break
+        if area_id == '0':
+            print('AreaID is 0 for UiMapID:', map_id, cata_uimap[map_id]["Name_lang"])
+            continue
+
+        if area_id not in area_id_to_ui_map_id:
+            area_id_to_ui_map_id[area_id] = map_id
+        else:
+            print('double for AreaID:', area_id)
+        if map_id not in ui_map_id_to_area_id:
+            ui_map_id_to_area_id[map_id] = area_id
+        else:
+            print('double for UiMapID:', map_id)
+
+print("Successfully generated Cata mappings")
+
+if not os.path.exists('cata'):
+    os.makedirs('cata')
+
+print("Printing results into cata/areaIdToUiMapId.lua...")
+with open('cata/areaIdToUiMapId.lua', 'w') as f:
+    f.write('ZoneDB.private.areaIdToUiMapId = [[return {\n')
+    for area_id in sorted(area_id_to_ui_map_id.keys(), key=lambda x: int(x)):
+        map_id = area_id_to_ui_map_id[area_id]
+        is_dungeon_or_raid = cata_uimap[map_id]['Type'] == '4' and cata_uimap[map_id]['ParentUiMapID'] != 0
+        f.write(f'    [{area_id}] = {map_id}, -- {cata_uimap[map_id]["Name_lang"]}{" - (Dungeon/Raid)" if is_dungeon_or_raid else ""}\n')
+    f.write('}]]')
+
+print("Printing results into cata/uiMapIdToAreaId.lua...")
+with open('cata/uiMapIdToAreaId.lua', 'w') as f:
+    f.write('ZoneDB.private.uiMapIdToAreaId = [[return {\n')
+    for map_id in sorted(ui_map_id_to_area_id.keys(), key=lambda x: int(x)):
+        area_id = ui_map_id_to_area_id[map_id]
+        is_dungeon_or_raid = cata_uimap[map_id]['Type'] == '4' and cata_uimap[map_id]['ParentUiMapID'] != 0
+        f.write(f'    [{map_id}] = {area_id}, -- {cata_uimap[map_id]["Name_lang"]}{" - (Dungeon/Raid)" if is_dungeon_or_raid else ""}\n')
+    f.write('}]]')
+
+print("Done with Cata!")
