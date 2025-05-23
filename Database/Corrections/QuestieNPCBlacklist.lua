1be5ac34e635a7653868fe8a957a9bc98432d983
BreakBB <b.boeing@online.de>
2025-05-23 22:53:24 +0200

    Refactor npc blacklisting to use Expansions

--- Database/Corrections/QuestieNPCBlacklist.lua
+++ Database/Corrections/QuestieNPCBlacklist.lua
@@ -1,26 +1,26 @@
 ---@class QuestieNPCBlacklist
 local QuestieNPCBlacklist = QuestieLoader:CreateModule("QuestieNPCBlacklist")
----@type QuestieCorrections
-local QuestieCorrections = QuestieLoader:ImportModule("QuestieCorrections")
+---@type Expansions
+local Expansions = QuestieLoader:ImportModule("Expansions")
 
 ---@return table<NpcId, boolean>
 function QuestieNPCBlacklist:Load()
     return {
-        [3032] =  QuestieCorrections.CATA_HIDE, -- Beram Skychaser
-        [7666] =  QuestieCorrections.CATA_HIDE, -- Archmage Allistarj
+        [3032] = Expansions.Current >= Expansions.Cata, -- Beram Skychaser
+        [7666] = Expansions.Current >= Expansions.Cata, -- Archmage Allistarj
         [8001] = true,
-        [12397] = QuestieCorrections.SOD_HIDE, -- Lord Kazzak
-        [13151] = QuestieCorrections.CLASSIC_HIDE, -- Syndicate Master Ryson
-        [13153] = QuestieCorrections.CLASSIC_HIDE, -- Commander Mulfort
-        [13154] = QuestieCorrections.CLASSIC_HIDE, -- Commander Louis Philips
-        [13319] = QuestieCorrections.CLASSIC_HIDE, -- Commander Duffy
-        [13320] = QuestieCorrections.CLASSIC_HIDE, -- Commander Karl Philips
-        [13377] = QuestieCorrections.CLASSIC_HIDE, -- Master Engineer Zinfizzlex
-        [13446] = QuestieCorrections.CLASSIC_HIDE, -- Field Marshal Teravaine
-        [13449] = QuestieCorrections.CLASSIC_HIDE, -- Warmaster Garrick
-        [15799] = QuestieCorrections.CLASSIC_HIDE, -- Colossus Researcher Eazel (AQ Opening event)
+        [12397] = Questie.IsSoD, -- Lord Kazzak
+        [13151] = Expansions.Current == Expansions.Era, -- Syndicate Master Ryson
+        [13153] = Expansions.Current == Expansions.Era, -- Commander Mulfort
+        [13154] = Expansions.Current == Expansions.Era, -- Commander Louis Philips
+        [13319] = Expansions.Current == Expansions.Era, -- Commander Duffy
+        [13320] = Expansions.Current == Expansions.Era, -- Commander Karl Philips
+        [13377] = Expansions.Current == Expansions.Era, -- Master Engineer Zinfizzlex
+        [13446] = Expansions.Current == Expansions.Era, -- Field Marshal Teravaine
+        [13449] = Expansions.Current == Expansions.Era, -- Warmaster Garrick
+        [15799] = Expansions.Current == Expansions.Era, -- Colossus Researcher Eazel (AQ Opening event)
         [17544] = true, -- M'uru in Silvermoon City removed starting with SWP patch
         [21155] = true, -- Bloodelf War Effort Recruiter
-        [178420] =  QuestieCorrections.WOTLK_HIDE + QuestieCorrections.CATA_HIDE,
+        [178420] = Expansions.Current >= Expansions.Wotlk,
     }
 end
