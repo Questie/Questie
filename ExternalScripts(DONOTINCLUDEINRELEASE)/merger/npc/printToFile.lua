5114c9fefb1a8a9093634e7c080ce1ef54741679
BreakBB <b.boeing@online.de>
2025-06-20 07:09:04 +0200

    Extract pairsByKeys.lua

--- ExternalScripts(DONOTINCLUDEINRELEASE)/merger/npc/printToFile.lua
+++ ExternalScripts(DONOTINCLUDEINRELEASE)/merger/npc/printToFile.lua
@@ -1,18 +1,5 @@
 local lfs = require("lfs")
-
-local function pairsByKeys(t, f)
-    local a = {}
-    for n in pairs(t) do table.insert(a, n) end
-    table.sort(a, f)
-    local i = 0      -- iterator variable
-    local iter = function ()   -- iterator function
-        i = i + 1
-        if a[i] == nil then return nil
-        else return a[i], t[a[i]]
-        end
-    end
-    return iter
-end
+local pairsByKeys = dofile("../pairsByKeys.lua")
 
 local function printToFile(npcData, npcKeys)
     lfs.mkdir("output")
