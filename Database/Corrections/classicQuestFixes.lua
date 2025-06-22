df6c410d9b496be6f4bb0efbb5b4549dd20b78ac
BreakBB <b.boeing@online.de>
2025-06-22 13:20:24 +0200

    [quest] Fix quest level for "Leggings of Arcana"
    Fixes #7028

--- Database/Corrections/classicQuestFixes.lua
+++ Database/Corrections/classicQuestFixes.lua
@@ -1781,7 +1781,8 @@ function QuestieQuestFixes:Load()
         [5063] = {
             [questKeys.specialFlags] = specialFlags.REPEATABLE, -- #1335
         },
-        [5067] = {
+        [5067] = { -- Leggings of Arcana
+            [questKeys.requiredLevel] = 60,
             [questKeys.specialFlags] = specialFlags.REPEATABLE, -- #1335
         },
         [5068] = {
