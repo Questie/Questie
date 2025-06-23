0386a1c395f1de17186d286f31ce8f8cc9c16f67
BreakBB <b.boeing@online.de>
2025-06-23 21:35:57 +0200

    [quest] Hide "Fall From Grace" for Cata onwards

--- Database/Corrections/QuestieQuestBlacklist.lua
+++ Database/Corrections/QuestieQuestBlacklist.lua
@@ -3069,6 +3069,7 @@ function QuestieQuestBlacklist:Load()
         [2781] = Expansions.Current >= Expansions.Cata, -- Removed with cata
         [2782] = Expansions.Current >= Expansions.Cata, -- Removed with cata
         [2783] = Expansions.Current >= Expansions.Cata, -- Removed with cata
+        [2784] = Expansions.Current >= Expansions.Cata, -- Removed with cata
         [2801] = Expansions.Current >= Expansions.Cata, -- Removed with cata
         [2821] = Expansions.Current >= Expansions.Cata, -- Removed with cata
         [2822] = Expansions.Current >= Expansions.Cata, -- Removed with cata
