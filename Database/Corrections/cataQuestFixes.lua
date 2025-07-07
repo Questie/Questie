e4b2303334b0ef71df853cc1ceda3104f83c79f4
coldpasta123 <170130800+coldpasta123@users.noreply.github.com>
2025-07-07 09:26:50 +0100

    Add prequests as Alliance only

--- Database/Corrections/cataQuestFixes.lua
+++ Database/Corrections/cataQuestFixes.lua
@@ -15592,6 +15592,14 @@ function CataQuestFixes:LoadFactionFixes()
         [24911] = { -- Tropical Paradise Beckons
             [questKeys.startedBy] = {{38578}},
         },
+        [25422] = { -- The Darkmist Legacy
+            [questKeys.exclusiveTo] = {},
+            [questKeys.preQuestSingle] = {25350},
+        },
+        [25423] = { -- Ancient Suffering
+            [questKeys.exclusiveTo] = {},
+            [questKeys.preQuestSingle] = {25350},
+        },
         [25513] = { -- Thunderdrome: Grudge Match!
             [questKeys.preQuestGroup] = {25065,25095},
         },
