0f572dfcaec80e9da03b5064d945f3411ec3a9d0
BreakBB <b.boeing@online.de>
2025-07-15 07:42:44 +0200

    Support esMX and zhTW

--- ExternalScripts(DONOTINCLUDEINRELEASE)/scraper/supported_locales.py
+++ ExternalScripts(DONOTINCLUDEINRELEASE)/scraper/supported_locales.py
@@ -2,8 +2,10 @@ LOCALES = [
     {"input": "de", "output": "deDE"},
     {"input": "fr", "output": "frFR"},
     {"input": "es", "output": "esES"},
+    {"input": "mx", "output": "esMX"},
     {"input": "ru", "output": "ruRU"},
     {"input": "pt", "output": "ptBR"},
     {"input": "ko", "output": "koKR"},
     {"input": "cn", "output": "zhCN"},
+    {"input": "tw", "output": "zhTW"},
 ]
