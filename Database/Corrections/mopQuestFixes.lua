3152362b423dc933f4e4f8f330a36279293c3970
coldpasta123 <170130800+coldpasta123@users.noreply.github.com>
2025-07-15 17:21:36 +0100

    [quest] Add prequest for Clever Ashyo

--- Database/Corrections/mopQuestFixes.lua
+++ Database/Corrections/mopQuestFixes.lua
@@ -549,6 +549,9 @@ function MopQuestFixes.Load()
             [questKeys.preQuestSingle] = {},
             [questKeys.preQuestGroup] = {29827,31112},
         },
+        [29871] = { -- Clever Ashyo
+            [questKeys.preQuestSingle] = {30086},
+        },
         [29877] = { -- A Poor Grasp of the Basics
             [questKeys.preQuestSingle] = {29907},
             [questKeys.objectives] = {nil,nil,nil,nil,{{{56146,56149,56150,56151,56278,56279,56280,56281,56146},56146},},},
