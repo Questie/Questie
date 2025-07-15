a893ac7839e3aa2a069fef89a61de28efc7a5dc8
BreakBB <b.boeing@online.de>
2025-07-15 07:56:22 +0200

    [quest] Fix pre-quest of "The Bees' Knees"

--- Database/Corrections/mopQuestFixes.lua
+++ Database/Corrections/mopQuestFixes.lua
@@ -464,6 +464,9 @@ function MopQuestFixes.Load()
         [29827] = { -- Acid Rain
             [questKeys.extraObjectives] = {{nil,Questie.ICON_TYPE_MOUNT_UP,l10n("Get in"),0,{{"monster",55674}}}},
         },
+        [29933] = { -- The Bees' Knees
+            [questKeys.preQuestSingle] = {31167},
+        },
         [30027] = { -- The Lesson of the Iron Bough
             [questKeys.requiredLevel] = 1,
             [questKeys.requiredSourceItems] = {73209},
