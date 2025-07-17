b003b4d5074b363637dcc11ff7eb8ad367492ebd
coldpasta123 <170130800+coldpasta123@users.noreply.github.com>
2025-07-17 21:49:41 +0100

    [quest] Prequest for Blinding the Riverblades

--- Database/Corrections/mopQuestFixes.lua
+++ Database/Corrections/mopQuestFixes.lua
@@ -1351,6 +1351,9 @@ function MopQuestFixes.Load()
             [questKeys.triggerEnd] = {"Accompany Koro to Crane Wing Refuge", {[zoneIDs.KRASARANG_WILDS]={{43.86,36.77}}}},
             [questKeys.extraObjectives] = {{nil,Questie.ICON_TYPE_TALK,l10n("Talk to Koro Mistwalker"),0,{{"monster",58547}}},{nil,Questie.ICON_TYPE_INTERACT,l10n("Talk to Koro Mistwalker"),0,{{"monster",58978}}}},
         },
+        [30270] = { -- Blinding the Riverblades
+            [questKeys.preQuestSingle] = {30269},
+        },
         [30277] = { -- The Crumbling Hall
             [questKeys.preQuestSingle] = {30640},
         },
