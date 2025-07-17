7e8f8e50c2c1eb5f49999b5f04dccf466f275d3a
coldpasta123 <170130800+coldpasta123@users.noreply.github.com>
2025-07-17 12:27:53 +0100

    [quest] Prequest for Mushan Mastery: Darkhide

--- Database/Corrections/mopQuestFixes.lua
+++ Database/Corrections/mopQuestFixes.lua
@@ -1184,6 +1184,9 @@ function MopQuestFixes.Load()
         [30172] = { -- Barreling Along
             [questKeys.triggerEnd] = {"Lead Mudmug back to Halfhill", {[zoneIDs.VALLEY_OF_THE_FOUR_WINDS]={{55.92,49.33}}}},
         },
+        [30184] = { -- Mushan Mastery: Darkhide
+            [questKeys.preQuestSingle] = {30184},
+        },
         [30188] = { -- Riding the Skies (Jade Cloud Serpent)
             [questKeys.preQuestGroup] = {30140,30187},
         },
