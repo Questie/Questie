32d87232cff7938e05dc426c6d052cb5911acd58
coldpasta123 <170130800+coldpasta123@users.noreply.github.com>
2025-07-17 22:21:20 +0100

    [quest] Prequests for Ahead on the Way

--- Database/Corrections/mopQuestFixes.lua
+++ Database/Corrections/mopQuestFixes.lua
@@ -1633,6 +1633,9 @@ function MopQuestFixes.Load()
             [questKeys.preQuestSingle] = {30269},
             [questKeys.objectives] = {nil,{{223819,nil,Questie.ICON_TYPE_INTERACT}}},
         },
+        [30272] = { -- Ahead on the Way
+            [questKeys.preQuestGroup] = {30268,30270,30694}, -- might not be all, my turn in order was 30268, 30694, 30270
+        },
         [30745] = { -- Trouble Brewing
             [questKeys.requiredLevel] = 85,
         },
