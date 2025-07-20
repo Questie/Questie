63d0f35b1082f65a1f63a4fe8c488c377541d977
coldpasta123 <170130800+coldpasta123@users.noreply.github.com>
2025-07-20 11:04:08 +0100

    [quest] Objectives for The Road to Kun-Lai Alliance

--- Database/Corrections/mopQuestFixes.lua
+++ Database/Corrections/mopQuestFixes.lua
@@ -2473,7 +2473,10 @@ function MopQuestFixes.Load()
         [31241] = { -- Wicked Wikkets
             [questKeys.preQuestSingle] = {29879},
         },
-        [31255] = { -- The Road to Kun-Lai
+        [31254] = { -- The Road to Kun-Lai [Alliance]
+            [questKeys.objectives] = {{{62738,nil,Questie.ICON_TYPE_TALK},{63367,nil,Questie.ICON_TYPE_TALK}}},
+        },
+        [31255] = { -- The Road to Kun-Lai [Horde]
             [questKeys.objectives] = {{{62738,nil,Questie.ICON_TYPE_TALK},{63367,nil,Questie.ICON_TYPE_TALK}}},
         },
         [31261] = { -- Captain Jack's Dead
