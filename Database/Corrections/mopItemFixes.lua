f6790f852dce2a96d1e8ebbbf614e0523537f5ed
coldpasta123 <170130800+coldpasta123@users.noreply.github.com>
2025-07-20 22:04:07 +0100

    [db] Add source for Pungent Sprite Needles

--- Database/Corrections/mopItemFixes.lua
+++ Database/Corrections/mopItemFixes.lua
@@ -234,6 +234,9 @@ function MopItemFixes.Load()
         [79884] = { -- Bucket of Slicky Water
             [itemKeys.objectDrops] = {209974},
         },
+        [79952] = { -- Pungent Sprite Needles
+            [itemKeys.npcDrops] = {55593},
+        },
         [80013] = { -- Shademaster Kiryn's Report
             [itemKeys.npcDrops] = {56841},
         },
