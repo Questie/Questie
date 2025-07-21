ff589707b9ba8a28e0e0dc64022b78f3608de81d
coldpasta123 <170130800+coldpasta123@users.noreply.github.com>
2025-07-21 07:39:13 +0100

    Remove duplicated translation

--- Localization/Translations/ExtraObjectives/MopObjectives.lua
+++ Localization/Translations/ExtraObjectives/MopObjectives.lua
@@ -602,18 +602,6 @@ local mopObjectiveLocales = {
         ["zhCN"] = false,
         ["zhTW"] = false,
     },
-    ["Talk to Mishi"] = {
-        ["enUS"] = true,
-        ["deDE"] = false,
-        ["esES"] = false,
-        ["esMX"] = false,
-        ["frFR"] = false,
-        ["koKR"] = false,
-        ["ptBR"] = false,
-        ["ruRU"] = false,
-        ["zhCN"] = false,
-        ["zhTW"] = false,
-    },
 }
 
 for k, v in pairs(mopObjectiveLocales) do
