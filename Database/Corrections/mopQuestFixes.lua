9753ef00dd311a18add3530936c49fb8d5160c45
Sommos <Sommosgaming@gmail.com>
2025-07-22 16:20:39 +0100

    [quest] Fix pre quest group for "Dawn's Blossom"

--- Database/Corrections/mopQuestFixes.lua
+++ Database/Corrections/mopQuestFixes.lua
@@ -1561,7 +1561,7 @@ function MopQuestFixes.Load()
         },
         [30015] = { -- Dawn's Blossom
             [questKeys.preQuestSingle] = {},
-            [questKeys.preQuestGroup] = {29966,29967},
+            [questKeys.preQuestGroup] = {29967},
         },
         [30027] = { -- The Lesson of the Iron Bough
             [questKeys.requiredLevel] = 1,
