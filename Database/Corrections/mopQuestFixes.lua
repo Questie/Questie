8aba50c68c12a492bee4913e579dbb37f0f9326e
coldpasta123 <170130800+coldpasta123@users.noreply.github.com>
2025-07-23 09:56:15 +0100

    Comment

--- Database/Corrections/mopQuestFixes.lua
+++ Database/Corrections/mopQuestFixes.lua
@@ -2288,7 +2288,7 @@ function MopQuestFixes.Load()
             [questKeys.exclusiveTo] = {30470,30471,30472,30473,30474,30475,30476,30477,30478},
         },
         [30480] = { -- The Ritual
-            [questKeys.preQuestGroup] = {30468,30967},
+            [questKeys.preQuestGroup] = {30468,30967}, -- might also be 30496
             [questKeys.objectives] = {nil,nil,{{60973,nil,Questie.ICON_TYPE_TALK},{61654},{61530,nil,Questie.ICON_TYPE_TALK}}},
         },
         [30484] = { -- Gauging Our Progress
