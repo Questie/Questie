57303d46389147f1cc07fe49700edb9d383db37a
coldpasta123 <170130800+coldpasta123@users.noreply.github.com>
2025-07-30 15:17:40 +0100

    [db] Add source for Stolen Mistfall Keg

--- Database/Corrections/mopItemFixes.lua
+++ Database/Corrections/mopItemFixes.lua
@@ -645,6 +645,9 @@ function MopItemFixes.Load()
         [85282] = { -- Tiger Flank
             [itemKeys.npcDrops] = {63537,63538,65612,67085},
         },
+        [85778] = { -- Stolen Mistfall Keg
+            [itemKeys.objectDrops] = {213365},
+        },
         [85782] = { -- Mistfall Water Bucket
             [itemKeys.objectDrops] = {213369},
         },
