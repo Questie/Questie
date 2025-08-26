dc30be19f910027540bdbaee0e0ca09dfdb6b2c8
Cabro <1155017+Cabro@users.noreply.github.com>
2025-08-26 13:24:26 +0300

    [quest] Adjust prequest for Back to Nature

--- Database/Corrections/mopQuestFixes.lua
+++ Database/Corrections/mopQuestFixes.lua
@@ -1070,7 +1070,7 @@ function MopQuestFixes.Load()
             [questKeys.preQuestSingle] = {29749},
         },
         [29753] = { -- Back to Nature
-            [questKeys.preQuestGroup] = {29750,29751,29752},
+            [questKeys.preQuestGroup] = {29750,29752},
             [questKeys.extraObjectives] = {{{[zoneIDs.THE_JADE_FOREST] = {{39.42,13.13},{40.36,12.34},{41.11,10.94},{37.92,8.02},{38.84,10}}},Questie.ICON_TYPE_EVENT,l10n("Smash the Spirit Bottles")}},
         },
         [29754] = { -- To Bridge Earth and Sky
