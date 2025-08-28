4745c5b542482843ba27a2ced6556723d4440bb0
BreakBB <b.boeing@online.de>
2025-08-28 08:02:05 +0200

    Remove outdated phasing tests

--- Modules/Phasing.test.lua
+++ Modules/Phasing.test.lua
@@ -1206,40 +1206,6 @@ describe("Phasing", function()
         end)
     end)
 
-    describe("Kil'ruk the Wind-Reaver", function()
-        it("should return true pillar location when 31066 is not complete", function()
-            Questie.db.char.complete[31066] = false
-            QuestLogCache.questLog_DO_NOT_MODIFY = {}
-
-            assert.is_true(Phasing.IsSpawnVisible(phases.KIL_RUK_AT_PILLAR))
-            assert.is_false(Phasing.IsSpawnVisible(phases.KIL_RUK_NEXT_TO_ZIKK))
-        end)
-
-        it("should return true pillar location when 31066 is not complete and incomplete in the quest log", function()
-            Questie.db.char.complete[31066] = false
-            QuestLogCache.questLog_DO_NOT_MODIFY = {[31066]={isComplete=0}}
-
-            assert.is_true(Phasing.IsSpawnVisible(phases.KIL_RUK_AT_PILLAR))
-            assert.is_false(Phasing.IsSpawnVisible(phases.KIL_RUK_NEXT_TO_ZIKK))
-        end)
-
-        it("should return true next to Zikk location when 31066 is not complete and complete in the quest log", function()
-            Questie.db.char.complete[31066] = false
-            QuestLogCache.questLog_DO_NOT_MODIFY = {[31066]={isComplete=1}}
-
-            assert.is_true(Phasing.IsSpawnVisible(phases.KIL_RUK_NEXT_TO_ZIKK))
-            assert.is_false(Phasing.IsSpawnVisible(phases.KIL_RUK_AT_PILLAR))
-        end)
-
-        it("should return true for next to Zikk location when 31066 is complete", function()
-            Questie.db.char.complete[31066] = true
-            QuestLogCache.questLog_DO_NOT_MODIFY = {}
-
-            assert.is_true(Phasing.IsSpawnVisible(phases.KIL_RUK_NEXT_TO_ZIKK))
-            assert.is_false(Phasing.IsSpawnVisible(phases.KIL_RUK_AT_PILLAR))
-        end)
-    end)
-
     describe("Captain Soggy Su-Dao", function()
         it("should return true for outside location when 31189 is complete", function()
             Questie.db.char.complete[31189] = true
