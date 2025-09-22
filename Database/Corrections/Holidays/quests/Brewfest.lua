a03a43b59fd95b3f6d9d6aebe5bdb6a06f8fac4e
Cabro <1155017+Cabro@users.noreply.github.com>
2025-09-22 19:29:02 +0300

    [db] Hide Brewfest quests

--- Database/Corrections/Holidays/quests/Brewfest.lua
+++ Database/Corrections/Holidays/quests/Brewfest.lua
@@ -23,12 +23,12 @@ tinsert(eventQuests, {"Brewfest", 11437}) -- [PH] Beer Garden A
 tinsert(eventQuests, {"Brewfest", 12420}) -- Brew of the Month Club
 tinsert(eventQuests, {"Brewfest", 12421}) -- Brew of the Month Club
 --tinsert(eventQuests, {"Brewfest", 12306}) -- Brew of the Month Club
-tinsert(eventQuests, {"Brewfest", 11120}) -- Pink Elekks On Parade
+tinsert(eventQuests, {"Brewfest", 11118, nil, nil, Expansions.Current >= Expansions.Cata}) -- Pink Elekks On Parade
+tinsert(eventQuests, {"Brewfest", 11120, nil, nil, Expansions.Current >= Expansions.Cata}) -- Pink Elekks On Parade
 tinsert(eventQuests, {"Brewfest", 11400}) -- Brewfest Riding Rams
 tinsert(eventQuests, {"Brewfest", 11442}) -- Welcome to Brewfest!
 tinsert(eventQuests, {"Brewfest", 11447}) -- Welcome to Brewfest!
 --tinsert(eventQuests, {"Brewfest", 12278}) -- Brew of the Month Club
-tinsert(eventQuests, {"Brewfest", 11118}) -- Pink Elekks On Parade
 tinsert(eventQuests, {"Brewfest", 11320}) -- [NYI] Now this is Ram Racing... Almost.
 tinsert(eventQuests, {"Brewfest", 11441}) -- Brewfest!
 tinsert(eventQuests, {"Brewfest", 11446}) -- Brewfest!
