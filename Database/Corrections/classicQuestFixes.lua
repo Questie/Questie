8014a2d88fd7cb974324443132bf842c31f5bbb1
anon1231823 <anon1231823@users.noreply.github.com>
2025-09-21 19:01:05 -0700

    fix exlusivity for scepter/lord of the council quests

--- Database/Corrections/classicQuestFixes.lua
+++ Database/Corrections/classicQuestFixes.lua
@@ -3084,6 +3084,7 @@ function QuestieQuestFixes:Load()
         },
         [8341] = {
             [questKeys.preQuestSingle] = {},
+            [questKeys.exclusiveTo] = {8343},
             [questKeys.extraObjectives] = {{nil, Questie.ICON_TYPE_EVENT, l10n("Summon a Duke using a full Twilight set and neck."),2,{{"object", 180461},{"object", 180534},{"object", 180554},}}},
         },
         [8348] = {
@@ -3091,6 +3092,7 @@ function QuestieQuestFixes:Load()
         },
         [8352] = {
             [questKeys.preQuestSingle] = {},
+            [questKeys.exclusiveTo] = {8351},
             [questKeys.extraObjectives] = {{nil, Questie.ICON_TYPE_EVENT, l10n("Summon a Lord using a full Twilight set, neck and ring."),0,{{"object", 180466},{"object", 180539},{"object", 180559},}}},
         },
         [8353] = {
