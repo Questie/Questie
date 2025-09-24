5454f6e4799cffb3c33e6737036e6bd11c999174
BreakBB <b.boeing@online.de>
2025-09-24 15:21:01 +0200

    validate finisher for Cata DB

--- cli/validate-cata.lua
+++ cli/validate-cata.lua
@@ -230,6 +230,7 @@ local function _CheckCataDatabase()
     Validators.checkPreQuestExclusiveness(QuestieDB.questData, QuestieDB.questKeys)
     Validators.checkParentChildQuestRelations(QuestieDB.questData, QuestieDB.questKeys)
     Validators.checkQuestStarters(QuestieDB.questData, QuestieDB.questKeys, QuestieDB.npcData, QuestieDB.objectData, QuestieDB.itemData)
+    Validators.checkQuestFinishers(QuestieDB.questData, QuestieDB.questKeys, QuestieDB.npcData, QuestieDB.objectData)
     Validators.checkObjectives(QuestieDB.questData, QuestieDB.questKeys, QuestieDB.npcData, QuestieDB.objectData, QuestieDB.itemData)
 end
 
