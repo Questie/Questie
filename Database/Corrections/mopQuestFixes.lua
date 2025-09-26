7190d24e56fe6a1b6f38138c1ad2c1fdfd248bbe
Cabro <1155017+Cabro@users.noreply.github.com>
2025-09-27 01:35:49 +0300

    [quest] Some Black Prince fixes

--- Database/Corrections/mopQuestFixes.lua
+++ Database/Corrections/mopQuestFixes.lua
@@ -7760,8 +7760,9 @@ function MopQuestFixes.Load()
         },
         [32457] = { -- The Thunder King
             [questKeys.startedBy] = {{64616}},
-            [questKeys.preQuestSingle] = {32390,32432},
+            [questKeys.preQuestSingle] = {32390},
             [questKeys.breadcrumbForQuestId] = 32591,
+            [questKeys.requiredRaces] = raceIDs.ALL_ALLIANCE,
         },
         [32474] = { -- A Test of Valor
             [questKeys.startedBy] = {{64616}},
@@ -7773,8 +7774,9 @@ function MopQuestFixes.Load()
         },
         [32590] = { -- Meet Me Upstairs
             [questKeys.startedBy] = {{64616}},
-            [questKeys.preQuestSingle] = {32390,32432},
+            [questKeys.preQuestSingle] = {32432},
             [questKeys.breadcrumbForQuestId] = 32591,
+            [questKeys.requiredRaces] = raceIDs.ALL_HORDE,
         },
         [32591] = { -- Secrets of the First Empire
             [questKeys.preQuestSingle] = {32390,32432},
