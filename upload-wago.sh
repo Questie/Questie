835fee6d730687e952de2b6d44feb735e9ce32ff
BreakBB <b.boeing@online.de>
2025-10-06 16:59:42 +0200

    [fix] Bump interface version for WotLK 3.4.5

--- upload-wago.sh
+++ upload-wago.sh
@@ -19,7 +19,7 @@ WAGO_METADATA=$(cat <<-EOF
    "label": "$LATEST_GIT_TAG",
    "stability": "$RELEASE_TYPE",
    "changelog": $CHANGELOG,
-   "supported_wotlk_patch": "3.4.4",
+   "supported_wotlk_patch": "3.4.5",
    "supported_mop_patch": "5.5.1",
    "supported_classic_patch": "1.15.7"
 }
