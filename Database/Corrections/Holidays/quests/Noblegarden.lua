7e0c8b4eba41c865f0f1b5901efaff1604196160
Cabro <1155017+Cabro@users.noreply.github.com>
2025-11-15 23:31:15 +0200

    [db] Internal cleanup for event quests

--- Database/Corrections/Holidays/quests/Noblegarden.lua
+++ Database/Corrections/Holidays/quests/Noblegarden.lua
@@ -8,7 +8,7 @@ local eventQuests = QuestieEvent.eventQuests
 
 -- WotLK quests
 tinsert(eventQuests, {"Noblegarden", 13483, nil, nil, Expansions.Current >= Expansions.Cata}) -- Spring Gatherers
-tinsert(eventQuests, {"Noblegarden", 13484}) -- Spring Collectors
+tinsert(eventQuests, {"Noblegarden", 13484, nil, nil, Expansions.Current >= Expansions.Cata}) -- Spring Collectors
 tinsert(eventQuests, {"Noblegarden", 13479}) -- The Great Egg Hunt
 tinsert(eventQuests, {"Noblegarden", 13480}) -- The Great Egg Hunt
 tinsert(eventQuests, {"Noblegarden", 13502}) -- A Tisket, a Tasket, a Noblegarden Basket
