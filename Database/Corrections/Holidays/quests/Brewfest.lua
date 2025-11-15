868e421976ed9ea3e6e4b497f4313df046cdb59e
Cabro <1155017+Cabro@users.noreply.github.com>
2025-11-16 01:18:38 +0200

    Use correct syntax

--- Database/Corrections/Holidays/quests/Brewfest.lua
+++ Database/Corrections/Holidays/quests/Brewfest.lua
@@ -14,12 +14,12 @@ tinsert(eventQuests, {"Brewfest", 11122}) -- There and Back Again
 tinsert(eventQuests, {"Brewfest", 11293}) -- Bark for the Barleybrews!
 tinsert(eventQuests, {"Brewfest", 11294}) -- Bark for the Thunderbrews!
 tinsert(eventQuests, {"Brewfest", 11318}) -- Now This is Ram Racing... Almost.
-tinsert(eventQuests, {"Brewfest", 11400, nil, nil, Expansions.Current >= Expansions.WotLK}) -- Brewfest Riding Rams
+tinsert(eventQuests, {"Brewfest", 11400, nil, nil, Expansions.Current >= Expansions.Wotlk}) -- Brewfest Riding Rams
 tinsert(eventQuests, {"Brewfest", 11407}) -- Bark for Drohn's Distillery!
 tinsert(eventQuests, {"Brewfest", 11408}) -- Bark for T'chali's Voodoo Brewery!
 tinsert(eventQuests, {"Brewfest", 11409}) -- Now This is Ram Racing... Almost.
 tinsert(eventQuests, {"Brewfest", 11412}) -- There and Back Again
-tinsert(eventQuests, {"Brewfest", 11419, nil, nil, Expansions.Current >= Expansions.WotLK}) -- Brewfest Riding Rams
+tinsert(eventQuests, {"Brewfest", 11419, nil, nil, Expansions.Current >= Expansions.Wotlk}) -- Brewfest Riding Rams
 tinsert(eventQuests, {"Brewfest", 11431, nil, nil, Expansions.Current >= Expansions.Cata}) -- Catch the Wild Wolpertinger!
 tinsert(eventQuests, {"Brewfest", 11441}) -- Brewfest!
 tinsert(eventQuests, {"Brewfest", 11442}) -- Welcome to Brewfest!
@@ -32,11 +32,11 @@ tinsert(eventQuests, {"Brewfest", 12191}) -- Chug and Chuck!
 tinsert(eventQuests, {"Brewfest", 12192}) -- This One Time, When I Was Drunk...
 --tinsert(eventQuests, {"Brewfest", 12193}) -- Say, There Wouldn't Happen to be a Souvenir This Year, Would There?
 --tinsert(eventQuests, {"Brewfest", 12194}) -- Say, There Wouldn't Happen to be a Souvenir This Year, Would There?
-tinsert(eventQuests, {"Brewfest", 12278, nil, nil, Expansions.Current >= Expansions.WotLK}) -- Brew of the Month Club
-tinsert(eventQuests, {"Brewfest", 12306, nil, nil, Expansions.Current >= Expansions.WotLK}) -- Brew of the Month Club
+tinsert(eventQuests, {"Brewfest", 12278, nil, nil, Expansions.Current >= Expansions.Wotlk}) -- Brew of the Month Club
+tinsert(eventQuests, {"Brewfest", 12306, nil, nil, Expansions.Current >= Expansions.Wotlk}) -- Brew of the Month Club
 tinsert(eventQuests, {"Brewfest", 12318}) -- Save Brewfest!
-tinsert(eventQuests, {"Brewfest", 12420, nil, nil, Expansions.Current < Expansions.WotLK}) -- Brew of the Month Club
-tinsert(eventQuests, {"Brewfest", 12421, nil, nil, Expansions.Current < Expansions.WotLK}) -- Brew of the Month Club
+tinsert(eventQuests, {"Brewfest", 12420, nil, nil, Expansions.Current < Expansions.Wotlk}) -- Brew of the Month Club
+tinsert(eventQuests, {"Brewfest", 12421, nil, nil, Expansions.Current < Expansions.Wotlk}) -- Brew of the Month Club
 tinsert(eventQuests, {"Brewfest", 12491}) -- Direbrew's Dire Brew
 tinsert(eventQuests, {"Brewfest", 12492}) -- Direbrew's Dire Brew
 
