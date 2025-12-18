57b62dd87c689e321ad759814e69bee378a12831
BreakBB <b.boeing@online.de>
2025-12-18 18:21:52 +0100

    Remove debug logs

--- Modules/Quest/IsleOfThunder.lua
+++ Modules/Quest/IsleOfThunder.lua
@@ -7,21 +7,11 @@ local function onEvent(_, event, addOnName)
         -- To work around this, we wait for this addon to load, wait for the related UI frame to close and then check if those quests were completed.
 
         if QuestChoiceFrame then
-            print("Hooking QuestChoiceFrame OnHide to check quest completion status...")
             QuestChoiceFrame:SetScript("OnHide", function()
                 f:RegisterEvent("QUEST_LOG_UPDATE")
             end)
-        else
-            print("QuestChoiceFrame not loaded!")
         end
     elseif event == "QUEST_LOG_UPDATE" then
-        print("QuestChoiceFrame hidden, checking quest completion status...")
-        print("C_QuestLog.IsQuestFlaggedCompleted(32259)", C_QuestLog.IsQuestFlaggedCompleted(32259))
-        print("C_QuestLog.IsQuestFlaggedCompleted(32260)", C_QuestLog.IsQuestFlaggedCompleted(32260))
-
-        print("C_QuestLog.IsQuestFlaggedCompleted(32258)", C_QuestLog.IsQuestFlaggedCompleted(32258))
-        print("C_QuestLog.IsQuestFlaggedCompleted(32261)", C_QuestLog.IsQuestFlaggedCompleted(32261))
-
         Questie.db.char.complete[32259] = C_QuestLog.IsQuestFlaggedCompleted(32259) and true or nil
         Questie.db.char.complete[32260] = C_QuestLog.IsQuestFlaggedCompleted(32260) and true or nil
 
