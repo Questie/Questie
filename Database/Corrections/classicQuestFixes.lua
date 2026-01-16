df488139da2598c4714702689e99ec19822d3c0a
anon1231823 <anon1231823@users.noreply.github.com>
2026-01-15 20:35:59 -0800

    fix last commit

--- Database/Corrections/classicQuestFixes.lua
+++ Database/Corrections/classicQuestFixes.lua
@@ -1824,10 +1824,12 @@ function QuestieQuestFixes:Load()
             [questKeys.breadcrumbs] = {3787,3788}, -- #885
         },
         [3903] = {
+            [questKeys.preQuestSingle] = {},
             [questKeys.preQuestGroup] = {18,33}, -- #543
             [questKeys.breadcrumbForQuestId] = 3904,
         },
         [3904] = {
+            [questKeys.preQuestSingle] = {},
             [questKeys.preQuestGroup] = {18,33}, -- #543
             [questKeys.breadcrumbs] = {3903},
         },
