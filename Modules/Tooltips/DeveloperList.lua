af42b9f2b49186aa08f605e88c5f6b393719b0e1
BreakBB <b.boeing@online.de>
2026-01-17 10:30:12 +0100

    Add BreakBBs characters to tooltip list

--- Modules/Tooltips/DeveloperList.lua
+++ Modules/Tooltips/DeveloperList.lua
@@ -1,8 +1,6 @@
----@type QuestieTooltips
-local QuestieTooltips = QuestieLoader:ImportModule("QuestieTooltips");
-local _QuestieTooltips = QuestieTooltips.private
-
 ---@class DeveloperCharacters
 Questie.devChars = {
-    ["6064"] = {"02FA1F22","03430D5D","02F7D6DD"},
+    ["4440"] = {"064FC13C", "064FC234", "064FC0EE"},
+    ["6064"] = {"02FA1F22", "03430D5D", "02F7D6DD"},
+    ["6409"] = {"04ACFA0C"},
 }
