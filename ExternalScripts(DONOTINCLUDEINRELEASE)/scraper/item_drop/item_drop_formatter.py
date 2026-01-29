7ba5b46b4dc4971f7e7b5e95db63ab3342d08ef9
Yttrium tYcLief <tyclief@gmail.com>
2026-01-29 13:53:30 -0500

    add sort functionality to itemdrop lua formatter

--- ExternalScripts(DONOTINCLUDEINRELEASE)/scraper/item_drop/item_drop_formatter.py
+++ ExternalScripts(DONOTINCLUDEINRELEASE)/scraper/item_drop/item_drop_formatter.py
@@ -15,26 +15,30 @@ class ItemDropFormatter:
                 g.write("return {\n")
                 with open(json_path, 'r') as file:
                     data = json.load(file)
+
                 excluded_keys = {"itemId", "name"}
-                for entry in data:
-                    #print(entry)
+                sorted_data = sorted(data, key=lambda x: int(x.get("itemId", 0)))
+
+                for entry in sorted_data:
                     if len(entry) > 2:
-                        g.write("    [{id}] = {{ -- {name}\n".format(id=entry["itemId"],name=entry["name"]))
-                        for key, value in entry.items():
-                            if key not in excluded_keys:
-                                if value[0] == 0 or value[1] == 0:
-                                    # we don't want to skip with a 'continue' because that will lead to empty NPC data,
-                                    # so instead, if we detect a potential divide by zero, we simply hardcode the droprate to 0%
-                                    # realistically this should never happen because wowhead will not report 0 drops but who knows!
-                                    droprate = 0
-                                else:
-                                    droprate = (value[0] / value[1]) * 100 # converts droprate fraction to percent
-                                g.write("        [{npcid}] = {rate},\n".format(npcid=key,rate=str(round(droprate, 4))))
+                        g.write("    [{id}] = {{ -- {name}\n".format(id=entry["itemId"], name=entry["name"]))
+
+                        npc_keys = [k for k in entry.keys() if k not in excluded_keys]
+                        npc_keys.sort(key=int)
+
+                        for key in npc_keys:
+                            value = entry[key]
+                            if value[0] == 0 or value[1] == 0:
+                                droprate = 0
+                            else:
+                                droprate = (value[0] / value[1]) * 100
+
+                            g.write("        [{npcid}] = {rate},\n".format(npcid=key, rate=str(round(droprate, 4))))
+
                         g.write("    },\n")
                 g.write("}\n")
             except FileNotFoundError:
                 print(f"Error: Could not find the file at '{json_path}'.")
-                print("Please check that the 'item_drop' folder and the JSON file exist.")
             except json.JSONDecodeError:
                 print(f"Error: The file '{json_path}' contains invalid JSON data.")
 
