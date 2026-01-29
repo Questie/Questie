db8f7607658626f2f4edd973d0c0ef508d25cc8c
Cabro <1155017+Cabro@users.noreply.github.com>
2026-01-29 19:24:50 +0200

    [locale] Use chinese characters for the CN only quests

--- Database/Corrections/wotlkQuestFixes.lua
+++ Database/Corrections/wotlkQuestFixes.lua
@@ -7838,13 +7838,13 @@ function QuestieWotlkQuestFixes:Load()
             [questKeys.reputationReward] = {{factionIDs.KIRIN_TOR,75}},
         },
         [93975] = {
-            [questKeys.name] = "Ragnaros Must Die!",
+            [questKeys.name] = "拉格纳罗斯必须死！", -- "Ragnaros Must Die!", only present on chinese servers
             [questKeys.startedBy] = {},
             [questKeys.finishedBy] = {},
             [questKeys.requiredLevel] = 80,
             [questKeys.questLevel] = 80,
             [questKeys.requiredRaces] = raceIDs.NONE,
-            [questKeys.objectivesText] = {"Kill Ragnaros."},
+            [questKeys.objectivesText] = {"团队消灭拉格纳罗斯。"}, -- {"Kill Ragnaros."}, only present on chinese servers
             [questKeys.objectives] = {{{11502}}},
             [questKeys.zoneOrSort] = zoneIDs.MOLTEN_CORE,
             [questKeys.specialFlags] = specialFlags.REPEATABLE,
@@ -7852,13 +7852,13 @@ function QuestieWotlkQuestFixes:Load()
             [questKeys.reputationReward] = {{factionIDs.KIRIN_TOR,75}},
         },
         [94577] = {
-            [questKeys.name] = "Kael'thas Must Die!",
+            [questKeys.name] = "凯尔萨斯必须死！", -- "Kael'thas Must Die!", only present on chinese servers
             [questKeys.startedBy] = {},
             [questKeys.finishedBy] = {},
             [questKeys.requiredLevel] = 80,
             [questKeys.questLevel] = 80,
             [questKeys.requiredRaces] = raceIDs.NONE,
-            [questKeys.objectivesText] = {"Kill Kael'thas Sunstrider in Tempest Keep."},
+            [questKeys.objectivesText] = {"消灭风暴要塞的凯尔萨斯逐日者。"}, -- "Kill Kael'thas Sunstrider in Tempest Keep." only present on chinese servers
             [questKeys.objectives] = {{{19622}}},
             [questKeys.zoneOrSort] = zoneIDs.TEMPEST_KEEP,
             [questKeys.specialFlags] = specialFlags.REPEATABLE,
