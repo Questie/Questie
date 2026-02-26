767200760f004b8bfe9804b9ea146c7e7a6ab897
anon1231823 <67269448+anon1231823@users.noreply.github.com>
2026-02-26 13:47:36 -0800

    [feature] Add "Select Event", "Select Battleground", "Select Scenario… (#7441)
    * [feature] Add "Select Event", "Select Battleground", "Select Scenario", "Select Dungeon"
    
    * update to say select category and remove references to zone
    
    * update translations
    
    * remove unused string
    
    * Slightly increase dropdown width to fit french word
    
    * increase faction width to match zone width
    
    ---------
    
    Co-authored-by: anon1231823 <anon1231823@users.noreply.github.com>

--- Modules/Journey/tabs/QuestsByZone/QuestsByZoneTab.lua
+++ Modules/Journey/tabs/QuestsByZone/QuestsByZoneTab.lua
@@ -32,7 +32,7 @@ function _QuestieJourney.questsByZone:DrawTab(container)
     treegroup = AceGUI:Create("SimpleGroup")
 
     local header = AceGUI:Create("Heading")
-    header:SetText(l10n('Select Continent and Zone'))
+    header:SetText(l10n('Select Category'))
     header:SetFullWidth(true)
     container:AddChild(header)
 
@@ -47,7 +47,7 @@ function _QuestieJourney.questsByZone:DrawTab(container)
     QuestieJourneyUtils:Spacer(container)
 
     header = AceGUI:Create("Heading")
-    header:SetText(l10n('Zone Quests'))
+    header:SetText(l10n('Quests'))
     header:SetFullWidth(true)
     container:AddChild(header)
 
@@ -121,7 +121,7 @@ end
 
 _CreateZoneDropdown = function()
     local dropdown = AceGUI:Create("Dropdown")
-
+    dropdown:SetWidth(205)
     local currentZoneId = QuestiePlayer:GetCurrentZoneId()
     if _QuestieJourney.lastZoneSelection[2] then
         currentZoneId = _QuestieJourney.lastZoneSelection[2]
@@ -211,6 +211,38 @@ _HandleContinentSelection = function(key, _)
         local zoneTree = _QuestieJourney.questsByZone:CollectZoneQuests(classKey)
         _QuestieJourney.questsByZone:ManageTree(treegroup, zoneTree)
         zoneDropdown.frame:Hide()
+    elseif (key.value == QuestieJourney.questCategoryKeys.DUNGEONS) then
+        local text = l10n('Select Dungeon')
+        local dungeonZones = QuestieJourney.zones[QuestieJourney.questCategoryKeys.DUNGEONS] or {}
+        local sortedDungeons = QuestieJourneyUtils:GetSortedZoneKeys(dungeonZones)
+        zoneDropdown:SetList(dungeonZones, sortedDungeons)
+        zoneDropdown:SetDisabled(false)
+        zoneDropdown:SetText(text)
+        zoneDropdown.frame:Show()
+    elseif (key.value == QuestieJourney.questCategoryKeys.BATTLEGROUNDS) then
+        local text = l10n('Select Battleground')
+        local bgZones = QuestieJourney.zones[QuestieJourney.questCategoryKeys.BATTLEGROUNDS] or {}
+        local sortedBGs = QuestieJourneyUtils:GetSortedZoneKeys(bgZones)
+        zoneDropdown:SetList(bgZones, sortedBGs)
+        zoneDropdown:SetDisabled(false)
+        zoneDropdown:SetText(text)
+        zoneDropdown.frame:Show()
+    elseif (key.value == QuestieJourney.questCategoryKeys.EVENTS) then
+        local text = l10n('Select Event')
+        local eventZones = QuestieJourney.zones[QuestieJourney.questCategoryKeys.EVENTS] or {}
+        local sortedEvents = QuestieJourneyUtils:GetSortedZoneKeys(eventZones)
+        zoneDropdown:SetList(eventZones, sortedEvents)
+        zoneDropdown:SetDisabled(false)
+        zoneDropdown:SetText(text)
+        zoneDropdown.frame:Show()
+    elseif (key.value == QuestieJourney.questCategoryKeys.SCENARIOS) then
+        local text = l10n('Select Scenario')
+        local scenarioZones = QuestieJourney.zones[QuestieJourney.questCategoryKeys.SCENARIOS] or {}
+        local sortedScenarios = QuestieJourneyUtils:GetSortedZoneKeys(scenarioZones)
+        zoneDropdown:SetList(scenarioZones, sortedScenarios)
+        zoneDropdown:SetDisabled(false)
+        zoneDropdown:SetText(text)
+        zoneDropdown.frame:Show()
     elseif (key.value == QuestieJourney.questCategoryKeys.PROFESSIONS) then
         local professionList = QuestieJourney.zones[key.value]
         local playerProfessions = QuestieProfessions:GetPlayerProfessionNames()
