d1588d42472408abd3bdd5b6a4fb78a8ccf607cf
BreakBB <b.boeing@online.de>
2026-03-07 15:21:58 +0100

    [feature] Allow every class to track reagent and poison vendors

--- Modules/QuestieMenu/QuestieMenu.lua
+++ Modules/QuestieMenu/QuestieMenu.lua
@@ -32,8 +32,6 @@ local Moonwell = QuestieLoader:ImportModule("Moonwell")
 
 local LibDropDown = LibStub:GetLibrary("LibUIDropDownMenuQuestie-4.0")
 
-local _, playerClass = UnitClass("player")
-
 local tinsert = tinsert
 
 local professionKeys = QuestieProfessions.professionKeys
@@ -60,12 +58,8 @@ local _townsfolk_texturemap = {
     ["Arcane Reforger"] = QuestieLib.AddonPath.."Icons\\reforge.png",
     ["Transmogrifier"] = QuestieLib.AddonPath.."Icons\\transmogrify.png",
     ["Battle Pet Trainer"] = QuestieLib.AddonPath.."Icons\\petbattle.png",
-    ["Reagents"] = (function()
-        if playerClass == "ROGUE" then
-            return "Interface\\Minimap\\tracking\\poisons"
-        end
-        return QuestieLib.AddonPath.."Icons\\reagents.blp"
-    end)(),
+    ["Reagents"] = QuestieLib.AddonPath.."Icons\\reagents.blp",
+    ["Poisons"] = "Interface\\Minimap\\tracking\\poisons",
     [professionKeys.FIRST_AID] = "Interface\\Icons\\spell_holy_sealofsacrifice",
     [professionKeys.BLACKSMITHING] = "Interface\\Icons\\trade_blacksmithing",
     [professionKeys.LEATHERWORKING] = "Interface\\Icons\\trade_leatherworking",
