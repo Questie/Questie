52674d848c2097e2e10de0e3b9790b77a4d74c62
Cabro <1155017+Cabro@users.noreply.github.com>
2026-03-13 18:37:15 +0200

    Update quest condition for Gurf's Dignity

--- Database/Corrections/tbcQuestFixes.lua
+++ Database/Corrections/tbcQuestFixes.lua
@@ -1995,6 +1995,7 @@ function QuestieTBCQuestFixes:Load()
         },
         [9564] = {
             [questKeys.preQuestSingle] = {9559},
+            [questKeys.availableStartingWith] = 9562,
         },
         [9565] = {
             [questKeys.preQuestGroup] = {},
