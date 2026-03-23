661b9f07b644d488a56fd9d2196548a35203a9e4
Cabro <1155017+Cabro@users.noreply.github.com>
2026-03-23 14:58:21 +0200

    [db] Updated spawns for Isaac Pearson, Avelina Lilly

--- Database/Corrections/classicNPCFixes.lua
+++ Database/Corrections/classicNPCFixes.lua
@@ -3317,15 +3317,15 @@ function QuestieNPCFixes:Load()
             [npcKeys.name] = "Avelina Lilly",
             [npcKeys.minLevel] = 22,
             [npcKeys.maxLevel] = 22,
+            [npcKeys.spawns] = {[zoneIDs.SILVERPINE_FOREST] = {{63.48,65.27}}},
             [npcKeys.zoneID] = zoneIDs.SILVERPINE_FOREST,
-            [npcKeys.spawns] = {[zoneIDs.SILVERPINE_FOREST] = {{63.5,65.3}}},
         },
         [185334] = { -- Isaac Pearson
             [npcKeys.name] = "Isaac Pearson",
             [npcKeys.minLevel] = 22,
             [npcKeys.maxLevel] = 22,
+            [npcKeys.spawns] = {[zoneIDs.THE_BARRENS] = {{61.8,38.53}}},
             [npcKeys.zoneID] = zoneIDs.THE_BARRENS,
-            [npcKeys.spawns] = {[zoneIDs.THE_BARRENS] = {{61.8,38.5}}},
         },
         [185335] = { -- Summoned Incubus
             [npcKeys.name] = "Summoned Incubus",
