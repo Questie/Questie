9aa11e6d37c109438b919dd045e49283a9b68f4a
anon1231823 <67269448+anon1231823@users.noreply.github.com>
2026-04-09 18:19:52 -0700

    Set Noblegarden date for 2027

--- Database/Corrections/Holidays/QuestieEvent.lua
+++ Database/Corrections/Holidays/QuestieEvent.lua
@@ -413,7 +413,7 @@ QuestieEvent.eventDateCorrections = {
     ["CLASSIC"] = {
         ["Brewfest"] = false,
         ["Pilgrim's Bounty"] = false,
-        ["Noblegarden"] = {startDate = "5/4", endDate = "5/4"}, -- One day event on Era, on the actual day of Easter
+        ["Noblegarden"] = {startDate = "28/3", endDate = "28/3"}, -- One day event on Era, on the actual day of Easter. Date is set for 2027. Please update this every year.
         ["Love is in the Air"] = {startDate = "11/2", endDate = "15/2"}, -- WARNING THIS DATE VARIES!!!!
     },
     ["TBC"] = {
