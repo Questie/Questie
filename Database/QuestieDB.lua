457f869191fc5f30d3ab9630a0d2b487be074e09
Cabro <1155017+Cabro@users.noreply.github.com>
2026-04-11 19:05:03 +0300

    Remove comment

--- Database/QuestieDB.lua
+++ Database/QuestieDB.lua
@@ -1256,8 +1256,6 @@ function QuestieDB.IsDoableVerbose(questId, debugPrint, returnText, returnBrief)
     end
 
     -- Check if this quest is visible if you have a certain quest in log or turned in (slightly different to preQuestSingle)
-    -- In order to not mess with the existing logic for preQuestSingle/preQuestGroup in all the many other places,
-    -- this field must be accompanied by preQuestSingle/preQuestGroup
     local availableStartingWith = QuestieDB.QueryQuestSingle(questId, "availableStartingWith")
     if availableStartingWith and availableStartingWith ~= 0 then
         if not completedQuests[availableStartingWith] and not currentQuestlog[availableStartingWith] then
