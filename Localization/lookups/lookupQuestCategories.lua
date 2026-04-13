2dc6fb5133820490e8deced7f6190b5c6747baa4
Cabro <1155017+Cabro@users.noreply.github.com>
2026-04-13 18:31:00 +0300

    Update quests

--- Localization/lookups/lookupQuestCategories.lua
+++ Localization/lookups/lookupQuestCategories.lua
@@ -60,5 +60,5 @@ l10n.questCategoryLookup = {
     [-400] = "Proving Grounds",
     [-641] = "Nightmare Incursions",
     [-644] = "Blackrock Eruption",
-    [-662] = "Titan-Reforged Realm", -- only chinese region has them
+    [-662] = "Titan-Reforged Realm", -- Chinese region
 };
