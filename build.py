42dc4da15747ae6b0907631c680539c7de7f1060
BreakBB <b.boeing@online.de>
2026-04-23 16:31:37 +0200

    replace ' with "

--- build.py
+++ build.py
@@ -7,7 +7,7 @@ import sys
 import fileinput
 import re
 
-'''
+"""
 This program accepts optional command line options:
 
     -r
@@ -38,27 +38,27 @@ This program accepts optional command line options:
     --version <versionString>
         Disregard git and toc versions, and use <versionString> instead
 
-'''
-addonDir = 'Questie'
+"""
+addonDir = "Questie"
 includedExpansions = []
-tocs = ['', 'Questie-Classic.toc', 'Questie-BCC.toc', 'Questie-WOTLKC.toc', 'Questie-Cata.toc', 'Questie-Mists.toc']
+tocs = ["", "Questie-Classic.toc", "Questie-BCC.toc", "Questie-WOTLKC.toc", "Questie-Cata.toc", "Questie-Mists.toc"]
 
 
 def main():
     isReleaseBuild = False
-    versionOverride = ''
+    versionOverride = ""
     if len(sys.argv) > 1:
         ver = False
         for arg in sys.argv[1:]:
             if ver:
                 versionOverride = arg
                 ver = False
-            elif arg in ['-r', '--release']:
+            elif arg in ["-r", "--release"]:
                 isReleaseBuild = True
                 print("Creating a release build")
-            elif arg in ['-v', '--version']:
+            elif arg in ["-v", "--version"]:
                 ver = True
-            elif arg in ['-a', '--all']:
+            elif arg in ["-a", "--all"]:
                 if 1 not in includedExpansions:
                     includedExpansions.append(1)
                 if 2 not in includedExpansions:
@@ -69,15 +69,15 @@ def main():
                     includedExpansions.append(4)
                 if 5 not in includedExpansions:
                     includedExpansions.append(5)
-            elif arg in ['-c', '--classic'] and 1 not in includedExpansions:
+            elif arg in ["-c", "--classic"] and 1 not in includedExpansions:
                 includedExpansions.append(1)
-            elif arg in ['-t', '--tbc'] and 2 not in includedExpansions:
+            elif arg in ["-t", "--tbc"] and 2 not in includedExpansions:
                 includedExpansions.append(2)
-            elif arg in ['-w', '--wotlk'] and 3 not in includedExpansions:
+            elif arg in ["-w", "--wotlk"] and 3 not in includedExpansions:
                 includedExpansions.append(3)
-            elif arg in ['-ca', '--cata'] and 4 not in includedExpansions:
+            elif arg in ["-ca", "--cata"] and 4 not in includedExpansions:
                 includedExpansions.append(4)
-            elif arg in ['-m', '--mop'] and 5 not in includedExpansions:
+            elif arg in ["-m", "--mop"] and 5 not in includedExpansions:
                 includedExpansions.append(5)
     if len(includedExpansions) == 0:
         # If expansions go online/offline their major version needs to be added/removed here
@@ -88,34 +88,34 @@ def main():
 
     release_dir = get_version_dir(isReleaseBuild, versionOverride)
 
-    if os.path.isdir('releases/%s' % release_dir):
+    if os.path.isdir("releases/%s" % release_dir):
         print("Warning: Folder already exists, removing!")
-        shutil.rmtree('releases/%s' % release_dir)
+        shutil.rmtree("releases/%s" % release_dir)
 
-    release_folder_path = 'releases/%s' % release_dir
-    release_addon_folder_path = release_folder_path + ('/%s' % addonDir)
+    release_folder_path = "releases/%s" % release_dir
+    release_addon_folder_path = release_folder_path + ("/%s" % addonDir)
 
     copy_content_to(release_addon_folder_path)
 
-    if versionOverride != '':
+    if versionOverride != "":
         for tocN in includedExpansions:
             toc = tocs[tocN]
             questie_toc_path = release_addon_folder_path + toc
             with fileinput.FileInput(questie_toc_path, inplace=True) as file:
                 for line in file:
-                    if line[:10] == '## Version':
-                        print('## Version: ' + versionOverride)
+                    if line[:10] == "## Version":
+                        print("## Version: " + versionOverride)
                     else:
-                        print(line, end='')
+                        print(line, end="")
 
-    zip_name = '%s-%s' % (addonDir, release_dir)
+    zip_name = "%s-%s" % (addonDir, release_dir)
     zip_release_folder(zip_name, release_dir, addonDir)
 
     interface_classic = get_interface_version()
-    interface_bcc = get_interface_version('BCC')
-    interface_wotlk = get_interface_version('WOTLKC')
-    interface_cata = get_interface_version('Cata')
-    interface_mop = get_interface_version('Mists')
+    interface_bcc = get_interface_version("BCC")
+    interface_wotlk = get_interface_version("WOTLKC")
+    interface_cata = get_interface_version("Cata")
+    interface_mop = get_interface_version("Mists")
 
     flavorString = ""
     if 1 in includedExpansions:
@@ -149,8 +149,8 @@ def main():
                     "interface": %s
                 },""" % interface_mop
 
-    with open(release_folder_path + '/release.json', 'w') as rf:
-        rf.write('''{
+    with open(release_folder_path + "/release.json", "w") as rf:
+        rf.write("""{
     "releases": [
         {
             "filename": "%s.zip",
@@ -159,14 +159,14 @@ def main():
             ]
         }
     ]
-}''' % (zip_name, flavorString[:-1]))
+}""" % (zip_name, flavorString[:-1]))
 
-    print('New release "%s" created successfully' % release_dir)
+    print("New release '%s' created successfully" % release_dir)
 
 
 def get_version_dir(is_release_build, versionOverride):
     version, nr_of_commits, recent_commit = get_git_information()
-    if versionOverride != '':
+    if versionOverride != "":
         version = versionOverride
     print("Tag: " + version)
     if is_release_build:
@@ -184,10 +184,10 @@ def get_version_dir(is_release_build, versionOverride):
     return release_dir
 
 
-directoriesToInclude = ['Database', 'Icons', 'Libs', 'Localization', 'Modules', 'Public']
-filesToInclude = ['embeds.xml', 'Questie.lua', 'Questie.toc', 'README.md']
-expansionStrings = ['', 'Classic', 'TBC', 'Wotlk', 'Cata', 'MoP']
-ignorePatterns = ['*.test.lua']
+directoriesToInclude = ["Database", "Icons", "Libs", "Localization", "Modules", "Public"]
+filesToInclude = ["embeds.xml", "Questie.lua", "Questie.toc", "README.md"]
+expansionStrings = ["", "Classic", "TBC", "Wotlk", "Cata", "MoP"]
+ignorePatterns = ["*.test.lua"]
 
 
 def copy_content_to(release_folder_path):
@@ -195,21 +195,21 @@ def copy_content_to(release_folder_path):
         if i in includedExpansions:
             filesToInclude.append(tocs[i])
         else:
-            ignorePatterns.append(f'{expansionStrings[i]}')
+            ignorePatterns.append(f"{expansionStrings[i]}")
 
-    for _, directories, files in os.walk('.'):
+    for _, directories, files in os.walk("."):
         for directory in directories:
             if directory in directoriesToInclude:
-                shutil.copytree(directory, '%s/%s' % (release_folder_path, directory), ignore=shutil.ignore_patterns(*ignorePatterns))
+                shutil.copytree(directory, "%s/%s" % (release_folder_path, directory), ignore=shutil.ignore_patterns(*ignorePatterns))
         for file in files:
             if file in filesToInclude:
-                shutil.copy2(file, '%s/%s' % (release_folder_path, file))
+                shutil.copy2(file, "%s/%s" % (release_folder_path, file))
         break
 
 
 def zip_release_folder(zip_name, version_dir, addon_dir):
     root = os.getcwd()
-    os.chdir('releases/%s' % version_dir)
+    os.chdir("releases/%s" % version_dir)
     print("Zipping %s" % zip_name)
     shutil.make_archive(zip_name, "zip", ".", addon_dir)
     os.chdir(root)
@@ -238,9 +238,9 @@ def get_branch():
         return branch
 
 
-def get_interface_version(expansion='Classic'):
-    with open('Questie-%s.toc' % expansion, 'r') as toc:
-        return re.match('## Interface: (.*?)\n', toc.read(), re.DOTALL).group(1)
+def get_interface_version(expansion="Classic"):
+    with open("Questie-%s.toc" % expansion, "r") as toc:
+        return re.match("## Interface: (.*?)\n", toc.read(), re.DOTALL).group(1)
 
 
 def is_tool(name):
