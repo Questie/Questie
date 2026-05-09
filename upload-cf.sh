2c82453c568d34273d8078824fdd1400e4cc6fbf
BreakBB <b.boeing@online.de>
2026-05-09 09:14:42 +0200

    [fix] Bump interface version for 5.5.4

--- upload-cf.sh
+++ upload-cf.sh
@@ -25,7 +25,7 @@ CF_METADATA=$(cat <<-EOF
     "releaseType": "$RELEASE_TYPE",
     "changelog": $CHANGELOG,
     "changelogType": "markdown",
-    "gameVersions": [16081, 14029, 14300, 14102],
+    "gameVersions": [16081, 14029, 14300, 16168],
     "relations": {
         "projects": [
             {slug: "Ace3", type: "embeddedLibrary"},
