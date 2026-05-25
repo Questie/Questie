a1307413db7b80b2e2b8bc6bde3a096e1fba953f
Loukky <12481807+Loukky@users.noreply.github.com>
2026-05-25 20:11:10 +0800

    [db]fix Wintergrasp quest level

--- Database/Corrections/mopQuestFixes.lua
+++ Database/Corrections/mopQuestFixes.lua
@@ -323,6 +323,54 @@ function MopQuestFixes.Load()
         [10277] = { -- The Caverns of Time
             [questKeys.triggerEnd] = {"Caverns of Time Explained", {[zoneIDs.CAVERNS_OF_TIME]={{44.31,38.73}}}},
         },
+        [13177] = { -- No Mercy for the Merciless
+            [questKeys.requiredLevel] = 85,
+            [questKeys.questLevel] = 90,
+        },
+        [13178] = { -- Slay them all!
+            [questKeys.requiredLevel] = 85,
+            [questKeys.questLevel] = 90,
+        },
+        [13179] = { -- No Mercy for the Merciless
+            [questKeys.requiredLevel] = 85,
+            [questKeys.questLevel] = 90,
+        },
+        [13180] = { -- Slay them all!
+            [questKeys.requiredLevel] = 85,
+            [questKeys.questLevel] = 90,
+        },
+        [13181] = { -- Victory in Wintergrasp
+            [questKeys.requiredLevel] = 85,
+            [questKeys.questLevel] = 90,
+        },
+        [13183] = { -- Victory in Wintergrasp
+            [questKeys.requiredLevel] = 85,
+            [questKeys.questLevel] = 90,
+        },
+        [13185] = { -- Stop the Siege
+            [questKeys.requiredLevel] = 85,
+            [questKeys.questLevel] = 90,
+        },
+        [13186] = { -- Stop the Siege
+            [questKeys.requiredLevel] = 85,
+            [questKeys.questLevel] = 90,
+        },
+        [13222] = { -- Defend the Siege
+            [questKeys.requiredLevel] = 85,
+            [questKeys.questLevel] = 90,
+        },
+        [13223] = { -- Defend the Siege
+            [questKeys.requiredLevel] = 85,
+            [questKeys.questLevel] = 90,
+        },
+        [13538] = { -- Southern Sabotage
+            [questKeys.requiredLevel] = 85,
+            [questKeys.questLevel] = 90,
+        },
+        [13539] = { -- Toppling the Towers
+            [questKeys.requiredLevel] = 85,
+            [questKeys.questLevel] = 90,
+        },
         [14007] = { -- Steady Shot
             [questKeys.requiredLevel] = 2,
             [questKeys.extraObjectives] = {},
