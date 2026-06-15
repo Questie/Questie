2aaeb1a1bbe8dae208917d6b33b71920c51b1033
BreakBB <b.boeing@online.de>
2026-06-15 07:55:59 +0200

    Move event location determination behind some conditions

--- Database/Corrections/Holidays/QuestieEvent.lua
+++ Database/Corrections/Holidays/QuestieEvent.lua
@@ -276,8 +276,6 @@ _GetDarkmoonFaireLocationEra = function(currentDate)
     local monthOffset = (currentDate.year - baseInfo.year) * 12 + (currentDate.month - baseInfo.month)
     local firstWeekday = C_Calendar.GetMonthInfo(monthOffset).firstWeekday
 
-    local eventLocation = (currentDate.month % 2) == 0 and DMF_LOCATIONS.MULGORE or DMF_LOCATIONS.ELWYNN_FOREST
-
     local startDay = DMF_START_DAY_BY_FIRST_WEEKDAY[firstWeekday]
     local endDay = startDay + 6 -- faire runs Monday - Sunday
 
@@ -288,7 +286,12 @@ _GetDarkmoonFaireLocationEra = function(currentDate)
     end
 
     if dayOfMonth >= startDay and dayOfMonth <= endDay then
-        return eventLocation
+        local remainder = currentDate.month % 2
+        if remainder == 1 then
+            return DMF_LOCATIONS.ELWYNN_FOREST
+        else -- remainder == 0
+            return DMF_LOCATIONS.MULGORE
+        end
     end
 
     return DMF_LOCATIONS.NONE
@@ -303,16 +306,6 @@ _GetDarkmoonFaireLocationTBC = function(currentDate)
     local monthOffset = (currentDate.year - baseInfo.year) * 12 + (currentDate.month - baseInfo.month)
     local firstWeekday = C_Calendar.GetMonthInfo(monthOffset).firstWeekday
 
-    local remainder = currentDate.month % 3
-    local eventLocation
-    if remainder == 1 then
-        eventLocation = DMF_LOCATIONS.TEROKKAR_FOREST
-    elseif remainder == 2 then
-        eventLocation = DMF_LOCATIONS.ELWYNN_FOREST
-    else -- remainder == 0
-        eventLocation = DMF_LOCATIONS.MULGORE
-    end
-
     local startDay = DMF_START_DAY_BY_FIRST_WEEKDAY[firstWeekday]
     local endDay = startDay + 6
 
@@ -323,7 +316,14 @@ _GetDarkmoonFaireLocationTBC = function(currentDate)
     end
 
     if dayOfMonth >= startDay and dayOfMonth <= endDay then
-        return eventLocation
+        local remainder = currentDate.month % 3
+        if remainder == 1 then
+            return DMF_LOCATIONS.TEROKKAR_FOREST
+        elseif remainder == 2 then
+            return DMF_LOCATIONS.ELWYNN_FOREST
+        else -- remainder == 0
+            return DMF_LOCATIONS.MULGORE
+        end
     end
 
     return DMF_LOCATIONS.NONE
