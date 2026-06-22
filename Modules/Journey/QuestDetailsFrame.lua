b33e20a0ce9fae013f54342f6868810ba1f95bb9
Cabro <1155017+Cabro@users.noreply.github.com>
2026-06-22 21:38:47 +0300

    Use double quotes

--- Modules/Journey/QuestDetailsFrame.lua
+++ Modules/Journey/QuestDetailsFrame.lua
@@ -101,7 +101,7 @@ function _QuestieJourney:DrawQuestDetailsFrame(container, quest)
     local breadcrumbForQuestId = QuestieDB.QueryQuestSingle(quest.Id, "breadcrumbForQuestId")
     if breadcrumbForQuestId and breadcrumbForQuestId ~= 0 then
         local completedStatus = Questie.db.char.complete[quest.Id] and Questie:Colorize(YES, 'green') or Questie:Colorize(NO, 'red')
-        local completedLabel = _QuestieJourney:CreateLabel(Questie:Colorize(l10n('Completed') .. l10n(": "), 'yellow') .. completedStatus, true)
+        local completedLabel = _QuestieJourney:CreateLabel(Questie:Colorize(l10n("Completed") .. l10n(": "), 'yellow') .. completedStatus, true)
         container:AddChild(completedLabel)
     end
 
@@ -128,7 +128,7 @@ function _QuestieJourney:DrawQuestDetailsFrame(container, quest)
     if quest.Starts and quest.Starts.NPC then
         local startNPCGroup = AceGUI:Create("InlineGroup")
         startNPCGroup:SetLayout("List")
-        startNPCGroup:SetTitle(l10n('Quest Start NPC Information'))
+        startNPCGroup:SetTitle(l10n("Quest Start NPC Information"))
         startNPCGroup:SetFullWidth(true)
         container:AddChild(startNPCGroup)
 
