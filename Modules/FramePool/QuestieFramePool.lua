9775c9da8f2ddc2561ab2fa66b0c851a051d875f
BreakBB <b.boeing@online.de>
2026-06-23 18:56:32 +0200

    Move frame leak detection to QuestieFramePool
    Related to #3127

--- Modules/FramePool/QuestieFramePool.lua
+++ Modules/FramePool/QuestieFramePool.lua
@@ -64,6 +64,10 @@ function QuestieFramePool:GetFrame()
         numberOfFrames = numberOfFrames + 1
 
         frame = QuestieFrame:New(numberOfFrames, MapIconTooltip.Show)
+
+        if numberOfFrames > 5000 then
+            Questie:Debug(Questie.DEBUG_CRITICAL, "[QuestieFramePool] Over 5000 frames... maybe there is a leak?", numberOfFrames)
+        end
     end
 
     _ReinitFrame(frame)
