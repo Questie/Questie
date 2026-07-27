54bc1a96059faec5e2840b9e7bf9591b52127b12
BreakBB <b.boeing@online.de>
2026-07-27 08:36:25 +0200

    Add Validators.checkQuestFieldTypes

--- cli/validate-sod.lua
+++ cli/validate-sod.lua
@@ -98,6 +98,7 @@ local function _CheckSoDDatabase()
     end
 
     Validators.checkItemFieldTypes(QuestieDB.itemData, QuestieDB.itemKeys)
+    Validators.checkQuestFieldTypes(QuestieDB.questData, QuestieDB.questKeys)
     Validators.checkNpcFieldTypes(QuestieDB.npcData, QuestieDB.npcKeys)
     Validators.checkObjectFieldTypes(QuestieDB.objectData, QuestieDB.objectKeys)
 
