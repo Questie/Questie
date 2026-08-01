cfbbe70af701ddda806aad867d1e70ee60af887a
BreakBB <b.boeing@online.de>
2026-08-01 19:00:32 +0200

    [fix] Bump interface version for 3.80.2

--- upload-wago.sh
+++ upload-wago.sh
@@ -21,7 +21,7 @@ WAGO_METADATA=$(cat <<-EOF
    "changelog": $CHANGELOG,
    "supported_classic_patch": "1.15.9",
    "supported_bc_patch": "2.5.6",
-   "supported_wotlk_patch": "3.80.1",
+   "supported_wotlk_patch": "3.80.2",
    "supported_mop_patch": "5.5.4"
 }
 EOF
