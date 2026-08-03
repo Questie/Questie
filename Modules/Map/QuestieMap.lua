be89a84905bca0146d6e838f6b8dfa39c61b2b92
BreakBB <b.boeing@online.de>
2026-08-03 08:18:09 +0200

    Move debug log

--- Modules/Map/QuestieMap.lua
+++ Modules/Map/QuestieMap.lua
@@ -95,6 +95,7 @@ function QuestieMap:UnloadQuestFrames(questId, iconType)
     assert(coroutine.running(), "UnloadQuestFrames must be called from a coroutine")
 
     if QuestieMap.questIdFrames[questId] then
+        Questie:Debug(Questie.DEBUG_DEVELOP, "[QuestieMap] Unloading quest frames for questid:", questId)
         local yieldCount = 0
         if not iconType then
             for _, frame in pairs(QuestieMap:GetFramesForQuest(questId)) do
@@ -129,7 +130,6 @@ function QuestieMap:UnloadQuestFrames(questId, iconType)
                 end
             end
         end
-        Questie:Debug(Questie.DEBUG_DEVELOP, "[QuestieMap] Unloading quest frames for questid:", questId)
     end
 end
 
