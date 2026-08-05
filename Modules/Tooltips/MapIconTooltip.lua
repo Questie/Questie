803860a1bfca4e624fb23851da7b0e89f2bbb407
anon1231823 <67269448+anon1231823@users.noreply.github.com>
2026-08-04 20:44:32 -0700

    Show breadcrumb above correct quest when tooltip has multiple quests

--- Modules/Tooltips/MapIconTooltip.lua
+++ Modules/Tooltips/MapIconTooltip.lua
@@ -249,13 +249,13 @@ function MapIconTooltip:Show()
                                     local breadcrumbQuest = QuestieDB.GetQuest(breadcrumbId)
                                     if breadcrumbQuest then
                                         local questTitle, rewardString = _MapIconTooltip.GetNextQuestInChainLines(breadcrumbId, breadcrumbQuest.level, "")
-                                        self:AddDoubleLine(questTitle, rewardString, 1, 1, 1)
+                                        tooltipRows:AddDoubleLine(questTitle, rewardString, 1, 1, 1)
                                         breadcrumbCount = breadcrumbCount + 1
                                     end
                                 end
                             end
                             if breadcrumbCount > 0 then
-                                self:AddLine("|TInterface\\Addons\\Questie\\Icons\\breadcrumbtooltip.png:16|t " .. l10n("Breadcrumb Quests"), 0.86, 0.86, 0.86)
+                                tooltipRows:AddLine("|TInterface\\Addons\\Questie\\Icons\\breadcrumbtooltip.png:16|t " .. l10n("Breadcrumb Quests"), 0.86, 0.86, 0.86)
                             end
                         end
                     end
