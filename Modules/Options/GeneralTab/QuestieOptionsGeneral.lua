eae97c741d6282eeab42f6288a5a7ea9aa696c8e
anon1231823 <67269448+anon1231823@users.noreply.github.com>
2026-09-25 11:59:22 -0700

    update translation

--- Modules/Options/GeneralTab/QuestieOptionsGeneral.lua
+++ Modules/Options/GeneralTab/QuestieOptionsGeneral.lua
@@ -283,7 +283,7 @@ function QuestieOptions.tabs.general:Initialize()
                     compartmentButtonEnabled = {
                         type = "toggle",
                         order = 5.35,
-                        name = function() return l10n("Show compartment button"); end,
+                        name = function() return l10n("Show Questie in addon compartment"); end,
                         desc = function() return l10n("Enable or disable the Questie button in the addon compartment. You can still access the options menu with /questie."); end,
                         width = 1.55,
                         hidden = function() return _G.AddonCompartmentFrame == nil end,
